c2c534029f6ef5560d3b1506625ae1ce9ee985f1
Frank Ssozi <raymossekandi@gmail.com>
2017-10-06 12:45:12 +0100

    modified the results filr and created and a css file

--- src/components/resultsTable.js
+++ src/components/resultsTable.js
@@ -1,9 +1,30 @@
 import React from 'react';
-
+import FakeBookings from "../data/fakeBookings.json";
 const ResultsTable = (props) => {
   return(
-    <div>
-      {JSON.stringify(props.bookings)}
+    <div >
+      <table>
+      <tr>
+          <th>Title</th>
+          <th>First Name</th>
+          <th>surname</th>
+          <th>email</th>
+          <th>roomId</th>
+          <th>checkInDate</th>
+          <th>checkOutDate</th>
+        </tr>
+      {FakeBookings.map((item, index) => (
+        <tr>
+          <td>{item.title}</td>
+          <td>{item.firstName} </td>
+          <td>{item.surname} </td>
+          <td>{item.email} </td>
+          <td>{item.roomId} </td>
+          <td>{item.checkInDate} </td>
+          <td>{item.checkOutDate} </td>
+      </tr>
+      ))}
+    </table>
     </div>
   )
 }
