a44e2ad357844e77fb3a6405b5ac0e017cf26819
Alasdair Smith <ali@alasdairsmith.co.uk>
2017-10-15 15:40:29 +0100

    Render results with fake bookings

--- src/containers/Bookings.js
+++ src/containers/Bookings.js
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
 import Search from "../components/Search.js";
-// import Results from "../components/Results.js";
-// import * as FakeBookings from "../data/fakeBookings.json";
+import Results from "../components/Results.js";
+import FakeBookings from "../data/fakeBookings.json";
 
 export default class Bookings extends Component {
-  search = () => {
-    console.info("to do!");
-  };
-  
+  constructor(props) {
+    super(props)
+
+    this.state = { results: FakeBookings }
+  }
+
+  handleSearch = () => {}
+
   render() {
     return (
       <div className="App-content">
         <div className="container">
-          <Search search={this.search} />
-          {/* <Results results={this.state.results} /> */}
+          <Search onSearch={this.handleSearch} />
+          <Results results={this.state.results} />
         </div>
       </div>
     );
