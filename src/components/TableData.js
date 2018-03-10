3be09d94e28166dda8c43cd761631fc32f787371
yevhensydorov <assid@ukr.net>
2018-03-10 11:47:01 +0000

    add num of nights with moment js

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import moment from 'moment';
 
 class TableData extends React.Component {
 	render() {
 		const customersData = this.props.customers;
+
 
 		let row = customersData.map((item, index) => {
 			return <tr key={index}>
@@ -13,6 +15,7 @@ class TableData extends React.Component {
 				        <td>{item.roomId}</td>
 				        <td>{item.checkInDate}</td>
 				        <td>{item.checkOutDate}</td>
+				        <td>{moment(item.checkOutDate).diff(item.checkInDate, 'days')}</td>
       				</tr>
 		});
 		return (
