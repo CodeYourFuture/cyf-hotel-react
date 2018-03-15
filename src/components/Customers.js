59551ae738afcbad46da129d960e07b1534dcb7b
yevhensydorov <assid@ukr.net>
2018-03-15 13:47:37 +0000

    pure sorting

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TableHead from './TableHead';
 import TableData from './TableData';
 import * as FakeBookings from "../data/fakeBookings.json";
-import orderBy from 'lodash/orderBy';
+// import orderBy from 'lodash/orderBy';
 
 const invertDirection = {
 	'asc': 'desc',
@@ -17,25 +17,79 @@ class Customers extends React.Component {
 			data: [],
 			isFetched: false,
 			columnToSort: '',
-			sortDirection: 'desc'
+			sortDirection: 'asc'
 		}
 	}
 
 
 	getData = () => {
+		// const testSort = this.sort(FakeBookings, 'firstName');
+		// console.log(testSort);
 		if (this.state.isFetched === false) {
 			this.setState({
-				data: FakeBookings,
+				data: this.sort(FakeBookings, 'firstName'),
 				isFetched: true
 			})	
 		}
 	}
 
+	sort = (data, columnName) => {
+		return data.sort((a, b) => {
+			// console.log(a[columnName]);
+			// console.log(b[columnName]);
+			if (this.state.sortDirection === 'asc') {
+				if (a[columnName] > b[columnName]) {
+					return 1
+				} else {	
+					return -1
+				}
+			} else {
+				if (a[columnName] < b[columnName]) {
+					return 1
+				} else {	
+					return -1
+				}
+			}
+			
+		});
+	} 
+
 	handleSort = (columnName) => {
-		this.setState(state => ({
-			columnToSort: columnName,
-			sortDirection: state.columnToSort === columnName ? invertDirection[state.sortDirection] : 'asc'
-		}));
+		// this.setState(state => ({
+		// 	columnToSort: columnName,
+		// 	sortDirection: state.columnToSort === columnName ? invertDirection[state.sortDirection] : 'asc'
+		// }));
+		// console.log('before', this.state.data)
+		// this.state.data.sort((a, b) => {
+		// 	// console.log(a[columnName]);
+		// 	// console.log(b[columnName]);
+		// 	if (this.state.sortDirection === 'asc') {
+		// 		if (a[columnName] > b[columnName]) {
+		// 			return 1
+		// 		} else {	
+		// 			return -1
+		// 		}
+		// 	} else {
+		// 		if (a[columnName] < b[columnName]) {
+		// 			return 1
+		// 		} else {	
+		// 			return -1
+		// 		}
+		// 	}
+			
+		// });
+		let foo;
+		if(this.state.sortDirection === 'asc') {
+			foo = 'desc';
+		} else {
+			foo = 'asc';
+		}
+		this.setState({
+			data: this.sort(this.state.data, columnName),
+			sortDirection: foo
+		});
+
+		// console.log('after', this.state.data)
 	}
 
 	render() {
@@ -47,10 +101,7 @@ class Customers extends React.Component {
 				</p>
 				<table className="table">
 					<TableHead sort={this.handleSort} />
-					<TableData customers={orderBy(
-							this.state.data, 
-							this.state.columnToSort, 
-							this.state.sortDirection)} 
+					<TableData customers={this.state.data} 
 					/>
 				</table>
 			</div>
