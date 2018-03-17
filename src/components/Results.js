2c23e994c775808b50ee8cbd54e6a9f0e3e6dbda
OlenaKashuba <amely.jazz@gmail.com>
2018-03-17 15:31:17 +0000

    Sorting

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,34 +4,56 @@ import TableBody from './TableBody';
 import * as FakeBookings from "../data/fakeBookings.json";
 
 
-class Results extends React.Component {
+class Results extends React.Component{
 	constructor(props) {
-		super(props)
+		super(props);
 
 		this.state = {
 			data: [],
-			isFetched: false
+			isFetched: false,
+			sortDirection: 'asc'
 		}
+	}
 
-		}
-
-		checkIfFetched = (event) => {
-			console.log('checkIfFetched is here');
-			if (this.state.isFetched === false) {
-				this.getData();
-			}
-		}
-		
-
-		getData = () => {
-			console.log('this is', FakeBookings);
+	getData = () => {
+		if (this.state.isFetched === false) {
 			this.setState({
 				data: FakeBookings,
 				isFetched: true
 			})
 		}
+	}
 
-	
+	sort = (data, columnName) => {
+		return data.sort((a,b) => {
+			if (this.state.sortDirection === 'asc') {
+				if (a[columnName] > b[columnName]) {
+					return 1;
+				} else {
+					return -1;
+				}
+			} else {
+				if (a[columnName] < b[columnName]) {
+					return 1;
+				} else {
+					return -1;
+				}
+			}
+		});
+	}
+
+	 handleSort = (columnName) => {
+	 	let direction;
+	 	if (this.state.sortDirection === 'asc') {
+	 		direction = 'desc';
+	 	} else {
+	 		direction = 'asc';
+	 	}
+	 	this.setState({
+	 		sortDirection: direction,
+	 		data: this.sort(this.state.data, columnName)
+	 	})
+	 }
 
 	render() {
 		return(
@@ -39,13 +61,13 @@ class Results extends React.Component {
 				<p>Results 
 					<span> Found: </span>
 					<button
-					onClick={this.checkIfFetched}> Get data </button>
+					onClick={this.getData}> Get data </button>
 				</p>
 			
 				<div className="row">
 		      		<div className="col">
 			        	<table className="table customers-table">
-			        		<TableHead />
+			        		<TableHead sort={this.handleSort}/>
 			        		<TableBody items={this.state.data}/>
 				         </table>
 
--- src/components/TableHead.js
+++ src/components/TableHead.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
 class TableHead extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+
 	render () {
 		return (
 			<thead>
 				<tr>
-					<th>Title</th>
-					<th>First name</th>
-					<th>Surname</th>
-					<th>Email</th>
-					<th>Room id</th>
-					<th>Check in date</th>
-					<th>Check out date</th>
-					<th>Full days</th>
+					<th onClick={() => this.props.sort('title')}>Title</th>
+					<th onClick={() => this.props.sort('firstName')}>First name</th>
+					<th onClick={() => this.props.sort('surname')}>Surname</th>
+					<th onClick={() => this.props.sort('email')}>Email</th>
+					<th onClick={() => this.props.sort('roomId')}>Room id</th>
+					<th onClick={() => this.props.sort('checkInDate')}>Check in date</th>
+					<th onClick={() => this.props.sort('checkOutDate')}>Check out date</th>
+					<th> Full days</th>
 				</tr>
 			</thead>
 		)
