ef6d2c431ad7a5bd94bbb7bc4f400a9ca143db6c
Awet Fessha <awetkifle94@gmail.com>
2018-05-09 16:47:35 +0100

    Deleted the my-hotel folder and modified code

---
+++ src/components/Button.js
@@ -0,0 +1,6 @@
+import React from "react";
+const Button = () => {
+  return <button className="btn btn-primary fn-submit-name">Search IDs</button>;
+};
+
+export default Button;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from "react";
+import Button from "./Button.js";
+import Button2 from "./Button2.js";
 
 const Search = props => (
   <div className="search">
@@ -10,8 +12,25 @@ const Search = props => (
         <div className="form-group search-box">
           <label htmlFor="customerId">Customer Id</label>
           <div className="search-row">
-            <input type="text" id="customerId" className="form-control" placeholder="Customer Id" />
-            <button className="btn btn-primary fn-submit-name">Search IDs</button>
+            <input
+              type="text"
+              id="customerId"
+              className="form-control"
+              placeholder="Customer Id"
+            />
+            <Button />
+          </div>
+        </div>
+        <div className="form-group search-box">
+          <label htmlFor="customerId">Booking Name</label>
+          <div className="search-row">
+            <input
+              type="text"
+              id="customerId"
+              className="form-control"
+              placeholder="Booking name"
+            />
+            <Button2 />
           </div>
         </div>
       </div>
