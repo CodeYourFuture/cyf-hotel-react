5709007482e67d80fc2b03dfd97e49685565702c
Awet Fessha <awetkifle94@gmail.com>
2018-05-12 13:04:35 +0100

    Createted the table

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,7 +1,32 @@
-/*
 import React from "react";
-const Results = () => {
-  return;
+const Results = prop => {
+  return (
+    <table className="results">
+      <thead>
+        <tr>
+          <th>Title </th>
+          <th>First name </th>
+          <th>Surname </th>
+          <th>Email</th>
+          <th>Room ID </th>
+          <th>Check in date </th>
+          <th>Check out date </th>
+        </tr>
+      </thead>
+      {prop.results.map(result => (
+        <tbody>
+          <tr>
+            <td>{result.title} </td>
+            <td>{result.firstName} </td>
+            <td>{result.surname} </td>
+            <td>{result.email} </td>
+            <td>{result.roomId} </td>
+            <td>{result.checkInDate} </td>
+            <td>{result.checkOutDate} </td>
+          </tr>
+        </tbody>
+      ))}
+    </table>
+  );
 };
 export default Results;
-*/
