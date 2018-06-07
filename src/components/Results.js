9c13c577615c90f2fa4cbf1eb7f01faa6d674ade
Austine Ogochukwu Okolo <okolochuks4u@gmail.com>
2018-06-07 17:49:25 +0100

    finished-#lesson1

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,23 +1,40 @@
 import React from "react";
+import moment from "moment";
 function Results(props) {
+  const fakeBookings = props.results.map(booking => {
+    const checkInDate = moment(booking.checkInDate);
+    const checkOutDate = moment(booking.checkOutDate);
+    return (
+      <tr key={booking.id}>
+        <td>{booking.title}</td>
+        <td>{booking.firstName}</td>
+        <td>{booking.surname}</td>
+        <td>{booking.email}</td>
+        <td>{booking.roomId}</td>
+        <td>{booking.checkInDate}</td>
+        <td>{booking.checkOutDate}</td>
+        <td>{checkOutDate.diff(checkInDate, "days")}</td>
+      </tr>
+    );
+  });
   return (
-    <table>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Age</th>
-      </tr>
-      <tr>
-        <td>Jill</td>
-        <td>Smith</td>
-        <td>50</td>
-      </tr>
-      <tr>
-        <td>Eve</td>
-        <td>Jackson</td>
-        <td>94</td>
-      </tr>
-    </table>
+    <div className="container">
+      <table className="table">
+        <tbody>
+          <tr>
+            <th>Title</th>
+            <th>First Name</th>
+            <th>Surname</th>
+            <th>Email</th>
+            <th>Room id</th>
+            <th>Check in date</th>
+            <th>Check out date</th>
+            <th># Days</th>
+          </tr>
+          {fakeBookings}
+        </tbody>
+      </table>
+    </div>
   );
 }
 export default Results;
