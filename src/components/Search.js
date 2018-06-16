e0e83a6d6a88fb47bf86766b23c66ffb3fb4e237
Nawej <bianzamutombo@gmail.com>
2018-06-16 16:07:50 +0100

    exercice react 3,4

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,40 @@
 import React from 'react';
+import {component} from 'react';
+const Button =() =>(<button className="btn btn-primary fn-submit-name">Search friend </button>
+
+);
 
 const Search = props => (
+ 
   <div className="search">
     <div className="page-header">
       <h4 className="text-left">Search Bookings</h4>
     </div>
+
     <div className="row search-wrapper">
       <div className="col">
         <div className="form-group search-box">
-          <label htmlFor="customerId">Customer Id</label>
+            <label htmlFor="customerId">Customer Id</label>
           <div className="search-row">
             <input type="text" id="customerId" className="form-control" placeholder="Customer Id" />
-            <button className="btn btn-primary fn-submit-name">Search IDs</button>
+           <Button/>
           </div>
         </div>
       </div>
     </div>
+
+    <div className="row search-wrapper">
+      <div className="col">
+        <div className="form-group search-box">
+            <label htmlFor="customer friend">Customer friend</label>
+          <div className="search-row">
+            <input type="text" id="customer friend " className="form-control" placeholder="Customer friend" />
+           <Button/>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 );
 
--- src/containers/Bookings.js
+++ src/containers/Bookings.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import Search from '../components/Search.js';
-// import Results from '../components/Results.js';
-// import FakeBookings from '../data/fakeBookings.json';
+import Results from '../components/Results.js';
+import FakeBookings from '../data/fakeBookings.json';
 
 export default class Bookings extends Component {
   search = () => {
     console.info('TO DO!');
   };
-  
+
   render() {
+    
+    const myresults = ['Justine1' , 'Nawal2', 'Ornneille3' ]
+    
     return (
       <div className="App-content">
         <div className="container">
           <Search search={this.search} />
-          {/* <Results results={this.state.results} /> */}
+          <Results results= {FakeBookings}/*{this.state.results}*/ />
         </div>
       </div>
     );
   }
 }
+
+
+
+
