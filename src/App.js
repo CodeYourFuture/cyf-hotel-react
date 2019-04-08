932a13a21ccdf88e461845a066523f8fcd9ee777
Kadir <kegilmez@gmail.com>
2019-04-09 00:16:55 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -11,10 +11,30 @@ import "./grid.css";
 class App extends Component {
   render() {
     const contactInfo = ["123 Fake Street, London, E1 4UD", "hello@fakehotel.com", "0123 456789"];
+    const location1 = {
+      name: 'London',
+      imageSource: 'https://cdn.londonandpartners.com/assets/73295-640x360-london-skyline-ns.jpg',
+      link: 'https://www.visitlondon.com/'
+    };
+
+    const location2 = {
+      name: 'Glasgow',
+      imageSource: 'https://ichef.bbci.co.uk/news/660/cpsprodpb/E213/production/_90057875_mediaitem90057873.jpg',
+      link: 'https://peoplemakeglasgow.com/'
+    };
+
+    const location3 = {
+      name: 'Manchester',
+      imageSource: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV1u2v3Poqi8gSsKb4C3nIXBpPQtSYdMbLjLDg5Aq2lMbl_R7xyw',
+      link: 'https://www.visitmanchester.com/'
+    };
+
+    const locations = [location1, location2, location3];
+
     return (
       <div className="App">
         <Heading />
-        <TouristInfoCards />
+        <TouristInfoCards cities={locations} />
         <Bookings />
         <Restaurant />
         <Footer contactInfo={contactInfo} />
--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -1,42 +1,24 @@
 import React from "react";
 
-const TouristInfoCards = () => {
+const TouristInfoCards = (props) => {
     return (
         <section className='row'>
-            <div className='xs-col-12 sm-col-4'>
-                <figure className='image-container'>
-                    <img src='https://cdn.londonandpartners.com/assets/73295-640x360-london-skyline-ns.jpg' alt='London' />
-                </figure>
-                <div class="card-body">
-                    <h5 class="card-title">London</h5>
-                    <p class="card-text">Visiting London for the first time?  We’ve come up with some ideas of things you should see and do while you are in the city - be sure to add them to your list!</p>
-                    <a href='https://www.visitlondon.com/' target='_blank' class="btn btn-primary">Visit London</a>
-                </div>
-            </div>
-            <div className='xs-col-12 sm-col-4'>
-                <figure className='image-container'>
-                    <img src='https://ichef.bbci.co.uk/news/660/cpsprodpb/E213/production/_90057875_mediaitem90057873.jpg' alt='Glasgow' />
-                </figure>
-                <div class="card-body">
-                    <h5 class="card-title">Glasgow</h5>
-                    <p class="card-text">Visiting Glasgow for the first time?  We’ve come up with some ideas of things you should see and do while you are in the city - be sure to add them to your list!</p>
-                    <a href='https://peoplemakeglasgow.com/' target='_blank' class="btn btn-primary">Visit Glasgow</a>
-                </div>
-
-            </div>
-            <div className='xs-col-12 sm-col-4'>
-                <figure className='image-container'>
-                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV1u2v3Poqi8gSsKb4C3nIXBpPQtSYdMbLjLDg5Aq2lMbl_R7xyw' alt='Manchester' />
-                </figure>
-                <div class="card-body">
-                    <h5 class="card-title">Glasgow</h5>
-                    <p class="card-text">Visiting Manchester for the first time?  We’ve come up with some ideas of things you should see and do while you are in the city - be sure to add them to your list!</p>
-                    <a href='https://www.visitmanchester.com/' target='_blank' class="btn btn-primary">Visit Manchester</a>
-                </div>
-            </div>
+            {props.cities.map(city => {
+                return (
+                    <div className='xs-col-12 sm-col-4'>
+                        <figure className='image-container'>
+                            <img src={city.imageSource} alt={city.name} />
+                        </figure>
+                        <div class="card-body">
+                            <h5 class="card-title">{city.name}</h5>
+                            <p class="card-text">Visiting {city.name} for the first time?  We’ve come up with some ideas of things you should see and do while you are in the city - be sure to add them to your list!</p>
+                            <a href={city.link} target='_blank' class="btn btn-primary">Visit {city.name}</a>
+                        </div>
+                    </div>
+                )
+            })}
         </section>
     )
 }
-
 
 export default TouristInfoCards;
