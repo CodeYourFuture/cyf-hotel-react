f64749370f0e2562c3a62f894e7e9a524e3e199c
clement <clemohiomoje@gmail.com>
2019-04-10 18:22:24 +0100

    first commit, header and logo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 
 import Bookings from "./Bookings";
 import Restaurant from "./Restaurant";
+import Heading from "./Heading"
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">CYF Hotel</header>
+        <Heading />
         <Bookings />
         <Restaurant />
       </div>
---
+++ src/Heading.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div>
+      <img
+        src="https://i.postimg.cc/Y0rxLyfV/log.png"
+        height="120px"
+        alt="logo"
+      />
+      <header className="App-header">CYF Hotel</header>
+    </div>
+  );
+};
+
+export default Heading; 
