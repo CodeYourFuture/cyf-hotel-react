fbc1de0258ee11db35b92cee65f8c64102b492ac
Kadir <kegilmez@gmail.com>
2019-04-13 21:49:40 +0100

    feedback implemented

--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,6 @@
 import React from "react";
 
-const Button = (props) => {
-    return (
-        <button className="btn btn-primary">{props.content}</button>
-    )
-}
+const Button = (props) =>
+    <button className="btn btn-primary">{props.content}</button>;
 
 export default Button;
