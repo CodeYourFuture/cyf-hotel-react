6e53c509ff5ae13182ecf92907b1af83a8cb5b75
markazie <markazie@gmail.com>
2019-04-16 17:44:33 +0100

    lesson 2.01

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,47 +1,41 @@
-import React, { Component } from 'react'
-import moment from "moment"
+import React, { Component } from "react";
+import moment from "moment";
 
 export default class SearchResults extends Component {
   constructor() {
-    super()
-    this.state = {
-      selected: false
-    }
-}
+    super();
+    this.state = {};
+  }
 
-  rowSelected = (event) => {
-console.log(this.state.selected)
+  rowSelected = event => {
     this.setState(pre => {
       return { selected: !pre.selected };
-    })
-    
+    });
+
     this.state.selected
-    ? (event.target.parentNode.className = "bg-warning")
-    : (event.target.parentNode.className = "");
-  }
+      ? (event.target.parentNode.className = "bg-warning")
+      : (event.target.parentNode.className = "");
+  };
 
   render() {
     const result = this.props.results.map(r => (
-      <tr key={r.id} onClick={this.rowSelected}>
-        <th scope="row">
-          {r.id}
-        </th>
+      <tr key={r.id} data-id={r.id} onClick={this.rowSelected}>
+        <th scope="row">{r.id}</th>
         <td>{`${r.title} ${r.firstName} ${r.surname}`}</td>
         <td>{r.email}</td>
         <td>{`room: ${r.roomId}`}</td>
         <td>{r.checkInDate}</td>
         <td>{r.checkOutDate}</td>
-            <td>{`${moment(r.checkOutDate).diff(moment(r.checkInDate), "days")} days`}</td>
+        <td>{`${moment(r.checkOutDate).diff(
+          moment(r.checkInDate),
+          "days"
+        )} days`}</td>
       </tr>
     ));
     return (
- 
       <table className="table table-dark">
-          <tbody>
-{result}
-          </tbody>
+        <tbody>{result}</tbody>
       </table>
-    )
+    );
   }
 }
-
