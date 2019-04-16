f3949a1a2e7d7c9427b81a5031a5d1c304e86d52
ahmed <ahmed.almadina@hotmail.com>
2019-04-16 12:33:43 +0100

    modified TouristInfoCards.js

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Cards from "./Cards";
+import Card from "./Cards";
 
-const TouristInfoCards = () => {
+const TouristInfoCards = () => {            //changed Cards to Card.
   return (
     <div className="cards">
-      <Cards website="https://peoplemakeglasgow.com/" city="Glasgow" />
-      <Cards website="https://www.visitmanchester.com/" city="Manchester" />
-      <Cards website="https://www.visitlondon.com/" city="London" />
+      <Card website="https://peoplemakeglasgow.com/" city="Glasgow" /> 
+      <Card website="https://www.visitmanchester.com/" city="Manchester" />
+      <Card website="https://www.visitlondon.com/" city="London" />
     </div>
   );
 };
-
+//changed Cards to Card.
 export default TouristInfoCards;
