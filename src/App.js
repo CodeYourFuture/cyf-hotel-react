2273efa56005272f5d90ae1ba393925346b5131f
ahmed <ahmed.almadina@hotmail.com>
2019-04-16 12:47:21 +0100

    second modify App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         <Bookings />
         <Restaurant />
         <Footer
-          Details={[
+          details={[
             "123 Fake Street, London, E1 4UD",
             "hello@fakehotel.com",
             "0123 456789"
