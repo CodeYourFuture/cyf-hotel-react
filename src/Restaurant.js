46de9c27b10a34dfbff40994d0cce1a571cecd9f
clement <clemohiomoje@gmail.com>
2019-04-20 17:23:34 +0100

    Restaurant order

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -15,21 +15,3 @@ class Restaurant extends Component {
 }
 
 export default Restaurant;
-
-// import React, { Component } from "react";
-// import Orders from "./Orders";
-
-// class Restaurant extends Component{
-//  render(){
-//    return (
-//      <div>
-//        <h3>Restaurant Orders</h3>
-//        <ul>
-//          <Orders orderType="Pizzas" />
-//          <Orders orderType="Salads" />
-//        </ul>
-//      </div>
-//    )
-//  }
-// }
-// export default Restaurant;
