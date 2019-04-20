8c3002368d413d3cc8ea8fc297bceb2667da7e31
clement <clemohiomoje@gmail.com>
2019-04-20 17:22:52 +0100

    Restaurant order

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,17 +1,35 @@
-import React from "react";
-
-const Restaurant = (props) => {
-  const pizzas = 0;
-  return (
-    <div>
-      <h3>Restaurant Orders</h3>
-      <ul>
-        <li>
-          Pizzas: {pizzas} <button className="btn btn-primary">{props.text}</button>
-        </li>
-      </ul>
-    </div>
-  );
-};
+import React, { Component } from "react";
+import Orders from "./Orders"
+class Restaurant extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Restaurant Orders</h3>
+        <ul>
+        <Orders orderType={"Pizza"} />
+        <Orders orderType={"Salad"} />
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Restaurant;
+
+// import React, { Component } from "react";
+// import Orders from "./Orders";
+
+// class Restaurant extends Component{
+//  render(){
+//    return (
+//      <div>
+//        <h3>Restaurant Orders</h3>
+//        <ul>
+//          <Orders orderType="Pizzas" />
+//          <Orders orderType="Salads" />
+//        </ul>
+//      </div>
+//    )
+//  }
+// }
+// export default Restaurant;
