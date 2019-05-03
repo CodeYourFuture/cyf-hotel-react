e7f5f30a8564aed48db367205d7ce136268ac56c
clement <clemohiomoje@gmail.com>
2019-05-03 23:03:19 +0100

    props name change from text to type

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = props => {
-  return <button className="btn btn-primary">{props.text}</button>;
+  return <button className="btn btn-primary">{props.type}</button>;
 };
 
 export default Button;
