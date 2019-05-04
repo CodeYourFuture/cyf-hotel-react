3f1fa01989da3ff0c038cf0e6ada689b356cc40f
clement <clemohiomoje@gmail.com>
2019-05-04 13:02:44 +0100

    Refactored the bookingform component

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -14,20 +14,69 @@ class BookingForm extends Component {
   };
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <div className=" addBooking__form">
+      <div className="newBooking__Form">
+        <h3>Add New Booking here!</h3>
+        <div className="form__wrapper">
+          <form onSubmit={this.handleSubmit}>
+          <div className="input__wrapper">
+            <input id="id" onChange={this.handleChange} placeholder="ID" />
+            <br />
+            <input
+              id="title"
+              onChange={this.handleChange}
+              placeholder="Title"
+            />
+            <br />
+            <input
+              id="firstName"
+              onChange={this.handleChange}
+              placeholder="FirstName"
+            />
+            <br />
+            <input
+              id="surname"
+              onChange={this.handleChange}
+              placeholder="Surname"
+            />
+            <br />
+            <input
+              id="email"
+              onChange={this.handleChange}
+              placeholder="Email"
+            />
+            <br />
+            <input
+              id="roomId"
+              onChange={this.handleChange}
+              placeholder="roomID"
+            />
+            <br />
+            <input
+              id="checkInDate"
+              onChange={this.handleChange}
+              placeholder="CheckInDate"
+            />
+            <br />
+            <input
+              id="checkOutDate"
+              onChange={this.handleChange}
+              placeholder="CheckOutDate"
+            />
+            <br />
+            <Button type={"submit"} />
+            {/* <div className=" addBooking__form">
           <div className="table-responsive">
             <table className="addBooking__table">
               <tbody>
                 <tr>
                   <td>
                     ID
-                    <input id="id" onChange={this.handleChange} />
+                    <input id="id" onChange={this.handleChange} /><br />
                   </td>
 
                   <td>
                     title
-                    <input id="title" onChange={this.handleChange} />
+                    <input id="title" onChange={this.handleChange} /><br />
                   </td>
 
                   <td>
@@ -60,14 +109,17 @@ class BookingForm extends Component {
                   </td>
                   <td />
                   <td>
-                    <Button type={"submit"} />
+                 <Button type={"submit"} />
                   </td>
                 </tr>
               </tbody>
             </table>
           </div>
+        </div> */}
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     );
   }
 }
