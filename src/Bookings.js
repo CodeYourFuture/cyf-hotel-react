7dc62dfe6af1363063f6e0e471efe555fa8dd59a
ZaimAbz <zaimabodi@gmail.com>
2019-05-09 20:26:39 +0100

    Abz 20

--- src/Bookings.js
+++ src/Bookings.js
@@ -6,8 +6,9 @@ import FakeBookings from "./data/fakeBookings.json";
 export default class Bookings extends Component {
   constructor(props) {
     super(props);
-    this.state = { bookings: FakeBookings };
+    this.state = { bookings: null, isLoading: true };
   }
+
   search = searchVal => {
     this.setState({
       bookings: this.state.bookings.filter(
@@ -17,14 +18,27 @@ export default class Bookings extends Component {
       )
     });
   };
+  componentDidMount() {
+    fetch(`https://cyf-react.glitch.me`)
+      .then(res => res.json())
+      .then(data => {
+        console.log("fdshdao;ijga;oifdjga;oihj;a", data);
+        this.setState({
+          bookings: data,
+          isLoading: false
+        });
+      });
+  }
 
   render() {
     return (
       <div className="App-content">
-        <div className="container">
-          <Search search={this.search} />
-          <SearchResults results={FakeBookings} />
-        </div>
+        {!this.state.isLoading ? (
+          <div className="container">
+            <Search search={this.search} />
+            <SearchResults results={this.state.bookings} />
+          </div>
+        ) : null}
       </div>
     );
   }
