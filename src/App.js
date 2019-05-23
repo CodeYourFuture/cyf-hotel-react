3ae2c30a2f147bdbe5d7a21e72a0099896012ad7
Loic Le Henaff <loic.lehenaff@bbc.co.uk>
2019-05-23 23:08:04 +0100

    Change class component with functional component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
 import Bookings from "./Bookings";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">CYF Hotel</header>
-        <Bookings />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">CYF Hotel</header>
+      <Bookings />
+    </div>
+  );
+};
 
 export default App;
--- src/Bookings.js
+++ src/Bookings.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import Search from "./Search.js";
 // import SearchResults from "./SearchResults.js";
 // import FakeBookings from "./data/fakeBookings.json";
 
-export default class Bookings extends Component {
-  search = searchVal => {
-    console.info("TO DO!", searchVal);
-  };
+const search = searchVal => {
+  console.info("TO DO!", searchVal);
+};
 
-  render() {
-    return (
-      <div className="App-content">
-        <div className="container">
-          <Search search={this.search} />
-          {/* <SearchResults results={FakeBookings} /> */}
-        </div>
+const Bookings = () => {
+  return (
+    <div className="App-content">
+      <div className="container">
+        <Search search={search} />
+        {/* <SearchResults results={FakeBookings} /> */}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Bookings;
