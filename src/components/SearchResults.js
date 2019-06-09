0071879d3765f540733853722a94f5408354997c
youssef910 <youssef.sadek910@gmail.com>
2019-06-09 21:12:27 +0100

    peterhelp

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,32 +2,44 @@ import React from "react";
 import FakeBookings from "../data/fakeBookings.json";
 import moment from "moment";
 // for (var i = 0; i < FakeBookings.length; i++);
-// class TableRow extends React.Component {
-//   render() {
-//     return (
-//       <tr
-//         className={item.id}
-//         key={item.id}
-//         onClick={this.props.clickedToChangeColor}
-//         style={this.props.givinStyle.backgroundColor}>
+class TableRow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      style: { backgroundColor: "" }
+    };
+  }
 
-//         <td>{item.id}</td>
-//         <td>{item.title}</td>
-//         <td>{item.firstName}</td>
-//         <td>{item.surname}</td>
-//         <td>{item.email}</td>
-//         <td>{item.roomId}</td>
-//         <td>{item.checkInDate}</td>
-//         <td>{item.checkOutDate}</td>
-//         <td>
-//           {moment(item.checkOutDate).diff(
-//             moment(item.checkInDate),
-//             "days"
-//           )}
-//         </td>
-//       </tr>  );
-//   }
-// }
+  onClicked = () => {
+    this.setState({ style: { backgroundColor: "skyblue" } });
+  };
+
+  render() {
+    return (
+      <tr
+        className={this.props.item.id}
+        key={this.props.item.id}
+        onClick={this.onClicked}
+        style={this.state.style}
+      >
+        <td>{this.props.item.id}</td>
+        <td>{this.props.item.title}</td>
+        <td>{this.props.item.firstName}</td>
+        <td>{this.props.item.surname}</td>
+        <td>{this.props.item.email}</td>
+        <td>{this.props.item.roomId}</td>
+        <td>{this.props.item.checkInDate}</td>
+        <td>{this.props.item.checkOutDate}</td>
+        <td>
+          {moment(this.props.item.checkOutDate).diff(
+            moment(this.props.item.checkInDate),
+            "days"
+          )}
+        </td>
+      </tr>
+    );
+  }
+}
 class SearcResults extends React.Component {
   constructor(props) {
     super(props);
@@ -58,27 +70,28 @@ class SearcResults extends React.Component {
         </thead>
         <tbody>
           {this.state.fakeBookingsList.map(item => (
-            <tr
-              className={item.id}
-              key={item.id}
-              onClick={this.onClicking}
-              style={this.state.style}
-            >
-              <td>{item.id} </td>
-              <td>{item.title}</td>
-              <td>{item.firstName}</td>
-              <td>{item.surname}</td>
-              <td>{item.email}</td>
-              <td>{item.roomId}</td>
-              <td>{item.checkInDate}</td>
-              <td>{item.checkOutDate}</td>
-              <td>
-                {moment(item.checkOutDate).diff(
-                  moment(item.checkInDate),
-                  "days"
-                )}
-              </td>
-            </tr>
+            <TableRow item={item} />
+            // <tr
+            //   className={item.id}
+            //   key={item.id}
+            //   onClick={this.onClicking}
+            //   style={this.state.style}
+            // >
+            //   <td>{item.id} </td>
+            //   <td>{item.title}</td>
+            //   <td>{item.firstName}</td>
+            //   <td>{item.surname}</td>
+            //   <td>{item.email}</td>
+            //   <td>{item.roomId}</td>
+            //   <td>{item.checkInDate}</td>
+            //   <td>{item.checkOutDate}</td>
+            //   <td>
+            //     {moment(item.checkOutDate).diff(
+            //       moment(item.checkInDate),
+            //       "days"
+            //     )}
+            //   </td>
+            // </tr>
           ))}
         </tbody>
       </table>
