e975b22a8f1c322a3b75705dc472acaf071b544e
ahmedahashim <ahmed.almadina@hotmail.com>
2019-08-04 10:09:41 +0100

    added some change in heading.js

--- src/Heading.js
+++ src/Heading.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Heading = () =>{
     return (
-        <div>
-        <img src="https://s3.amazonaws.com/designmantic-logos/logos/2019/Apr/small-4314-5cb1bd101eb3f.png" alt=""/>
+      <div>
+        <header className="App-header">
+          <img className="logo" src="https://s3.amazonaws.com/designmantic-logos/logos/2019/Apr/small-4314-5cb1bd101eb3f.png" alt="" />
+        </header>
       </div>
     );
 }
