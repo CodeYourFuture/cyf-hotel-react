59ef44c4aa80f55ffe27ed12204020303853d0ae
Perasperad <you@example.com>
2019-10-03 22:48:58 +0200

    Lesson 2.9

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 class Restaurant extends React.Component {
+  addOrder = () => {
+    console.log("Thanks, your order has been placed!");
+  };
   render() {
     const pizzas = 0;
     return (
@@ -8,7 +11,10 @@ class Restaurant extends React.Component {
         <h3>Restaurant Orders</h3>
         <ul>
           <li>
-            Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
+            Pizzas: {pizzas}{" "}
+            <button className="btn btn-primary" onClick={this.addOrder}>
+              Add
+            </button>
           </li>
         </ul>
       </div>
