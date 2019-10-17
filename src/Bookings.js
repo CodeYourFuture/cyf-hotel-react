3eafb752cd9710593a60cd2301d675c57b988e99
Perasperad <you@example.com>
2019-10-17 23:43:24 +0200

    Lesson 3.4

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Search from "./Search.js";
 import FakeBookings from "./data/fakeBookings.json";
 import SearchResults from "./components/SearchResults.js";
@@ -7,15 +7,23 @@ const search = searchVal => {
   console.info("TO DO!", searchVal);
 };
 
-const Bookings = () => {
-  return (
-    <div className="App-content">
-      <div className="container">
-        <Search search={search} />
-        <SearchResults results={FakeBookings} />
+class Bookings extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fakeBookings: []
+    };
+  }
+  render() {
+    return (
+      <div className="App-content">
+        <div className="container">
+          <Search search={search} />
+          <SearchResults results={FakeBookings} />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Bookings;
