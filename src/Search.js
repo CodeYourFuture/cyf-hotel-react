a737ccbf22f0a4179800fdf9668b3303b7bfb9aa
Perasperad <you@example.com>
2019-10-17 20:32:43 +0200

    Lesson 3.2

--- src/Search.js
+++ src/Search.js
@@ -1,30 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import Button from "./components/Search-Button";
 
-const Search = () => {
-  return (
-    <div className="search">
-      <div className="page-header">
-        <h4 className="text-left">Search Bookings</h4>
-      </div>
-      <div className="row search-wrapper">
-        <div className="col">
-          <form className="form-group search-box">
-            <label htmlFor="customerName">Customer name</label>
-            <div className="search-row">
-              <input
-                type="text"
-                id="customerName"
-                className="form-control"
-                placeholder="Customer Id"
-              />
-              <Button />
-            </div>
-          </form>
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInput: ""
+    };
+  }
+  handleSearchInput = () => {
+    this.setState({
+      searchInput: event.target.value
+    });
+  };
+  render() {
+    return (
+      <div className="search">
+        <div className="page-header">
+          <h4 className="text-left">Search Bookings</h4>
+        </div>
+        <div className="row search-wrapper">
+          <div className="col">
+            <form className="form-group search-box">
+              <label htmlFor="customerName">Customer name</label>
+              <div className="search-row">
+                <input
+                  type="text"
+                  id="customerName"
+                  className="form-control"
+                  placeholder="Customer Id"
+                  value={this.state.searchInput}
+                  onChange={this.handleSearchInput}
+                />
+                <Button />
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
+    );
+  }
+}
 export default Search;
--- src/components/Clock.js
+++ src/components/Clock.js
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 class Clock extends Component {
   constructor(props) {
     super(props);
-
     this.state = { date: new Date() };
   }
 
