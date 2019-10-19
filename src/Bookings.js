6e475a717673a593e7e5140af4d72ad758ab149c
Perasperad <you@example.com>
2019-10-19 18:28:27 +0200

    fixing search

--- src/Bookings.js
+++ src/Bookings.js
@@ -3,27 +3,58 @@ import Search from "./Search.js";
 import FakeBookings from "./data/fakeBookings.json";
 import SearchResults from "./components/SearchResults.js";
 
-const search = searchVal => {
-  console.info("TO DO!", searchVal);
-};
-
-class Bookings extends Component {
+export default class Bookings extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fakeBookings: []
+      results: FakeBookings
     };
   }
+
+  /*  componentDidMount() {
+    fetch("https://cyf-react.glitch.me/error")
+      .then(res => {
+        if (res.status >= 200 && res.status < 300) {
+          return res;
+        } else {
+          throw new Error("HTTP error");
+        }
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          isLoading: false,
+          results: data
+        });
+      })
+      .catch(err => {
+        this.setState({
+          isLoading: false,
+          error: err
+        });
+      });
+  } */
+  search = searchVal => {
+    let searchResult = FakeBookings.filter(
+      customer =>
+        customer.firstName.toUpperCase().includes(searchVal.toUpperCase()) ||
+        customer.surname.toUpperCase().includes(searchVal.toUpperCase()) ||
+        customer.title.toUpperCase().includes(searchVal.toUpperCase())
+    );
+    this.setState({ results: searchResult });
+    console.info("TO DO!", searchResult);
+  };
+
   render() {
     return (
       <div className="App-content">
         <div className="container">
-          <Search search={search} />
-          <SearchResults results={FakeBookings} />
+          <Search search={this.search} />
+          {/* <SearchResults /> */}
+          <SearchResults results={this.state.results} />
+          {console.log(FakeBookings)}
         </div>
       </div>
     );
   }
 }
-
-export default Bookings;
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,46 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import moment from "moment";
 
-const SearchResults = props => {
-  return (
-    <table className="table table-hover">
-      <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">TITLE</th>
-          <th scope="col">FIRST NAME</th>
-          <th scope="col">SURNAME</th>
-          <th scope="col">EMAIL</th>
-          <th scope="col">ROOM ID</th>
-          <th scope="col">CHECK IN DATE</th>
-          <th scope="col">CHECK OUT DATE</th>
-          <th scope="col"> Numbers of nights</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.results.map((customer, index) => {
-          return (
-            <tr key={index}>
-              <td>{customer.id}</td>
-              <td>{customer.title}</td>
-              <td>{customer.firstName}</td>
-              <td>{customer.surname}</td>
-              <td>{customer.email}</td>
-              <td>{customer.roomId}</td>
-              <td>{customer.checkInDate}</td>
-              <td>{customer.checkOutDate}</td>
-              <td>
-                {moment(customer.checkOutDate).diff(
-                  customer.checkInDate,
-                  "days"
-                )}
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
-};
-
+class SearchResults extends Component {
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">TITLE</th>
+            <th scope="col">FIRST NAME</th>
+            <th scope="col">SURNAME</th>
+            <th scope="col">EMAIL</th>
+            <th scope="col">ROOM ID</th>
+            <th scope="col">CHECK IN DATE</th>
+            <th scope="col">CHECK OUT DATE</th>
+            <th scope="col"> Numbers of nights</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.results.map((customer, index) => {
+            return (
+              <tr key={index}>
+                <td>{customer.id}</td>
+                <td>{customer.title}</td>
+                <td>{customer.firstName}</td>
+                <td>{customer.surname}</td>
+                <td>{customer.email}</td>
+                <td>{customer.roomId}</td>
+                <td>{customer.checkInDate}</td>
+                <td>{customer.checkOutDate}</td>
+                <td>
+                  {moment(customer.checkOutDate).diff(
+                    customer.checkInDate,
+                    "days"
+                  )}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
+}
 export default SearchResults;
