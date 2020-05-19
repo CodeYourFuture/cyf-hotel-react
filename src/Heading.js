007439542e075c9d6e94ebf5c18ea97ed4801c77
hamidreza <hrfarzadeh@gmail.com>
2020-05-19 14:15:32 +0100

    component

---
+++ src/Heading.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <header className="App-header">
+      {" "}
+      CYF Hotel <Logo />
+    </header>
+  );
+}
+
+function Logo() {
+  return <img src="https://image.flaticon.com/icons/svg/139/139899.svg" />;
+}
+
+export default Heading;
