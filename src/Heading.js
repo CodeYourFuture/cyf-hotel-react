52927099a228ec743e1ef079ce961dc948ebea96
ellie <ellie.tms1371@gmail.com>
2020-05-19 10:56:07 +0100

    heading component

---
+++ src/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <img
+        className="img-header"
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+      />
+      CYF Hotel
+    </header>
+  );
+};
+
+export default Heading;
