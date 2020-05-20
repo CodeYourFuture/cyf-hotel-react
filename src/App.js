878d26e9da98858f5f443d4fe0c8283f9f2fd4f9
Afshin <47743758+AKaramifar@users.noreply.github.com>
2020-05-20 14:54:40 +0100

    Lesson 01 - Step 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
-
-import Bookings from "./Bookings";
 import "./App.css";
+import Bookings from "./Bookings";
+import Heading from "./Heading";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Heading />
       <Bookings />
     </div>
   );
 };
 
 export default App;
-// test
---
+++ src/Heading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div id="Heading">
+      <header className="App-header">CYF Hotel</header>
+    </div>
+  );
+};
+
+export default Heading;
