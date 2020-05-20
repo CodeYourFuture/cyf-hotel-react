834f9ffe670609d4697080654305594eefd87b2c
Afshin <47743758+AKaramifar@users.noreply.github.com>
2020-05-20 16:52:52 +0100

    Lesson-01-Step-02

--- src/Heading.js
+++ src/Heading.js
@@ -2,8 +2,12 @@ import React from "react";
 
 const Heading = () => {
   return (
-    <div id="Heading">
-      <header className="App-header">CYF Hotel</header>
+    <div id="Heading" className="Heading">
+      <header className="App-header">
+        <i class="App-logo fas fa-hotel" />
+        &nbsp;CYF Hotel
+      </header>
+      {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREJ2B2c3XWvQ57HqGHCENbSt5tB5YAkuXUmf3sdenfP5HRJ21c&usqp=CAU"/> */}
     </div>
   );
 };
