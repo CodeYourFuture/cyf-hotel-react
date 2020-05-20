bea932608d3467ba09bb5ba3dd81bd4756a9e17b
Afshin <47743758+AKaramifar@users.noreply.github.com>
2020-05-20 19:10:50 +0100

    Lesson 01- Step 03

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
-import Bookings from "./Bookings";
 import Heading from "./Heading";
+import TouristInfoCards from "./TouristInfoCards.js";
+import Bookings from "./Bookings";
 
 const App = () => {
   return (
     <div className="App">
       <Heading />
+      <TouristInfoCards />
       <Bookings />
     </div>
   );
