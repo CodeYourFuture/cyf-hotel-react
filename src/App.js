52f4c0856ad59a0f2c34a915524e78ae4a6a4f38
Sabia <sabiamushtaq2000@gmail.com>
2020-05-20 17:55:39 +0100

    ex 4 done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Bookings from "./Bookings";
 import Heading from "./Heading";
 import TouristInfoCards from "./TouristCards";
+import Footer from "./Footer";
 import "./App.css";
 
 const App = () => {
@@ -12,6 +13,13 @@ const App = () => {
       {/* <header className="App-header">CYF Hotel</header> */}
       <TouristInfoCards />
       <Bookings />
+      <Footer
+        addresses={[
+          "123 Fake Street, London, E1 4UD",
+          "hello@fakehotel.com",
+          "0123 456789"
+        ]}
+      />
     </div>
   );
 };
