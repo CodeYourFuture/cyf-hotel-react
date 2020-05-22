0d7bc6d65ba4ed92f25c470353f12417f8e422c3
melanieet <melanieet@users.noreply.github.com>
2020-05-22 15:39:24 +0100

    Steps 1 - 3 complete

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React from "react";
 
 import Bookings from "./Bookings";
 import "./App.css";
+import Heading from "./Heading";
+import TouristInfoCards from "./TouristInfoCards";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Heading />
+      <TouristInfoCards
+        city="Glasgow"
+        site="https://peoplemakeglasgow.com/"
+        image="https://frontline-collections.com/wp-content/uploads/2018/02/Glasgow-Debt-Collection.jpg"
+      />
+      <TouristInfoCards
+        city="Manchester"
+        site="https://www.visitmanchester.com/"
+        image="https://www.prolificnorth.co.uk/sites/default/files/styles/lightbox_large/public/images/news/manchester-cityscape0.jpg?itok=VjQDdTYJ"
+      />
+      <TouristInfoCards
+        city="London"
+        site="https://www.visitlondon.com/"
+        image="https://www.fodors.com/assets/destinations/2869/tower-bridge-london-england_980x650.jpg"
+      />
       <Bookings />
     </div>
   );
---
+++ src/Heading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Heading = () => (
+  <div className="Header">
+    <header className="App-header">CYF Hotel</header>
+    <img
+      className="Logo"
+      src="https://image.flaticon.com/icons/svg/139/139899.svg"
+    />
+  </div>
+);
+
+export default Heading;
