170021c56f6e70b1d5ee8f99e8602e1568d63098
BANI GIT <bani.rezaie@gmail.com>
2020-05-23 15:50:57 +0100

    cyf-hotel-first-lesson(1)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-
+import Heading from "./Heading";
 import Bookings from "./Bookings";
 import "./App.css";
+import SearchButton from "./SearchButton";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Heading />
       <Bookings />
     </div>
   );
