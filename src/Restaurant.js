fefae371ce206da1a9c5724bf6d863c3e4ca0145
Roxana <roxana.c2007@gmail.com>
2020-05-25 12:56:34 +0100

    hotel react

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Restaurant = () => {
-  const pizzas = 0;
+  const [count, setCount] = useState(0);
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
+          Pizzas: {count} <button className="btn btn-primary" onClick={ () =>
+          setCount(count +1)}>
+            Add</button>
         </li>
       </ul>
     </div>
   );
 };
+
 
 export default Restaurant;
