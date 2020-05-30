022543e17d2b14c5159c365f61b803a7b80c5448
Harris Sheraz <58743620+HarrisSheraz20@users.noreply.github.com>
2020-05-30 18:07:56 +0100

    exersise five done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Heading from "./Heading";
 import TouristInfoCards from "./TouristInfoCards";
 import Footer from "./Footer";
-import SearchResults from "./SearchResults";
+// import SearchResults from "./SearchResults";
 
 import Bookings from "./Bookings";
 import "./App.css";
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,20 +1,81 @@
 import React from "react";
 
-const SearchResults = () => {
+const SearchResults = props => {
   return (
-    <div>
+    <div className="table">
       <table>
         <tr>
-          <th>ID</th>
-          <th>Title</th>
-          <th>First Name</th>
-          <th>surname</th>
-          <th>Email</th>
-          <th>Room Id</th>
-          <th>Check in date</th>
-          <th>Check out date</th>
-          <th>Duration</th>
+          <th scope="col">id</th>
+          <th scope="col">title</th>
+          <th scope="col">firstName</th>
+          <th scope="col">surname</th>
+          <th scope="col">email</th>
+          <th scope="col">roomId</th>
+          <th scope="col">checkInDate</th>
+          <th scope="col">checkOutDate</th>
         </tr>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mr</td>
+            <td>Harris</td>
+            <td>Sheraz</td>
+            <td>harris@gmail.com</td>
+            <td>Id n.1</td>
+            <td>02/10/20</td>
+            <td>03/11/20</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Miss</td>
+            <td>Aila</td>
+            <td>Sheraz</td>
+            <td>Aila@gmail.com</td>
+            <td>Id n.2</td>
+            <td>02/10/20</td>
+            <td>03/11/20</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Jr</td>
+            <td>Gon</td>
+            <td>Freecss</td>
+            <td>Gon@gmail.com</td>
+            <td>Id n.3</td>
+            <td>02/10/20</td>
+            <td>03/11/20</td>
+          </tr>
+          <tr>
+            <th scope="row">4</th>
+            <td>Dr</td>
+            <td>Steaven</td>
+            <td>Strange</td>
+            <td>Strange@gmail.com</td>
+            <td>Id n.4</td>
+            <td>02/10/20</td>
+            <td>03/11/20</td>
+          </tr>
+          <tr>
+            <th scope="row">5</th>
+            <td>Miss</td>
+            <td>Kim</td>
+            <td>Posibel</td>
+            <td>Kim@gmail.com</td>
+            <td>Id n.5</td>
+            <td>02/10/20</td>
+            <td>03/11/20</td>
+          </tr>
+          <tr>
+            <th scope="row">6</th>
+            <td>Dr</td>
+            <td>Jonny</td>
+            <td>Browo</td>
+            <td>Jonny@gmail.com</td>
+            <td>Id n.6</td>
+            <td>02/10/20</td>
+            <td>03/11/20</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
