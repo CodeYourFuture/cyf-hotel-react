652682a229a4c14c509b88e6b2564215dc63b8cc
Babak <babakjahangiri@gmail.com>
2020-06-02 01:33:22 +0100

    L2-12 done

---
+++ src/Order.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import RestaurantButton from "./RestaurantButton";
+
+const Order = () => {
+  const [orders, setOrders] = useState(0);
+
+  function orderOne() {
+    setOrders(orders + 1);
+  }
+  return (
+    <li>
+      Pizzas: {orders}
+      <RestaurantButton handleOrder={orderOne} />
+    </li>
+  );
+};
+
+export default Order;
--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,20 +1,12 @@
-import React, { useState } from "react";
-import RestaurantButton from "./RestaurantButton";
+import React from "react";
+import Order from "./Order";
 
 const Restaurant = () => {
-  const [orders, setOrders] = useState(0);
-
-  function orderOne() {
-    setOrders(orders + 1);
-  }
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
-        <li>
-          Pizzas: {orders}
-          <RestaurantButton handleOrder={orderOne} />
-        </li>
+        <Order />
       </ul>
     </div>
   );
