d19cc588fcda01ecacf7eb9f016d37851969d5b6
ellie <ellie.tms1371@gmail.com>
2020-06-02 12:29:20 +0100

    add changes

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -1,92 +1,33 @@
 import React from "react";
-import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  Button
-} from "reactstrap";
+import EachCard from "./eachCard";
 
-const TouristInfoCards = props => {
+const TouristInfoCards = () => {
+  const Countrydatas = [
+    {
+      name: "Glasgow",
+      cardText:
+        "Glasgow Life delivers services on behalf of Glasgow City Council to both citizens and visitors to the city, providing opportunities through culture, sport and learning. Find out all the essential information for travelling to Glasgow for a holiday or vacation by rail, air, car, or by sea",
+      image:
+        "https://images.unsplash.com/photo-1531152127291-ea24c3b2a1da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+      name: "London",
+      cardText:
+        "Whether you are visiting London for the first time or coming back for a special occasion, you’ll discover a city full of unforgettable landmarks and exciting things to do. There are London attractions waiting for you around every corner... ",
+      image:
+        "https://images.unsplash.com/photo-1523289607602-d9abf1bbd7dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+      name: "Manchester",
+      cardText:
+        "Manchester city centre is jam-packed with unique and eclectic restaurants,bars,museums,hotels and places to stay whilst the surrounding Greater Manchester boroughs offer a patch-work of visitor experiences including quaint market towns,beautiful green spaces and ... ",
+      image:
+        "https://images.unsplash.com/photo-1551983627-d46f03818923?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    }
+  ];
   return (
-    <div className="col-12 d-lg-flex mx-auto">
-      <Card className="card bg-dark text-white flex-sm-column col-12 ml-auto mr-auto ml-md-4  mr-lg-2 col-md-10 mx-auto mx-md-auto col-lg-4 mt-md-5 mt-4 pr-2">
-        <CardImg
-          className="card-img"
-          top
-          src="https://images.unsplash.com/photo-1531152127291-ea24c3b2a1da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle>
-            <strong>Visit Glasgow</strong>
-          </CardTitle>
-          <CardText>
-            Glasgow Life delivers services on behalf of Glasgow City Council to
-            both citizens and visitors to the city, providing opportunities
-            through culture, sport and learning. Find out all the essential
-            information for travelling to Glasgow for a holiday or vacation by
-            rail, air, car, or by sea.
-          </CardText>
-          <a href="https://peoplemakeglasgow.com/" target="blank">
-            <Button className="btn btn-danger">Go Glasgow</Button>
-          </a>
-        </CardBody>
-      </Card>
-      <Card className="card bg-dark text-white flex-sm-column col-12 ml-auto mr-auto ml-md-4  mr-lg-2 col-md-10 mx-auto mx-md-auto col-lg-4 mt-md-5 mt-4 pr-2">
-        <CardImg
-          className="card-img"
-          top
-          src="https://images.unsplash.com/photo-1523289607602-d9abf1bbd7dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle>
-            <strong>Visit London</strong>
-          </CardTitle>
-          <CardText>
-            Whether you are visiting London for the first time or coming back
-            for a special occasion, you’ll discover a city full of unforgettable
-            landmarks and exciting things to do. There are London attractions
-            waiting for you around every corner...
-          </CardText>
-          <a
-            className="mt-auto"
-            href="https://www.visitlondon.com/"
-            target="blank"
-          >
-            <Button className="btn btn-danger">Go London</Button>
-          </a>
-        </CardBody>
-      </Card>
-      <Card className="card bg-dark text-white flex-sm-column col-12 ml-auto mr-auto ml-md-4  mr-lg-2 col-md-10 mx-auto mx-md-auto col-lg-4 mt-md-5 mt-4 pr-2">
-        <CardImg
-          className="card-img"
-          top
-          src="https://images.unsplash.com/photo-1551983627-d46f03818923?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle>
-            <strong>Visit Manchester</strong>
-          </CardTitle>
-          <CardText>
-            Manchester city centre is jam-packed with unique and eclectic
-            restaurants,bars,museums,hotels and places to stay whilst the
-            surrounding Greater Manchester boroughs offer a patch-work of
-            visitor experiences including quaint market towns,beautiful green
-            spaces and ...
-          </CardText>
-          <a
-            className="mt-auto"
-            href="https://www.visitmanchester.com/"
-            target="blank"
-          >
-            <Button className="btn btn-danger">Go Manchester</Button>
-          </a>
-        </CardBody>
-      </Card>
+    <div className="d-lg-flex flex-lg-row mt-2">
+      <EachCard datas={Countrydatas} />
     </div>
   );
 };
