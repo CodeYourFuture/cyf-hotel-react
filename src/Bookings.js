4b81fcef6504bfdf2b289ca9726c4afb35ed6012
Babak <babakjahangiri@gmail.com>
2020-06-02 02:04:24 +0100

    L2-15 done

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,14 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults";
 import FakeBookingsData from "./data/fakeBookings.json";
 
+//https://cyf-react.glitch.me/
 const Bookings = () => {
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
 
-  const [bookings, SetBookings] = useState(FakeBookingsData);
+  const [bookings, SetBookings] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me`)
+      .then(res => res.json())
+      .then(data => SetBookings(data));
+  }, []);
 
   return (
     <div className="App-content">
