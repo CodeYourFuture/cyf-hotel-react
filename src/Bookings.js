1bfeb56f76871605b86e114bd39e7b09b98d0f29
byohannes <42332389+byohannes@users.noreply.github.com>
2020-06-02 18:43:03 +0100

    Lesson 2 is all done

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search";
 import SearchResults from "./components/SearchResults";
-import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
+  const [bookings, setBookings] = useState([]);
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
+
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me`)
+      .then(res => res.json())
+      .then(data => setBookings(data));
+  }, []);
+  if (!bookings) {
+    return null;
+  }
 
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults results={FakeBookings} />
+        <SearchResults results={bookings} />
       </div>
     </div>
   );
--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 const TableRow = ({ list }) => {
   const checkIn = list.checkInDate;
@@ -6,8 +6,15 @@ const TableRow = ({ list }) => {
   const duration = moment(checkOut).diff(checkIn);
   const durationInHours = duration / 1000 / 60 / 60;
   const durationInDays = Math.round(durationInHours / 24);
+
+  const [rowActive, setRowActive] = useState(false);
+
+  const changeRowColor = () => {
+    setRowActive(!rowActive);
+  };
+
   return (
-    <tr>
+    <tr onClick={changeRowColor} className={rowActive ? "row-color" : ""}>
       <th scope="row">{list.id}</th>
       <td>{list.title}</td>
       <td>{list.firstName}</td>
