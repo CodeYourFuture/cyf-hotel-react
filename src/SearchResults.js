e4c1cd3f931568d5d0e01c188e22468f62bf1bec
Harris Sheraz <58743620+HarrisSheraz20@users.noreply.github.com>
2020-06-03 17:14:27 +0100

    update on last exersise for lesson 1.
    this is what I have come up with in this exercise but I am not sure if its correct. can anyone help me with a comment.

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,4 +1,5 @@
 import React from "react";
+import moment from "moment";
 
 function name(fullInfo) {
   return (
@@ -9,8 +10,15 @@ function name(fullInfo) {
       <td>{fullInfo.surname}</td>
       <td>{fullInfo.email}</td>
       <td>{fullInfo.roomId}</td>
-      <td>{fullInfo.checkInDate}</td>
+      <td>{fullInfo.checkInDate}</td>{" "}
+      {/*moment().diff(Moment|String|Number|Date|Array); */}
       <td>{fullInfo.checkOutDate}</td>
+      <td>
+        {moment(fullInfo.numOfDays).diff(
+          moment(fullInfo.checkInDate),
+          fullInfo.checkOutDate
+        )}
+      </td>
     </tr>
   );
 }
@@ -28,7 +36,7 @@ const SearchResults = props => {
           <th scope="col">roomId</th>
           <th scope="col">checkInDate</th>
           <th scope="col">checkOutDate</th>
-          <th scope="col">numOfNights</th>
+          <th scope="col">numOfDays</th>
         </tr>
         <tbody>{props.results.map(name)}</tbody>
       </table>
