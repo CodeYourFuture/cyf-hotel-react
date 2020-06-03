853d1e32b040e0485b5b60ebebf6af8d25ffe43a
Harris Sheraz <58743620+HarrisSheraz20@users.noreply.github.com>
2020-06-03 16:11:05 +0100

    exersise 6 done

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,5 +1,20 @@
 import React from "react";
 
+function name(fullInfo) {
+  return (
+    <tr>
+      <th scope="row">{fullInfo.id}</th>
+      <td>{fullInfo.title}</td>
+      <td>{fullInfo.firstName}</td>
+      <td>{fullInfo.surname}</td>
+      <td>{fullInfo.email}</td>
+      <td>{fullInfo.roomId}</td>
+      <td>{fullInfo.checkInDate}</td>
+      <td>{fullInfo.checkOutDate}</td>
+    </tr>
+  );
+}
+
 const SearchResults = props => {
   return (
     <div className="table">
@@ -13,69 +28,9 @@ const SearchResults = props => {
           <th scope="col">roomId</th>
           <th scope="col">checkInDate</th>
           <th scope="col">checkOutDate</th>
+          <th scope="col">numOfNights</th>
         </tr>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mr</td>
-            <td>Harris</td>
-            <td>Sheraz</td>
-            <td>harris@gmail.com</td>
-            <td>Id n.1</td>
-            <td>02/10/20</td>
-            <td>03/11/20</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Miss</td>
-            <td>Aila</td>
-            <td>Sheraz</td>
-            <td>Aila@gmail.com</td>
-            <td>Id n.2</td>
-            <td>02/10/20</td>
-            <td>03/11/20</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Jr</td>
-            <td>Gon</td>
-            <td>Freecss</td>
-            <td>Gon@gmail.com</td>
-            <td>Id n.3</td>
-            <td>02/10/20</td>
-            <td>03/11/20</td>
-          </tr>
-          <tr>
-            <th scope="row">4</th>
-            <td>Dr</td>
-            <td>Steaven</td>
-            <td>Strange</td>
-            <td>Strange@gmail.com</td>
-            <td>Id n.4</td>
-            <td>02/10/20</td>
-            <td>03/11/20</td>
-          </tr>
-          <tr>
-            <th scope="row">5</th>
-            <td>Miss</td>
-            <td>Kim</td>
-            <td>Posibel</td>
-            <td>Kim@gmail.com</td>
-            <td>Id n.5</td>
-            <td>02/10/20</td>
-            <td>03/11/20</td>
-          </tr>
-          <tr>
-            <th scope="row">6</th>
-            <td>Dr</td>
-            <td>Jonny</td>
-            <td>Browo</td>
-            <td>Jonny@gmail.com</td>
-            <td>Id n.6</td>
-            <td>02/10/20</td>
-            <td>03/11/20</td>
-          </tr>
-        </tbody>
+        <tbody>{props.results.map(name)}</tbody>
       </table>
     </div>
   );
