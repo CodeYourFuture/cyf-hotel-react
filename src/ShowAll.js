40d299db60c6d6530e01aaef6b574a1eb3fb19f8
Erwin6997 <59776155+Erwin6997@users.noreply.github.com>
2020-06-04 13:19:56 +0100

    Update ShowAll.js

--- src/ShowAll.js
+++ src/ShowAll.js
@@ -1,44 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Nights from "./Nights";
 const ShowAll = props => {
-  const select = () => {
-    console.log("dfdfd");
-    return <tr className={"selectTr"} />;
-  };
+  const [clicked, setClicked] = useState(false);
   return (
     <div className="table">
       <table>
-        <thead>
-          <tr>
-            <th> Title </th>
-            <th> First Name </th>
-            <th> SureName </th>
-            <th> Email </th>
-            <th> Room Number </th>
-            <th> CheckInDate </th>
-            <th> CheckOutDate </th>
-            <th> Nights </th>
-          </tr>
-        </thead>
+        <tr>
+          <th />
+          <th> Title </th>
+          <th> First Name </th>
+          <th> SureName </th>
+          <th> Email </th>
+          <th> Room Number </th>
+          <th> CheckInDate </th>
+          <th> CheckOutDate </th>
+          <th> Nights </th>
+        </tr>
         {props.data.map((booking, index) => {
           return (
-            <tbody key={index} id={booking.id}>
-              <tr onSelect={select}>
-                <td>{booking.title}</td>
-                <td>{booking.firstName}</td>
-                <td>{booking.surname}</td>
-                <td>{booking.email}</td>
-                <td>{booking.roomId}</td>
-                <td>{booking.checkInDate}</td>
-                <td>{booking.checkOutDate}</td>
-                <td>
-                  <Nights
-                    timeline={booking.checkInDate}
-                    timeout={booking.checkOutDate}
-                  />
-                </td>
-              </tr>
-            </tbody>
+            <tr
+              onClick={() => setClicked(!clicked)}
+              className={clicked ? "bg-warning" : "show-all"}
+            >
+              <th scope="row" key={index}>
+                {booking.id}
+              </th>
+              <td>{booking.title}</td>
+              <td>{booking.firstName}</td>
+              <td>{booking.surname}</td>
+              <td>{booking.email}</td>
+              <td>{booking.roomId}</td>
+              <td>{booking.checkInDate}</td>
+              <td>{booking.checkOutDate}</td>
+              <td>
+                <Nights
+                  timeline={booking.checkInDate}
+                  timeout={booking.checkOutDate}
+                />
+              </td>
+            </tr>
           );
         })}
       </table>
