595de83fcf751ff4fcc2287f69c2f40f40d456a1
[Alisina92] <asb92.jobs@gmail.com>
2020-06-04 18:23:47 +0100

    Hotel proj level 10 completed

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -2,8 +2,13 @@ import React, { useState } from "react"; // imported the useState react hook.
 
 const Restaurant = () => {
   const [orders, setOrders] = useState(0); // declared orders variable and passed setOrder function to it.
-
   //const pizzas= 0; commented Pizzas variables
+
+  const orderOne = () => {
+    // added the orderOne function that calls set orders function
+    setOrders(orders + 1); //setOrder function gets the order as a parameter and increases with +1;
+  };
+
   return (
     <div>
       <h3>Restaurant Orders</h3>
@@ -11,7 +16,14 @@ const Restaurant = () => {
         <li>
           Pizzas: {orders}
           {/* replaced pizzas variable with orders*/}{" "}
-          <button className="btn btn-primary">Add</button>
+          <button
+            className="btn btn-primary"
+            onClick={
+              orderOne
+            } /*will call on order function to increase the order by 1*/
+          >
+            Add
+          </button>
         </li>
       </ul>
     </div>
