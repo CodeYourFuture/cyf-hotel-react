6325c13f85c0912d63f52b097ef11bf2a738297d
[Alisina92] <asb92.jobs@gmail.com>
2020-06-05 17:39:57 +0100

    Hotel proj level 16 completed

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,8 +1,12 @@
-import React from "react";
-import moment from "moment"; // imported the moment js
+import React, { useState } from "react";
+import TableRows from "./TableRows";
+
 const SearchResults = props => {
   /*Table added added to website using JSX */
   // 1
+
+  const [clickedRow, setClickedRow] = useState(null);
+
   return (
     <div>
       <table className="table">
@@ -21,24 +25,15 @@ const SearchResults = props => {
         </thead>
         <tbody>
           {/* loops through the guest list pass it to props*/}
-          {props.guestList.map((element, index) => {
-            const a = moment(element.checkOutDate); // using moment js function to pass in checkout date
-            const b = moment(element.checkInDate); // using moment js function to pass in checkout date
-            const differenceOfDays = a.diff(b, "days"); // calculates the differenceOfDays
+          {props.guestList.map(element => {
             return (
-              <tr key={index}>
-                {/*assigns key called index to table row*/}
-                {/*retrieves the data for the table using array element by accessing object properties*/}
-                <th scope="row">{element.id}</th>
-                <td>{element.title}</td>
-                <td>{element.firstName}</td>
-                <td>{element.surname}</td>
-                <td>{element.email}</td>
-                <td>{element.roomId}</td>
-                <td>{element.checkInDate}</td>
-                <td>{element.checkOutDate}</td>
-                <td>{differenceOfDays}</td>
-              </tr>
+              <TableRows
+                key={element.id}
+                element={element}
+                isClicked={clickedRow === element.id}
+                setClickedRow={setClickedRow}
+                id={element.id}
+              />
             );
           })}
         </tbody>
