091cb6f3eab6c6f4d144d869ded2f615f42332d0
[Alisina92] <asb92.jobs@gmail.com>
2020-06-05 03:45:16 +0100

    Hotel proj level 12 completed

---
+++ src/Order.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react"; // imported the useState react hook.
+import RestaurantButton from "./RestaurantButton";
+const Order = () => {
+  const [orders, setOrders] = useState(0); // declared orders variable and passed setOrder function to it.
+  //const pizzas= 0; commented Pizzas variables
+
+  const orderOne = () => {
+    // added the orderOne function that calls set orders function
+    setOrders(orders + 1); //setOrder function gets the order as a parameter and increases with +1;
+  };
+  return (
+    <li>
+      Pizzas: {orders}
+      {/* replaced pizzas variable with orders*/}{" "}
+      <RestaurantButton order={orderOne} />
+    </li>
+  );
+};
+
+export default Order;
--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,23 +1,15 @@
-import React, { useState } from "react"; // imported the useState react hook.
-import RestaurantButton from "./RestaurantButton";
-const Restaurant = () => {
-  const [orders, setOrders] = useState(0); // declared orders variable and passed setOrder function to it.
-  //const pizzas= 0; commented Pizzas variables
+import React from "react"; // imported the useState react hook.
 
-  const orderOne = () => {
-    // added the orderOne function that calls set orders function
-    setOrders(orders + 1); //setOrder function gets the order as a parameter and increases with +1;
-  };
+import Order from "./Order";
+const Restaurant = () => {
+  // declared orders variable and passed setOrder function to it.
+  //const pizzas= 0; commented Pizzas variables
 
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
-        <li>
-          Pizzas: {orders}
-          {/* replaced pizzas variable with orders*/}{" "}
-          <RestaurantButton order={orderOne} />
-        </li>
+        <Order />
       </ul>
     </div>
   );
