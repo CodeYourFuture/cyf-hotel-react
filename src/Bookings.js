951b146158a59c7181851da8f700f974537e03eb
Rayhan94 <rayhanart94@yahoo.co.uk>
2020-06-05 14:19:26 +0100

    completed 15

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import FakeBookings from "./data/fakeBookings.json";
 import SearchResults from "./SearchResults";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then(res => res.json())
+      .then(data => setBookings(data));
+  }, []);
+
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
