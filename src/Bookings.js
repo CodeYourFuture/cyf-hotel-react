5826915dd8e3ea5fb51890fd5e0f652ae2b5bfd9
[Alisina92] <asb92.jobs@gmail.com>
2020-06-05 14:41:53 +0100

    Hotel proj level 15 completed

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react"; //imported useState and useEffect
 import Search from "./Search.js";
 import SearchResults from "./SearchResults";
 // import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
+//import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]); // created the state hook passing booking and setBookings functions give it empty array.
+
+  useEffect(() => {
+    // useEffect function
+    fetch(`https://cyf-react.glitch.me`) // fetched the booking data
+      .then(Response => Response.json())
+      .then(data => setBookings(data))
+      .catch(err => console.log(err)); // error handing
+  }, []); // square bracket will prevent fetch infinite loop by react
 
   const search = searchVal => {
     console.info("TO DO!", searchVal);
@@ -17,6 +25,7 @@ const Bookings = () => {
       <div className="container">
         <Search search={search} />
         <SearchResults guestList={bookings} />
+        {/*replaced the fake booking with bookings */}
         {/* <SearchResults results={FakeBookings} /> which displays the table*/}
       </div>
     </div>
