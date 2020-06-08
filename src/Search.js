40d1e2ede0c243bdacc5b8461118c97c0e10b302
ola <olasvoice@gmail.com>
2020-06-08 17:49:27 +0100

    3 week started

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchButton from "./SearchButton";
 const Search = () => {
-  return <SearchButton />;
+  const [searchInput, setSearchInput] = useState("");
+  console.log("searchInput", searchInput);
+  const handleSearchInput = e => {
+    setSearchInput(e.target.value);
+  };
+  return (
+    <div className="search">
+      <div className="page-header">
+        <h4 className="text-left">Search Bookings</h4>
+      </div>
+      <div className="row search-wrapper">
+        <div className="col">
+          <form
+            className="form-group search-box"
+            onSubmit={event => {
+              event.preventDefault(searchInput);
+            }}
+          >
+            <label htmlFor="customerName">Customer name</label>
+            <div className="search-row">
+              <input
+                onChange={handleSearchInput}
+                type="text"
+                id="customerName"
+                className="form-control"
+                placeholder="Customer name"
+                value={searchInput}
+              />
+              <SearchButton />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
