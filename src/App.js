673cc0e4f51a25606a3a4d6964522bf9e001d5de
supertata <laetitiachira@gmail.com>
2020-07-12 12:58:34 +0100

    header components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import SearchButton from "./SearchButton";
+import Heading from "./Heading";
 
 import Bookings from "./Bookings";
 import "./App.css";
@@ -6,8 +8,9 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel </header>
+      <Heading />
       <Bookings />
+      <SearchButton />
     </div>
   );
 };
