546c4da8a2ba5ac9d3f446bcfd8c610ab855f671
Halit <60817499+halitos@users.noreply.github.com>
2020-07-13 11:54:27 +0100

    done some style for footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,11 +3,18 @@ import React from "react";
 const Footer = ({ adress }) => {
   console.log(adress);
   return (
-    <ul>
-      {adress.map((item, index) => {
-        return <li key={index}>{item}</li>;
-      })}
-    </ul>
+    <div className="nav flex-column align-items-center bg-secondary text-white p-3">
+      <h3>Contact Details</h3>
+      <ul className="nav flex-column">
+        {adress.map((item, index) => {
+          return (
+            <li className="nav-item" key={index}>
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
