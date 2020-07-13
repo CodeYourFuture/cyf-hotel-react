b8b4a69ec582c75e098566c71150da84405b493a
Halit <60817499+halitos@users.noreply.github.com>
2020-07-13 14:44:24 +0100

    lvl-6 done

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,9 @@
 import React from "react";
 
 const Footer = ({ adress }) => {
-  console.log(adress);
   return (
     <div className="nav flex-column align-items-center bg-secondary text-white p-3">
-      <h3>Contact Details</h3>
+      <h4>Contact Details</h4>
       <ul className="nav flex-column">
         {adress.map((item, index) => {
           return (
---
+++ src/SearchResults.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const SearchResults = ({ results }) => {
+  console.log(results);
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Title</th>
+          <th scope="col">First name</th>
+          <th scope="col">Surname</th>
+          <th scope="col">E-mail</th>
+          <th scope="col">Room No</th>
+          <th scope="col">Check-in Date</th>
+          <th scope="col">Check-out Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {results.map((props, index) => {
+          return (
+            <tr key={index}>
+              <td>{props.id}</td>
+              <td>{props.title}</td>
+              <td>{props.firstName}</td>
+              <td>{props.surname}</td>
+              <td>{props.email}</td>
+              <td>{props.roomId}</td>
+              <td>{props.checkInDate}</td>
+              <td>{props.checkOutDate}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default SearchResults;
