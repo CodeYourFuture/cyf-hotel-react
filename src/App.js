8acc2a3a8d75823d3d6747384f70db34a2c54b73
Andrei <andsemenov@gmail.com>
2020-07-28 16:37:55 +0100

    Completed step 1 and 2

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import React from "react";
 import Bookings from "./Bookings";
 import "./App.css";
 
+const Heading = () => {
+  return (
+    <header className="App-header">
+      CYF Hotel
+      <img
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        alt="logo image"
+      />
+    </header>
+  );
+};
+
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Heading />
       <Bookings />
     </div>
   );
