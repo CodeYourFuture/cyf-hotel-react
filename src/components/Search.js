6c7be8fc596a56a45c010e9884971ecd81183be3
supertata <laetitiachira@gmail.com>
2020-07-30 10:16:25 +0100

    added error handling

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 import SearchButton from "./SearchButton.js";
 
-const Search = () => {
+const Search = props => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleSearchInput = event => {
+    setSearchInput(event.target.value);
+    // console.log(searchInput);
+  };
+
+  const handleSearchSubmit = event => {
+    event.preventDefault();
+    props.search(searchInput);
+  };
+
   return (
     <div className="search">
       <div className="page-header">
@@ -10,12 +22,14 @@ const Search = () => {
       </div>
       <div className="row search-wrapper">
         <div className="col">
-          <form className="form-group search-box">
+          <form className="form-group search-box" onSubmit={handleSearchSubmit}>
             <label htmlFor="customerName">Customer name</label>
             <div className="search-row">
               <input
                 type="text"
                 id="customerName"
+                value={searchInput}
+                onChange={handleSearchInput}
                 className="form-control"
                 placeholder="Customer name"
               />
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
 import "./SearchResults.css";
 import moment from "moment";
+import CustomerProfile from "./CustomerProfile";
+
 const SearchResults = props => {
   const [isSelected, setIsSelected] = useState([]);
+  const [rowSelected, setRowSelected] = useState(null);
+  console.log(rowSelected);
+
+  // const profile = <CustomerProfile />;
+  const handleClick = e => {
+    setRowSelected(e.target.id);
+    console.log(e.target.id);
+  };
+
   const setClassName = id => {
-    debugger;
     if (!isSelected.includes(id)) {
       // setIsSelected([...isSelected, id]);
       setIsSelected(isSelected.concat([id]));
@@ -13,51 +23,61 @@ const SearchResults = props => {
     }
   };
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">Id</th>
-          <th scope="col">Title</th>
-          <th scope="col">FirstName</th>
-          <th scope="col">Surname</th>
-          <th scope="col">Email</th>
-          <th scope="col">Room Id</th>
-          <th scope="col">Check-In-Date</th>
-          <th scope="col">Check-Out-Date</th>
-          <th scope="col">Number of Nights</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.results.map((field, index) => {
-          const nightsSpent = moment(field.checkOutDate).diff(
-            field.checkInDate,
-            "days"
-          );
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Title</th>
+            <th scope="col">FirstName</th>
+            <th scope="col">Surname</th>
+            <th scope="col">Email</th>
+            <th scope="col">Room Id</th>
+            <th scope="col">Check-In-Date</th>
+            <th scope="col">Check-Out-Date</th>
+            <th scope="col">Number of Nights</th>
+            <th scope="col">Show Profile</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.results.map((field, index) => {
+            const nightsSpent = moment(field.checkOutDate).diff(
+              field.checkInDate,
+              "days"
+            );
 
-          return (
-            <tr
-              key={index}
-              onClick={() => setClassName(index)}
-              style={
-                isSelected.includes(index)
-                  ? { backgroundColor: "red" }
-                  : { backgroundColor: "white" }
-              }
-            >
-              <th scope="row">{field.id}</th>
-              <td>{field.title}</td>
-              <td>{field.firstName}</td>
-              <td>{field.lastName}</td>
-              <td>{field.email}</td>
-              <td>{field.roomId}</td>
-              <td>{field.checkInDate}</td>
-              <td>{field.checkOutDate}</td>
-              <td>{nightsSpent}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+            return (
+              <tr
+                key={index}
+                onClick={() => setClassName(index)}
+                style={
+                  isSelected.includes(index)
+                    ? { backgroundColor: "red" }
+                    : { backgroundColor: "white" }
+                }
+              >
+                <th scope="row">{field.id}</th>
+                <td>{field.title}</td>
+                <td>{field.firstName}</td>
+                <td>{field.surname}</td>
+                <td>{field.email}</td>
+                <td>{field.roomId}</td>
+                <td>{field.checkInDate}</td>
+                <td>{field.checkOutDate}</td>
+                <td>{nightsSpent}</td>
+
+                <td>
+                  <button onClick={handleClick} id={index + 1}>
+                    Show Profile
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      {rowSelected && <CustomerProfile id={rowSelected} />}
+    </div>
   );
 };
 
