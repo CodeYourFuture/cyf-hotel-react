b570182b1a11ea8a3ea855c82cf7cd50054ddfeb
Andrei <andsemenov@gmail.com>
2020-07-31 20:05:49 +0100

    Added additional column

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -8,6 +8,7 @@ const SearchResults = props => {
           {Object.keys(props.results[0]).map(function(key) {
             return <th scope="col">{key}</th>;
           })}
+          <th scope="col">Number of nights</th>
         </tr>
       </thead>
       <tbody>
@@ -24,32 +25,5 @@ const SearchResults = props => {
     </table>
   );
 };
-
-/* 
-
-
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">id</th>
-          <th scope="col">title</th>
-          <th scope="col">first name</th>
-          <th scope="col">surname</th>
-          <th scope="col">email</th>
-          <th scope="col">room id</th>
-          <th scope="col">check in date</th>
-          <th scope="col">check out date</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
- {props.results.map((result) => {
-        return <li>{result.id}</li>;
-      })}
-        </tr>
-        
-      </tbody>
-    </table>
-  ); */
 
 export default SearchResults;
