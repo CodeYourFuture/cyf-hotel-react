82021b1c1cbf198380e97bcb018052634af05233
Taslima <61126921+Taslima26@users.noreply.github.com>
2020-08-01 21:01:11 +0100

    sort functionality added half way

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -3,28 +3,76 @@ import React, { useState } from "react";
 import Bookings from "./Bookings";
 import CustomerProfile from "./CustomerProfile";
 import TableRow from "./TableRow";
-function SearchResults(props) {
+function SearchResults({ results }) {
   const [customerId, setCustomerId] = useState(null);
+  const sortedResults = [...results];
+  const [sortedField, setSortedField] = useState(null);
+  if (sortedField !== null) {
+    sortedResults.sort((a, b) => {
+      if (a[sortedField] < b[sortedField]) {
+        return -1;
+      }
+      if (a[sortedField] > b[sortedField]) {
+        return 1;
+      }
+      return 0;
+    });
+  }
 
   return (
     <div>
       <table className="table">
         <thead className="thead-dark">
           <tr>
-            <th scope="col">Id</th>
-            <th scope="col">Title</th>
-            <th scope="col">Name</th>
-            <th scope="col">Surname</th>
-            <th scope="col">email</th>
-            <th scope="col">RoomId</th>
-            <th scope="col">Check in date</th>
-            <th scope="col">Check Out Date</th>
+            <th scope="col">
+              <button type="button" onClick={() => setSortedField("id")}>
+                Id
+              </button>
+            </th>
+            <th scope="col">
+              <button type="button" onClick={() => setSortedField("firstName")}>
+                firstName
+              </button>
+            </th>
+
+            <th scope="col">
+              <button type="button" onClick={() => setSortedField("surname")}>
+                surname
+              </button>
+            </th>
+            <th scope="col">
+              <button type="button" onClick={() => setSortedField("email")}>
+                email
+              </button>
+            </th>
+            <th scope="col">
+              <button type="button" onClick={() => setSortedField("roomId")}>
+                RoomId
+              </button>
+            </th>
+            <th scope="col">
+              <button
+                type="button"
+                onClick={() => setSortedField("checkInDate")}
+              >
+                check In Date
+              </button>
+            </th>
+            <th scope="col">
+              <button
+                type="button"
+                onClick={() => setSortedField("checkOutDate")}
+              >
+                check Out Date
+              </button>
+            </th>
+
             <th scope="col">Number Of Nights</th>
             <th scope="col">Show Profile</th>
           </tr>
         </thead>
         <tbody>
-          {props.results.map((customer, index) => {
+          {sortedResults.map((customer, index) => {
             return (
               <TableRow
                 customer={customer}
