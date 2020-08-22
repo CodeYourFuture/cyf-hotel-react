1461114d80077d390719d0e0a71ff85f3193a16d
Andrei <andsemenov@gmail.com>
2020-08-22 15:14:55 +0100

    Not working second sort

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -3,23 +3,90 @@ import CustomerProfile from "./CustomerProfile";
 import moment from "moment";
 
 const SearchResults = props => {
+  const [alphaSort, setAlphaSort] = useState({
+    id: 1,
+    title: 1,
+    firstName: 1,
+    surname: 1,
+    email: 1,
+    roomId: 1,
+    checkInDate: 1,
+    checkOutDate: 1,
+    "Number of nights": 1
+  });
+  console.log(alphaSort);
   let selectedTd;
 
   let [currentId, setCurrentId] = useState("");
+  let readyArr = props.results.slice(0);
+  //////////////////////////////
+  readyArr.forEach(element => {
+    element["Number of nights"] = NumberNights(element);
+  });
+  const [passedArr, setPassedArr] = useState(readyArr);
+  //////////////////////////////////////////
 
+  ////////////////////////////////////////////
   function highlightWhenClicked(event) {
     selectedTd = event.target;
-    console.log(selectedTd.className);
+    let selectedKey = selectedTd.className;
+    function azSorting(a, b) {
+      let textA = a[selectedKey]; //let textA = a[selectedKey].toUpperCase();
+      let textB = b[selectedKey]; //let textB = b[selectedKey].toUpperCase();
+      return textA < textB ? -1 : textA > textB ? 1 : 0;
+    }
+
+    function zaSorting(a, b) {
+      let textA = a[selectedKey];
+      let textB = b[selectedKey];
+
+      return textA > textB ? -1 : textA < textB ? 1 : 0;
+    }
+
+    function sortedArray(func) {
+      setPassedArr(readyArr.sort(func).slice(0));
+    }
+    ///////////////////////////////////////////////////////
+
     if (selectedTd.parentNode.className.includes("bookings highlight")) {
       selectedTd.parentNode.classList.remove("highlight");
       return;
     }
     selectedTd.parentNode.classList.add("highlight");
+    //////////////////////////////////////////////////////
+    /*   const updatedUserData = {
+      ...userData,
+      [event.target.name]: event.target.value,
+    };
+
+    setUserData(updatedUserData);
+ */
+    console.log(selectedKey);
+    console.log([selectedKey]);
+    console.log(alphaSort[selectedKey]);
+    //////////////////////////////////////////////////////
+    if (alphaSort.selectedKey === 0) {
+      // sortedArray(azSorting);
+      const updatedData = {
+        ...alphaSort,
+        [selectedKey]: 1
+      };
+      setAlphaSort(updatedData);
+    } else {
+      // sortedArray(zaSorting);
+      const updatedData = {
+        ...alphaSort,
+        [selectedKey]: 1
+      };
+      setAlphaSort(updatedData);
+    }
+    console.log(alphaSort);
+    //////////////////////////////////////////////////////////
   }
 
   function handleShowProfile(event) {
-    /*   currentId = event.target.id;
-    console.log(currentId); */
+    currentId = event.target.id;
+    console.log(currentId);
     setCurrentId(event.target.id);
   }
 
@@ -36,15 +103,16 @@ const SearchResults = props => {
                 </th>
               );
             })}
-            <th scope="col">Number of nights</th>
           </tr>
         </thead>
         <tbody>
-          {props.results.map(result => {
+          {passedArr.map(result => {
             return (
               <tr
                 className="bookings"
-                key={(Math.random() / 10000000000).toString()} //here was onClick
+                key={(Math.random() / 10000000000).toString()}
+                onClick={highlightWhenClicked}
+                //here was onClick
               >
                 <td>
                   <button id={result.id} onClick={handleShowProfile}>
@@ -55,14 +123,14 @@ const SearchResults = props => {
                   return (
                     <td
                       key={(Math.random() * 10000000000).toString()}
-                      onClick={highlightWhenClicked}
-                      className={index} //replaced from line 44
+                      className={key}
+                      //replaced from line 44
                     >
                       {result[key]}
                     </td>
                   );
                 })}
-                <td>{NumberNights(result)}</td>
+                {/*   <td>{NumberNights(result)}</td> */}
               </tr>
             );
           })}
