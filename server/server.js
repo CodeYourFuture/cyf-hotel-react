1add7b1c4de97f2cc7aa640183db6e300cb7a0ea
F Nawal Abdul Careem <nawal@fs-macbook-air.home>
2020-09-11 19:51:09 +0100

    changed server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, "..", "public");
+const publicPath = path.join(__dirname, "build", "public");
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get("*", (req, res) => {
