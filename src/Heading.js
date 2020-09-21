b0c6fa80a43fcc9d94c5efe01c9ba178793cd3c4
Mursel Aysan <aysanmursel@gmail.com>
2020-09-21 11:33:13 +0100

    Lessson 1 and 2 DONE

---
+++ src/Heading.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Heading() {
+  return (
+    <div>
+      <header className="App-header">CYF Hotel</header>
+      <img src="https://image.flaticon.com/icons/svg/139/139899.svg" />
+    </div>
+  );
+}
