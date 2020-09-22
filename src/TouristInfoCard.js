9d2cc52d5088116c55735cd351108d0ea5e67c0a
g1st <gintstan@gmail.com>
2020-09-22 09:29:09 +0100

    Style InfoCards and fix links

--- src/TouristInfoCard.js
+++ src/TouristInfoCard.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-const TouristInfoCard = ({ link, city }) => {
+const TouristInfoCard = ({ link, imageSource, city }) => {
   return (
-    <div className="card">
-      <img src={link} alt={`City of ${city}`} className="card-img-top" />
-      <div className="card-body">
-        <a href="https://peoplemakeglasgow.com" className="btn btn-primary">
+    <div className="card mx-auto mx-sm-1 mx-md-2 mx-lg-3">
+      <img
+        src={imageSource}
+        alt={`City of ${city}`}
+        className="card-img-top infoCardImage"
+      />
+      <div className="card-body d-flex justify-content-center align-items-center">
+        <a href={link} className="btn btn-primary">
           Visit {city}
         </a>
       </div>
