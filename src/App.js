fc90d4a784c01c28dc7cb4949a26cbae71fbb1f9
zubeda <zubedauk@gmail.com>
2020-09-24 11:58:12 +0100

    lesson1

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Heading from "./Heading";
 import Bookings from "./Bookings";
 import TouristInfoCards from "./TouristInfoCards";
 import Restaurant from "./Restaurant";
-
-import "./App.css";
 import Footer from "./Footer";
+import "./App.css";
+
 const App = () => {
   return (
     <div className="App">
--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,5 @@
 import React from "react";
-import SearchButton from "/home/cyf/Desktop/react/cyf-hotel-react/src/SearchButton.js";
+import SearchButton from "./SearchButton.js";
 const Search = () => {
   return (
     <div className="search">
