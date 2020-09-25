d6298e4e1515bad3387d4b5f29af4c622d09027c
MartinBoylan <63868146+MartinBoylan@users.noreply.github.com>
2020-09-25 19:26:44 +0100

    week1 React cyfhotel

---
+++ src/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <header className="App-header">CYF Hotel</header>
+      <img
+        alt="logo"
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+      />
+    </div>
+  );
+}
+
+export default Header;
