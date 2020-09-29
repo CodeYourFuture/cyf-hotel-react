40debc68405b908a1d988aedc81e23129d99b09e
edksam <edsam@DESKTOP-M08GAP8.localdomain>
2020-09-29 15:21:07 +0100

    Extract add button

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -6,22 +6,19 @@ const RestaurantButton = ({ addOrder }) => (
     Add
   </button>
 );
-const Orders = () => {
+
+const Restaurant = () => {
   const [orders, setOrders] = useState(0);
   // const pizzas = 0;
   const orderOne = () => {
     setOrders(orders + 1);
   };
-  return <li>Pizzas: {orders}</li>;
-};
-
-const Restaurant = () => {
   return (
     <Fragment>
       <div className="App-content">
         <h3>Restaurant Orders</h3>
         <ul>
-          <Orders />
+          <li>Pizzas: {orders}</li>
           <RestaurantButton addOrder={orderOne} />
         </ul>
       </div>
