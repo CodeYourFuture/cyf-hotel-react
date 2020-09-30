e8a8c2a82c8bc967dbbf8bbe2ecc3c35cb2b3d8b
Mursel Aysan <aysanmursel@gmail.com>
2020-09-30 10:28:53 +0100

    lesson 2 , task 14-15 DONE

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
+//import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then(res => res.json())
+      .then(data => setBookings(data));
+  }, []);
 
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-
-        <SearchResults results={FakeBookings} />
+        {/* <SearchResults results={FakeBookings} /> */}
+        <SearchResults results={bookings} />
       </div>
     </div>
   );
