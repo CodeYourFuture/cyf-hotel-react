35dc1d261613a2afc7e447b3f9ff5e82b1e00cbb
Abdennour Hachemi <nouri89@hotmail.com>
2020-09-30 22:34:38 +0100

    Update Bookings.js

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,21 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./Search.js";
-// import SearchResults from "./SearchResults.js";
-// import FakeBookings from "./data/fakeBookings.json";
+import SearchResults from "./SearchResults";
+import FakeBookings from "./data/fakeBookings.json";
 
-const Bookings = () => {
-  const search = searchVal => {
-    console.info("TO DO!", searchVal);
-  };
-
-  return (
-    <div className="App-content">
-      <div className="container">
-        <Search search={search} />
-        {/* <SearchResults results={FakeBookings} /> */}
-      </div>
-    </div>
-  );
+function Bookings() {
+	const [bookings, setBookings] = useState(FakeBookings);
+ 
+	return (
+		<div className="App-content">
+			<div className="container">
+				<Search />
+				<SearchResults results={bookings} />
+			</div>
+		</div>
+	);
 };
 
 export default Bookings;
+//<SearchResults results={FakeBookings} />
