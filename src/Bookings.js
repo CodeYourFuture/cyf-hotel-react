ac2166b78b6583262b836cf7d670ec115ee86edf
iggajewski <64140239+iggajewski@users.noreply.github.com>
2020-10-03 16:23:28 +0100

    fixed API fetching

--- src/Bookings.js
+++ src/Bookings.js
@@ -2,29 +2,33 @@ import React from "react";
 import { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
+// import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  let [bookings, setBookings] = useState(FakeBookings);
+  let [bookings, setBookings] = useState(null);
 
-  // useEffect(async function() {
-  //   await fetch("https://cyf-react.glitch.me")
-  //   .then(response => response.json())
-  //   .then(json => bookings = json)
-  // });
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then(response => response.json())
+      .then(json => setBookings(json));
+  }, []);
 
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
 
-  return (
-    <div className="App-content">
-      <div className="container">
-        <Search search={search} />
-        <SearchResults results={bookings} />
+  if (bookings) {
+    return (
+      <div className="App-content">
+        <div className="container">
+          <Search search={search} />
+          <SearchResults results={bookings} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 };
 
 export default Bookings;
