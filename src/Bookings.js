d5ac2e429001ec21c5787132c5d5cf54c9731765
Daniel Carter <62474051+carterd888@users.noreply.github.com>
2020-10-04 13:18:18 +0100

    fixed lesson 2 part 15, added conditional

--- src/Bookings.js
+++ src/Bookings.js
@@ -4,26 +4,29 @@ import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
   useEffect(() => {
     fetch("https://cyf-react.glitch.me")
       .then(response => response.json())
       .then(data => setBookings(data))
       .catch(error => console.error(error));
-  });
+  }, []);
   // console.log(bookings);
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
-
-  return (
-    <div className="App-content">
-      <div className="container">
-        <Search search={search} />
+  if (bookings.length > 0) {
+    return (
+      <div className="App-content">
+        <div className="container">
+          <Search search={search} />
+        </div>
+        <SearchResults data={bookings} />
       </div>
-      <SearchResults data={bookings} />
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 };
 
 export default Bookings;
