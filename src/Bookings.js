d56d669ae7265d2501a116af1758469c3460247f
zubeda <zubedauk@gmail.com>
2020-10-05 23:07:54 +0100

    final version lesson 2

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import fakeBookings from "./data/fakeBookings.json";
+//import fakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
+  const [input, setInput] = useState([]);
+  useEffect(function() {
+    fetch("https://cyf-react.glitch.me")
+      .then(function(obj) {
+        return obj.json();
+      })
+      .then(function(data) {
+        setInput(data);
+      });
+  }, []);
   return (
     <div className="App-content">
       <div className="container">
         <Search />
-        {<SearchResults people={fakeBookings} />}
+        {<SearchResults people={input} />}
       </div>
     </div>
   );
