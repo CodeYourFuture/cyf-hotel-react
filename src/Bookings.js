4b95c2ad5174644f251293258959fe7473ac9bb7
bireworld <mail2berhane@gmail.com>
2020-10-06 18:19:37 +0100

    Load bookings remotely completed

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
-// import SearchResults from "./SearchResults.js";
-// import FakeBookings from "./data/fakeBookings.json";
+import SearchResults from "./SearchResults.js";
+import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
+  const [bookings, setBookings] = useState([]);
+  useEffect(() => {
+    console.log("Fetching for Pokemon");
+
+    fetch(`https://cyf-react.glitch.me`)
+      .then(res => res.json())
+      .then(data => {
+        setBookings(data);
+      });
+  }, []);
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
