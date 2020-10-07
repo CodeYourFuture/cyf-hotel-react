8d8a3fa5322dac9bf2b360b0b9d3a717f9d566ce
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-10-07 17:29:18 +0100

    lesson 3 , step 20-21 DONE

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,15 +1,35 @@
 import React, { useState } from "react";
 
+import CustomerProfile from "./CustomerProfile";
+
 import moment from "moment";
+
 moment().format();
 
 const SearchResults = ({ results }) => {
-  const [color, setColor] = useState("");
-  const highlight = () => {
-    if (color === "") {
-      setColor("red");
-    } else setColor("");
+  const [rowSelected, setRowSelected] = useState(null);
+
+  const [isSelected, setIsSelected] = useState([]);
+
+  const handleClick = e => {
+    setRowSelected(e.target.id);
+    console.log(e.target.id);
   };
+
+  //const [color, setColor] = useState("");
+
+  // const highlight = () => {
+  //   if (color === "") {
+  //     setColor("red");
+  //   } else setColor("");
+  // };
+
+  const setClassName = id => {
+    if (!isSelected.includes(id)) {
+      setIsSelected([...isSelected, id]);
+    }
+  };
+
   return (
     <div>
       <table className="table table-striped">
@@ -24,18 +44,25 @@ const SearchResults = ({ results }) => {
             <th>Check In Date</th>
             <th>Check Out Date</th>
             <th>Stay(Days)</th>
+            <th>Show Profile</th>
           </tr>
         </thead>
 
         <tbody>
-          {results.map(result => {
+          {results.map((result, index) => {
             let checkInDate = moment(result.checkInDate);
             let checkOutDate = moment(result.checkOutDate);
             return (
               <tr
-                key={result.id}
-                style={{ backgroundColor: color }}
-                onClick={highlight}
+                key={index}
+                onClick={() => setClassName(index)}
+                style={
+                  isSelected.includes(index)
+                    ? {
+                        backgroundColor: "cyan"
+                      }
+                    : { backgroundColor: "white" }
+                }
               >
                 <td>{result.id}</td>
                 <td>{result.title}</td>
@@ -46,11 +73,22 @@ const SearchResults = ({ results }) => {
                 <td>{result.checkInDate}</td>
                 <td>{result.checkOutDate}</td>
                 <td>{checkOutDate.diff(checkInDate, "days")}</td>
+                <td>
+                  {" "}
+                  <button
+                    onClick={handleClick}
+                    id={index + 1}
+                    className="btn btn-primary"
+                  >
+                    Show Profile
+                  </button>{" "}
+                </td>
               </tr>
             );
           })}
         </tbody>
       </table>
+      {rowSelected && <CustomerProfile id={rowSelected} />}
     </div>
   );
 };
