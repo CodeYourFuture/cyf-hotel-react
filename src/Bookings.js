cf5df41d5dff8d0117486510611c146b78d7032d
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-10-07 21:15:43 +0100

    Lesson 3, step 22 DONE

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
+import "./Booking.css";
 //import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
 
-  //const [bookings, setBookings] = useState(null);
-  //const [loading, setLoading] = useState(true);
-  //const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const [errorMessage, setErrorMessage] = useState(null);
+
   const search = searchVal => {
     console.info("TO DO!", searchVal);
     let filteredBooking = bookings.filter(
@@ -20,20 +22,36 @@ const Bookings = () => {
   };
 
   useEffect(() => {
-    fetch("https://cyf-react.glitch.me")
+    fetch("https://cyf-react.glitch.me/delayed")
       .then(res => res.json())
-      .then(data => setBookings(data));
+      .then(data => {
+        console.log(data);
+        if (data.error) {
+          throw data;
+        }
+        setBookings(data);
+
+        setLoading(false);
+      })
+      .catch(error => setErrorMessage(error));
+    console.log("500 HTTP error");
   }, []);
 
-  return (
-    <div className="App-content">
-      <div className="container">
-        <Search search={search} />
-        {/* <SearchResults results={FakeBookings} /> */}
-        <SearchResults results={bookings} />
+  if (loading && !errorMessage) {
+    return <p className="loading ">Please wait , System Loading...</p>;
+  } else if (errorMessage == null) {
+    return (
+      <div className="App-content">
+        <div className="container">
+          <Search search={search} />
+          {/* <SearchResults results={FakeBookings} /> */}
+          <SearchResults results={bookings} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return <div>{errorMessage.error}</div>;
+  }
 };
 
 export default Bookings;
