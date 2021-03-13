1cb195b2dd24a6e353ccfb35112e90aa6110cba5
SenaitEkubai <Senaitekubai@gmail.com>
2021-03-13 21:57:46 +0000

    done Q-4 add footer

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import Bookings from "./Bookings";
 import Heading from "./Heading";
 import TouristInfoCards from "./TouristInfoCards";
-
+import Footer from "./Footer";
 import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <Heading />
-      <TouristInfoCards />
-      <Bookings />
-    </div>
+    <>
+      <div className="App">
+        <Heading />
+        <TouristInfoCards />
+        <Bookings />
+      </div>
+      <Footer />;
+    </>
   );
 };
 
--- src/Bookings.js
+++ src/Bookings.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Search from "./Search.js";
+import SearchResults from "./SearchResults";
 // import SearchResults from "./SearchResults.js";
-// import FakeBookings from "./data/fakeBookings.json";
+import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
   const search = searchVal => {
@@ -12,6 +13,7 @@ const Bookings = () => {
     <div className="App-content">
       <div className="container">
         <Search search={search} />
+        <SearchResults props={FakeBookings} />
         {/* <SearchResults results={FakeBookings} /> */}
       </div>
     </div>
