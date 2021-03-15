5cc0f5e7135981f98829a0225c5d7e73cb7d285c
PatOn <mahomed2278@gmail.com>
2021-03-15 21:12:49 +0000

    setting up

---
+++ src/Components/Heading/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Heading = props => {
+  return (
+    <header className="App-header">
+      <img
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        alt="logo"
+      />
+      CYF Hotel
+    </header>
+  );
+};
+
+export default Heading;
