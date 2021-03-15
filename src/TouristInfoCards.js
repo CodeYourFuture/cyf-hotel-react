3023b9dbb3da7eaf26e70c1ddd0bf18c942f3ebd
Nirmeet <nirmeet.baweja@gmail.com>
2021-03-15 21:32:17 +0000

    Lesson 1 - Part 3
    Some minor bug fixes in TouristInfoCards component

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -3,9 +3,14 @@ import React from "react";
 const TouristInfoCards = props => {
   return (
     <div className="card">
-      <img src={props.imgSrc} className="card-img-top" />
+      <img src={props.imgSrc} className="card-img-top" alt={props.cityName} />
       <div className="card-body">
-        <a href={props.link} target="_blank" className="btn btn-primary">
+        <a
+          href={props.link}
+          target="_blank"
+          className="btn btn-primary"
+          rel="noopener noreferrer"
+        >
           Learn more about {props.cityName}
         </a>
       </div>
