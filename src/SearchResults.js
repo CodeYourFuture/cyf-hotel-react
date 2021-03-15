b4cbe034fdc86676e8de45d8b89d7f242fe55d5d
Chizim Chinuru <garrohat@yahoo.co.uk>
2021-03-15 11:44:28 +0000

    Completed lesson 1 tasks 6 and 7

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,63 +1,55 @@
+import moment from "moment";
 import React from "react";
 
 const TableHead = () => {
   return (
-    <thead class="thead-dark">
+    <thead className="thead-dark">
       <tr>
         <th scope="col">Id</th>
         <th scope="col">Title</th>
-        <th scope="col">Firstname</th>
+        <th scope="col">First Name</th>
         <th scope="col">Surname</th>
         <th scope="col">email</th>
         <th scope="col">Room #</th>
         <th scope="col">Check in date</th>
         <th scope="col">Check out date</th>
+        <th scope="col">Number of Night(s)</th>
       </tr>
     </thead>
   );
 };
 
-const TableBody1 = () => {
+const TableBody = props => {
   return (
     <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Mr</td>
-        <td>James</td>
-        <td>Caine</td>
-        <td>meg4u@yahoo.co.uk</td>
-        <td>45</td>
-        <td>13/03/21</td>
-        <td>15/03/21</td>
-      </tr>
+      {props.bookings.map((booking, index) => (
+        <tr key={index}>
+          <th scope="row">{booking.id}</th>
+          <td>{booking.title}</td>
+          <td>{booking.firstName}</td>
+          <td>{booking.surname}</td>
+          <td>{booking.email}</td>
+          <td>{booking.roomId}</td>
+          <td>{booking.checkInDate}</td>
+          <td>{booking.checkOutDate}</td>
+          <td>
+            {moment(booking.checkOutDate).diff(
+              moment(booking.checkInDate),
+              "days"
+            )}
+          </td>
+        </tr>
+      ))}
     </tbody>
   );
 };
 
-const TableBody2 = () => {
-  return (
-    <tbody>
-      <tr>
-        <th scope="row">2</th>
-        <td>Mr</td>
-        <td>Patricia</td>
-        <td>Okosun</td>
-        <td>romana@yahoo.co.uk</td>
-        <td>7</td>
-        <td>12/03/21</td>
-        <td>17/03/21</td>
-      </tr>
-    </tbody>
-  );
-};
-
-const SearchResults = () => {
+const SearchResults = props => {
   return (
     <div>
-      <table class="table">
+      <table className="table">
         <TableHead />
-        <TableBody1 />
-        <TableBody2 />
+        <TableBody bookings={props.results} />
       </table>
     </div>
   );
