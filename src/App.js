465ed1fb0aea12f729033b2ed3d0bac12773100d
Nirmeet <nirmeet.baweja@gmail.com>
2021-03-15 18:09:45 +0000

    Lesson 1 - Part 4 Completed
    Footer component added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Heading from "./Heading";
 import TouristInfoCards from "./TouristInfoCards";
 import Bookings from "./Bookings";
+import Footer from "./Footer";
 
 import "./App.css";
 
@@ -32,6 +33,13 @@ const App = () => {
       </div>
 
       <Bookings />
+      <Footer
+        list={[
+          "123 Fake Street, London, E1 4UD",
+          "hello@fakehotel.com",
+          "0123 456789"
+        ]}
+      />
     </div>
   );
 };
