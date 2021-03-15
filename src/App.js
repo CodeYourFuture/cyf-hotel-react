ad1089dbea15f50cfcf6eda09c77c7f449b444cc
Amanuel <weditedi25@gmail.com>
2021-03-15 18:00:59 +0000

    SearchBtn and Heading moved to a new script

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-
 import Bookings from "./Bookings";
 import "./App.css";
+import SearchButton from "./SearchButton";
+import Heading from "./Heading";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Heading />
       <Bookings />
+      <SearchButton />
     </div>
   );
 };
