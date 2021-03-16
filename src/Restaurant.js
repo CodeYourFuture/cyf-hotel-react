c1d0bc1da67cce67ca86ccb8c1483c4cb5ee33a6
PatOn <mahomed2278@gmail.com>
2021-03-16 20:27:29 +0000

    step by step

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,36 +1,15 @@
 import React, { useState } from "react";
-import Button from "./Components/Button/Button";
+
+import Order from "./Order";
 
 const Restaurant = () => {
-  const [noOfPizza, pizzaOrderHandler] = useState(0);
-
-  const addPizzaHandler = e => {
-    let updatedVal = noOfPizza + 1;
-    pizzaOrderHandler(updatedVal);
-  };
-  const minusPizzaHandler = e => {
-    if (noOfPizza === 0) return;
-    let updatedVal = noOfPizza - 1;
-    pizzaOrderHandler(updatedVal);
-  };
-
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
-        <li>
-          Pizzas: {noOfPizza}
-          <Button
-            function={minusPizzaHandler}
-            arrOfClasses={["btn", "btn-primary"]}
-            inner={"-"}
-          />
-          <Button
-            function={addPizzaHandler}
-            arrOfClasses={["btn", "btn-primary"]}
-            inner={"+"}
-          />
-        </li>
+        <Order product={"Pizza"} />
+        <Order product={"Salads"} />
+        <Order product={"Chocolate cake"} />
       </ul>
     </div>
   );
