0e0fa1115ce0365e9a490bf0c980eec623d0097e
Amanuel <weditedi25@gmail.com>
2021-03-16 14:30:58 +0000

    hotel logo styled

--- src/Heading.js
+++ src/Heading.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Heading = () => {
-  return <header className="App-header">CYF Hotel</header>;
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <header className="navbar-brand">
+        <img
+          src="https://image.flaticon.com/icons/svg/139/139899.svg"
+          width="25"
+          height="25"
+          className="d-inline-block align-top"
+          alt="Site Logo"
+          className="mr-1 mb-1"
+        />
+        CYF Hotel
+      </header>
+    </nav>
+  );
 };
 
 export default Heading;
