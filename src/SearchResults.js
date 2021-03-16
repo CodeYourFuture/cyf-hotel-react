9361e921781d1d39b676ecb4f1b156c189d487ec
Michael <antenehnigussie1@yahoo.com>
2021-03-16 16:35:15 +0000

    Introduced a Table component. Added a Table component. Updated code in SearchReasults.js accordingly.

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,46 +1,28 @@
 import React from "react";
 import moment from "moment";
+import Table from "./Table";
+
+const labels = [
+  "ID",
+  "Title",
+  "First Name",
+  "Surname",
+  "Email",
+  "Room ID",
+  "Checkin Date",
+  "Checkout Date",
+  "No. of Nights"
+];
 
 const SearchResults = props => {
-  return (
-    <table className="table">
-      <thead className="thead-dark">
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Title</th>
-          <th scope="col">First Name</th>
-          <th scope="col">Surname</th>
-          <th scope="col">Email</th>
-          <th scope="col">Room ID</th>
-          <th scope="col">Checkin Date</th>
-          <th scope="col">Checkout Date</th>
-          <th scope="col">No. of Nights</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.results.map(booking => {
-          return (
-            <tr>
-              <th scope="row">{booking.id}</th>
-              <td>{booking.title}</td>
-              <td>{booking.firstName}</td>
-              <td>{booking.surname}</td>
-              <td>{booking.email}</td>
-              <td>{booking.roomId}</td>
-              <td>{booking.checkInDate}</td>
-              <td>{booking.checkOutDate}</td>
-              <td>
-                {moment(booking.checkOutDate).diff(
-                  moment(booking.checkInDate),
-                  "days"
-                )}
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+  const results = props.results.map(booking => {
+    booking["noOfNights"] = moment(booking.checkOutDate).diff(
+      moment(booking.checkInDate),
+      "days"
+    );
+    return booking;
+  });
+  return <Table labels={labels} data={results} />;
 };
 
 export default SearchResults;
---
+++ src/Table.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+const Table = props => {
+  return (
+    <table className="table">
+      <THead labels={props.labels} />
+      <TBody data={props.data} />
+    </table>
+  );
+};
+export default Table;
+
+const THead = props => {
+  return (
+    <thead className="thead-dark">{<LabelsRow labels={props.labels} />}</thead>
+  );
+};
+
+const LabelsRow = props => {
+  return (
+    <tr>
+      {props.labels.map((label, index) => (
+        <Label key={index} label={label} />
+      ))}
+    </tr>
+  );
+};
+
+const Label = props => {
+  return <th>{props.label}</th>;
+};
+
+const TBody = props => {
+  return (
+    <tbody>
+      {props.data.map((values, index) => {
+        return <DataRow key={index} values={values} />;
+      })}
+    </tbody>
+  );
+};
+
+const DataRow = props => {
+  return (
+    <tr>
+      {Object.values(props.values).map((value, index) => (
+        <Data key={index} value={value} />
+      ))}
+    </tr>
+  );
+};
+
+const Data = props => {
+  return <td>{props.value}</td>;
+};
