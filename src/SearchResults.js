8911d6e5c24c3cb08e160a20c71722412c768365
Amanuel <weditedi25@gmail.com>
2021-03-16 22:43:31 +0000

    css updated

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -12,6 +12,7 @@ const SearchResults = props => {
           <th scope="col">room id</th>
           <th scope="col">check in date</th>
           <th scope="col">check out date</th>
+          <th scope="col">No of nights</th>
         </tr>
       </thead>
       <tbody>
@@ -25,6 +26,7 @@ const SearchResults = props => {
             <td>{result.roomId}</td>
             <td>{result.checkInDate}</td>
             <td>{result.checkOutDate}</td>
+            <td>10</td>
           </tr>
         ))}
       </tbody>
