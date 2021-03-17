08f0532caa2a95bd506beeeb4b1238d553ea5147
ltteh <67978926+ltteh@users.noreply.github.com>
2021-03-17 13:11:36 +0000

    Completed 5 to 7

--- src/App.js
+++ src/App.js
@@ -6,14 +6,41 @@ import Heading from "./components/Heading.js";
 import TouristInfoCards from "./components/TouristInfoCards.js";
 import Footer from "./components/footer.js";
 
+const contactDetail = [
+  "123 Fake Street, London, E1 4UD",
+  "hello@fakehotel.com",
+  "0123 456789"
+];
+
+const cards = [
+  {
+    city: "Glasgow",
+    link: "https://peoplemakeglasgow.com/",
+    img:
+      "https://cdn.pixabay.com/photo/2015/09/27/17/03/secc-961001_960_720.jpg"
+  },
+  {
+    city: "Manchester",
+    link: "https://www.visitmanchester.com/",
+    img:
+      "https://cdn.pixabay.com/photo/2014/02/09/23/25/manchester-263210_960_720.jpg"
+  },
+  {
+    city: "London",
+    link: "https://visitlondon.com/",
+    img:
+      "https://cdn.pixabay.com/photo/2014/09/11/18/23/tower-bridge-441853_960_720.jpg"
+  }
+];
+
 const App = () => {
   return (
     <div className="App">
       {/* <header className="App-header">CYF Hotel</header> */}
       <Heading />
-      <TouristInfoCards />
+      <TouristInfoCards cards={cards} />
       <Bookings />
-      <Footer />
+      <Footer contactDetail={contactDetail} />
     </div>
   );
 };
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,10 @@
 import React from "react";
 
-const footerData = [
-  "123 Fake Street, London, E1 4UD",
-  "hello@fakehotel.com",
-  "0123 456789"
-];
-
-const Footer = () => {
-  console.log(footerData[0]);
-
+const Footer = props => {
   return (
     <div>
       <ul>
-        {footerData.map((elem, index) => (
+        {props.contactDetail.map((elem, index) => (
           <li key={index}>{elem}</li>
         ))}
       </ul>
