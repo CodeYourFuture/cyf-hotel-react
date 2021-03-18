491679c5272b87cd6bef2754762a81bca3770670
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-03-18 09:56:34 +0000

    imported the FakeBooking component to App.js

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Search from "./Search.js";
-// import SearchResults from "./SearchResults.js";
-// import FakeBookings from "./data/fakeBookings.json";
-
+import SearchResult from "./Hotel.js";
+import FakeBookings from "./data/fakeBookings.json";
+//
+// const results = FakeBookings;
+// console.log(FakeBookings);
 const Bookings = () => {
   const search = searchVal => {
     console.info("TO DO!", searchVal);
@@ -12,7 +14,8 @@ const Bookings = () => {
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        {/* <SearchResults results={FakeBookings} /> */}
+        <SearchResult.SearchResults results={FakeBookings} />
+        {/* <SearchResult.SearchResults/> */}
       </div>
     </div>
   );
