c15429dbdf2b45aadfcc1d3e248cc8b296a0ebe5
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-03-18 09:55:15 +0000

    imported the components to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
-
-import Bookings from "./Bookings";
+import Bookings from "./Bookings.js";
 import "./App.css";
-
+// import {TouristInfoCards, Heading, SearchResults, Footer} from "./Hotel.js";
+import allComponents from "./Hotel.js";
+const Info = [
+  "123 Fake Street, London, E1 4UD",
+  "hello@fakehotel.com",
+  "0123 456789"
+];
+// const tables;
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <allComponents.Heading />
       <Bookings />
+      <allComponents.TouristInfoCards />
+      {/* <allComponents.SearchResults/> */}
+      <allComponents.Footer Info={Info} />
     </div>
   );
 };
