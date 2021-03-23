3157e9d444be818d0076dd3b9c0f9bfe117a988a
Amanuel <weditedi25@gmail.com>
2021-03-23 17:19:28 +0000

    Button adding pizzas

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Restaurant = () => {
-  const pizzas = 0;
+  const [orders, setOrders] = useState(0);
+
+  const orderOne = () => {
+    setOrders(orders + 1);
+  };
   return (
     <div className="text-center mt-3">
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {pizzas} <button className="btn btn-dark">Add</button>
+          Pizzas: {orders}{" "}
+          <button className="btn btn-dark" onClick={orderOne}>
+            Add
+          </button>
         </li>
       </ul>
     </div>
