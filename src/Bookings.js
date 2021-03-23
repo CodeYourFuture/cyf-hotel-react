8bc12209cead4f80c32f3bf7be2936e02deeb6d0
Biruk Kebede <biruk.key@gmail.com>
2021-03-23 18:42:54 +0000

    Removed unused code

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  //const [bookings, setBookings] = useState(FakeBookings);
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
@@ -14,7 +14,7 @@ const Bookings = () => {
       <div className="container">
         <Search search={search} />
         <SearchResults
-          results={bookings}
+          results={FakeBookings}
           tableHeaders={[
             "id",
             "title",
