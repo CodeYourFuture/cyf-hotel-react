1f2276982ec17c42abbe1b64cf932a0371101779
JK1320 <kaurjus1@gmail.com>
2021-03-25 23:13:07 +0000

    Complete - Lesson 2

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -3,16 +3,16 @@ import moment from "moment";
 import "./index.css";
 
 export const SearchResults = ({ results }) => {
-  const [colour, setColour] = useState("green");
+  const [colour, setColour] = useState("transparent");
 
-  function highlightWhenClicked() {
-    setColour("orange");
+  function highlightWhenClicked(index) {
+    setColour(index === colour ? "" : index);
   }
   return (
     <div>
       <table className="table table-dark">
         <thead>
-          <tr className={colour} onClick={highlightWhenClicked}>
+          <tr onClick={highlightWhenClicked}>
             <th scope="col">Title</th>
             <th scope="col">First Name</th>
             <th scope="col">Surname</th>
@@ -25,7 +25,11 @@ export const SearchResults = ({ results }) => {
         </thead>
         <tbody>
           {results.map((item, index) => (
-            <tr className={colour} onClick={highlightWhenClicked} key={index}>
+            <tr
+              className={colour === index ? "orange" : "transparent"}
+              onClick={() => highlightWhenClicked(index)}
+              key={index}
+            >
               <th scope="row">{item.title}</th>
               <th scope="row">{item.firstName}</th>
               <th scope="row">{item.surname}</th>
