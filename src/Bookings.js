93ba5a02cbe17a14a48503a67a67933f73fd6237
SenaitEkubai <Senaitekubai@gmail.com>
2021-03-27 20:31:11 +0000

    completed lesson 3 question 16 and 17

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults";
-import FakeBooking from "./data/fakeBookings.json";
-const Bookings = () => {
-  const [bookings] = useState(FakeBooking);
 
+const Bookings = () => {
+  const [bookings, setBookings] = useState([]);
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then(response => response.json())
+      .then(data => setBookings(bookings => data));
+  }, []);
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchButton from "./SearchButton";
 
 const Search = () => {
+  const [searchInput, setSearchInput] = useState("");
+
+  function handlerSearchInput(event) {
+    console.log(event.target.value);
+    setSearchInput(event.target.value);
+  }
   return (
     <div className="search">
       <div className="page-header">
@@ -13,10 +19,12 @@ const Search = () => {
             <label htmlFor="customerName">Customer name</label>
             <div className="search-row">
               <input
+                value={searchInput}
                 type="text"
                 id="customerName"
                 className="form-control"
                 placeholder="Customer name"
+                onChange={handlerSearchInput}
               />
               <SearchButton />
             </div>
