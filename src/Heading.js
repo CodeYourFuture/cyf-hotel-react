e91a1aa6795c99454035b118c7553403ac8c688e
alsafi25 <62480618+alsafi25@users.noreply.github.com>
2021-03-29 21:49:55 +0100

    hotel logo updated

--- src/Heading.js
+++ src/Heading.js
@@ -4,7 +4,7 @@ const Heading = () => {
   return (
     <header className="App-header">
       CYF Hotel
-      <img alt="" src="https://image.flaticon.com/icons/svg/139/139899.svg" />
+      <img alt="" src="https://i.pinimg.com/originals/da/b9/13/dab913ab4fec9a9cb5b38b6ce8c00902.png" />
     </header>
   );
 };
