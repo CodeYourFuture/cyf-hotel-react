2ab8b4a317ce7138fe165e6b1339911d73e90581
alsafi25 <62480618+alsafi25@users.noreply.github.com>
2021-03-29 10:20:43 +0100

    create new branch

--- src/Heading.js
+++ src/Heading.js
@@ -4,10 +4,7 @@ const Heading = () => {
   return (
     <header className="App-header">
       CYF Hotel
-      <img
-        alt=""
-        src="https://images.unsplash.com/photo-1568084680786-a84f91d1153c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"
-      />
+      <img alt="" src="https://image.flaticon.com/icons/svg/139/139899.svg" />
     </header>
   );
 };
