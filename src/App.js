29ed1d0f03a37f5dc8b8f91893e459dbe42ae470
victoria ukaonu <73253494+Sozchi@users.noreply.github.com>
2021-03-29 21:24:41 +0100

    made changes in css

--- src/App.js
+++ src/App.js
@@ -27,11 +27,3 @@ const App = () => {
 };
 
 export default App;
-
-//  ** Instructions:** In the`<Restaurant />` component, create a new function named `orderOne`.
-//   The`orderOne` function doesn't take any parameters and should use the `setOrders` function to
-// increment the`orders` state variable by 1. Then, add a`onClick` handler to the Add`<button>`
-// that calls the`orderOne` function when it's being clicked.
-
-// **Test:** Try to click on the Add button a few times and verify that the number
-//  of pizzas increases accordingly.
