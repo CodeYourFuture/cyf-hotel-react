0e195b9541a7a1308e77c43fbd0efec80bc1d79b
Amanuel <weditedi25@gmail.com>
2021-03-30 15:57:26 +0100

    Search Button is submitting and displaying search value in console

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
+// import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
 
-  const [booking, setBookings] = useState(FakeBookings);
+  const [booking, setBookings] = useState([]);
+
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setBookings(data);
+      });
+  }, []);
 
   return (
     <div className="App-content">
--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchButton from "./SearchButton";
 
-const Search = () => {
+const Search = props => {
+  const [searchInput, setSearchInput] = useState("");
+  const handleSearchInput = event => {
+    setSearchInput(event.target.value);
+  };
+
+  const onSubmitHandLer = event => {
+    props.search(searchInput);
+    event.preventDefault();
+  };
+
   return (
     <div className="search">
       <div className="page-header">
@@ -9,14 +19,16 @@ const Search = () => {
       </div>
       <div className="row search-wrapper">
         <div className="col">
-          <form className="form-group search-box">
+          <form onSubmit={onSubmitHandLer} className="form-group search-box">
             <label htmlFor="customerName">Customer name</label>
             <div className="search-row">
               <input
                 type="text"
+                value={searchInput}
                 id="customerName"
                 className="form-control"
                 placeholder="Customer name"
+                onChange={handleSearchInput}
               />
               <SearchButton />
             </div>
