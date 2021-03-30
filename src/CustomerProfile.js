b747af430517d551ab93776add95d2cb16e36d19
Biruk Kebede <biruk.key@gmail.com>
2021-03-30 16:43:26 +0100

    Removed an unnecessary conditional statement tidy up the code

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
-//import GuestDetails from "./GuestsDetails";
 
 const CustomerProfile = props => {
   const [profile, setProfile] = useState(false);
+
   const profileSearch = () => {
     if (profile !== props.id) return setProfile(props.id);
     else return setProfile(false);
   };
+
   useEffect(() => {
     fetch(`https://cyf-react.glitch.me/customers/${profile}`)
       .then(res => res.json())
@@ -14,41 +15,27 @@ const CustomerProfile = props => {
         if (profile === props.id) return setProfile(data);
       });
   }, [profile, props.id]);
-  if (profile && props.id) {
-    return (
-      <div>
-        <td>
-          <button onClick={profileSearch} className="btn btn-primary">
-            Show Profile
-          </button>
-        </td>
-        <td>
-          <ul className={profile ? "d-block" : "d-none"}>
-            <li>id: {profile.id}</li>
-            <li>title: {profile.title}</li>
-            <li>first name: {profile.firstName}</li>
-            <li>surname: {profile.surname}</li>
-            <li>email: {profile.email}</li>
-            <li>phone number: {profile.phoneNumber}</li>
-            <li>vip: {profile.vip}</li>
-          </ul>
-        </td>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <td>
-          <button onClick={profileSearch} className="btn btn-primary">
-            Show Profile
-          </button>
-        </td>
-        <td className={profile ? "d-block" : "d-none"}>
-          Customer {profile} profile
-        </td>
-      </div>
-    );
-  }
+
+  return (
+    <div>
+      <td>
+        <button onClick={profileSearch} className="btn btn-primary">
+          Show Profile
+        </button>
+      </td>
+      <td>
+        <ul className={profile ? "d-block" : "d-none"}>
+          <li>id: {profile.id}</li>
+          <li>title: {profile.title}</li>
+          <li>first name: {profile.firstName}</li>
+          <li>surname: {profile.surname}</li>
+          <li>email: {profile.email}</li>
+          <li>phone number: {profile.phoneNumber}</li>
+          <li>vip: {profile.vip}</li>
+        </ul>
+      </td>
+    </div>
+  );
 };
 
 export default CustomerProfile;
