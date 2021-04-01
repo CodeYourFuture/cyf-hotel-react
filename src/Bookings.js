5887ef824d77340bdc4dcc9fd697c47a3d8aebdb
Nirmeet <nirmeet.baweja@gmail.com>
2021-04-01 09:24:13 +0100

    Removed unused variable FakeBookings, so the site could deploy

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
