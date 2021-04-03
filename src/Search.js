b564dc1c8736d1e7290aae97cf82b4829b450888
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-04-03 15:07:16 +0100

    added the handleSearchInput, handleSubmitForm components

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,17 @@
-import React from "react";
-const Search = () => {
+import React, { useState } from "react";
+const Search = props => {
+  const [searchInput, setSearchInput] = useState("");
+  const handleSearchInput = event => {
+    console.log(event.target.value);
+    setSearchInput(event.target.value);
+  };
+
+  const handleSubmitForm = event => {
+    event.preventDefault();
+    // console.log(event.target.querySelector('#customerName').value);
+    props.search(searchInput);
+  };
+
   return (
     <div className="search">
       <div className="page-header">
@@ -7,14 +19,16 @@ const Search = () => {
       </div>
       <div className="row search-wrapper">
         <div className="col">
-          <form className="form-group search-box">
+          <form onSubmit={handleSubmitForm} className="form-group search-box">
             <label htmlFor="customerName">Customer name</label>
             <div className="search-row">
               <input
+                onChange={handleSearchInput}
                 type="text"
                 id="customerName"
                 className="form-control"
                 placeholder="Customer name"
+                value={searchInput}
               />
               <SearchButton />
             </div>
