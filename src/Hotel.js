7cb362260cfa62876f0f95608eaeb5f8d7b1a767
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-04-03 15:04:12 +0100

    added the customerProfile component and a useEffect

--- src/Hotel.js
+++ src/Hotel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, Profiler } from "react";
 import Moment from "moment";
 const TouristInfoCards = () => {
   return (
@@ -79,55 +79,99 @@ const Footer = props => {
 };
 
 const SearchResults = props => {
+  const [custID, setCustID] = useState(null);
+  const handleButton = event => {
+    const elID = event.currentTarget.childNodes[0].innerText;
+    setCustID(elID);
+  };
+
   const bookingDetails = props.results;
-  console.log(bookingDetails);
+  // console.log(bookingDetails);
   const bookingKeys = Object.keys(bookingDetails[0]);
   return (
     <div>
-      <table className="table table-striped table-bordered">
-        <thead className="table table-danger thead-light">
+      <table className="table table-stripe">
+        <thead className="table table-danger thead-dark">
           <tr>
             {bookingKeys.map(bookingKey => (
               <th scope="col" key={bookingKey}>
-                {" "}
-                {bookingKey}{" "}
+                {bookingKey}
               </th>
             ))}
             <th> number of nights </th>
+            <th> Customer Profile </th>
           </tr>
         </thead>
         <tbody>
           {bookingDetails.map((bookingDetail, i) => {
             return (
               <TableRow
-                i={i}
+                key={i}
                 bookingKeys={bookingKeys}
                 bookingDetail={bookingDetail}
+                handleButton={handleButton}
               />
             );
           })}
         </tbody>
       </table>
+      <CustomerProfile custID={custID} />
     </div>
   );
 };
 
+const CustomerProfile = props => {
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me/customers/${props.custID}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setProfile(data);
+      });
+  }, [props.custID]);
+
+  return props.custID ? (
+    <div>
+      <h3>Customer {profile.id} Profile</h3>
+      <ul>
+        <li> {"Customer Identity: " + profile.id} </li>
+        <li> {"Email address: " + profile.email} </li>
+        <li> {"Is a VIP: " + profile.vip}</li>
+        <li> {"Contact: " + profile.phoneNumber} </li>
+      </ul>
+    </div>
+  ) : null;
+};
+
 const TableRow = props => {
   return (
-    <tr key={props.i}>
+    <tr key={props.i} onClick={props.handleButton}>
       {props.bookingKeys.map(bookingKey => (
         <td key={bookingKey}> {props.bookingDetail[bookingKey]}</td>
       ))}
 
       <td>
-        {Moment.duration(
-          Moment(props.bookingDetail["checkOutDate"]).diff(
-            Moment(props.bookingDetail["checkInDate"])
-          )
-        ).asDays()}
+        {Math.round(
+          Moment.duration(
+            Moment(props.bookingDetail["checkOutDate"]).diff(
+              Moment(props.bookingDetail["checkInDate"])
+            )
+          ).asDays()
+        )}
+      </td>
+      <td>
+        <button>Show Profile</button>
       </td>
     </tr>
   );
 };
 
-export default { TouristInfoCards, Heading, SearchResults, Footer };
+export default {
+  TouristInfoCards,
+  Heading,
+  SearchResults,
+  Footer,
+  CustomerProfile
+};
