906c32617ab6adc54b607739fbfdbf03c810a024
Murat Demirtas <muratdemirtas2001@gmail.com>
2021-04-18 23:37:56 +0100

    Adding Heading Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Bookings from "./Bookings";
+import Heading from "./Heading";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      {/* <header className="App-header">CYF Hotel</header> */}
+      <Heading />
       <Bookings />
     </div>
   );
---
+++ src/Heading.js
@@ -0,0 +1,13 @@
+import React from "react";
+function Heading() {
+  return (
+    <div className="App-header">
+      <img
+        className="App-logo"
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+      />
+      <header className="App-title">CYF Hotel</header>
+    </div>
+  );
+}
+export default Heading;
