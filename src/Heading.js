95c3582d642c53cedaf5cdfef864d0e8bfbf710d
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-04-19 21:48:28 +0100

    added the files

---
+++ src/Heading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Heading = () => (
+  <header className="App-header">
+    <p>CYF Hotel</p>
+    <img
+      className="App-logo"
+      src="https://image.flaticon.com/icons/svg/139/139899.svg"
+      alt="hotel logo"
+    />
+  </header>
+);
+
+export default Heading;
