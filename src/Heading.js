4443ff67278ec0299357e4879266bafb95457e6b
alinosratipour <alinosratipour@gmail.com>
2021-04-19 02:17:43 +0100

    task 2 made Heading component

---
+++ src/Heading.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <img
+          class="App-logo"
+          src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        />
+        <spne class="App-title">CYF Hotel</spne>
+      </header>
+    </div>
+  );
+};
+
+export default Heading;
