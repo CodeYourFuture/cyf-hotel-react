35c38a5827805e723ad33517634a07600bd263d9
Murat Demirtas <muratdemirtas2001@gmail.com>
2021-04-19 22:54:48 +0100

    Adding number of nights to SearchResult component by using moment.js

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import moment from "moment";
 function SearchResults({ results }) {
   console.log(results);
   return (
@@ -10,10 +10,11 @@ function SearchResults({ results }) {
             <th scope="col">id</th>
             <th scope="col">First Name</th>
             <th scope="col">Last Name</th>
-            <th scope="col">email</th>
+            <th scope="col">Email</th>
             <th scope="col">Room Id</th>
             <th scope="col">Check In Date</th>
             <th scope="col">Check Out date</th>
+            <th scope="col">Number of Nights</th>
           </tr>
         </thead>
         <tbody>
@@ -21,13 +22,18 @@ function SearchResults({ results }) {
             return (
               <tr key={index}>
                 <th scope="row">{result.id}</th>
-                {/* <td>{result.id}</td> */}
                 <td>{result.firstName}</td>
                 <td>{result.surname}</td>
                 <td>{result.email}</td>
                 <td>{result.roomId}</td>
                 <td>{result.checkInDate}</td>
                 <td>{result.checkOutDate}</td>
+                <td>
+                  {moment(result.checkOutDate).diff(
+                    moment(result.checkInDate),
+                    "days"
+                  )}
+                </td>
               </tr>
             );
           })}
