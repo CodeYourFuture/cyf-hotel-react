84bb577f029f473bf1b3c234542c95d81481e0b1
NeginDn <negin.derakhshaan@gmail.com>
2021-04-20 16:40:03 +0100

    Lesson01/Exercise02

---
+++ src/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div>
+      <header className="App-header">CYF Hotel</header>
+      <img
+        className="App-logo"
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+      />
+    </div>
+  );
+};
+
+export default Heading;
