ac5e52365686905c654b8a92b6628e3fcc809035
Oksana Kostenko <kseniavlad31@gmail.com>
2021-04-20 14:15:38 +0100

    adds app logo

--- src/Heading.js
+++ src/Heading.js
@@ -3,7 +3,12 @@ import React from "react";
 const Heading = () => {
   return (
     <>
-      <header className="App-header">CYF Hotel</header>
+      <header className="App-header">
+        <img
+          src="https://image.flaticon.com/icons/svg/139/139899.svg"
+          className="App-logo"
+        />
+      </header>
     </>
   );
 };
