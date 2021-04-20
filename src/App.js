ffe7caf59049f6439e499a249433f4540c71ac9a
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-04-20 08:24:20 +0100

    Added tourist info cards\

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Heading from "./Heading.js";
+import TouristInfoCards from "./TouristInfoCards.js";
 import Bookings from "./Bookings";
 import "./App.css";
 
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <div className="App">
       <Heading />
+      <TouristInfoCards />
       <Bookings />
     </div>
   );
