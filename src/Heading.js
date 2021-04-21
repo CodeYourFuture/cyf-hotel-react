a1ab9683bec535f8a89a3cdb5f63bed44371a3a6
Kara <kara.mh191@gmail.com>
2021-04-21 05:40:40 +0100

    2. Extract the header in its own component
    2. Extract the header in its own component

---
+++ src/Heading.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div>
+      <img
+        className="App-logo"
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        alt="Hotel Logo"
+      />
+      ;<header className="App-header">CYF Hotel</header>;
+    </div>
+  );
+};
+
+export default Heading;
