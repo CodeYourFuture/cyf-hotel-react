b81fdbc20c8af6b49492397230ab970d905757df
Mustafa <30720101+akbasmustafa@users.noreply.github.com>
2021-04-25 01:37:32 +0100

    extract search button and header

---
+++ src/components/Heading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <div className="App-header">
+      <img src="https://image.flaticon.com/icons/svg/139/139899.svg" />
+    </div>
+  );
+}
+
+export default Heading;
