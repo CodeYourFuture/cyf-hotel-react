378ed162f470d2c9c62a7e1909092c01afd6e41a
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-04-25 20:00:52 +0100

    Adding more Pizza

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
-
+import Heading from "./Heading.js";
+import TouristInfoCards from "./TouristInfoCards.js";
 import Bookings from "./Bookings";
+import Footer from "./Footer.js";
 import "./App.css";
+import Restaurant from "./Restaurant.js";
 
 const App = () => {
+  let contact = [
+    "123 Fake Street, London, E1 4UD",
+    "hello@fakehotel.com",
+    "0123 456789"
+  ];
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Heading />
+      <TouristInfoCards />
       <Bookings />
+      <Restaurant />
+      <Footer contact={contact} />
     </div>
   );
 };
--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Restaurant = () => {
-  const pizzas = 0;
+  const [order, setOrder] = useState(0);
+  const incrementPizzaNumber = () => setOrder(order + 1);
+
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
+          Pizzas: {order}{" "}
+          <button className="btn btn-primary" onClick={incrementPizzaNumber}>
+            Add More Pizza
+          </button>
         </li>
       </ul>
     </div>
