29ea2ac791c02e99cc0556bdb155462e2e53991b
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-04-25 20:05:00 +0100

    rebuild the repo due to not loading properly on netlify

---
+++ src/SearchResults.js
@@ -0,0 +1,45 @@
+import moment from "moment";
+import React from "react";
+
+const SearchResults = props => (
+  <table className="table table-striped">
+    <thead>
+      <tr className="bg-dark text-white">
+        <th scope="col">ID</th>
+        <th scope="col">Title</th>
+        <th scope="col">First Name</th>
+        <th scope="col">Surname</th>
+        <th scope="col">email</th>
+        <th scope="col">Room ID</th>
+        <th scope="col">Check In Date</th>
+        <th scope="col">Check Out Date</th>
+        <th scope="col">Number of nights stay</th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.results.map(guest => {
+        console.log(moment(guest.checkInDate.date));
+        return (
+          <tr>
+            <td>{guest.id}</td>
+            <td>{guest.title}</td>
+            <td>{guest.firstName}</td>
+            <td>{guest.surname}</td>
+            <td>{guest.email}</td>
+            <td>{guest.roomId}</td>
+            <td>{guest.checkInDate}</td>
+            <td>{guest.checkOutDate}</td>
+            <td>
+              {moment(guest.checkOutDate).diff(
+                moment(guest.checkInDate),
+                "nights"
+              )}
+            </td>
+          </tr>
+        );
+      })}
+    </tbody>
+  </table>
+);
+
+export default SearchResults;
