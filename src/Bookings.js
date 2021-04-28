7cc993bbf938868db4d75e9d6216c9cdcb6ee185
Daniel Ghanbari <danielghanbari7@gmail.com>
2021-04-28 22:22:03 +0100

    Task 13 & 14

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
@@ -7,12 +7,13 @@ const Bookings = () => {
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
-
+  const [bookings, setBookings] = useState(FakeBookings);
+  // setBookings(bookings.concat(FakeBookings))
   return (
     <div className="app-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults results={FakeBookings} />
+        <SearchResults results={bookings} />
       </div>
     </div>
   );
