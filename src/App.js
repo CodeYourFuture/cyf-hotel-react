fd4b26886855cf0c27a67ce40cd26af189283f2a
BenyamWorku <benworku@gmail.com>
2021-05-01 00:32:00 +0100

    level 2.14

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import TouristInfoCards from "./TouristInfoCards";
 import Bookings from "./Bookings";
 import Footer from "./Footer";
 import "./App.css";
+import Restaurant from "./Restaurant";
+import Order from "./Order.js";
 const App = () => {
   return (
     <div className="App">
       <Heading />
       <TouristInfoCards />
       <Bookings />
+      <Restaurant />
       <Footer
         contact={[
           "123 Fake Street, London, E1 4UD",
--- src/Bookings.js
+++ src/Bookings.js
@@ -1,13 +1,16 @@
 /* eslint-disable indent*/
-import React from "react";
+import React, { useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-
+import bookingData from "./data/fakeBookings.json";
 const Bookings = () => {
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
-
+  const [bookings, setBookings] = useState(bookingData);
+  //   function updateBookings() {
+  //   setBookings;
+  // }
   return (
     <div className="App-content">
       <div className="container">
