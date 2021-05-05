169fa0ac80127581dae76182d57b325f8a44fd56
NeginDn <negin.derakhshaan@gmail.com>
2021-05-05 16:30:48 +0100

    Lesson03/Exercise16

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me`)
+      .then(res => res.json())
+      .then(data => setBookings(data));
+  }, []);
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
