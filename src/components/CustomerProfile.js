3e6dc88f1d2f55e6e9b88ede6169b31d67e90695
nader <nader90akhgari@gmail.com>
2021-05-08 01:01:22 +0100

    optimize the customer profile component

--- src/components/CustomerProfile.js
+++ src/components/CustomerProfile.js
@@ -1,24 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-const CustomerProfile = props => {
-  const [customerData, setCustomerData] = useState({});
-  console.log(customerData);
-  const [requestOption, setRequestOption] = useState("GET");
-
-  // console.log(props.className);
-  useEffect(() => {
-    fetch(
-      `https://cyf-nader-hotel-server.herokuapp.com/bookings/${props.profileId}`
-    )
-      .then(res => res.json())
-      .then(data => setCustomerData(data));
-  }, [props.profileId]);
+const CustomerProfile = ({ customerData }) => {
   return (
-    <ul className={props.className}>
-      <li>{customerData.id}</li>
+    <ul className="customer-profile lg-col-10 display-show">
+      <li>{customerData._id}</li>
       <li>{customerData.firstName}</li>
       <li>{customerData.surname}</li>
       <li>{customerData.email}</li>
+      <li>{customerData.checkInDate}</li>
+      <li>{customerData.checkOutDate}</li>
       {customerData.vip ? <li>{customerData.phoneNumber}</li> : null}
     </ul>
   );
