7e7ae91467635d3a6c831791c408b72d205b2c6e
Mustafa <30720101+akbasmustafa@users.noreply.github.com>
2021-05-09 03:13:53 +0100

    Done step 21

--- src/components/CustomerProfile.js
+++ src/components/CustomerProfile.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function CustomerProfile({ id }) {
-  return <div>{id !== -1 ? `Customer ${id} Profile` : ""}</div>;
+  const [profile, setProfile] = useState(null);
+  useEffect(() => {
+    if (id !== -1) {
+      fetch(`https://cyf-react.glitch.me/customers/${id}`)
+        .then(res => res.json())
+        .then(data => {
+          setProfile(data);
+        })
+        .catch(err => console.log("Customer Profile", err));
+    }
+  }, [id]);
+  return (
+    <div>
+      {id !== -1 ? (
+        profile !== null ? (
+          <ul>
+            <li>Customer ID: {profile.id}</li>
+            <li>Email: {profile.email}</li>
+            <li>Phone: {profile.phoneNumber}</li>
+            <li>VIP: {profile.vip ? "true" : "false"}</li>
+          </ul>
+        ) : (
+          "null"
+        )
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
 
 export default CustomerProfile;
