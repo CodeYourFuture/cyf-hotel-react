35629986f4ce0872b56be5dc2c5807fbb84f6ddd
Dan <danielghanbari7@gmail.com>
2021-05-14 13:27:57 +0100

    Task 22

--- src/Bookings.js
+++ src/Bookings.js
@@ -4,6 +4,7 @@ import SearchResults from "./SearchResults.js";
 // import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
+  const [loadingData, setLoadingData] = useState(true);
   const search = searchVal => {
     console.info("TO DO!", searchVal);
     let filterBookings = bookings.filter(booking => {
@@ -17,14 +18,24 @@ const Bookings = () => {
 
   const [bookings, setBookings] = useState([]);
   useEffect(() => {
-    fetch(`https://cyf-react.glitch.me`)
+    fetch(`https://cyf-react.glitch.me/delayed`)
+      // fetch(`https://cyf-react.glitch.me`)
       .then(res => res.json())
-      .then(data => setBookings(data));
+      .then(data => setBookings(data))
+      .then(() => {
+        setLoadingData(false);
+      });
   }, []);
   return (
     <div className="app-content">
       <div className="container">
         <Search search={search} />
+        {loadingData && (
+          <div className="text-center" role="status">
+            <span className="spinner-grow" />
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        )}
         <SearchResults results={bookings} />
       </div>
     </div>
