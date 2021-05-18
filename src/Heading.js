4bc36fd827f1464cc465c3079e6b82f45c3f97cd
ShawenHarker <harkershawen@gmail.com>
2021-05-18 21:50:31 +0200

    React Hotel - lesson 1 - exercise 1 & 2 completed

---
+++ src/Heading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <h1>CYF-ShawenHarker-Hotel-React</h1>
+    </header>
+  );
+};
+
+export default Heading;
