15077d4909663d0dca471ba4bb8eb5b1c393f65a
ShawenHarker <harkershawen@gmail.com>
2021-05-18 23:21:38 +0200

    Cards are created for exercise 3 of lesson one, still need to to CSS styling to make the website look presentable.

--- src/Heading.js
+++ src/Heading.js
@@ -4,6 +4,11 @@ const Heading = () => {
   return (
     <header className="App-header">
       <h1>CYF-ShawenHarker-Hotel-React</h1>
+      <img
+        className="Header-log-image"
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        alt="Heading logo"
+      />
     </header>
   );
 };
