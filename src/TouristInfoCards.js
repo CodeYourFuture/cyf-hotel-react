40e7abb1d94bd980341e3f9265830a11f062dff4
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-05-24 16:41:26 +0200

    Complete lesson 1 exercise 3

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -7,12 +7,26 @@ const TouristInfoCards = () =>{
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Dawn_over_Glasgow_%2848659596342%29.jpg/1024px-Dawn_over_Glasgow_%2848659596342%29.jpg" className="card-img-top" alt="" title="By Ian Dick from Glasgow, UK"></img>
                 <div className="card-body">
                     <h5 className="card-title">Glasgow</h5>
-                    <p className="card-text">Visit vibrant Glasgow</p>
-                    <a className="btn btn-primary" href="https://peoplemakeglasgow.com/" target="_blank">See more</a>
+                    <p className="card-text">Visit vibrant Glasgow, Glasgow is fantastic, great night life, the people are fantastic, it's a big city but easy to get around, one of the best shopping cities in the UK</p>
+                    <a className="btn btn-primary" href="https://peoplemakeglasgow.com/">See more</a>
                 </div>
             </div>
-            <div className="card"></div>
-            <div className="card"></div>
+            <div className="card">
+                <img src="https://i2-prod.manchestereveningnews.co.uk/incoming/article17428679.ece/ALTERNATES/s810/1_MWOCT19-121051JPG.jpg" className="card-img-top" alt=""></img>
+                <div className="card-body">
+                    <h5 className="card-title">Manchester</h5>
+                    <p className="card-text">Visit lively Manchester.Manchester is a lively city brimming with character, whether it’s the Industrial Revolution or a musical revolution, Manchester is at the heart of it. From shopping at the Trafford Centre to watching football at Old Trafford, there’s something for everyone.</p>
+                    <a className="btn btn-primary" href="https://www.visitmanchester.com/">See more</a>
+                </div>
+            </div>
+            <div className="card">
+                <img src="https://london.ac.uk/sites/default/files/styles/promo_large/public/2018-10/london-aerial-cityscape-river-thames_1.jpg?itok=BMaDUhjp" className="card-img-top" alt=""></img>
+                <div className="card-body">
+                    <h5 className="card-title">London</h5>
+                    <p className="card-text">Visit historic London. With the diversity, the vibrancy, the sheer noise, the sense of global connectivity, the pace, the exuberance and so much more.</p>
+                    <a className="btn btn-primary" href="https://www.visitlondon.com/">See more</a>
+                </div>
+            </div>
         </div>
     )
 }
