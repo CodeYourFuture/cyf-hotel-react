a09278d1e83dd18a9dc5abf0c3319bdbca94b6e9
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-05-24 20:37:12 +0200

    Add image ID and adjust css for images

--- src/Heading.js
+++ src/Heading.js
@@ -4,7 +4,7 @@ const Heading = () => {
     return(
         <div>
             <header className="App-header">CYF Hotel</header>
-            <img src="https://image.flaticon.com/icons/svg/139/139899.svg" alt="Hotel clip-art"></img>
+            <img src="https://image.flaticon.com/icons/svg/139/139899.svg" alt="Hotel clip-art" id="Jumbotron"></img>
         </div>
     )
 }
