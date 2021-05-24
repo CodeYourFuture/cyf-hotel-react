9be4571db38a07a16ae06714b1d5ec48e850e65e
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-05-24 21:10:13 +0200

    Initialize Table for lesson 1 - exercise 5

---
+++ src/SearchResults.js
@@ -0,0 +1,30 @@
+import React from "react";
+// import Bookings from "./Bookings";
+
+const SearchResults = ()=>{
+return(
+    <table className="table">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Title</th>
+                <th scope="col">First name</th>
+                <th scope="col">Surname</th>
+                <th scope="col">Email</th>
+                <th scope="col">Room Id</th>
+                <th scope="col">Check in date</th>
+                <th scope="col">Check out date</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">
+                    
+                </th>
+            </tr>
+        </tbody>
+    </table>
+)
+}
+
+export default SearchResults;
