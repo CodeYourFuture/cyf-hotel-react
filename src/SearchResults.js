e8d6230c959a372c53c850f52922e1746e56c534
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-05-25 21:19:36 +0200

    STart lesson 1 exercise 6

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -3,14 +3,17 @@ import TableHeader from "./TableHeader";
 import TableBody from "./TableBody";
 // import Bookings from "./Bookings";
 
-const SearchResults = ()=>{
+const SearchResults = (props)=>{
+    
 return(
     <table className="table">
         <thead>
             <TableHeader Headings={[`id`, `title`, `first name`, `surname`, `email`, `room id`, `check in date`,`check out date`]}/>
         </thead>
         <tbody>
-            <TableBody Body={[`1578`,`Mr/He/His`,'Jeanviere',`Boudouir`,`JBoudouir@something.com`,`7357`,`21/05/21`,`28/05/21`]}/>
+            <TableBody Body={props.results}/>
+            {/* <TableBody Body={[`1578`,`Mr/He/His`,'Jeanviere',`Boudouir`,`JBoudouir@something.com`,`7357`,`21/05/21`,`28/05/21`]}/>
+            <TableBody Body={[`1781`,`Mrs/Her/Hers`,`Genevieve`,`Basson`,`GBasson@here.com`,`9357`,`21/05/21`,`28/05/21`]}/> */}
         </tbody>
     </table>
 )
--- src/TableBody.js
+++ src/TableBody.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 const TableBody = (props)=>{
-
+    console.log(props.Body);
     return(
         <tr>
             {props.Body.map((data, index)=>{
-                if(data === `id`){
-                return <th scope="row">{data}</th>
+                if(props.Body.indexOf(data) === 0){
+                    return <th scope="row" key={index}>{data}</th>
                 }
-             return <td key={index}>{data}</td>})}
+                    return <td key={index}>{data}</td>})}
         </tr>
     )
 }
