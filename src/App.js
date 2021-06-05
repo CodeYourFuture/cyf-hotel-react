507891e078efb382b4ce87a524b0906c7a27e5ba
RyanS-Cpt <ryan.30@icloud.com>
2021-06-05 22:06:41 +0200

    Complete lesson 2 exercise 8

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Bookings from "./Bookings";
 import TouristInfoCards from "./TouristInfoCards";
 import Footer from "./Footer";
 import "./App.css";
+import Restaurant from "./Restaurant";
 
 const App = () => {
   return (
     <div className="App">
       <Heading />
       <Bookings />
+      <Restaurant />
       <TouristInfoCards />
       <Footer
         Address={[
