7f51f40b825f841dccdbb20136bd89f67bf5fe19
JarrodBen <jarrodb.247@gmail.com>
2021-06-11 14:29:25 +0200

    Lesson 3 - 22

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -3,7 +3,6 @@ import Search from "./bookings/Search.js";
 import SearchResults from "./bookings/SearchResults.js";
 
 const Bookings = () => {
-  
   const search = searchVal => {
     const newBookings = bookings;
     let filteredBooking = newBookings.filter(
@@ -11,43 +10,65 @@ const Bookings = () => {
         client.firstName.toUpperCase() === searchVal.toUpperCase() ||
         client.surname.toUpperCase() === searchVal.toUpperCase()
     );
-    setBookings(filteredBooking)
+    setBookings(filteredBooking);
   };
 
+  console.log("component loaded");
   const [error, setError] = useState(null);
-  const [loaded, setLoaded] = useState(false);
-  const [bookings, setBookings] = useState([]);
+
+  const [state, setState] = useState({
+    status: "loading",
+    bookings: null,
+    error: null
+  });
 
   useEffect(() => {
-    fetch("https://cyf-react-glitch.me")
-    .then(response => response.json())
-    .then(
-      result => {
-        setLoaded(true);
-        setBookings(result)
-      },
+    fetch("https://cyf-react.glitch.me/")
+      .then(
+        response => response.json(),
+        () => {
+          console.log("reached here");
+          return {
+            status: "loading",
+            bookings: null,
+            error: "Error while Loading API..."
+          };
+        }
+      )
+      .then(
+        result => {
+          setState({
+            status: "complete",
+            bookings: result,
+            error: result.error
+          });
+        }
+      );
+  }, []);
 
-      error => {
-        setLoaded(true);
-        setError(error)
-      }
+  if (state.error) {
+    return (
+      <div>
+        <h1>{state.error}</h1>
+      </div>
     );
-  }, [])
-
-  if (error) {
-    return <div>Error: {error.message}</div>
-  } else if (!loaded) {
-    return <div>Loading, Please wait a moment...</div>
+  } else if (state.status === "loading") {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
   } else {
     return (
       <div className="App-content">
         <div className="container">
           <Search search={search} />
-          <SearchResults bookings={bookings}/>
+          <br />
+          <SearchResults bookings={state.bookings} />
         </div>
       </div>
     );
   }
-}
+};
 
 export default Bookings;
