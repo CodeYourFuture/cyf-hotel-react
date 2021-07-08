099faad182ebd334829c163a8210946c6c5baa37
“Ricky <grickyh@gmail.com>
2021-07-08 06:11:08 +0100

    lesson 3 CS

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,14 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
   const search = searchVal => {
-    console.info("TO DO!", searchVal);
+    console.info("Booking for", searchVal);
   };
 
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+  // fetch data from API
+  useEffect(() => {
+    console.log("fetching");
+
+    fetch(`https://cyf-react.glitch.me`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setBookings(data);
+      })
+      // error handling
+      .catch(err => console.log(err));
+  }, []);
 
   return (
     <div className="App-content">
