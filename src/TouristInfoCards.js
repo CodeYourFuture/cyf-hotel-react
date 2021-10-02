f0250685bbe3a803f879f6b457b409dbcb01ba77
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2021-10-02 20:55:43 +0100

    3 task attempt

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -3,17 +3,38 @@ const TouristInfoCards = () => {
   return (
     <div className="cardWrapper">
       <div className="card">
-        <img src="..." className="card-img-top" />
+        <img
+          src="https://tse3.mm.bing.net/th?id=OIP.VURrH1cM4rNMrNgm0z_bVgHaEo&pid=Api&P=0&w=259&h=162"
+          className="card-img-top"
+        />
         <div className="card-body">
-          <a href="#" className="btn btn-primary">
+          <p>
+            London is the capital and largest city of England and the United
+            Kingdom. It stands on the River Thames in south-east England at the
+            head of a 50-mile estuary down to the North Sea. It has been a major
+            settlement for two millennia
+          </p>
+          <a
+            href="http://visitlondon.com"
+            target="_blank"
+            className="btn btn-primary"
+          >
             Go somewhere
           </a>
         </div>
       </div>
       <div className="card">
-        <img src="..." className="card-img-top" />
+        <img
+          src="https://tse1.mm.bing.net/th?id=OIP.K_zQ812cnQDY18ocKh0ijAHaEK&pid=Api&P=0&w=327&h=185"
+          alt="Manchester City"
+          className="card-img-top"
+        />
         <div className="card-body">
-          <a href="#" className="btn btn-primary">
+          <a
+            href="http://visitmanchester.com"
+            target="_blank"
+            className="btn btn-primary"
+          >
             Go somewhere
           </a>
         </div>
@@ -21,6 +42,7 @@ const TouristInfoCards = () => {
       <div className="card">
         <img
           src="https://www.comewalkwithme.com.au/wp-content/uploads/2017/04/Photo-4_1-People-Make-Glasgow.jpg"
+          alt="Glasgow City"
           className="card-img-top"
         />
         <div className="card-body">
