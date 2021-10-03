8aa488c1df920318c100c14122847b91b0337e1a
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2021-10-03 07:55:57 +0100

    7 task

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,4 +1,6 @@
 import React from "react";
+import moment from "moment";
+
 const SearchResults = prop => {
   return (
     <table class="table table-hover table-dark">
@@ -12,23 +14,32 @@ const SearchResults = prop => {
           <th scope="col">RoomId</th>
           <th scope="col">CheckInDate</th>
           <th scope="col">CheckOutDate</th>
+          <th scope="col">Nights Stay</th>
         </tr>
       </thead>
       <tbody>
         {prop.results.map(element => {
+          let a = moment(element.checkInDate);
+
+          let b = moment(element.checkOutDate);
+
+          let c = b.diff(a, "days");
+
           return (
             <tr>
               <td>{element.id}</td>
               <td>{element.title}</td>
               <td>{element.firstName}</td>
 
-              <td>{element.lastName}</td>
+              <td>{element.surname}</td>
 
               <td> {element.email} </td>
               <td>{element.roomId}</td>
 
               <td>{element.checkInDate}</td>
               <td>{element.checkOutDate}</td>
+
+              <td>{c}</td>
             </tr>
           );
         })}
