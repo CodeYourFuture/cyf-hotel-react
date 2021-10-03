dde005de828c7bed9757c3c3c7569dddcae60afc
musayuksel <musayuxel@gmail.com>
2021-10-03 22:26:00 +0100

    feat: Step-3 add TouristInfoCards and styles

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import React from "react";
 import Bookings from "./Bookings";
 import "./App.css";
 import Heading from "./components/Heading";
+import TouristInfoCards from "./components/TouristInfoCards";
 
 const App = () => {
   return (
     <div className="App">
       <Heading />
+      <div className="cardContainer">
+        <TouristInfoCards city="Glasgow" />
+        <TouristInfoCards city="Manchester" />
+        <TouristInfoCards city="London" />
+      </div>
       <Bookings />
     </div>
   );
