879f9dce5787f88cb54e8365681c7f1a1f827c6c
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-10-03 19:35:24 +0100

    Lesson 1.6 complete - Show more bookings in the table

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-// import FakeBookings from "./data/fakeBookings.json";
+import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
   const search = searchVal => {
@@ -12,20 +12,7 @@ const Bookings = () => {
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults
-          results={[
-            {
-              id: "5",
-              title: "Mr",
-              firstName: "Andrew",
-              surname: "robertson",
-              email: "dfdfd@dfdf",
-              roomId: "55",
-              checkInDate: "12/12/21",
-              checkOutDate: "12/34/56"
-            }
-          ]}
-        />
+        <SearchResults results={FakeBookings} />
       </div>
     </div>
   );
