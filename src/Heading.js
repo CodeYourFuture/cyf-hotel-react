0801c2e651681b4bf6c92b5c52ea7e61e5ee76e6
aalassv <a.alas.sv@gmail.com>
2021-10-04 00:18:17 +0100

    header extracted and image tag done

---
+++ src/Heading.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <img
+          src="https://image.flaticon.com/icons/svg/139/139899.svg"
+          alt="hotel draw"
+          width="70"
+          height="70"
+        />
+      </header>
+      ;
+    </div>
+  );
+};
+
+export default Heading;
