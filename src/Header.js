f45d838f43b8279fb42a8760e58e53ab6f2bdaa6
Mya <myaskylar14@gmail.com>
2021-10-03 09:59:37 +0100

    Extract Header

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="App-header">
+      <img
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        alt="cyf-hotel"
+      />
+      <h1>CYF Hotel</h1>
+    </header>
+  );
+};
+
+export default Header;
