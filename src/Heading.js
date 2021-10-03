4bbddd16bdb0a5bceec0915df75bf97dfd82ca41
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-10-03 10:52:40 +0100

    Lesson 1.2 complete - Extract the header in its own component

---
+++ src/Heading.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Renders page heading, called by `App.js`
+let Heading = () => {
+  return (
+    <header className="App-header">
+      <img
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        alt="pic"
+      />
+      CYF Hotel
+    </header>
+  );
+};
+
+export default Heading;
--- src/SearchButton.js
+++ src/SearchButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-// Render search button, called from Search.js
+// Render search button, called by `Search.js`
 let SearchButton = () => {
   return <button className="btn btn-primary">Search</button>;
 };
