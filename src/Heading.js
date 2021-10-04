dbfb0e2ae2fad0035be2478aa2a8f79143fe4768
maham511 <maha.malik511@gmail.com>
2021-10-04 19:19:04 +0100

    Logo added

---
+++ src/Heading.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Logo from "./Logo";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <Logo />
+      <div>CYF Hotel </div>
+    </header>
+  );
+};
+
+export default Heading;
