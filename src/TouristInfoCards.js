9b5b11f1e8156c47ace47c47fed7cf6578543ef6
aalassv <a.alas.sv@gmail.com>
2021-10-04 02:07:32 +0100

    adding images and using bootstrap to style cards

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -6,9 +6,13 @@ const TouristInfoCards = () => {
       <div className="row">
         <div className="col">
           <div className="card">
-            <img src="" className="card-img-top" />
+            <img
+              src="https://images.unsplash.com/photo-1598476959236-fd5b8e15e64c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80"
+              className="card-img-top"
+              alt="Glasgow city"
+            />
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+              <h5 className="card-title">Glasgow</h5>
               <p className="card-title">
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
@@ -17,17 +21,22 @@ const TouristInfoCards = () => {
                 href="https://www.peoplemakeglasgow.com"
                 className="btn btn-primary"
                 target="_blank"
+                rel="noopener noreferrer"
               >
-                Go somewhere
+                Go Glasgow
               </a>
             </div>
           </div>
         </div>
         <div className="col">
           <div className="card">
-            <img src="..." className="card-img-top" />
+            <img
+              src="https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1093&q=80"
+              className="card-img-top"
+              alt="Manchester city"
+            />
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+              <h5 className="card-title">Manchester</h5>
               <p className="card-title">
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
@@ -36,17 +45,22 @@ const TouristInfoCards = () => {
                 href="https://www.visitmanchester.com"
                 className="btn btn-primary"
                 target="_blank"
+                rel="noopener noreferrer"
               >
-                Go somewhere
+                Go Manchester
               </a>
             </div>
           </div>
         </div>
         <div className="col">
           <div className="card">
-            <img src="..." className="card-img-top" />
+            <img
+              src="https://images.unsplash.com/photo-1476958526483-36efcaa80b1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+              className="card-img-top"
+              alt="London"
+            />
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+              <h5 className="card-title">London</h5>
               <p className="card-title">
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
@@ -55,8 +69,9 @@ const TouristInfoCards = () => {
                 href="https://www.visitlondon.com"
                 className="btn btn-primary"
                 target="_blank"
+                rel="noopener noreferrer"
               >
-                Go somewhere
+                Go London
               </a>
             </div>
           </div>
