ff6169939e41dee491d9baf067543c3fa565dd58
maham511 <maha.malik511@gmail.com>
2021-10-04 20:25:18 +0100

    all info added to card components

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -7,7 +7,7 @@ const cities = [
       "https://images.unsplash.com/photo-1611605877189-b9743c2ef324?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80",
     info:
       "From independent stores to high street retailers, Glasgow is the best shopping city.",
-    link: "peoplemakeglasgow.com"
+    link: "https://peoplemakeglasgow.com"
   },
   {
     name: "Manchester",
@@ -15,7 +15,7 @@ const cities = [
       "https://images.unsplash.com/photo-1605021149343-bb75d2a2fa44?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
     info:
       "Youthful, diverse, energetic and bursting with character; Manchester is one of the most exciting places to visit in the UK right now where everybody and anybody is very warmly welcomed.",
-    link: "visitmanchester.com"
+    link: "https://visitmanchester.com"
   },
   {
     name: "London",
@@ -23,7 +23,7 @@ const cities = [
       "https://images.unsplash.com/photo-1513026705753-bc3fffca8bf4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
     info:
       "Explore the city on sightseeing tours, soak up culture at museums and galleries, or choose from the many free things to do. Treat yourself on famous shopping streets or celebrate special occasions at world-class theatre shows, restaurants and bars.",
-    link: "visitlondon.com."
+    link: "https://visitlondon.com/"
   }
 ];
 
@@ -49,9 +49,15 @@ const TouristInfoCards = () => {
     <div className="card">
       <img src={city.image} alt="city-img" className="card-img-top" />
       <div className="card-body">
-        <a href="#" className="btn btn-primary">
+        <p>{city.info}</p>
+        <a href={city.link} className="btn btn-primary" target="_blank">
           Go somewhere
         </a>
+        {/* <a
+          href="https://visitlondon.com/"
+          className="btn btn-primary"
+          target="_blank"
+        /> */}
       </div>
     </div>
   ));
