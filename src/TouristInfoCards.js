940349a9732c0b2532a8e8443fdaedada4476cd7
Sharm-Dev <sharmainetaylor@ymail.com>
2021-10-05 23:11:17 +0100

    Added card city description text
    3. Create and use a new component to show info cards

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const TouristInfoCards = props => {
-  //const images = ["ManchesterImg", "LondonImg", "GlasgowImg"]
   return (
     <div className="card">
       <img scr="" className="card-img-top" />
       <div className="card-body">
         <img src={props.img} width="200px" />
         <h3>{props.city}</h3>
+        <p>{props.cardtext}</p>
         <a href={props.url} target="_blank" className="btn btn-primary">
           Go Somewhere
         </a>
