b9e62d20ae2a542f425e161d0eeb91b92558c681
Dharma <dharmaguadeloupe@gmail.com>
2021-10-05 17:06:22 +0100

    feat: TouristInfoCards component added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import TouristInfoCards from "./TouristInfoCards";
 import Bookings from "./Bookings";
 import "./App.css";
 
@@ -7,6 +8,27 @@ const App = () => {
   return (
     <div className="App">
       <Header />
+      <TouristInfoCards
+        img={require("./images/glasgow_cathedral.png")}
+        title="Glasgow"
+        description="From independent stores to high street retailers, Glasgow is the best shopping city.
+      The city is also rich in artists and creators which makes for a thriving and exciting independent shopping scene."
+        button="https://peoplemakeglasgow.com/"
+      />
+
+      <TouristInfoCards
+        img={require("./images/manchester.png")}
+        title="Manchester"
+        description="Known throughout the world as the birthplace of the industrial revolution, Manchester has a proud history in science, politics, music, arts and sport. And today the city combines this heritage with a progressive vision to be a city that delivers surprise and delight in equal measures. "
+        button="https://www.visitmanchester.com/"
+      />
+
+      <TouristInfoCards
+        img={require("./images/london.png")}
+        title="London"
+        description="Welcome to London! Discover the best of London with Visit London, the official guide to England’s exciting capital. Find things to do in London, from iconic sightseeing spots and fun-filled days out to top restaurants, theatre and unmissable London events. If you’re not able to visit just yet, plan ahead to make the most of your next visit."
+        button="https://www.visitlondon.com/"
+      />
       <Bookings />
     </div>
   );
