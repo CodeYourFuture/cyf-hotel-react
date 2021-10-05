82fa2338dd191d7856c5c4df601732c98e88f451
Tara <81445969+Tara-Q@users.noreply.github.com>
2021-10-05 12:43:38 +0100

    Create and use a new component to show info cards

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Heading from "./Heading";
 import Bookings from "./Bookings";
 import "./App.css";
+import TouristInfoCards from "./TouristInfoCards";
 
 const App = () => {
   return (
     <div className="App">
       <Heading />
+      <TouristInfoCards />
       <Bookings />
     </div>
   );
