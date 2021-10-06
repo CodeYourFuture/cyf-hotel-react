670159e34f11b95067bee461377b089631e33fe4
maham511 <maha.malik511@gmail.com>
2021-10-06 10:28:32 +0100

    refactored code

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -2,10 +2,7 @@ import React from "react";
 import FakeBookings from "./data/fakeBookings.json";
 // import Bookings from "./Bookings";
 
-const SearchResults = props => {
-  // const results = props.results;
-
-  console.log(props);
+const SearchResults = () => {
   return (
     <table>
       <thead>
@@ -47,11 +44,5 @@ const TableRow = props => {
     </tbody>
   );
 };
-
-//props is arr of objs
-//each obj has properties x8 --> id, title, first name, surname, email, room id, check in date and check out date.
-//Make new row for each obj
-//each row has <td> x8 children
-//each td elem
 
 export default SearchResults;
