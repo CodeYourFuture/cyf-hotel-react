e1d6bbe9fe036334fc2f21c636233533fa782a54
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2021-10-06 16:20:27 +0100

    Search Button and header

---
+++ src/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <div className="App-header">
+      <img
+        className="App-logo"
+        src="https://png.pngtree.com/png-clipart/20190516/original/pngtree-hotel-icon-png-image_3570097.jpg"
+        alt="header"
+      />
+    </div>
+  );
+}
+
+export default Heading;
