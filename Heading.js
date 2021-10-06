41f11f9960d8a431c083185637c3ba046e00499d
blasisi <Bimbolamarialasisi@gmail.com>
2021-10-06 11:38:33 +0100

    added the header section

---
+++ Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+function Heading() {
+  return (
+    <header className="App-header">
+      CYF Hotel
+      <img
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        alt="hotel logo"
+        width="100"
+        height="50"
+      />
+    </header>
+  );
+}
+export default Heading;
