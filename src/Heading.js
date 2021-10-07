ffa55d92b0f1fcf6ff0be803825b2cf3197a2743
Colette <83985997+TheCheekyGit@users.noreply.github.com>
2021-10-07 23:40:25 +0100

    CodeYourFuture/cyf-hotel-react-search-button

---
+++ src/Heading.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+<header className="App-header">CYF Hotel</header>;
+
+export default App - header;
---
+++ src/SearchButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div>
+      <button className="btn btn-primary">Search</button>;
+    </div>
+  );
+};
+
+export default Search;
