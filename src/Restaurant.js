a5ab9456d0207ff412e083a5855be6818c5b9811
aalassv <a.alas.sv@gmail.com>
2021-10-09 00:39:15 +0100

    lesson 2 exercise 10 done

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -6,13 +6,18 @@ const Restaurant = () => {
   function setOrders() {
     setCountOrders(order => order + 1);
   }
+
+  function orderOne() {
+    return setOrders();
+  }
+
   return (
     <div className="container border-top py-5">
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
           Pizzas: {orders}{" "}
-          <button onClick={() => setOrders()} className="btn btn-primary">
+          <button onClick={() => orderOne()} className="btn btn-primary">
             Add
           </button>
         </li>
