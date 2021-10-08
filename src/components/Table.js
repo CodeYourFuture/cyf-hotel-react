44caf3767cdd5597d59cc44003a2a428b9a87947
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-10-08 20:56:44 +0100

    feat:add table

---
+++ src/components/Table.js
@@ -0,0 +1,18 @@
+import React from "react";
+const Table = () => {
+  return (
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>FirstName</th>
+        <th>Surname</th>
+        <th>email</th>
+        <th>Room ID</th>
+        <th>Check-in date</th>
+        <th>Check-out date</th>
+      </tr>
+    </thead>
+  );
+};
+export default Table;
