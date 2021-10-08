9a7ad98f8ef158adbc92eef1b5474cd9c40d36b5
aalassv <a.alas.sv@gmail.com>
2021-10-08 23:36:11 +0100

    Lesson 2 exercise 9 Done with a help of Abadi

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Restaurant = () => {
-  const pizzas = 0;
+  const [orders, setCountOrders] = useState(0);
+
+  function setOrders() {
+    setCountOrders(order => order + 1);
+  }
   return (
     <div className="container border-top py-5">
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
+          Pizzas: {orders}{" "}
+          <button onClick={() => setOrders()} className="btn btn-primary">
+            Add
+          </button>
         </li>
       </ul>
     </div>
