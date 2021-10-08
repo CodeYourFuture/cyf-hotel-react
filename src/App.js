e65a7a2ff4db09991936be6514ed74cd0dfc2d81
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-10-08 20:48:54 +0100

    feat: call functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
+import Search from "./Search";
+import Header from "./components/Header";
+import TouristInfoCards from "./components/TouristInfoCards";
+import Footer from "./components/Footer";
 
 import Bookings from "./Bookings";
 import "./App.css";
+const contactInfo = [
+  "123 Fake Street, London, E1 4UD",
+  "hello@fakehotel.com",
+  "0123 456789"
+];
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Header />
+      <TouristInfoCards />
       <Bookings />
+      <Search />
+      <Footer contactInfo={contactInfo} />
     </div>
   );
 };
