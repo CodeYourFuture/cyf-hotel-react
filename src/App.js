21e400fbbfb60e2abb83fa5bf8323c6c408e0a0f
ali bavarsad <a.bavarsad007@gmail.com>
2021-10-08 22:46:28 +0100

    Task4 done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from "react";
 import Heading from "./Heading";
 import Bookings from "./Bookings";
 import TouristInfoCards from "./TouristInfoCards";
+import Footer from "./Footer";
 
 import "./App.css";
 
 const App = () => {
+  const footerDetails = [
+    "123 Fake Street, London, E1 4UD",
+    "hello@fakehotel.com",
+    "0123 456789"
+  ];
   return (
     <div className="App">
       <Heading />
       <TouristInfoCards />
       <Bookings />
+      <Footer footerDetails={footerDetails} />
     </div>
   );
 };
