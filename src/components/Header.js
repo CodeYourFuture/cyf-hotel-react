b5fe04212076f97507a6fab465090bb7c22c957a
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-10-08 20:50:06 +0100

    feat: add header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+
+const Header = () => {
+  return (
+    <div className="App-header">
+      <header>CYF Hotel</header>
+      <img
+        className="App-logo"
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        alt="logo"
+      />
+    </div>
+  );
+};
+
+export default Header;
