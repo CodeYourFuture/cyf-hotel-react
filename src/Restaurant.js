0b33578625aeb64989e48890a6b9427502f739a6
musayuksel <musayuxel@gmail.com>
2021-10-09 23:34:22 +0100

    feat: Add leve-12 Extract pizza order to its own Order component

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,18 +1,11 @@
-import React, { useState } from "react";
-import RestaurantButton from "./components/RestaurantButton";
-
+import React from "react";
+import Order from "./components/Order";
 const Restaurant = () => {
-  const [orders, setOrders] = useState(0);
-  function orderOne() {
-    setOrders(orders + 1);
-  }
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
-        <li>
-          Pizzas: {orders} <RestaurantButton handleClick={orderOne} />
-        </li>
+        <Order />
       </ul>
     </div>
   );
