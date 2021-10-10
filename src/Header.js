2d5a0c55c4516f3fb33e5046f2766dbb41def6b6
kenda <mariayousef2017@gmail.com>
2021-10-10 15:28:49 +0100

    added img

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Heading = () => {
+  return (
+    <div>
+      <header className="App-header">CYF Hotel</header>;
+      <img
+        className="logo"
+        src="https://media.cntraveler.com/photos/5b97ea9959ff057868b4ea22/5:4/w_3330,h_2664,c_limit/The-Peninsula-Bangkok_2018_The-Peninsula-Bangkok_The-Pool-11.jpg"
+        alt=""
+      />
+    </div>
+  );
+};
+export default Heading;
