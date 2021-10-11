96cecf99e9f04acd93628a72961596baaff8b229
O'neil Malik <oneilmalik09@gmail.com>
2021-10-11 15:21:14 +0100

    upto 13 done

--- src/Order.js
+++ src/Order.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import RestaurantButton from "./RestaurantButton";
 
-const Order = () => {
-  <li>Pizzas: {order}</li>;
+const Order = props => {
+  const [order, setOrder] = useState(0);
+  const orderOne = () => {
+    setOrder(order + 1);
+  };
+  return (
+    <li>
+      {props.orderType}: {order}
+      <RestaurantButton handleClick={orderOne} />
+    </li>
+  );
 };
 
 export default Order;
--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,20 +1,15 @@
-import React, { useState } from "react";
-import RestaurantButton from "./RestaurantButton";
+import React from "react";
 import Order from "./Order";
 // let useState = React.useState;
 
 const Restaurant = () => {
-  const [order, setOrder] = useState(0);
-  // const pizzas = 0;
-  const orderOne = () => {
-    setOrder(1);
-  };
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
-        <li>Pizzas: {order}</li>
-        <RestaurantButton handleClick={orderOne} />
+        <Order orderType="Pizzas" />
+        <Order orderType="Salads" />
+        <Order orderType="Chocolate cake" />
       </ul>
     </div>
   );
