fb8be6826e762096a057ba94dda1fcf794dff1ec
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-10-11 12:57:11 +0100

    Lesson 3.16 complete - Load bookings remotely

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import FakeBookings from "./data/fakeBookings.json";
+import React, { useState, useEffect } from "react";
+// import FakeBookings from "./data/fakeBookings.json";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 
@@ -9,8 +9,21 @@ const Bookings = () => {
     console.info("TO DO!", searchVal);
   };
 
-  const [bookings] = useState(FakeBookings);
+  // Fetch and convert data to a usable format with error/status checking.
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then(response =>
+        response.status >= 200 && response.status <= 299
+          ? response.json()
+          : new Error(
+              `Unexpected Error: ${response.status} ${response.statusText}`
+            )
+      )
+      .then(data => setBookings(data))
+      .catch(error => console.log(`Error received: ${error}`));
+  });
 
+  const [bookings, setBookings] = useState([]);
   return (
     <div className="Bookings-content">
       <div className="container">
