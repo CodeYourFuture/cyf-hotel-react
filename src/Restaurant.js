d9915841b4b37edd260e30642339793cea4abd3d
omerffaruk <farukyamak11044@gmail.com>
2021-10-11 17:28:40 +0100

    feat: exercises completed until step 12 in Readme

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import RestaurantButton from "./RestaurantButton";
 
 const Restaurant = () => {
-  const pizzas = 0;
+  let [orders, setOrders] = useState(0);
+  function orderOne () {
+    setOrders(orders + 1);
+  }
   return (
     <div className="Restaurant-orders container">
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
+          Pizzas: {orders}
+          <RestaurantButton onClick={orderOne} /> 
         </li>
       </ul>
     </div>
