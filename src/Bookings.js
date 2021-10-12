b266adc1f5b708fb7aa0719e6c16e0866b45275a
Megumi Yanaka <margaret_mori0102@yahoo.com>
2021-10-12 20:58:04 +0100

    Lesson1.5 completed, create<SearchResults />

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Search from "./Search.js";
+import SearchResults from "./SearchResults.js";
 // import SearchResults from "./SearchResults.js";
 // import FakeBookings from "./data/fakeBookings.json";
 
@@ -7,12 +8,24 @@ const Bookings = () => {
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
-
+  const tableInfo = [
+    {
+      id: 123,
+      title: "Miss",
+      firstName: "Megumi",
+      surName: "Yanaka",
+      email: "margaret_mori0102@yahoo.com",
+      roomId: 7,
+      checkin: "10/10/2021",
+      checkout: "12/10/2021"
+    }
+  ];
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
         {/* <SearchResults results={FakeBookings} /> */}
+        <SearchResults tableInfo={tableInfo} />
       </div>
     </div>
   );
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,5 +1,36 @@
 import React from "react";
 
-function SearchResults() {}
+function SearchResults(props) {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Title</th>
+          <th scope="col">First name</th>
+          <th scope="col">Surname</th>
+          <th scope="col">Email</th>
+          <th scope="col">Room id</th>
+          <th scope="col">Check in date</th>
+          <th scope="col">Check out date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.tableInfo.map((body, i) => (
+          <tr key={i}>
+            <th scope="row">{body.id}</th>
+            <th scope="row">{body.title}</th>
+            <th scope="row">{body.firstName}</th>
+            <th scope="row">{body.surName}</th>
+            <th scope="row">{body.email}</th>
+            <th scope="row">{body.roomId}</th>
+            <th scope="row">{body.checkin}</th>
+            <th scope="row">{body.checkout}</th>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
 export default SearchResults;
