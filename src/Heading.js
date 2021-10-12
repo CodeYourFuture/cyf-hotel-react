c1068e720d949b62e58a3544a1bd197f7ff80832
Mirabela Butiseaca <43749486+Mira-Bela@users.noreply.github.com>
2021-10-12 20:55:51 +0100

    render the hotel's logo in an <img>

--- src/Heading.js
+++ src/Heading.js
@@ -4,6 +4,7 @@ function Heading() {
   return (
     <div>
       <header className="App-header">CYF Hotel</header>
+      <img src="https://image.flaticon.com/icons/svg/139/139899.svg" />
     </div>
   );
 }
