a73f25c1b6114ecc2748a0b23ca4877cfd3a123b
Mirabela Butiseaca <43749486+Mira-Bela@users.noreply.github.com>
2021-10-12 23:06:58 +0100

    Create a Footer component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from "react";
 import Bookings from "./Bookings";
 import Heading from "./Heading";
 import TouristInfoCards from "./TouristInfoCards";
+import Footer from "./Footer";
 
 import "./App.css";
 
 const App = () => {
+  const contactList = [
+    "123 Fake Street, London, E1 4UD",
+    "hello@fakehotel.com",
+    "0123 456789"
+  ];
   return (
     <div className="App">
       <Heading />
       <TouristInfoCards />
       <Bookings />
+      <Footer contacts={contactList} />
     </div>
   );
 };
