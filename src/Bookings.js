be8302f90ff400d153fb914e05a852ef146e0827
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-10-12 20:35:57 +0100

    Lesson 3.19 complete - Implementing the search functionality

--- src/Bookings.js
+++ src/Bookings.js
@@ -4,11 +4,8 @@ import SearchResults from "./SearchResults.js";
 
 // Renders bookings container (eventually will perform a search), called by `App.js`.
 const Bookings = () => {
-  const search = searchVal => {
-    console.info("TO DO!", searchVal);
-  };
-
   const [bookings, setBookings] = useState(null);
+  const [filteredBookings, setFilteredBookings] = useState([]);
 
   // Fetch and convert data to a usable format with error/status checking.
   useEffect(() => {
@@ -24,14 +21,33 @@ const Bookings = () => {
       .catch(error => console.log(`Error received: ${error}`));
   }, []);
 
-  // Render bookings table if fetch had complete and bookings contains data.
+  // Filter booking data and set the results to state variable `filteredBookings`, called by `SearchResults`
+  const search = searchVal => {
+    if (searchVal === "") {
+      setFilteredBookings(bookings);
+    } else {
+      let bookingMatches = bookings.filter(booking => {
+        return (
+          booking.firstName.toUpperCase().includes(searchVal.toUpperCase()) ||
+          booking.surname.toUpperCase().includes(searchVal.toUpperCase())
+        );
+      });
+      setFilteredBookings(bookingMatches);
+    }
+  };
+
+  // Render bookings table if fetch completed and bookings contains data.
   return (
     <div>
       {bookings ? (
         <div className="Bookings-content">
           <div className="container">
             <Search search={search} />
-            <SearchResults results={bookings} />
+            <SearchResults
+              results={
+                filteredBookings.length === 0 ? bookings : filteredBookings
+              }
+            />
           </div>
         </div>
       ) : null}
