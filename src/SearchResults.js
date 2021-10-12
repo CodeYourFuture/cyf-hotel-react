d599fb7156fbe6b9693030e3a7d8aa03cd4594d5
Mirabela Butiseaca <43749486+Mira-Bela@users.noreply.github.com>
2021-10-13 00:26:30 +0100

    Calculate and show the number of nights for each booking

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,19 +1,21 @@
 import React from "react";
+import moment from "moment";
 
 function SearchResults(props) {
   return (
     <div>
-      <table>
+      <table className="table table-hover">
         <thead>
           <tr>
-            <th> id</th>
-            <th>title</th>
-            <th> first name</th>
-            <th> surname</th>
-            <th> email</th>
-            <th> room id</th>
-            <th> check in date</th>
-            <th> check out date</th>
+            <th scope="col"> id</th>
+            <th scope="col">title</th>
+            <th scope="col"> first name</th>
+            <th scope="col"> surname</th>
+            <th scope="col"> email</th>
+            <th scope="col"> room id</th>
+            <th scope="col"> check in date</th>
+            <th scope="col"> check out date</th>
+            <th scope="col"> number of nights</th>
           </tr>
         </thead>
         <tbody>
@@ -28,6 +30,10 @@ function SearchResults(props) {
                 <td>{result.roomId}</td>
                 <td>{result.checkInDate}</td>
                 <td>{result.checkOutDate}</td>
+                <td>
+                  {moment(result.checkOutDate).diff(result.checkInDate, "day") +
+                    " nights"}
+                </td>
               </tr>
             );
           })}
