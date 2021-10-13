2acf003a752f0954af8a8dd3acca0c943a161bda
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2021-10-13 13:59:25 +0100

    12. Extract pizza order to its own Order component

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,20 +1,12 @@
-import React, { useState } from "react";
-import RestaurantButton from "./ResturantButton";
+import React from "react";
+import Order from "./Order";
 
 const Restaurant = () => {
-  const [orders, setOrders] = useState(0);
-  //const pizzas = 0;
-
-  function orderOne() {
-    setOrders(orders + 1);
-  }
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
-        <li>
-          Pizzas: {orders} <RestaurantButton clickFunc={orderOne} />
-        </li>
+        <Order />
       </ul>
     </div>
   );
