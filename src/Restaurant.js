83d5970ce5affecb3feb19f0740e941a7990bab1
Rebeca Lopez-Santos <rebecal.santos@hotmail.com>
2021-10-14 00:28:47 +0100

    react 2
    exercises to 13 with errors

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,26 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import RestaurantButton from "./RestaurantButton";
+import Order from "./Order";
 const Restaurant = () => {
-  // const pizzas = 0;
-  const [orders, setOrders] = useState(0);
-  console.log(orders);
-  //function restaurant(){
-  function orderOne() {
-    setOrders(orders => {
-      return orders + 1;
-    });
-    console.log(orders);
-  }
   return (
-    <div>
-      <h3>Restaurant Orders</h3>
-      <ul>
-        <li>
-          Pizzas: {orders}
-          <RestaurantButton adding={orderOne} />
-        </li>
-      </ul>
-    </div>
+    <ul>
+      <Order orderType={"Pizzas"} />
+      <Order orderType={"Salad"} />
+    </ul>
   );
 };
 
