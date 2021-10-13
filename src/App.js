0007a85cc8ae339a3ce2082d56d45364ad4b15d0
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2021-10-13 13:27:59 +0100

    Render the Restaurant component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Heading from "./Heading";
 import TouristInfoCards from "./TouristInfoCards";
 import Footer from "./Footer";
 import "./App.css";
+import Restaurant from "./Restaurant";
 
 const App = () => {
   return (
     <div className="App">
       <Heading />
       <Bookings />
+      <Restaurant />
       <TouristInfoCards />
       <Footer />
     </div>
