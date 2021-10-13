d8f07672000c48e4488a51e232b1927194c3d7c5
Dilekcn <dcanturk1@gmail.com>
2021-10-13 22:54:56 +0100

    search

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -2,7 +2,6 @@ import React from "react";
 import moment from "moment";
 
 function SearchResults(props) {
-  console.log(props);
   return (
     <table className="table table-light mb-5">
       <thead>
