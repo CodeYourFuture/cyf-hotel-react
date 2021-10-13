3285a32c253b9de6f14167b842a13a1e6cb28af2
mohammed <mohammedalabdulhamid@gmail.com>
2021-10-13 05:20:20 +0100

    extract the Header out of APP component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Bookings from "./Bookings";
 import "./App.css";
+import SearchButton from "./SearchButton";
+import Header from "./Header";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
       <Bookings />
+      <SearchButton />
     </div>
   );
 };
