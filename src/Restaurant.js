850a3dd932a8b015d76604226ee701c90bde1e74
Rebeca Lopez-Santos <rebecal.santos@hotmail.com>
2021-10-13 23:56:28 +0100

    week2
    completed to exercise 11 with errors

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,13 +1,23 @@
-import React from "react";
-
+import React, { useState } from "react";
+import RestaurantButton from "./RestaurantButton";
 const Restaurant = () => {
-  const pizzas = 0;
+  // const pizzas = 0;
+  const [orders, setOrders] = useState(0);
+  console.log(orders);
+  //function restaurant(){
+  function orderOne() {
+    setOrders(orders => {
+      return orders + 1;
+    });
+    console.log(orders);
+  }
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
+          Pizzas: {orders}
+          <RestaurantButton adding={orderOne} />
         </li>
       </ul>
     </div>
