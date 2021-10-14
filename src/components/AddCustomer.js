dc483386625251001c44a7e1ec9ca31bc2b51ee1
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-10-15 00:03:48 +0100

    feat: customerEntry updated

--- src/components/AddCustomer.js
+++ src/components/AddCustomer.js
@@ -1,32 +1,12 @@
 import React from "react";
-import moment from "moment";
+import TableRow from "./TableRow";
+
 // import fakeBookings from "../data/fakeBookings.json";
 
 const AddCustomer = props => {
-  console.log(props.result);
   const customerEntry = props.result.map(customer => {
-    return (
-      <tr key={customer.id}>
-        <td>{customer.id}</td>
-        <td>{customer.title}</td>
-        <td>{customer.firstName}</td>
-        <td>{customer.surname}</td>
-        <td>{customer.email}</td>
-        <td>{customer.roomId}</td>
-        <td>{customer.checkInDate}</td>
-        <td>{customer.checkOutDate}</td>
-        <td>
-          {calculateDuration(customer.checkInDate, customer.checkOutDate)}{" "}
-          day(s)
-        </td>
-      </tr>
-    );
+    return <TableRow key={customer.id} customer={customer} />;
   });
   return <tbody>{customerEntry}</tbody>;
 };
-function calculateDuration(checkInDate, checkOutDate) {
-  let checkInDay = moment(checkInDate);
-  let checkOutDay = moment(checkOutDate);
-  return checkOutDay.diff(checkInDay, "days");
-}
 export default AddCustomer;
