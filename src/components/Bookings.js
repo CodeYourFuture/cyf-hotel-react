7ba9535c45cfbe5150e2131700d17dc262de3268
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2021-10-16 21:55:14 +0100

    lesson 3 task 16,17,18,19

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "../data/fakeBookings.json";
+import FakeBooking from "../data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings] = useState(FakeBookings);
-  const search = searchVal => {
+  const [bookings, setBookings] = useState(FakeBooking);
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then(res => res.json())
+      .then(data => setBookings(data));
+  }, []);
+
+  let search = searchVal => {
+    let searched = bookings.filter(
+      booking =>
+        booking.firstName.toLowerCase() === searchVal ||
+        booking.surname.toLowerCase() === searchVal
+    );
     console.info("TO DO!", searchVal);
+    //console.log(searched);
+    return setBookings(searched);
   };
 
   return (
