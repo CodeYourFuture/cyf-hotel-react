2d48c82bc564453378f71b5a3e54a3314ee65757
blasisi <Bimbolamarialasisi@gmail.com>
2021-10-17 22:42:23 +0100

    working on lesson 3 customer profile

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 const CustomerProfile = props => {
-  const [customerData, setCustomerData] = useState(null);
+  const [customerData, setCustomerData] = useState("");
   useEffect(() => {
     fetch(`https://cyf-react.glitch.me/customers/${props.id}`)
       .then(response => response.json())
@@ -9,17 +9,20 @@ const CustomerProfile = props => {
         setCustomerData(data);
       });
   }, [props.id]);
+
   return (
     customerData && (
       <div>
         <h3>Customer {props.id} Profile</h3>
-        <p>
-          Customer Name: {customerData.title} {customerData.firstName}
-          {""} {customerData.surname}
-        </p>
-        <p>VIP: {customerData.vip ? "yes" : "No"}</p>
-        <p>Email: {customerData.email}</p>
-        <p>Phone Number No: {customerData.phoneNumber}</p>
+        <ul>
+          <li>
+            Customer Name: {customerData.title} {customerData.firstName}
+            {""} {customerData.surname}
+          </li>
+          <p>VIP: {customerData.vip === true ? "yes" : "No"}</p>
+          <p>Email: {customerData.email}</p>
+          <p>Phone Number No: {customerData.phoneNumber}</p>
+        </ul>
       </div>
     )
   );
