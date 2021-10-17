488fbacb97225d1235965e2348cfabe52eaa52fb
musayuksel <musayuxel@gmail.com>
2021-10-17 12:31:28 +0100

    feat: Add Level-19 Implementing the search functionality

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,29 +1,28 @@
 import React, { useState, useEffect } from "react";
 import Search from "./components/Search";
 import SearchResults from "./components/SearchResults";
-import FakeBookings from "./data/fakeBookings.json";
-
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch("https://cyf-react.glitch.me");
-      const data = await response.json();
-      setBookings(data);
-    }
-    fetchData();
-  }, []);
-
   const search = searchVal => {
-    const searchedNames = FakeBookings.filter(
-      //TO DO : UPDATE FakeBookings DATA
+    //TO DO: update the function or DATA
+    const searchedNames = bookings.filter(
       person =>
         person.firstName.toLowerCase().includes(searchVal) ||
         person.surname.toLowerCase().includes(searchVal)
     );
     setBookings(searchedNames);
   };
+
+  useEffect(() => {
+    async function fetchData() {
+      console.log("data fetch....");
+      const response = await fetch("https://cyf-react.glitch.me");
+      const data = await response.json();
+      setBookings(data);
+    }
+    fetchData();
+  }, []);
 
   return (
     <div className="App-content">
