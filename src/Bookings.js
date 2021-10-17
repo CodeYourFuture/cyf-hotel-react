4af605eecd5f4a92ccce4f8a7bfef72cae478536
musayuksel <musayuxel@gmail.com>
2021-10-17 11:35:18 +0100

    feat: Add Level-16 Load bookings remotely

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,13 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./components/Search";
 import SearchResults from "./components/SearchResults";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://cyf-react.glitch.me");
+      const data = await response.json();
+      setBookings(data);
+    }
+    fetchData();
+  }, []);
 
   const search = searchVal => {
     const searchedNames = FakeBookings.filter(
+      //TO DO : UPDATE FakeBookings DATA
       person =>
         person.firstName.toLowerCase().includes(searchVal) ||
         person.surname.toLowerCase().includes(searchVal)
