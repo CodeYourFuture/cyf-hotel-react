b268851248ccdde27f2e639ab4dc7ec59f2cce4e
aalassv <a.alas.sv@gmail.com>
2021-10-17 19:30:09 +0100

    solving errors on react code.

--- src/Bookings.js
+++ src/Bookings.js
@@ -8,6 +8,7 @@ const Bookings = () => {
     console.info("TO DO!", searchVal);
   };
   const [bookings, setBookings] = useState(fakeBookings);
+  console.log(setBookings);
 
   return (
     <div className="App-content">
