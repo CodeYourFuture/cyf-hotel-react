07ab7f05888281f4a04a7f92374bb033f8f5493b
musayuksel <musayuxel@gmail.com>
2021-10-18 12:07:11 +0100

    feat: Add Level-22 Show a loading message

--- src/Bookings.js
+++ src/Bookings.js
@@ -3,9 +3,8 @@ import Search from "./components/Search";
 import SearchResults from "./components/SearchResults";
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
-
   const search = searchVal => {
-    //TO DO: update the function or DATA
+    //TO DO: update the function or DATA, currently after search my data is gone, need new fetch
     const searchedNames = bookings.filter(
       person =>
         person.firstName.toLowerCase().includes(searchVal) ||
@@ -13,21 +12,31 @@ const Bookings = () => {
     );
     setBookings(searchedNames);
   };
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       console.log("data fetch....");
-      const response = await fetch("https://cyf-react.glitch.me");
+      const response = await fetch("https://cyf-react.glitch.me/delayed");
       const data = await response.json();
       setBookings(data);
+      setIsLoading(!isLoading);
     }
     fetchData();
   }, []);
 
   return (
     // <div className="App-content">
-    <section className="container">
+    <section className="searchSection">
       <Search search={search} />
+      <div
+        style={{ display: `${!isLoading ? "none" : ""}` }}
+        className="loading-message"
+      >
+        <div class="spinner-border text-info" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+      </div>
       <SearchResults results={bookings} />
     </section>
     // </div>
