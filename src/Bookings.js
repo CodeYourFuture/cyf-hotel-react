bbfcf5dae009e6654c8c50b73d2c6ae6f6c154be
Aj <ahmedjalalsd@gmail.com>
2021-10-19 13:45:52 +0100

    Lesson 3 Task 16 solution

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 // import SearchButton from "./SearchButton.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
+// import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [booking] = useState(FakeBookings);
+  const [booking, setBooking] = useState([]);
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
+
+  useEffect(() => {
+    console.log("Fetch Data Remotely");
+
+    fetch(`https://cyf-react.glitch.me`)
+      .then(res => res.json())
+      .then(bookingData => setBooking(bookingData));
+  }, []);
 
   return (
     <div className="App-content">
