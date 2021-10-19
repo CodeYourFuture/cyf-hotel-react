ac1a447b7e1da5b306b68e83b2fdaaacd90c2889
musayuksel <musayuxel@gmail.com>
2021-10-19 23:06:19 +0100

    fix:Add stopPropagation for seperate button click and row click

--- src/components/TableRows.js
+++ src/components/TableRows.js
@@ -25,9 +25,11 @@ export default function TableRows({ booking, setCustomerProfileId }) {
       <td>{booking.checkInDate}</td>
       <td>{booking.checkOutDate}</td>
       <td>{howManyDay(booking.checkInDate, booking.checkOutDate)}</td>
-      <td>
+      <td onClick={e => e.stopPropagation()}>
+        {/* https://developer.mozilla.org/en-US/docs/Web/API/Event/stopPropagation */}
         <button
           className="btn btn-primary"
+          style={{ zIndex: 1 }}
           onClick={handleSetCustomerProfileId}
         >
           Show profile
