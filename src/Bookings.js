a9a6f86c071745ea5a10515e04a1ec6e9fe99d33
Megumi Yanaka <margaret_mori0102@yahoo.com>
2021-10-19 22:45:58 +0100

    Lesson3-16 Completed Load bookings remotely

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
@@ -7,7 +7,15 @@ const Bookings = () => {
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me`)
+      .then(res => res.json())
+      .then(data => {
+        setBookings(data);
+      });
+  }, []);
+
   return (
     <div className="App-content">
       <div className="container">
