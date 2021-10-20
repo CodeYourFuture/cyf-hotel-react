5f51a4c6edf12ff3925ceba3da53024a5782feb8
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-10-20 21:46:48 +0100

    feat: add fetch, useEffect and filter

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,19 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./components/SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
+// import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [booking, setBooking] = useState(FakeBookings);
+  const [booking, setBooking] = useState([]);
   const search = searchVal => {
+    const filterCustomer = pageLoad.filter(
+      customer =>
+        customer.firstName === searchVal || customer.surname === searchVal
+    );
+    console.log(filterCustomer);
     console.info("TO DO!", searchVal);
+    // console.log(pageLoad);
   };
+
+  const [pageLoad, setPageLoad] = useState([]);
+  console.log(pageLoad);
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me`)
+      .then(res => res.json())
+      .then(data => setPageLoad(data));
+    console.log("loading...");
+  }, []);
 
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults results={booking} />
+        <SearchResults results={pageLoad} />
       </div>
     </div>
   );
