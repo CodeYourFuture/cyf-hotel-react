81985a1b187da67519e493c7ab2a7329688f919e
Aj <ahmedjalalsd@gmail.com>
2021-10-20 15:44:32 +0100

    Lesson 3 Task 21 solution

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -1,8 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const CustomerProfile = props => {
-  useEffect(() => {});
-  return <div>{props.id && <p>Customer {props.id} profile</p>}</div>;
+  const [customerData, setCustomerData] = useState([]);
+  useEffect(() => {
+    if (props.id) {
+      fetch(`https://cyf-react.glitch.me/customers/${props.id}`)
+        .then(res => res.json())
+        .then(fetchedCustomerData => setCustomerData(fetchedCustomerData))
+        .catch(err => console.log(err));
+    }
+  }, [props.id]);
+  return (
+    <div>
+      {customerData.id && (
+        <div>
+          <p>customer ID: {customerData.id}</p>
+          <p>customer Email: {customerData.email}</p>
+          <p>customer Phone : {customerData.phoneNumber}</p>
+          {customerData.vip ? <p>A VIP customer</p> : <p>Not a VIP customer</p>}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default CustomerProfile;
