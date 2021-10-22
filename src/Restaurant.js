1e25fd634fcbae577485bfabd344747c444014ee
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-10-22 21:03:10 +0100

    feat: add className

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -3,7 +3,7 @@ import Order from "../src/components/Orders";
 
 const Restaurant = () => {
   return (
-    <div>
+    <div className="restaurant">
       <h3>Restaurant Orders</h3>
       <ul>
         <Order orderType={"pizza"} />
