777af6c0649b4ad144b4c53a765111430af10d37
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-10-22 20:54:34 +0100

    feat: add useState id/setId

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "./Table";
 import AddCustomer from "./AddCustomer";
+import CustomerProfile from "./CustomerProfile";
 
 const SearchResults = props => {
+  const [id, setId] = useState(null);
+  function getProfileId(id) {
+    setId(id);
+  }
   return (
     <div>
       <table className="table">
         <Table />
-        <AddCustomer result={props.results} />
+        <AddCustomer getProfileId={getProfileId} result={props.results} />
       </table>
+      <CustomerProfile id={id} />
     </div>
   );
 };
