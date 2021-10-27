be4c899eb1fb73dc27ab487c432e88e5f7ef7e2e
Sharm-Dev <sharmainetaylor@ymail.com>
2021-10-27 21:34:11 +0100

    Step 19 completed

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,15 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 
 // import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
+// import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
   const search = searchVal => {
-    console.info("TO DO!", searchVal);
+    console.info("searchVal", searchVal);
+    const filteredBooking = bookings.filter(booking => {
+      return booking.firstName === searchVal || booking.surname === searchVal;
+    });
+    console.log({ filteredBooking });
+    setBookings(filteredBooking);
   };
-  const [bookings, setBookings] = useState(FakeBookings);
+
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then(res => res.json())
+      .then(data => {
+        setBookings(data);
+      });
+  }, []);
+
+  // console.log(bookings)
+
   return (
     <div className="App-content">
       <div className="container">
