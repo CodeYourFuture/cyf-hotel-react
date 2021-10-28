fb899ccb027a704ee3ffc2f58e156c3c136027da
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-10-29 00:32:41 +0100

    update customer profile

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 function CustomerProfile(props) {
   //console.log(props.id)
   // State variable of profile. the initial value is an empty object
-  const [profile, setProfile] = useState({});
+  const [profile, setProfile] = useState([]);
   useEffect(() => {
     // the id we got from searchResult and we passed as prop in CustomerProfile is used in URL.
     fetch(`https://cyf-react.glitch.me/customers/${props.id}`)
@@ -11,21 +11,22 @@ function CustomerProfile(props) {
       .then(data => {
         console.log(data);
         // update profile and get what we want
-        setProfile({
-          id: data.id,
-          email: data.email,
-          vip: data.vip.toString()
-        });
+        setProfile(data);
       })
       .catch(error => console.log(error));
   }, []);
   return (
     <div>
       <h1>Show Profile for</h1>
-      <p>
-        {/* use the profile */}
-        ID: {profile.id} <br /> Email: {profile.email} <br /> VIP: {profile.vip}
-      </p>
+      <ul>
+        <li>ID: {profile.id}</li>
+        <li>
+          Name: {profile.firstName} {profile.surname}
+        </li>
+        <li>VIP: {profile.vip ? "Yes" : "No"}</li>
+        <li>Phone No.: {profile.phoneNumber}</li>
+        <li>Email: {profile.email}</li>
+      </ul>
     </div>
   );
 }
