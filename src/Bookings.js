af19c00ac5c37ffc42cc43acce5c05ee3cef4842
O'neil Malik <oneilmalik09@gmail.com>
2021-10-29 12:41:52 +0100

    completed upto level 19

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
+// import FakeBookings from "./data/fakeBookings.json";
 
-const Bookings = () => {
+const Bookings = props => {
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me/")
+      .then(res => res.json())
+      .then(data => setBookings(data));
+  }, []);
+
   const search = searchVal => {
-    console.info("TO DO!", searchVal);
+    // console.info("TO DO!", searchVal);
+    let searchFilter = bookings.filter(
+      booking =>
+        booking.firstName.toLowerCase() === searchVal ||
+        booking.surname.toLowerCase() === searchVal
+    );
+    setBookings(searchFilter);
   };
 
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults results={FakeBookings} />
+        <SearchResults results={bookings} />
       </div>
     </div>
   );
--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchButton = () => {
   return <button className="btn btn-primary">Search</button>;
 };
 
-const Search = () => {
+const Search = props => {
+  const [searchInput, setSearchInput] = useState("");
+
+  function handleSearchInput(event) {
+    setSearchInput(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+    props.search(searchInput);
+  }
+
   return (
     <div className="search">
       <div className="page-header">
@@ -12,7 +24,7 @@ const Search = () => {
       </div>
       <div className="row search-wrapper">
         <div className="col">
-          <form className="form-group search-box">
+          <form className="form-group search-box" onSubmit={handleSubmit}>
             <label htmlFor="customerName">Customer name</label>
             <div className="search-row">
               <input
@@ -20,6 +32,8 @@ const Search = () => {
                 id="customerName"
                 className="form-control"
                 placeholder="Customer name"
+                value={searchInput}
+                onChange={handleSearchInput}
               />
               <SearchButton />
             </div>
