e582e6e1d3fc589432f7d721524dd40cadc789c8
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-11-16 13:48:51 +0000

    time staying is broken
    I have attempted to implement a result on the table for how long a person is staying/has stayed. However it doesn't work as expected so I'm seeking help on slack.

--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+
+function findDateDifference(date1, date2) {
+  date1 = parseInt(date1);
+  date2 = parseInt(date2);
+  return Math.ceil(Math.abs(date1 - date2) / (1000 * 60 * 60 * 24));
+}
+
 const SearchResults = props => {
   return (
     <table className="search-results-table">
       <tbody>
         <tr>
-          <th>ID</th>
-          <th>Title</th>
-          <th>First Name</th>
-          <th>Surname</th>
-          <th>Email</th>
-          <th>Room ID</th>
-          <th>Check In Date</th>
-          <th>Check Out Date</th>
+          <th>ID:</th>
+          <th>Title:</th>
+          <th>First Name:</th>
+          <th>Surname:</th>
+          <th>Email:</th>
+          <th>Room ID:</th>
+          <th>Check In Date:</th>
+          <th>Check Out Date:</th>
+          <th>Time Staying:</th>
         </tr>
 
         {props.results.map(person => {
@@ -25,6 +33,9 @@ const SearchResults = props => {
               <td>{person.roomID}</td>
               <td>{person.checkInDate}</td>
               <td>{person.checkOutDate}</td>
+              <td>
+                {findDateDifference(person.checkInDate, person.checkOutDate)}
+              </td>
             </tr>
           );
         })}
