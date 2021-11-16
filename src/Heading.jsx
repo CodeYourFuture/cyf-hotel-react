bf663ec451d7b8d278fe597ce84255cea2f60d72
haseeb3927 <haseeb3927@gmail.com>
2021-11-16 20:03:22 +0000

    deployment fix 2

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -6,7 +6,7 @@ const Heading = () => {
       <img
         className="App-logo"
         src="https://pix10.agoda.net/hotelImages/124/1246280/1246280_16061017110043391702.jpg?s=1024x768"
-        alt="hotel Image"
+        alt="hotel logo"
       />
     </div>
   );
