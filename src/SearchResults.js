521d0287ae37ca0a0926d28559ea6cd7433754c3
MireilleNyoubissi <nmireillemat@yahoo.fr>
2021-11-20 15:04:35 +0000

    final level 1

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -4,21 +4,22 @@ import moment from "moment";
 // import Tbody from './Tbody.js';
 
 function SearchResults(props) {
+  console.log(props);
   return (
     <div className="table-container">
       <table className="table table-bordered table-hover">
         <Thead />
         <tbody className="text-center">
-          {props.results.map((detail, index) => (
+          {props.results.map(detail => (
             <tr>
-              <td key={index}>{detail.title}</td>
-              <td key={index}>{detail.firstName}</td>
-              <td key={index}>{detail.surname}</td>
-              <td key={index}>{detail.email}</td>
-              <td key={index}>{detail.id}</td>
-              <td key={index}>{detail.checkInDate}</td>
-              <td key={index}>{detail.checkOutDate}</td>
-              <td key={index}>
+              <td key={detail.title}>{detail.title}</td>
+              <td key={detail.firstName}>{detail.firstName}</td>
+              <td key={detail.surname}>{detail.surname}</td>
+              <td key={detail.email}>{detail.email}</td>
+              <td key={detail.id}>{detail.id}</td>
+              <td key={detail.checkInDate}>{detail.checkInDate}</td>
+              <td key={detail.checkOutDate}>{detail.checkOutDate}</td>
+              <td key={detail}>
                 {moment(detail.checkOutDate, "YYYY/MM/DD").diff(
                   moment(detail.checkInDate, "YYYY/MM/DD"),
                   "days"
