e4007e4db474a64634d88b98f973d7be35fc9e75
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-11-22 01:07:59 +0000

    fixed up to lesson 2 - 14
    I had an issue from misunderstanding the instructions, however after reverting my changes and carefully going back through with the knowledge of the end goal I was able to fix the code and now each item is orderable and has individual counts for each item.

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,17 +1,13 @@
-import React, { useState } from "react";
-import RestaurantButton from "./RestaurantButton";
+import React from "react";
+import Order from "./Order";
 const Restaurant = () => {
-  let [pizzas, setPizzas] = useState(0);
-  const orderOne = () => {
-    setPizzas(pizza => ++pizza);
-  };
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
-        <li>
-          Pizzas: {pizzas} <RestaurantButton clickHandler={orderOne} />
-        </li>
+        <Order orderType="pizza" />
+        <Order orderType="salads" />
+        <Order orderType="chocolate cakes" />
       </ul>
     </div>
   );
