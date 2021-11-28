d45dbb0f812e968820d95a77f76e88cb9db509de
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-11-28 15:28:29 +0330

    Style PrideMonth

---
+++ src/components/common/Header/view.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const PrideMonth = styled.div({
+  width: "100vw",
+  height: "0.5rem",
+  background:
+    "linear-gradient(90deg, rgb(100, 91, 83) 0%, rgb(235, 82, 82) 18.23%,rgb(247, 143, 47) 34.37%,rgb(244, 193, 81) 48.96%,rgb(82, 187, 118) 66.15%,rgb(38, 165, 215) 82.29%,rgb(224, 105, 183) 100%)",
+});
