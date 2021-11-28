0aef4c282160a85bc5fef546d30099223374d473
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-11-28 15:28:21 +0330

    Add PrideMonth

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -1,5 +1,15 @@
+import { PrideMonth } from "./view";
+
 function Header() {
-  return <header className="App-header">CYF Hotel</header>;
+  return (
+    <header>
+      <div className="header d-flex align-items-center">
+        <img className="logo" src="https://image.flaticon.com/icons/svg/139/139899.svg" alt="hotel logo" />
+        <div className="title">Welcome to Amir's Hotel!</div>
+      </div>
+      <PrideMonth />
+    </header>
+  );
 }
 
 export default Header;
