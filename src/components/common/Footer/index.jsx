65be22143a24d047fd0f71e96d843f6743ab3e37
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-11-28 20:02:43 +0330

    Add Footer Component

---
+++ src/components/common/Footer/index.jsx
@@ -0,0 +1,24 @@
+const Footer = ({ hotelInfo }) => {
+  return (
+    <footer className="bg-secondary pt-2 text-center">
+      <ul className="list-inline text-light">
+        {hotelInfo.map((info, index) => (
+          <li key={index}>{info}</li>
+        ))}
+        <li>
+          View Source on{" "}
+          <a className="text-warning" href="https://github.com/Amir-Pourhadi/CYF-React-Hotel" target="_blank">
+            GitHub
+          </a>
+        </li>
+        <li>Copyright, 2021 &copy; All Rights Reserved.</li>
+      </ul>
+    </footer>
+  );
+};
+
+Footer.defaultProps = {
+  hotelInfo: ["Najaf Abad, Isfahan, Iran", "Alex.CE1379@Gmail.com", "+98 937 010 4798"],
+};
+
+export default Footer;
