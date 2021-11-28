d16d253f4cb439202005ebadfabdb1f786aa9b10
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-11-28 18:51:59 +0330

    Render TouristInfoCards

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { GlobalStyle } from "../GlobalStyle";
 import "./App.css";
 import Bookings from "./Bookings";
+import TouristsInfoCards from "./Bookings/TouristInfoCards";
 import { Header } from "./common";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <div className="App">
       <GlobalStyle />
       <Header />
+      <TouristsInfoCards />
       <Bookings />
     </div>
   );
