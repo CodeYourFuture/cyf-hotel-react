9bf9b9fe795b71cc17ec9d8764fb8124e32a9612
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-11-28 15:11:34 +0330

    Create folder for restaurant component

---
+++ src/components/Restaurant/index.jsx
@@ -0,0 +1,15 @@
+const Restaurant = () => {
+  const pizzas = 0;
+  return (
+    <div>
+      <h3>Restaurant Orders</h3>
+      <ul>
+        <li>
+          Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Restaurant;
