faec97a3861cd25b7b2412fe1c2e4e71b1c76c6a
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-11-28 18:51:05 +0330

    Change styled component syntax

--- src/components/common/Header/view.js
+++ src/components/common/Header/view.js
@@ -1,32 +1,41 @@
-import styled, { css, keyframes } from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const logoSpin = keyframes({
-  from: {
-    transform: "rotateY(0deg)",
-  },
-  to: {
-    transform: "rotateY(360deg)",
-  },
-});
+const logoSpin = keyframes`
+  from {
+    transform: rotateY(0deg);
+  }
 
-export const Heading = styled.div({
-  backgroundColor: "#333",
-  height: "4.2rem",
-  fontSize: "0.7em",
-});
+  to {
+    transform: rotateY(360deg);
+  }
+`;
 
-export const Logo = styled.img({
-  animation: css`${logoSpin} infinite 20s ease-in-out`,
-  width: 120,
-  padding: "2rem",
-});
+export const Heading = styled.div`
+  background-color: #333;
+  height: 4.2rem;
+  font-size: 0.7em;
+`;
 
-export const Title = styled.div({
-  fontSize: "2em",
-});
+export const Logo = styled.img`
+  animation: ${logoSpin} infinite 20s linear;
+  width: 120px;
+  padding: 2rem;
+`;
 
-export const PrideMonth = styled.div({
-  height: "0.6rem",
-  background:
-    "linear-gradient(90deg, rgb(100, 91, 83) 0%, rgb(235, 82, 82) 18.23%,rgb(247, 143, 47) 34.37%,rgb(244, 193, 81) 48.96%,rgb(82, 187, 118) 66.15%,rgb(38, 165, 215) 82.29%,rgb(224, 105, 183) 100%)",
-});
+export const Title = styled.div`
+  font-size: 2em;
+`;
+
+export const PrideMonth = styled.div`
+  height: 0.6rem;
+  background: linear-gradient(
+    90deg,
+    rgb(100, 91, 83) 0%,
+    rgb(235, 82, 82) 18.23%,
+    rgb(247, 143, 47) 34.37%,
+    rgb(244, 193, 81) 48.96%,
+    rgb(82, 187, 118) 66.15%,
+    rgb(38, 165, 215) 82.29%,
+    rgb(224, 105, 183) 100%
+  );
+`;
