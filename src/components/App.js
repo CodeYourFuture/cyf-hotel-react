b5f5af9595b341d0ac2396b1cb6c65176aaeca94
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-11-28 20:02:58 +0330

    Render Footer

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,17 @@ import { GlobalStyle } from "../GlobalStyle";
 import "./App.css";
 import Bookings from "./Bookings";
 import TouristsInfoCards from "./Bookings/TouristInfoCards";
-import { Header } from "./common";
+import { Footer, Header } from "./common";
 
 const App = () => {
   return (
-    <div className="App">
+    <>
       <GlobalStyle />
       <Header />
       <TouristsInfoCards />
       <Bookings />
-    </div>
+      <Footer />
+    </>
   );
 };
 
