37bdade27789112bed429a8a1b0fc19f2db3ac89
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-11-29 17:11:52 +0330

    Extract RestaurantButton to its own component

---
+++ src/components/Restaurant/RestaurantButton.jsx
@@ -0,0 +1,9 @@
+function RestaurantButton({ orderOne }) {
+  return (
+    <button className="btn btn-primary" onClick={orderOne}>
+      Add
+    </button>
+  );
+}
+
+export default RestaurantButton;
