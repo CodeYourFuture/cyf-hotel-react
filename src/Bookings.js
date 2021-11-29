b91324348d1b17ce3995f6c8edef205cd0cc2c40
smkamaldar <smkamaldar@gmail.com>
2021-11-29 15:58:23 +0000

    lesson 3-search functionality has been done.

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,19 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+  const [searchedBookings, setSearchedBookings] = useState([]);
   const search = searchVal => {
-    console.info("TO DO!", searchVal);
+    const filteredBookings = bookings.filter(booking => {
+      const { firstName, surname } = booking;
+      return firstName.includes(searchVal) || surname.includes(searchVal);
+    });
+    setSearchedBookings(filteredBookings);
   };
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then(response => response.json())
+      .then(data => {
+        setBookings(data);
+        setSearchedBookings(data);
+      });
+  }, []);
 
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults results={bookings} />
+        <SearchResults results={searchedBookings} />
       </div>
     </div>
   );
