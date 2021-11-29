b668c5442b500b8da48bb101df5a99e6a7ab5ebc
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-11-29 15:24:43 +0330

    Style Footer Icons

--- src/components/common/Footer/index.jsx
+++ src/components/common/Footer/index.jsx
@@ -1,10 +1,15 @@
 import { FaCopyright, FaEnvelope, FaGithub, FaMapMarkerAlt, FaPhoneAlt } from "react-icons/fa";
+import styled from "styled-components";
 
 const Footer = ({ hotelInfo }) => {
-  const icons = [<FaMapMarkerAlt />, <FaEnvelope />, <FaPhoneAlt />];
+  const icons = [
+    <FaMapMarkerAlt className="icon text-danger" />,
+    <FaEnvelope className="icon text-info" />,
+    <FaPhoneAlt className="icon" />,
+  ];
 
   return (
-    <footer className="bg-secondary py-2 text-center text-light d-flex flex-column fs-5">
+    <StyledFooter className="bg-secondary py-2 text-center text-light d-flex flex-column fs-5">
       <ul className="list-inline d-flex justify-content-around m-0">
         {hotelInfo.map((info, index) => (
           <li key={index}>
@@ -15,22 +20,33 @@ const Footer = ({ hotelInfo }) => {
       </ul>
       <div className="d-flex justify-content-around">
         <div>
-          <FaCopyright />
+          <FaCopyright className="icon" />
           Copyright, 2021 &copy; All Rights Reserved.
         </div>
         <div>
-          View Source on <FaGithub />
-          <a className="text-warning" href="https://github.com/Amir-Pourhadi/CYF-React-Hotel" target="_blank">
+          View Source on &nbsp;
+          <a
+            className="text-dark text-decoration-none"
+            href="https://github.com/Amir-Pourhadi/CYF-React-Hotel"
+            target="_blank">
+            <FaGithub className="icon text-dark" />
             GitHub
           </a>
         </div>
       </div>
-    </footer>
+    </StyledFooter>
   );
 };
 
 Footer.defaultProps = {
-  hotelInfo: ["Najaf Abad, Isfahan, Iran", "Alex.CE1379@Gmail.com", "+98 937 010 4798"],
+  hotelInfo: ["NajafAbad, Isfahan, Iran", "Alex.CE1379@Gmail.com", "+98 123 456 7890"],
 };
+
+const StyledFooter = styled.footer`
+  .icon {
+    font-size: 1.6rem;
+    margin-right: 0.5rem;
+  }
+`;
 
 export default Footer;
