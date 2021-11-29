e25a3510b9e159e4254bc38798b7e690efce35d0
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-11-29 16:28:00 +0330

    Add container class

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,17 @@ import { GlobalStyle } from "../GlobalStyle";
 import Bookings from "./Bookings";
 import TouristsInfoCards from "./Bookings/TouristInfoCards";
 import { Footer, Header } from "./common";
+import Restaurant from "./Restaurant";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
       <Header />
-      <main>
+      <main className="container">
         <TouristsInfoCards />
         <Bookings />
+        <Restaurant />
       </main>
       <Footer />
     </>
