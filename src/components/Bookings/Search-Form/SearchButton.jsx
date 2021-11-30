783924773b2a5045f2024c2f377ae75912073a83
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-11-30 12:51:22 +0330

    Remove Search button

--- src/components/Bookings/Search-Form/SearchButton.jsx
+++
@@ -1,5 +0,0 @@
-function SearchButton() {
-  return <button className="btn btn-primary">Search</button>;
-}
-
-export default SearchButton;
--- src/components/Bookings/Search-Form/index.jsx
+++ src/components/Bookings/Search-Form/index.jsx
@@ -1,10 +1,7 @@
-import SearchButton from "./SearchButton";
-
 const Search = () => {
   return (
     <form className="form-group d-flex justify-content-center py-3">
-      <input type="text" className="form-control w-50" placeholder="Customer name" />
-      <SearchButton />
+      <input type="text" className="form-control w-50" placeholder="Search Customers" />
     </form>
   );
 };
