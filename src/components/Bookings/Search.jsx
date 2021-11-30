c4f5eaaa87faa636682a4c6818da2ece3e5d2b19
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-11-30 13:06:43 +0330

    Handle Search Input

--- src/components/Bookings/Search.jsx
+++ src/components/Bookings/Search.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 const Search = () => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleSearchInput = (e) => {
+    setSearchInput(e.target.value);
+  };
+
   return (
     <form className="form-group d-flex justify-content-center py-3">
-      <input type="text" className="form-control w-50" placeholder="Search Customers" />
+      <input
+        type="text"
+        className="form-control w-50"
+        placeholder="Search Customers"
+        value={searchInput}
+        onChange={handleSearchInput}
+      />
     </form>
   );
 };
