9f466264dc9a0ce90d33db6907c188c8e1dafc15
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-11-30 12:42:18 +0330

    Load bookings remotely

--- src/components/Bookings/index.jsx
+++ src/components/Bookings/index.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
-import FakeBookings from "../../data/fakeBookings.json";
+import { useEffect, useState } from "react";
 import Search from "./Search-Form";
 import SearchResults from "./SearchResults";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const response = await fetch("https://cyf-react.glitch.me");
+      const data = await response.json();
+      setBookings(data);
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
 
   const search = (searchVal) => {
     console.info("TO DO!", searchVal);
