5c7d9d648f9639fbaa74df3b654300daa1540ccd
Katie <professionalkateg@gmail.com>
2021-11-30 22:39:00 +0000

    added functionality to the search bar. Right now it works, it filters out based on the users input and the persons last and first name. But it removes them forever right now because I don't save the old bookings array anywhere, I just overwrite it. I'll fix that tomorrow.

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,20 +1,31 @@
 import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./Components/SearchResults.jsx";
+import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
+  const [bookings, setBookings] = useState(FakeBookings);
+
   const search = searchVal => {
-    console.info("TO DO!", searchVal);
+    searchVal = searchVal.toLowerCase();
+
+    setBookings(bookings =>
+      // return any array element where the first or second name contains the users input. Users input and the persons name is sanitized to be lowercase.
+      bookings.filter(person => {
+        return (
+          person.firstName.toLowerCase().includes(searchVal) ||
+          person.surname.toLowerCase().includes(searchVal)
+        );
+      })
+    );
   };
 
-  const [bookings, setBookings] = useState([]);
-
-  useEffect(() => {
-    fetch("https://cyf-react.glitch.me")
-      .then(response => response.json())
-      .then(data => setBookings(bookings => (bookings = data)))
-      .catch(error => console.log(error));
-  });
+  // useEffect(() => {
+  //   fetch("https://cyf-react.glitch.me")
+  //     .then((response) => response.json())
+  //     .then((data) => setBookings((bookings) => (bookings = data)))
+  //     .catch((error) => console.log(error));
+  // }, []);
 
   return (
     <div className="App-content">
