765b0d6d2ceda1168632b6c81fa08235cc0e796f
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-11-30 12:50:09 +0330

    Delete Restaurant component

--- src/components/Restaurant/RestaurantButton.jsx
+++
@@ -1,9 +0,0 @@
-function RestaurantButton({ orderOne }) {
-  return (
-    <button className="btn btn-primary" onClick={orderOne}>
-      Add
-    </button>
-  );
-}
-
-export default RestaurantButton;
