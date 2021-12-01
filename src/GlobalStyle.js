afe6df5c9cdc7b7c71e4616b8238bb534c4b2702
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-12-02 01:06:42 +0330

    Remove Importing font

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
-  
+export const GlobalStyle = createGlobalStyle`  
   body {
     user-select: none;
     -moz-user-select: none;
