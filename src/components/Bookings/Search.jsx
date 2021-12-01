23e6017d3c28cd651c90f9d191693a189f18f047
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-12-01 19:04:48 +0330

    Make Search live

--- src/components/Bookings/Search.jsx
+++ src/components/Bookings/Search.jsx
@@ -1,28 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Search = ({ search }) => {
   const [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    search(searchInput);
+  }, [searchInput]);
 
   const handleSearchInput = (e) => {
     setSearchInput(e.target.value);
   };
 
-  const handleSearchSubmit = (e) => {
-    e.preventDefault();
-    search(searchInput);
-  };
-
   return (
-    <form className="form-group d-flex justify-content-center py-3" onSubmit={handleSearchSubmit}>
-      <input
-        type="text"
-        className="form-control w-50"
-        placeholder="Search Customers"
-        value={searchInput}
-        onChange={handleSearchInput}
-      />
-      <button className="btn btn-primary ms-3">Search</button>
-    </form>
+    <input
+      type="text"
+      className="form-control w-50 m-auto"
+      placeholder="Live Search Customers"
+      value={searchInput}
+      onChange={handleSearchInput}
+    />
   );
 };
 
