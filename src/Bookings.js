b91f3ec75a30c8854758413b5cd81718485fe4c6
MireilleNyoubissi <nmireillemat@yahoo.fr>
2021-12-03 17:23:37 +0000

    remove useCallBack from Booking.js

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 //import FakeBookings from "./data/fakeBookings.json";
