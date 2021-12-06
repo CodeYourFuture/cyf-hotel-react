66173e42fe19b78b964aefc8da8dd1dccebab6d2
Johng117 <83654331+Johng117@users.noreply.github.com>
2021-12-06 15:27:55 +0000

    Added some styles.

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -3,13 +3,17 @@ import Order from "./Order";
 
 const Restaurant = () => {
   return (
-    <div>
-      <h3>Restaurant Orders</h3>
-      <ul>
-        <Order orderType="Pizzas" />
-        <Order orderType="Salads" />
-        <Order orderType="Chocolate cake" />
-      </ul>
+    <div className="restaurant-container">
+      <div className="restaurant-text">
+        <h3>Restaurant Orders</h3>
+      </div>
+      <div className="food-order-list">
+        <ul>
+          <Order orderType="Pizzas" />
+          <Order orderType="Salads" />
+          <Order orderType="Chocolate cake" />
+        </ul>
+      </div>
     </div>
   );
 };
