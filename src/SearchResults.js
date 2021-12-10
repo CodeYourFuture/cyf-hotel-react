160fb881cd69f36b19bf2f03880a8a5f3113088e
Halla-S <86550752+Halla-S@users.noreply.github.com>
2021-12-10 07:43:33 +0000

    making the app responsive

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -48,127 +48,130 @@ const SearchResults = ({ results, table }) => {
   };
   return (
     <>
-      <table className="table thead-dark table-bordered">
-        <thead className=" thead-dark ">
-          <tr>
-            {table.map((element, index) => (
-              <th key={index} scope="col">
-                {element}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody className="tbody">
-          {results.map((element, index) => {
-            return (
-              <Row
-                key={index}
-                Id={element.id}
-                Title={element.title}
-                FirstName={element.firstName}
-                SureName={element.surname}
-                Email={element.email}
-                CheckOutDate={element.checkOutDate}
-                RoomId={element.roomId}
-                CheckInDate={element.checkInDate}
-              />
-            );
-          })}
-          {booking.map((element, index) => {
-            return (
-              <Row
-                key={index}
-                Id={element.id}
-                Title={element.title}
-                FirstName={element.firstName}
-                SureName={element.surname}
-                Email={element.email}
-                CheckOutDate={element.checkOutDate}
-                RoomId={element.roomId}
-                CheckInDate={element.checkInDate}
-              />
-            );
-          })}
-        </tbody>
-      </table>
-
-      <form onSubmit={submitHandler}>
-        <h3>Add a booking</h3>
-        <div className="form-align">
-          <label htmlFor="id">Id</label>
-          <input
-            type="text"
-            name="id"
-            required="required"
-            placeholder="Enter an id"
-            onChange={addHandler}
-            value={addData.id}
-          />
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            name="title"
-            required="required"
-            placeholder="Enter a title"
-            onChange={addHandler}
-            value={addData.title}
-          />
-          <label htmlFor="firstName">First Name</label>
-          <input
-            type="text"
-            name="firstName"
-            required="required"
-            placeholder="Enter a first name"
-            onChange={addHandler}
-            value={addData.firstName}
-          />
-          <label htmlFor="surname">SureName</label>
-          <input
-            type="text"
-            name="surname"
-            required="required"
-            placeholder="Enter a surname"
-            onChange={addHandler}
-            value={addData.surname}
-          />
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            name="email"
-            required="required"
-            placeholder="Enter an email"
-            onChange={addHandler}
-            value={addData.email}
-          />
-          <label htmlFor="checkInDate">Check In Date</label>
-          <input
-            type="date"
-            name="checkInDate"
-            required="required"
-            placeholder="Enter a checkInDate"
-            onChange={addHandler}
-            value={addData.checkInDate}
-          />
-          <label htmlFor="checkOutDate">Check Out Date</label>
-          <input
-            type="date"
-            name="checkOutDate"
-            required="required"
-            onChange={addHandler}
-            value={addData.checkOutDate}
-          />
-          <label htmlFor="roomId">Room Id</label>
-          <input
-            type="text"
-            name="roomId"
-            required="required"
-            placeholder="Enter a roomId"
-            onChange={addHandler}
-            value={addData.roomId}
-          />
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
+      <div className="table-container">
+        <table className="table thead-dark table-bordered">
+          <thead className=" thead-dark ">
+            <tr>
+              {table.map((element, index) => (
+                <th key={index} scope="col">
+                  {element}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="tbody">
+            {results.map((element, index) => {
+              return (
+                <Row
+                  key={index}
+                  Id={element.id}
+                  Title={element.title}
+                  FirstName={element.firstName}
+                  SureName={element.surname}
+                  Email={element.email}
+                  CheckOutDate={element.checkOutDate}
+                  RoomId={element.roomId}
+                  CheckInDate={element.checkInDate}
+                />
+              );
+            })}
+            {booking.map((element, index) => {
+              return (
+                <Row
+                  key={index}
+                  Id={element.id}
+                  Title={element.title}
+                  FirstName={element.firstName}
+                  SureName={element.surname}
+                  Email={element.email}
+                  CheckOutDate={element.checkOutDate}
+                  RoomId={element.roomId}
+                  CheckInDate={element.checkInDate}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div className="form-container">
+        <form onSubmit={submitHandler}>
+          <h3>Add a booking</h3>
+          <div className="form-align">
+            <label htmlFor="id">Id</label>
+            <input
+              type="text"
+              name="id"
+              required="required"
+              placeholder="Enter an id"
+              onChange={addHandler}
+              value={addData.id}
+            />
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              name="title"
+              required="required"
+              placeholder="Enter a title"
+              onChange={addHandler}
+              value={addData.title}
+            />
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              name="firstName"
+              required="required"
+              placeholder="Enter a first name"
+              onChange={addHandler}
+              value={addData.firstName}
+            />
+            <label htmlFor="surname">SureName</label>
+            <input
+              type="text"
+              name="surname"
+              required="required"
+              placeholder="Enter a surname"
+              onChange={addHandler}
+              value={addData.surname}
+            />
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              name="email"
+              required="required"
+              placeholder="Enter an email"
+              onChange={addHandler}
+              value={addData.email}
+            />
+            <label htmlFor="checkInDate">Check In Date</label>
+            <input
+              type="date"
+              name="checkInDate"
+              required="required"
+              placeholder="Enter a checkInDate"
+              onChange={addHandler}
+              value={addData.checkInDate}
+            />
+            <label htmlFor="checkOutDate">Check Out Date</label>
+            <input
+              type="date"
+              name="checkOutDate"
+              required="required"
+              onChange={addHandler}
+              value={addData.checkOutDate}
+            />
+            <label htmlFor="roomId">Room Id</label>
+            <input
+              type="text"
+              name="roomId"
+              required="required"
+              placeholder="Enter a roomId"
+              onChange={addHandler}
+              value={addData.roomId}
+            />
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     </>
   );
 };
--- src/order.js
+++ src/order.js
@@ -2,14 +2,30 @@ import React, { useState } from "react";
 import RestaurantButton from "./RestaurantButton";
 const Order = () => {
   const [pizzas, setPizzas] = useState(0);
+  const [chocolate, setChocolate] = useState(0);
+  const [salads, setSalads] = useState(0);
+
   const orderOne = () => {
     setPizzas(pizza => pizza + 1);
   };
+  const orderTow = () => {
+    setChocolate(chocolate => chocolate + 1);
+  };
+  const orderThree = () => {
+    setSalads(salads => salads + 1);
+  };
+
   return (
-    <li>
-      Pizzas: {pizzas}
-      <RestaurantButton order={orderOne} />
-    </li>
+    <ul>
+      <li>
+        Pizzas: {pizzas}
+        <RestaurantButton order={orderOne} />
+        Chocolate: {chocolate}
+        <RestaurantButton order={orderTow} />
+        Salads: {salads}
+        <RestaurantButton order={orderThree} />
+      </li>
+    </ul>
   );
 };
 export default Order;
