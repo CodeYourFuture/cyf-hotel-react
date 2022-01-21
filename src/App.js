78fdd5f097e7dd4f479457055bf8c02bc872d791
gynaa78 <gynaa78@yahoo.com>
2022-01-21 20:05:56 +0000

    commit all work done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import React from "react";
 
 import Bookings from "./Bookings";
+import TouristInfoCards from "./TouristInfoCards";
 import "./App.css";
+import Search from "./Search";
+import Heading from "./Heading";
 
+<Heading />;
+const Footer = props => {
+  return (
+    <div>
+      <ul className="footer">
+        {props.info.map(element => (
+          <li>{element}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 const App = () => {
   return (
+    // <div className="App-content">
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      {/* <header className="App-header">CYF Hotel</header> */}
+      <Heading />
+      <TouristInfoCards />
       <Bookings />
+      <Footer
+        info={[
+          "123 Fake Street, London, E1 4UD",
+          "hello@fakehotel.com",
+          "0123 456789"
+        ]}
+      />
     </div>
+    // </div>
   );
 };
 
---
+++ src/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <img
+        className="App-logo"
+        src="https://i2.wp.com/static.free-logo-design.net/uploads/2020/07/free-hotel-logo-design.jpg"
+        alt=""
+      />
+      <h1 className="App-title">CYF Hotel</h1>
+    </header>
+  );
+};
+export default Heading;
