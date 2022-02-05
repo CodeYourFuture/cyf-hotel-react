80625ac7e14666f8f20a2b6c51c3b69a80b2b20e
Roman-Hal <85693082+Roman-Hal@users.noreply.github.com>
2022-02-05 09:58:28 +0000

    Table_5

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,7 +1,46 @@
 import React from "react";
 
 const SearchResults = () => {
-  return <div />;
+  return (
+    <div>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">title</th>
+            <th scope="col">first name</th>
+            <th scope="col">surname</th>
+            <th scope="col">email</th>
+            <th scope="col">room id</th>
+            <th scope="col">check in date</th>
+            <th scope="col">check out date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+          </tr>
+          <tr>
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default SearchResults;
