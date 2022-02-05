2e4f5ebd745af780cbcf3a90e88a22d957c58a42
Roman-Hal <85693082+Roman-Hal@users.noreply.github.com>
2022-02-05 12:14:39 +0000

    lesson1_finished_corrected

--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,8 @@ const Header = () => {
   return (
     <div>
       <header className="App-header">CYF Hotel</header>
-      <img
-        src="https://image.flaticon.com/icons/svg/139/139899.svg"
-        alt="headerImage"
-      />
+      {/*<img src='https://image.flaticon.com/icons/svg/139/139899.svg' alt="headerImage"></img>*/
+      /*link doesn't work*/}
     </div>
   );
 };
