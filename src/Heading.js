17bef0009f2edf48c62b05dcfc474efd1bf8cc73
Aaron-CYF <86688603+Aaron-CYF@users.noreply.github.com>
2022-03-02 19:50:07 +0000

    Added logo to heading

--- src/Heading.js
+++ src/Heading.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Heading = () => {
-  return <header className="App-header">CYF Hotel</header>;
+  return (
+    <header className="App-header">
+      <img src="https://www.svgrepo.com/show/20484/hotel.svg" />
+      &ensp;CYF Hotel
+    </header>
+  );
 };
 
 export default Heading;
