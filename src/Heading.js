4d5c7f3b383d51b9351e0627241d0317d6e83746
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2022-03-04 10:03:45 +0000

    Update hotel logo

--- src/Heading.js
+++ src/Heading.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "./Heading.css";
+import logo from "./Images/logo.png";
 
 // Renders page heading, called by `App.js`.
 const Heading = () => {
   return (
     <header className="Heading-header Heading-title d-flex shadow">
-      <img
-        className="Heading-logo"
-        src="https://image.flaticon.com/icons/svg/139/139899.svg"
-        alt="CYF Hotel Logo"
-      />
+      <img className="Heading-logo" src={logo} alt="CYF Hotel Logo" />
       <h1>CYF Hotel</h1>
     </header>
   );
