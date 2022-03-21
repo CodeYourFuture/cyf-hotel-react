b79db25ca211fe1bbd19ccd680b6b76503a9a7a6
Rahwa <rahwafesseha@gmail.com>
2022-03-21 13:32:41 +0000

    Extract the search button and the header in its own component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import Heading from "./Heading";
 import Bookings from "./Bookings";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Heading />
       <Bookings />
     </div>
   );
---
+++ src/Heading.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Heading = () => {
+  return (
+    <div>
+      <header className="App-header">CYF Hotel</header>
+    </div>
+  );
+};
+export default Heading;
