4fbcedcbf1faa11b99ce2aea7347bb624ccd427d
Helen <g.helen.rog@gmail.com>
2022-03-22 16:56:38 +0000

    Update SearchResults.js

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -69,9 +69,14 @@ function SearchResults() {
       <table class="table table-striped">
         <thead>
           <tr>
-            <th>Sr.NO</th>
-            <th>Name</th>
-            <th>City</th>
+            {/* <th scope="col">#</th> */}
+            <th scope="col">Id</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Surname</th>
+            <th scope="col">Email</th>
+            <th scope="col">Room Id</th>
+            <th scope="col">Check In Date</th>
+            <th scope="col">Check Out Date</th>
           </tr>
         </thead>
         <tbody>{DisplayData}</tbody>
