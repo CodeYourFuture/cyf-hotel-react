6df743f3da52dff0575cbef5eacdaeeb83f778b4
Harsheek Thanki <harsheekthanki@gmail.com>
2022-03-23 21:27:41 +0000

    Lesson 1 exercise 1 and 2 complete

---
+++ src/TouristInfoCards.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TouristInfoCards = () => {
+  return (
+    <div className="card">
+      <img src="..." className="card-img-top" />
+      <div className="card-body">
+        <a href="#" className="btn btn-primary">
+          More Information
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default TouristInfoCards;
