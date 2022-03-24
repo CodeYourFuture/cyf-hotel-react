998dd0e215297b0b803ac15052502c6a3344db6d
Monika39-maker <dangolmonika@yahoo.com>
2022-03-24 20:52:00 +0000

    initial commit-lesson 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
+import Header from "./heading";
 
 import Bookings from "./Bookings";
 import "./App.css";
+import TouristInfoCards from "./cards";
+import Footer from "./footer";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Header />
+      <TouristInfoCards />
+
       <Bookings />
+      <Footer
+        contactDetail={[
+          "123 Fake Street, London, E1 4UD",
+          "hello@fakehotel.com",
+          "0123 456789"
+        ]}
+      />
     </div>
   );
 };
