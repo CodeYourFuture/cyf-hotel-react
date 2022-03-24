ccff03273e8e5c9f3e5a346f4f0ff57bc3df9098
IwonaKrysp <iwona.krysp@gmail.com>
2022-03-24 18:49:46 +0000

    Lesson1, steps 1,2,3,4

--- src/Heading.js
+++ src/Heading.js
@@ -3,7 +3,7 @@ import React from "react";
 const Heading = () => {
   return (
     <div className="logo-wrapper">
-      <img className="logo-img" src="react.svg" width={"50px"} />
+      <img className="logo-img" src="react.svg" alt="logo" width={"50px"} />
       <header className="App-header">CYF Hotel</header>
     </div>
   );
