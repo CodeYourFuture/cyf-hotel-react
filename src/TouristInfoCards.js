36c8a7fa5e32ab20f70c3cf6414c3a3b121b62b1
Helen <g.helen.rog@gmail.com>
2022-03-24 00:53:26 +0000

    Update TouristInfoCards.js

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -24,6 +24,7 @@ function TouristInfoCard() {
       <div className="card">
         <img
           src="https://cdn.pixabay.com/photo/2017/04/14/23/47/bridge-2231612_960_720.jpg"
+          alt="Manchester"
           className="card-img-top"
         />
         <div className="card-body">
@@ -41,6 +42,7 @@ function TouristInfoCard() {
       <div className="card">
         <img
           src="https://cdn.pixabay.com/photo/2017/06/11/18/03/big-ben-2393098_960_720.jpg"
+          alt="London"
           className="card-img-top"
         />
         <div className="card-body">
