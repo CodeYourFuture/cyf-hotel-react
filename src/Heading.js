07d4cd3e9d392e2536527a2d3fc765023333c746
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-03-24 14:42:50 +0000

    Add header component and style, extract content from app.js file

---
+++ src/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div className="header">
+      <img
+        src="https://as2.ftcdn.net/v2/jpg/02/24/25/85/1000_F_224258503_HGePHamZo4z9szjos0TaGolT41Y9TdZa.jpg"
+        alt="Hotel Logo"
+      />
+      <header className="App-header">CYF Hotel</header>
+    </div>
+  );
+};
+
+export default Heading;
