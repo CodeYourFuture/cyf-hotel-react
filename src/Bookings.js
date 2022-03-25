de08b21ac364228262fe400012aa1d68244441e9
Timea <timea.reich@gmail.com>
2022-03-25 16:23:41 +0000

    removed unused var

--- src/Bookings.js
+++ src/Bookings.js
@@ -2,18 +2,16 @@ import React from "react";
 import Search from "./Search.js";
 import SearchResult from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
-import moment from "moment";
 
 const Bookings = () => {
   const search = (searchVal) => {
     console.info("TO DO!", searchVal);
   };
-  const fakeBookingData = FakeBookings;
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResult data={fakeBookingData} />
+        <SearchResult data={FakeBookings} />
         {/* <SearchResults results={FakeBookings} /> */}
       </div>
     </div>
