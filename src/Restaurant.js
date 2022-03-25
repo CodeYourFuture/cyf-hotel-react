ebe598ef4a9db4aef2593638a51df25641534548
AmarachiOkereke <90873234+AmarachiOkereke@users.noreply.github.com>
2022-03-25 19:44:35 +0000

    Task 10 completed

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,14 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Restaurant = () => {
-  const [order, setOrder] = useState(0);
   const pizzas = 0;
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {order} <button className="btn btn-primary">Add</button>
+          Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
         </li>
       </ul>
     </div>
