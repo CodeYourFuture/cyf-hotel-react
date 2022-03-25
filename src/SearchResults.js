61f386f7cf3021c72b299d1a14db7d2cd56c76e9
Hamra <hamra.khan.b@gmail.com>
2022-03-25 12:53:34 +0000

    SearchResults component - Created array of headings for table column titles - Created SearchResults component to show hotel bookings in a table and render a column for each booking attribute - Displayed all bookings from `src/data/fakeBookings.json` in the table - Added another column for table showing number of nights a guest is staying

---
+++ src/SearchResults.js
@@ -0,0 +1,51 @@
+import React from "react";
+import moment from "moment";
+
+const headings = [
+  `id`,
+  `title`,
+  `firstName`,
+  `surname`,
+  `email`,
+  `roomId`,
+  `checkInDate`,
+  `checkOutDate`,
+  `numberOfNights`
+];
+
+function SearchResults(props) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headings.map(heading => {
+            return <th scope="col">{heading}</th>;
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {props.bookings.map(booking => {
+          return (
+            <tr>
+              <th scope="row">{booking.id}</th>
+              {headings.map(heading => {
+                if (heading === "id") {
+                  return null;
+                } else if (heading === "numberOfNights") {
+                  const checkIn = moment(booking.checkInDate);
+                  const checkOut = moment(booking.checkOutDate);
+                  const dateDifference = checkOut.diff(checkIn, "days");
+                  return <td>{dateDifference}</td>;
+                } else {
+                  return <td>{booking[heading]}</td>;
+                }
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default SearchResults;
