b176fb775f75e8fe243f97a092bb4b98a388303b
Hamra <hamra.khan.b@gmail.com>
2022-03-25 13:27:51 +0000

    TouristInfoCards component - made changes to value in places array

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -3,21 +3,21 @@ import React from "react";
 function TouristInfoCards() {
   const places = [
     {
-      name: "manchester",
+      name: "Manchester",
       link: "https://visitmanchester.com",
       image:
         "https://www.visitbritain.com/sites/default/files/styles/consumer_vertical_hero__1920x1080/public/consumer/paragraphs-bundles/image-header-with-text/manchester_town_hall_ve13463_1200x600_0_1_0_0.jpg?itok=xS8QOYbJ",
       alt: "image of manchester"
     },
     {
-      name: "glasgow",
+      name: "Glasgow",
       link: "https://peoplemakeglasgow.com",
       image:
         "https://img.gs/vvjnmlqfld/756/https://www.conferences-uk.org.uk/logos/university-of-glasgow-image-12006-main-big.jpg",
       alt: "image of glasgow"
     },
     {
-      name: "london",
+      name: "London",
       link: "https://visitlondon.com",
       image:
         "http://sagitterone.co.uk/wp-content/uploads/2016/11/westminster-palace-big-ben-london-wallpaper-1200x600.jpg",
