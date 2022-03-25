31239969ea9c9a20bef432375c5677200b87964b
IwonaKrysp <iwona.krysp@gmail.com>
2022-03-25 13:40:37 +0000

    Lesson1 step6

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,18 +1,5 @@
 import React from "react";
 
-const fakeData = [
-  {
-    id: 1,
-    title: "Mr",
-    firstName: "John",
-    surname: "Doe",
-    email: "johndoe@doe.com",
-    roomId: 2,
-    checkInDate: "2017-11-21",
-    checkOutDate: "2017-11-23"
-  }
-];
-
 const headings = [
   "id",
   "title",
@@ -35,7 +22,7 @@ function SearchResults(props) {
         </tr>
       </thead>
       <tbody>
-        {fakeData.map(booking => {
+        {props.bookings.map(booking => {
           return (
             <tr>
               <th scope="row"> {booking.id} </th>
