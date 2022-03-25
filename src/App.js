351ce14c97b37317635d464591f89428cc9c6775
teniolao <Tennybetty100@gmail.com>
2022-03-25 11:52:02 +0000

    Revert "Revert "exercise update""
    This reverts commit e75eeed4d6736db03021e27da9b35582d67ed829.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from "react";
 
 import Bookings from "./Bookings";
 import "./App.css";
+import Heading from "./Heading";
+import TouristInfoCards from "./TouristInfoCards";
+import Footer from "./Footer";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Heading />
+      <TouristInfoCards />
       <Bookings />
+      <Footer
+        details={[
+          "123 Fake Street, London, E1 4UD",
+          "hello@fakehotel.com",
+          "0123 456789"
+        ]}
+      />
     </div>
   );
 };
--- src/Bookings.js
+++ src/Bookings.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Search from "./Search.js";
-// import SearchResults from "./SearchResults.js";
-// import FakeBookings from "./data/fakeBookings.json";
+import SearchResults, { TableHeader } from "./SearchResults.js";
+import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
   const search = searchVal => {
@@ -13,6 +13,20 @@ const Bookings = () => {
       <div className="container">
         <Search search={search} />
         {/* <SearchResults results={FakeBookings} /> */}
+        <TableHeader />
+        {FakeBookings.map(data => (
+          <SearchResults
+            key={data["id"]}
+            id={data["id"]}
+            title={data["title"]}
+            firstName={data["firstName"]}
+            surname={data["surname"]}
+            email={data["email"]}
+            room={data["roomId"]}
+            dateIn={data["checkInDate"]}
+            dateOut={data["checkOutDate"]}
+          />
+        ))}
       </div>
     </div>
   );
