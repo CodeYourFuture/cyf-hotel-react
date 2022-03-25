4a89da13b145ef49e5dce3934eca1804fc1b05e0
mandeepsangha <91365504+mandeepsangha@users.noreply.github.com>
2022-03-25 20:47:54 +0000

    Lesson 1 complete

---
+++ src/Heading.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      CYF Hotel
+      <div>
+        <img
+          alt="hotel logo"
+          src="https://cdn.dribbble.com/users/4354067/screenshots/13167254/media/5bdaf8ca6b78e6cef8d2ceafc17c2ddd.png?compress=1&resize=400x300&vertical=top"
+        />
+      </div>
+    </header>
+  );
+};
+export default Heading;
---
+++ src/SearchResults.js
@@ -0,0 +1,51 @@
+import moment from "moment";
+import React from "react";
+
+const headers = [
+  "id",
+  "title",
+  "firstName",
+  "surname",
+  "email",
+  "roomId",
+  "checkInDate",
+  "checkOutDate",
+  "#Nights"
+];
+
+function SearchResults(props) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headers.map(heading => {
+            return <th scope="col">{heading} </th>;
+          })}
+        </tr>
+        >
+      </thead>
+      <tbody>
+        {props.bookings.map(booked => {
+          return (
+            <tr>
+              <th scope="row">{booked.id}</th>
+              {headers.map(heading => {
+                if (heading === "id") {
+                  return null;
+                } else if (heading === "#Nights") {
+                  const inDate = moment(booked.checkOutDate);
+                  const outDate = moment(booked.checkInDate);
+                  return <td>{inDate.diff(outDate, "days")} </td>;
+                } else {
+                  return <td>{booked[heading]}</td>;
+                }
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default SearchResults;
