0a2d74305b92c2a308e16e56cabf3e6ecb57c56a
Mohamed Khirallah <mohamedkhirallah2812@gmail.com>
2022-03-25 22:38:36 +0000

    React-week1

---
+++ src/Heading.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <img
+          className="App-header App-logo"
+          src="https://thumbs.dreamstime.com/b/simple-illustration-dark-blue-hotel-logo-design-template-business-icon-inspiration-travel-tourism-sticker-idea-simple-165633303.jpg"
+          alt="Hotel"
+        />
+        CYF Hotel
+      </header>
+    </div>
+  );
+};
+
+export default Heading;
