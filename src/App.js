626f314773ccb83c0178961f19f6d9751990c1c3
Simon Lawrence <90318503+SimonLawrenceGB@users.noreply.github.com>
2022-03-25 22:39:07 +0000

    Lesson 1, Part 3
    TouristInfoCards.js created and completed.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Heading from "./Heading";
+import TouristInfoCards from "./TouristInfoCards";
 
 import Bookings from "./Bookings";
 import "./App.css";
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className="App">
       <Heading />
+      <TouristInfoCards />
       <Bookings />
     </div>
   );
