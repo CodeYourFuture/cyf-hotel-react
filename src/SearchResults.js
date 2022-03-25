4f1a194c70b1ab8002ae710d88aa8245a6ce39b1
IwonaKrysp <iwona.krysp@gmail.com>
2022-03-25 12:04:38 +0000

    Lesson1 step5

---
+++ src/SearchResults.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const fakeData = [
+  {
+    id: 1,
+    title: "Mr",
+    firstName: "John",
+    surname: "Doe",
+    email: "johndoe@doe.com",
+    roomId: 2,
+    checkInDate: "2017-11-21",
+    checkOutDate: "2017-11-23"
+  }
+];
+
+const headings = [
+  "id",
+  "title",
+  "first name",
+  "surname",
+  "email",
+  "room id",
+  "checkInDate",
+  "checkOutDate"
+];
+
+function SearchResults(props) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headings.map(heading => {
+            return <th scope="col"> {heading}</th>;
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {fakeData.map(booking => {
+          return (
+            <tr>
+              <th scope="row"> {booking.id} </th>
+              {headings.map(heading => {
+                if (heading === "id") {
+                  return null;
+                } else {
+                  return <td> {booking[heading]} </td>;
+                }
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default SearchResults;
