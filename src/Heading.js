aeb3b236ad74136bc3bcf5989531ac54fe263bfc
Matilda Ako <matildaako@gmail.com>
2022-03-25 22:49:30 +0000

    added alt to logo

--- src/Heading.js
+++ src/Heading.js
@@ -4,6 +4,7 @@ const Heading = () => (
   <header className="App-header">
     <img
       className="logo-image"
+      alt={"Logo"}
       src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Mercure_Hotels_Logo_2013.svg/1200px-Mercure_Hotels_Logo_2013.svg.png"
     />
   </header>
