033840ec6700416a3a51705a92d9db53ec1be3c1
Simon Lawrence <90318503+SimonLawrenceGB@users.noreply.github.com>
2022-03-25 21:32:17 +0000

    Lesson 1, Part 1 & 2
    SearchButton.js & Heading.js complete.

---
+++ src/Heading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <img className="Hotel-Logo" src="https://bit.ly/3JCqIcC" alt="#" />
+      CYF Hotel
+    </header>
+  );
+};
+
+export default Heading;
