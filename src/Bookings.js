925e0a0a336c2829053469dc44e4a28f4246525c
Timea <timea.reich@gmail.com>
2022-03-25 01:16:07 +0000

    step 7 updated

--- src/Bookings.js
+++ src/Bookings.js
@@ -2,6 +2,7 @@ import React from "react";
 import Search from "./Search.js";
 import SearchResult from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
+import moment from "moment";
 
 const Bookings = () => {
   const search = (searchVal) => {
