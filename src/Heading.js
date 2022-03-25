7628b3aba67aaff06c0a266eed1bc2806a591ea0
Hamra <hamra.khan.b@gmail.com>
2022-03-25 12:35:14 +0000

    Heading component - Extracted header element and created it as component - Added img tag with src link

---
+++ src/Heading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <header className="App-header">
+      <img src={"https://www.pngmart.com/files/4/Hotel-PNG-Clipart.png"} />
+    </header>
+  );
+}
+
+export default Heading;
