0fb3be9836c43dd6ef657cee82741db3a949b736
Rahwa <rahwafesseha@gmail.com>
2022-03-25 20:23:49 +0000

    added some styles

--- src/Heading.js
+++ src/Heading.js
@@ -7,6 +7,7 @@ const Heading = () => {
       <img
         id="logo"
         src="https://d3jmn01ri1fzgl.cloudfront.net/photoadking/webp_thumbnail/61023bed99921_json_image_1627536365.webp"
+        alt="hotel logo"
       />
     </div>
   );
