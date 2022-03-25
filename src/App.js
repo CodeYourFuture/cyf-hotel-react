e75eeed4d6736db03021e27da9b35582d67ed829
teniolao <Tennybetty100@gmail.com>
2022-03-25 11:44:23 +0000

    Revert "exercise update"
    This reverts commit 99d4962b0298fa64a047c5f676b2001773c458e6.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,12 @@ import React from "react";
 
 import Bookings from "./Bookings";
 import "./App.css";
-import Heading from "./Heading";
-import TouristInfoCards from "./TouristInfoCards";
-import Footer from "./Footer";
 
 const App = () => {
   return (
     <div className="App">
-      <Heading />
-      <TouristInfoCards />
+      <header className="App-header">CYF Hotel</header>
       <Bookings />
-      <Footer
-        details={[
-          "123 Fake Street, London, E1 4UD",
-          "hello@fakehotel.com",
-          "0123 456789"
-        ]}
-      />
     </div>
   );
 };
