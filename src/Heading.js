3ba458ae4cde68d9e8dd7163f087853121a63b7b
andersonuk <andersonf.work@gmail.com>
2022-03-25 16:21:18 +0000

    .:. lesson 02
    Extract the header in its own component

---
+++ src/Heading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <header className="App-header">
+      <img src="https://cdn-icons-png.flaticon.com/512/139/139899.png" />
+      CYF Hotel
+    </header>
+  );
+}
+
+export default Heading;
