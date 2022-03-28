61037daa3398213085e3bc09cd66ba80062d3e23
Hatice06 <haticeaydogan00@gmail.com>
2022-03-28 12:20:14 +0100

    Make object and map through in TouristInfoCards

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -1,50 +1,51 @@
 import React from "react";
 
 const TouristInfoCards = () => {
+  const places = [
+    {
+      name: "Glasgow",
+      img: {
+        src:
+          "https://www.whatsonnetwork.co.uk/uploads/800x600/251d1ca49b01dd9048ef9967e46bf339.jpg",
+        alt: "Glasgow scene"
+      },
+      link: "https://peoplemakeglasgow.com"
+    },
+    {
+      name: "Manchester",
+      img: {
+        src:
+          "https://static.independent.co.uk/2021/11/03/15/ManchesterRecoveryCampaign-32.jpg?quality=75&width=982&height=726&auto=webp",
+        alt: "Manchester scene"
+      },
+      link: "https://visitmanchester.com"
+    },
+
+    {
+      name: "London",
+      img: {
+        src:
+          "https://media.architecturaldigest.com/photos/56fd9d65ecd154e0329c1627/4:3/w_768/london-travel-guide-lede.jpg",
+
+        alt: "London scene"
+      },
+      link: "https://visitlondon.com"
+    }
+  ];
   return (
     <div className="card-container">
-      <div className="card">
-        <img
-          src={
-            "https://www.whatsonnetwork.co.uk/uploads/800x600/251d1ca49b01dd9048ef9967e46bf339.jpg"
-          }
-          alt={"Glasgow scene"}
-          className="card-img-top"
-        />
-        <div className="card-body">
-          <a href="https://peoplemakeglasgow.com" className="btn btn-primary">
-            Go Glasgow
-          </a>
-        </div>
-      </div>
-      <div className="card">
-        <img
-          src={
-            "https://static.independent.co.uk/2021/11/03/15/ManchesterRecoveryCampaign-32.jpg?quality=75&width=982&height=726&auto=webp"
-          }
-          alt={"Manchester scene"}
-          className="card-img-top"
-        />
-        <div className="card-body">
-          <a href="https://visitmanchester.com" className="btn btn-primary">
-            Go Manchester
-          </a>
-        </div>
-      </div>
-      <div className="card">
-        <img
-          src={
-            "https://media.architecturaldigest.com/photos/56fd9d65ecd154e0329c1627/4:3/w_768/london-travel-guide-lede.jpg"
-          }
-          alt={"London scene"}
-          className="card-img-top"
-        />
-        <div className="card-body">
-          <a href="https://visitlondon.com" className="btn btn-primary">
-            Go London
-          </a>
-        </div>
-      </div>
+      {places.map(place => {
+        return (
+          <div className="card">
+            <img src={place.img.src} className="card-img-top" />
+            <div className="card-body">
+              <a href={place.link} className="btn btn-primary">
+                Go {place.name}
+              </a>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
