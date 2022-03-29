d6f5014f81af4a25349919661a579db7a6e49088
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-03-29 12:13:33 +0100

    Update Bookings.js

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -6,6 +6,8 @@ import FakeBookings from "../data/fakeBookings.json";
 const Bookings = () => {
   const [bookings, setBookings] = useState(FakeBookings);
 
+  console.log(setBookings);
+
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
