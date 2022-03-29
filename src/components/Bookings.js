9689b6ab79b5e77b06e8d3ccfc47a3706e1d2c36
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-03-29 11:59:18 +0100

    Lesson 2: 14. Passing bookings from a state variable

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./search/Search";
 import SearchResults from "./search/SearchResults";
 import FakeBookings from "../data/fakeBookings.json";
 
 const Bookings = () => {
+  const [bookings, setBookings] = useState(FakeBookings);
+
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
@@ -12,7 +14,7 @@ const Bookings = () => {
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults bookings={FakeBookings} />
+        <SearchResults bookings={bookings} />
       </div>
     </div>
   );
