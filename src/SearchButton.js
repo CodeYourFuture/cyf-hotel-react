7c1676d23f449d7d14bfe21079858a9cca61c918
KatarzynaRuksza <91133235+KatarzynaRuksza@users.noreply.github.com>
2022-03-31 22:11:57 +0100

    Create SearchButton.js

---
+++ src/SearchButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchButton = () => {
+  return (
+    <button className="btn btn-primary btn-outline-light bg-info">
+      Search
+    </button>
+  );
+};
+
+export default SearchButton;
