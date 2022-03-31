ac40dc4f8ed8b225bbcca6d16debd7c84bd05582
KatarzynaRuksza <91133235+KatarzynaRuksza@users.noreply.github.com>
2022-03-31 22:12:28 +0100

    Create SearchResults.js

---
+++ src/SearchResults.js
@@ -0,0 +1,53 @@
+import moment from "moment";
+import React from "react";
+import "./SearchResults.css";
+
+const SearchResults = (props) => {
+  return (
+    <table className="table bg-light table-hover ">
+      <thead className="thead-dark">
+        <tr>
+          <th>Id</th>
+          <th>Title</th>
+          <th>First Name</th>
+          <th>Surname</th>
+          <th>Email</th>
+          <th>Room Id</th>
+          <th>Check in Date</th>
+          <th>Check out Date</th>
+          <th>Nights In</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.results.map((result) => {
+          let {
+            id,
+            title,
+            firstName,
+            surname,
+            email,
+            roomId,
+            checkInDate,
+            checkOutDate,
+          } = result;
+
+          return (
+            <tr>
+              <td>{id}</td>
+              <td>{title}</td>
+              <td>{firstName}</td>
+              <td>{surname}</td>
+              <td>{email}</td>
+              <td>{roomId}</td>
+              <td>{checkInDate}</td>
+              <td>{checkOutDate}</td>
+              <td>{moment(checkOutDate).diff(moment(checkInDate), "days")}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default SearchResults;
