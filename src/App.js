54f912615790360ee9e2ddd7b7b8e1e7a059e92d
mairacagri <mairacagri@gmail.com>
2022-03-31 12:32:14 +0100

    lesson 1:6

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Header from "./Header";
 import TouristInfoCards from "./TouristInfoCards";
 import Bookings from "./Bookings";
 import Footer from "./Footer";
+import BookingData from "./data/fakeBookings.json";
 
 import "./App.css";
+import SearchResults from "./SearchResults";
 
 const App = () => {
   return (
@@ -14,6 +16,7 @@ const App = () => {
 
       <Bookings />
       <TouristInfoCards />
+      <SearchResults bookings={BookingData} />
       <Footer
         info={[
           "123 Fake Street, London, E1 4UD",
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,22 +1,67 @@
 import React from "react";
-import moment from "moment";
+
+let headers = [
+  "Id",
+  "Title",
+  "First Name",
+  "Last Name",
+  "Email",
+  "Room Id",
+  "Check In Date",
+  "Check Out Date",
+  "# Nights"
+];
 
 const SearchResults = props => {
   return (
     <table class="table">
       <thead>
         <tr>
-          <th scope="col">ID</th>
+          {/* <th scope="col">ID</th>
           <th scope="col">Title</th>
           <th scope="col">First Name</th>
           <th scope="col">Email</th>
           <th scope="col">Room Id</th>
           <th scope="col">Check In</th>
           <th scope="col">Check Out</th>
-          <th scope="col">Nights</th>
+          <th scope="col">Nights</th> */}
+          {headers.map(header => {
+            return <th scope="col"> {header}</th>;
+          })}
         </tr>
       </thead>
-      <tbody />
+      <tbody>
+        {props.bookings.map(
+          (
+            {
+              id,
+              title,
+              firstName,
+              lastName,
+              email,
+              roomId,
+              checkIn,
+              checkOut,
+              nights
+            },
+            index
+          ) => {
+            return (
+              <tr key={index}>
+                <td>{id}</td>
+                <td>{title}</td>
+                <td>{firstName}</td>
+                <td>{lastName}</td>
+                <td>{email}</td>
+                <td>{roomId}</td>
+                <td>{checkIn}</td>
+                <td>{checkOut}</td>
+                <td>{nights}</td>
+              </tr>
+            );
+          }
+        )}
+      </tbody>
     </table>
   );
 };
