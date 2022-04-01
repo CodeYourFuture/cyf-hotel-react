43bf329c7bb73ce8fca5477060e58d64289b3fb0
Hatice06 <haticeaydogan00@gmail.com>
2022-04-01 19:31:00 +0100

    Lesson 2 works

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -9,7 +9,9 @@ const TouristInfoCards = () => {
           "https://www.whatsonnetwork.co.uk/uploads/800x600/251d1ca49b01dd9048ef9967e46bf339.jpg",
         alt: "Glasgow scene"
       },
-      link: "https://peoplemakeglasgow.com"
+      link: "https://peoplemakeglasgow.com",
+      info:
+        " Glasgow is Scotland’s largest city and is easily accessible from the rest of the UK and overseas by train, sea, road and air.You could easily fill your time exploring the different neighbourhoods, and enjoying the amazing shopping, dining and attractions in Glasgow."
     },
     {
       name: "Manchester",
@@ -18,7 +20,9 @@ const TouristInfoCards = () => {
           "https://static.independent.co.uk/2021/11/03/15/ManchesterRecoveryCampaign-32.jpg?quality=75&width=982&height=726&auto=webp",
         alt: "Manchester scene"
       },
-      link: "https://visitmanchester.com"
+      link: "https://visitmanchester.com",
+      info:
+        "Youthful, diverse, energetic and bursting with character; Manchester is one of the most exciting places to visit in the UK right now where everybody and anybody is very warmly welcomed."
     },
 
     {
@@ -29,7 +33,9 @@ const TouristInfoCards = () => {
 
         alt: "London scene"
       },
-      link: "https://visitlondon.com"
+      link: "https://visitlondon.com",
+      info:
+        "Welcome to London! Discover the best of London with Visit London, the official guide to England’s exciting capital. Find things to do in London, from iconic sightseeing spots and fun-filled days out to top restaurants, theatre and unmissable London events."
     }
   ];
   return (
@@ -39,8 +45,10 @@ const TouristInfoCards = () => {
           <div className="card">
             <img src={place.img.src} className="card-img-top" />
             <div className="card-body">
+              <h2>{place.name}</h2>
+              <p>{place.info}</p>
               <a href={place.link} className="btn btn-primary">
-                Go {place.name}
+                Go {place.name}{" "}
               </a>
             </div>
           </div>
