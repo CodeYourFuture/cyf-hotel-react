c7b10d138cd6843c2bc0232322e8d65c134efeec
Jude <lovespictures2@gmail.com>
2022-04-04 10:56:10 +0100

    Update CSS

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -8,13 +8,13 @@ const Heading = () => {
         <i className="fa fa-h-square" aria-hidden="true" alt="Hotel Logo"></i>
         <h1> CYF Hotel</h1>
 
-        {/* <img
-          src="https://res.cloudinary.com/rebelwalls/image/upload/b_black,c_fit,f_auto,fl_progressive,q_auto,w_1333/v1428564556/article/R10622_image1"
+        <img
+          className="Header-background-image"
+          src="https://i.pinimg.com/originals/c8/fa/45/c8fa45c7cf32e04c991b390908efcb2e.jpg"
+          // src="http://yarori.sideka.id/wp-content/uploads/sites/6234/2019/03/header-image-1-2.png"
           width="100%"
-          height="10%"
-          alt="Hotel Logo"
-          className="App-logo"
-        /> */}
+          alt="City scene"
+        />
       </header>
     </div>
   );
