e47eb7457a1a5c98816aa8b80d87059a83a7e689
Hatice06 <haticeaydogan00@gmail.com>
2022-04-04 23:05:21 +0100

    exercise 16-17-18 works

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import fakeBookings from "./data/fakeBookings.json";
+//import fakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(fakeBookings);
+  const [bookings, setBookings] = useState([]);
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me`)
+      .then(res => res.json())
+      .then(data => {
+        setBookings(data);
+      });
+  }, []);
 
   return (
     <div className="App-content">
