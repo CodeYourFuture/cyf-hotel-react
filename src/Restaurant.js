cc42013f6b9b0d1242c5509f59d1d695bfb95e73
bita2019 <58438858+bita2019@users.noreply.github.com>
2022-04-05 21:20:45 +0100

    3

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { eseState } from "react";
 
-const Restaurant = () => {
-  const pizzas = 0;
+const Restaurant = props => {
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
-        <li>
-          Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
-        </li>
+        {/* Pizzas: {pizzas} <button className="btn btn-primary">Add</button> */}
+        <Order orderType={"Pizzas"} />
       </ul>
     </div>
   );
+};
+
+const Order = ({ orderType }) => {
+  const [orders, setOrders] = useState(0);
+
+  function orderOne() {
+    setOrders(currentCount => currentCount + 1);
+  }
+  return (
+    <li>
+      {orderType}:{orders} <Restaurant handleClick={orderOne} />
+    </li>
+  );
+};
+
+const RestaurantButton = props => {
+  return <button onClick={(props.handleClick = "btn btn-primary")}>Add</button>;
 };
 
 export default Restaurant;
