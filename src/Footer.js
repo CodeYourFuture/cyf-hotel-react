bdf208d4ed9f2c3015a8ca3bbc47bf89b65f51ea
KatarzynaRuksza <91133235+KatarzynaRuksza@users.noreply.github.com>
2022-04-06 21:13:10 +0100

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = (props) => {
   console.log(props);
   return (
-    <div>
+    <div id="footer" className="bg bg-info">
       <p className="text-info mt-5 ml-4">How to Find Us: </p>
       <ul>
         {props.details.map((detail, index) => (
