d9e0bb84c9519c7b32a3c5af80841c9514da93b5
KatarzynaRuksza <91133235+KatarzynaRuksza@users.noreply.github.com>
2022-04-06 21:22:32 +0100

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ const Footer = (props) => {
   console.log(props);
   return (
     <div id="footer" className="bg bg-info">
-      <p className="text-info mt-5 ml-4">How to Find Us: </p>
+      <p className="mt-5 ml-4">How to Find Us: </p>
       <ul>
         {props.details.map((detail, index) => (
           <li key={index}>{detail}</li>
