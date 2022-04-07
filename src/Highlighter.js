02380d31a96fa5b9c73579ea672a2a65c4db4cda
KatarzynaRuksza <91133235+KatarzynaRuksza@users.noreply.github.com>
2022-04-07 21:20:54 +0100

    Update Highlighter.js

--- src/Highlighter.js
+++ src/Highlighter.js
@@ -1,24 +1,64 @@
 import React, { useState } from "react";
+import "./SearchResults.css";
+import Highlighter from "./Highlighter";
+import CustomerProfile from "./CustomerProfile";
 
-const Highlighter = (props) => {
-  const [clicked, setClicked] = useState(false);
+const SearchResults = (props) => {
+  const [customerId, setCustomerId] = useState();
 
-  const highlight = () => {
-    setClicked(!clicked);
+  const changeCustomerId = (id) => {
+    setCustomerId(id);
   };
 
   return (
-    <tr onClick={highlight} className={clicked ? "table-info" : ""}>
-      <td>{props.id}</td>
-      <td>{props.title}</td>
-      <td>{props.firstName}</td>
-      <td>{props.surname}</td>
-      <td>{props.email}</td>
-      <td>{props.roomId}</td>
-      <td>{props.checkInDate}</td>
-      <td>{props.checkOutDate}</td>
-    </tr>
+    <div>
+      <table className="table bg-light table-hover ">
+        <thead className="thead-dark">
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>First Name</th>
+            <th>Surname</th>
+            <th>Email</th>
+            <th>Room Id</th>
+            <th>Check in Date</th>
+            <th>Check out Date</th>
+            <th>Show Profile</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.results.map((result) => {
+            let {
+              id,
+              title,
+              firstName,
+              surname,
+              email,
+              roomId,
+              checkInDate,
+              checkOutDate,
+            } = result;
+
+            return (
+              <Highlighter
+                id={id}
+                title={title}
+                firstName={firstName}
+                surname={surname}
+                email={email}
+                roomId={roomId}
+                checkInDate={checkInDate}
+                checkOutDate={checkOutDate}
+                changeId={changeCustomerId}
+                clickId={id}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+      <CustomerProfile id={customerId} />
+    </div>
   );
 };
 
-export default Highlighter;
+export default SearchResults;
