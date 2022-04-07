40a355471de92d48f16946459ab746e1e12f0901
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-04-07 18:58:40 +0100

    update Customer Profile

--- src/components/search/CustomerProfile.js
+++ src/components/search/CustomerProfile.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function CustomerProfile({ id }) {
-  return <>`Customer ${id} Profile`;</>;
+  return <>Customer {id} Profile</>;
 }
 
 export default CustomerProfile;
