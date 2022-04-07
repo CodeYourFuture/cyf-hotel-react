6fc0c6d71381a5569b51c28329037bfa698fd7de
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-04-07 11:27:06 +0100

    lesson 3 : 16
    16. Load bookings remotely

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./search/Search";
 import SearchResults from "./search/SearchResults";
-import FakeBookings from "../data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then(res => res.json())
+      .then(data => setBookings(data));
+  }, []); // only run once after the first render (please)
 
   const search = searchVal => {
     console.info("TO DO!", searchVal);
-    setBookings("TO DO!", searchVal);
+    // setBookings("TO DO!", searchVal);
   };
 
   return (
