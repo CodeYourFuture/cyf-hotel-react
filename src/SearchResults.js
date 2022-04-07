a70adee6bcfe39c6f5f3fbc4d2ead3033f314275
KatarzynaRuksza <91133235+KatarzynaRuksza@users.noreply.github.com>
2022-04-07 21:21:18 +0100

    Update SearchResults.js

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,52 +1,64 @@
 
-import React from "react";
+import React, { useState } from "react";
 import "./SearchResults.css";
 import Highlighter from "./Highlighter";
+import CustomerProfile from "./CustomerProfile";
 
 const SearchResults = (props) => {
-  return (
-    <table className="table bg-light table-hover ">
-      <thead className="thead-dark">
-        <tr>
-          <th>Id</th>
-          <th>Title</th>
-          <th>First Name</th>
-          <th>Surname</th>
-          <th>Email</th>
-          <th>Room Id</th>
-          <th>Check in Date</th>
-          <th>Check out Date</th>
-         
-        </tr>
-      </thead>
-      <tbody>
-        {props.results.map((result) => {
-          let {
-            id,
-            title,
-            firstName,
-            surname,
-            email,
-            roomId,
-            checkInDate,
-            checkOutDate,
-          } = result;
+  const [customerId, setCustomerId] = useState();
 
-        return (
-            <Highlighter
-              id={id}
-              title={title}
-              firstName={firstName}
-              surname={surname}
-              email={email}
-              roomId={roomId}
-              checkInDate={checkInDate}
-              checkOutDate={checkOutDate}
-            />
-          );
-        })}
-      </tbody>
-    </table>
+  const changeCustomerId = (id) => {
+    setCustomerId(id);
+  };
+
+  return (
+    <div>
+      <table className="table bg-light table-hover ">
+        <thead className="thead-dark">
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>First Name</th>
+            <th>Surname</th>
+            <th>Email</th>
+            <th>Room Id</th>
+            <th>Check in Date</th>
+            <th>Check out Date</th>
+            <th>Show Profile</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.results.map((result) => {
+            let {
+              id,
+              title,
+              firstName,
+              surname,
+              email,
+              roomId,
+              checkInDate,
+              checkOutDate,
+            } = result;
+
+            return (
+              <Highlighter
+                id={id}
+                title={title}
+                firstName={firstName}
+                surname={surname}
+                email={email}
+                roomId={roomId}
+                checkInDate={checkInDate}
+                checkOutDate={checkOutDate}
+                changeId={changeCustomerId}
+                clickId={id}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+      <CustomerProfile id={customerId} />
+    </div>
   );
 };
 
