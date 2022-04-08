6380922b0ac821afa3006cfdb15143762b91b20a
bita2019 <58438858+bita2019@users.noreply.github.com>
2022-04-08 22:20:12 +0100

    19

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults";
 // import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+
   const search = searchVal => {
     console.info("TO DO!", searchVal);
+    const filteredBookings = bookings.filter(({ firstName, surename }) => {
+      if (firstName === searchVal || surename === searchVal) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    setBookings(filteredBookings);
   };
+
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then(res => res.json())
+      .then(data => setBookings(data));
+  }, []); //only run once, after the first render .
 
   return (
     <div className="App-content">
