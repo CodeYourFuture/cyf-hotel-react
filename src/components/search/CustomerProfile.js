d54ac93a02d6927933842abfeaea3ea03e677624
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-04-08 11:11:21 +0100

    lesson 3: 21
    21. Display a customer profile - step 2

--- src/components/search/CustomerProfile.js
+++ src/components/search/CustomerProfile.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function CustomerProfile({ id }) {
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me/customers/${id}`)
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }, [id]);
+
   return <>Customer {id} Profile</>;
 }
 
