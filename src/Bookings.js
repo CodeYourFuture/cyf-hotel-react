12843d6ae14cbe4e7ff2782da0f3263b33d3e8fe
Harsheek Thanki <harsheekthanki@gmail.com>
2022-04-10 21:42:28 +0100

    Lesson 3 exercise 16 complete

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import fakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(fakeBookings);
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then(res => res.json())
+      .then(data => setBookings(data));
+  }, []);
 
   const search = searchVal => {
     console.info("TO DO!", searchVal);
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,5 +1,4 @@
 import React from "react";
-import moment from "moment";
 import SearchResultRows from "./SearchResultRows";
 
 const headings = [
@@ -31,25 +30,6 @@ function SearchResults(props) {
       </tbody>
     </table>
   );
-
-  // (
-  //   <tr>
-  //     <th scope="row">{booking.id}</th>
-  //     {headings.map((heading) => {
-  //       if (heading === "id") {
-  //         return null;
-  //       } else if (heading === "numberOfNights") {
-  //         const checkIn = moment(booking.checkInDate);
-  //         const checkOut = moment(booking.checkOutDate);
-  //         const difference = checkOut.diff(checkIn, "days");
-  //         return <td>{difference}</td>;
-  //       } else {
-  //         return <td>{booking[heading]}</td>;
-  //       }
-  //     })}
-  //   </tr>
-  // );
-  // })}
 }
 
 export default SearchResults;
