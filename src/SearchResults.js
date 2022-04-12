825e9f1d34bd34a0cce82cd8ee64f54c1bbca0c4
hachi-ops <elen20@protonmail.com>
2022-04-12 22:21:58 +0100

    update

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,37 +1,22 @@
-import React from "react";
-import FakeBookings from "./data/fakeBookings.json";
+import React, { useState } from "react";
+// import ourBookings from "./data/fakeBookings.json";
 import moment from "moment";
 
-function SearchResults() {
-  const headings = [
-    `id`,
-    `title`,
-    `first name`,
-    `surname`,
-    `email`,
-    `room id`,
-    `check in date`,
-    `check out date`,
-    `number of nights`
-  ];
-  const DisplayData = FakeBookings.map(info => {
-    return (
-      <tr>
-        <th scope="row">
-          <td>{info.id}</td>
-        </th>
-        <td>{info.title}</td>
-        <td>{info.firstName}</td>
-        <td>{info.surname}</td>
-        <td>{info.email}</td>
-        <td>{info.roomId}</td>
-        <td>{info.checkInDate}</td>
-        <td>{info.checkOutDate}</td>
-        <td>
-          {moment(info.checkOutDate).diff(moment(info.checkInDate), "day")}
-        </td>
-      </tr>
-    );
+const headings = [
+  `id`,
+  `title`,
+  `first name`,
+  `surname`,
+  `email`,
+  `room id`,
+  `check in date`,
+  `check out date`,
+  `number of nights`
+];
+
+function SearchResults(props) {
+  const DisplayData = props.bookings.map(booking => {
+    return <OurCustomRow booking={booking} />;
   });
 
   return (
@@ -47,6 +32,31 @@ function SearchResults() {
         <tbody>{DisplayData}</tbody>
       </table>
     </div>
+  );
+}
+
+function OurCustomRow({ booking }) {
+  const [isClicked, setIsClicked] = useState(false);
+  return (
+    <tr
+      onClick={() => {
+        setIsClicked(!isClicked);
+      }}
+    >
+      <th scope="row">
+        <td>{booking.id}</td>
+      </th>
+      <td>{booking.title}</td>
+      <td>{booking.firstName}</td>
+      <td>{booking.surname}</td>
+      <td>{booking.email}</td>
+      <td>{booking.roomId}</td>
+      <td>{booking.checkInDate}</td>
+      <td>{booking.checkOutDate}</td>
+      <td>
+        {moment(booking.checkOutDate).diff(moment(booking.checkInDate), "day")}
+      </td>
+    </tr>
   );
 }
 
