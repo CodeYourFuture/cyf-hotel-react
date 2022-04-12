c6dd03f436d004fe8af6eab3aef2c284d9492fda
Alamin-eng <alaminruben@gmail.com>
2022-04-13 00:54:01 +0100

    auto fetch for ID fixed

--- src/tableRow.js
+++ src/tableRow.js
@@ -1,34 +1,16 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import CustomerProfile from "./customerProfile";
+import { normalizeUnits } from "moment";
 
 const TableRow = ({ e }) => {
   // above {e}, instead of writing props, we are just defining the variable name directlty using array destructuring
 
-  // use States
-  const [id, setId] = useState(null); // only way i can show no profile info is this set to null , But i dont get datas back from fetching if don't set to array []
-  const [email, setEmail] = useState(null);
-  const [vip, setVip] = useState("");
-  const [phone, setPhone] = useState(null);
+  const [boolean, setBoolean] = useState(false);
 
-  // use effect
-  useEffect(() => {
-    fetch(`https://cyf-react.glitch.me/customers/${id}`)
-      .then(res => res.json())
-      .then(data => {
-        data.vip ? setVip("true") : setVip("false");
-        data.phoneNumber
-          ? setPhone(data.phoneNumber)
-          : setPhone("No Phone number found.");
-        console.log(data);
-        return data;
-      });
-  }, [id]);
-
+  // handle button click
   let handleBtnClick = () => {
-    setId(e.id);
-    setEmail(e.email);
-    setVip(vip);
+    setBoolean(!boolean);
   };
 
   const [bgColor, changeColor] = useState("bg-info");
@@ -39,10 +21,11 @@ const TableRow = ({ e }) => {
       changeColor("bg-info");
     }
   };
-
+  // diff method used to calculate number of days stayed
   let inDate = moment(e.checkInDate);
   let outDate = moment(e.checkOutDate);
   const difference = outDate.diff(inDate, "days"); // takes 2 arguments, last one is for if we want output as days or hour or months
+
   return (
     <tr className={bgColor} id="row-text" key={e.id} onClick={switchColors}>
       <td>{e.id}</td>
@@ -54,20 +37,12 @@ const TableRow = ({ e }) => {
       <td>{e.checkInDate}</td>
       <td>{e.checkOutDate}</td>
       <td>{difference}</td>
-      <button className="btn btn-dark text mr-1" onClick={handleBtnClick}>
-        Show profile
-      </button>
-      <tr>
-        {id ? (
-          <CustomerProfile
-            id={id}
-            email={email}
-            vip={vip}
-            phone={phone}
-            handleBtnClick={handleBtnClick}
-          />
-        ) : null}
-      </tr>
+      <td>
+        <button className="btn btn-dark text mr-1" onClick={handleBtnClick}>
+          Show profile
+        </button>
+        {boolean && <CustomerProfile id={e.id} />}
+      </td>
     </tr>
   );
 };
