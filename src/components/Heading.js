8fbfc60207f1dda3dbf0d241492519eee0d9229f
Jude <lovespictures2@gmail.com>
2022-04-14 13:06:18 +0100

    Adjustments to header

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,24 +1,22 @@
 import React from "react";
-// import logo from "./img/Background-1";
+// import Image from "../Header.jpg";
 
 const Heading = () => {
   return (
     <>
-      <div>
-        <i className="fa fa-h-square" aria-hidden="true" alt="Hotel Logo"></i>
-        <h1> CYF Hotel</h1>
-      </div>
-      <div>
-        <header className="App-header">
+      <header>
+        <div className="Background-image">
+          <i className="fa fa-h-square" aria-hidden="true" alt="Hotel Logo"></i>
+          <h1> CYF Hotel</h1>
+          {/* <createImageBitmap source={require("./Header.JPG")} /> */}
           <img
-            className="Header-background-image"
             src="https://i.pinimg.com/originals/c8/fa/45/c8fa45c7cf32e04c991b390908efcb2e.jpg"
             // src="http://yarori.sideka.id/wp-content/uploads/sites/6234/2019/03/header-image-1-2.png"
             width="100%"
             alt="City scene"
           />
-        </header>
-      </div>
+        </div>
+      </header>
     </>
   );
 };
--- src/components/TouristInfoCards.js
+++ src/components/TouristInfoCards.js
@@ -7,8 +7,9 @@ const TouristInfoCards = () => {
     // wrapper for the cards
     <div className="Tourist-Cards">
       {/* card 1 */}
-      <div className="card">
+      <div className="card ">
         <img
+          className="card-img"
           src="https://media.istockphoto.com/photos/london-the-uk-red-buses-and-big-ben-the-palace-of-westminster-black-picture-id693802176?k=20&m=693802176&s=612x612&w=0&h=oe-2kyLVVbdNMlMaJ4YxmeXs2AD8mGJ1wLnGTJ0RdJ4="
           alt="London"
         />
@@ -30,8 +31,9 @@ const TouristInfoCards = () => {
       </div>
 
       {/* card 2 */}
-      <div className="card">
+      <div className="card ">
         <img
+          className="card-img"
           src="https://images.fineartamerica.com/images-medium-large-5/merchant-city-glasgow-john-farnan.jpg"
           alt="London"
           //   Merchant City Glasgow Photograph by John Farnan  https://pixels.com/featured/merchant-city-glasgow-john-farnan.html
@@ -54,8 +56,9 @@ const TouristInfoCards = () => {
       </div>
 
       {/* card 3 */}
-      <div className="card">
+      <div className="card ">
         <img
+          className="card-img"
           src="https://images.squarespace-cdn.com/content/v1/5b5f6cc4cef372b2c114e5fd/1547560777806-7XKBZI733HT9LY6NVGLJ/Perry+M.+Anderson+-+Manchester+.jpg"
           alt="London"
           // Manchester, UK  by Perry M Anderson https://www.perryanderson.global/event/learn-to-acquire-a-business-without-using-any-of-your-own-capital-manchester-september
