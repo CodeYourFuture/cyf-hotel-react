7774037303979c13a610ef5bfe6d62509f350a5c
DeagoBrowne <89912823+DeagoBrowne@users.noreply.github.com>
2022-04-19 16:14:08 +0100

    extracted table rows, placed them inside seperate component, mapped back into table with highlight functionality

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,8 +1,7 @@
 import moment from "moment";
-import React from "react";
+import React, { useState } from "react";
 
 const SearchResults = ({ results }) => {
-  console.log(results);
   return (
     <div className="table">
       <table>
@@ -17,28 +16,41 @@ const SearchResults = ({ results }) => {
           <th>Check-out Date</th>
           <th># of Nights</th>
         </tr>
+
+        {/* Data to be passed into the table */}
         {results.map((info, key) => {
-          let a = moment(info.checkInDate);
-          console.log(a);
-          let b = moment(info.checkOutDate);
-          console.log(b);
-          return (
-            <tr key={key}>
-              <td>{info.id}</td>
-              <td>{info.title}</td>
-              <td>{info.firstName}</td>
-              <td>{info.surname}</td>
-              <td>{info.email}</td>
-              <td>{info.roomId}</td>
-              <td>{info.checkInDate}</td>
-              <td>{info.checkOutDate}</td>
-              <td>{b.diff(a, "days")}</td>
-            </tr>
-          );
+          return <CustomRow info={info} key={key} />;
         })}
       </table>
     </div>
   );
 };
 
+// Component used to render table rows
+function CustomRow({ info, key }) {
+  const [highlight, setHighlight] = useState(false);
+
+  // Variables declared to log number of nights booked
+  let a = moment(info.checkInDate);
+  let b = moment(info.checkOutDate);
+
+  return (
+    // Adding table data
+    <tr
+      key={key}
+      style={highlight ? { border: "2px solid green" } : {}}
+      onClick={() => setHighlight(!highlight)}
+    >
+      <td>{info.id}</td>
+      <td>{info.title}</td>
+      <td>{info.firstName}</td>
+      <td>{info.surname}</td>
+      <td>{info.email}</td>
+      <td>{info.roomId}</td>
+      <td>{info.checkInDate}</td>
+      <td>{info.checkOutDate}</td>
+      <td>{b.diff(a, "days")}</td>
+    </tr>
+  );
+}
 export default SearchResults;
