e234bfaf7fedd4391286bd605a66f9df24f93218
Davood Khoshnood <45537769+DavoodKhoshnood@users.noreply.github.com>
2022-04-19 19:10:44 +0100

    Sort

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -17,9 +17,9 @@ const SearchResults = (props) => {
   }
 
   const sortedBookings = props.list.sort((a, z) => {
-    return sortAscending
-      ? a[{ colSort }] - z[{ colSort }]
-      : z[{ colSort }] - a[{ colSort }]
+    let col = { colSort }.colSort
+    console.log(col)
+    return sortAscending ? (a[col] > z[col] ? 1 : -1) : z[col] > a[col] ? 1 : -1
   })
   console.log(sortedBookings)
   return (
@@ -27,14 +27,54 @@ const SearchResults = (props) => {
       <table className="table table-sm">
         <thead className="table-header">
           <tr>
-            <th onClick={() => columnSort('id')}>ID</th>
-            <th onClick={() => columnSort('title')}>Title</th>
-            <th onClick={() => columnSort('firstName')}>First Name</th>
-            <th onClick={() => columnSort('surname')}>Surname</th>
-            <th onClick={() => columnSort('email')}>Email</th>
-            <th onClick={() => columnSort('roomId')}>Room ID</th>
-            <th onClick={() => columnSort('checkInDate')}>CheckIn Date</th>
-            <th onClick={() => columnSort('checkOutDate')}>CheckOut Date</th>
+            <th
+              className={colSort === 'id' && 'sorted'}
+              onClick={() => columnSort('id')}
+            >
+              ID
+            </th>
+            <th
+              className={colSort === 'title' && 'sorted'}
+              onClick={() => columnSort('title')}
+            >
+              Title
+            </th>
+            <th
+              className={colSort === 'firstName' && 'sorted'}
+              onClick={() => columnSort('firstName')}
+            >
+              First Name
+            </th>
+            <th
+              className={colSort === 'surname' && 'sorted'}
+              onClick={() => columnSort('surname')}
+            >
+              Surname
+            </th>
+            <th
+              className={colSort === 'email' && 'sorted'}
+              onClick={() => columnSort('email')}
+            >
+              Email
+            </th>
+            <th
+              className={colSort === 'roomId' && 'sorted'}
+              onClick={() => columnSort('roomId')}
+            >
+              Room ID
+            </th>
+            <th
+              className={colSort === 'checkInDate' && 'sorted'}
+              onClick={() => columnSort('checkInDate')}
+            >
+              CheckIn Date
+            </th>
+            <th
+              className={colSort === 'checkOutDate' && 'sorted'}
+              onClick={() => columnSort('checkOutDate')}
+            >
+              CheckOut Date
+            </th>
             <th>Number of Nights</th>
             <th>Profile</th>
           </tr>
