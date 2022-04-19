dd0213ef0db550665c68924a0901687dcda08003
DeagoBrowne <89912823+DeagoBrowne@users.noreply.github.com>
2022-04-19 13:28:05 +0100

    successfully passed booking from state variable

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -15,6 +15,12 @@ const Restaurant = () => {
     </div>
   );
 
+  /* Disclaimer: housing the extra components within the restaurant component
+  was a conscious choice. Whilst I appreciate that CYF is teaching Trainees to
+  handle components from separate files (as I have done already), in this instance,
+  I feel that it is better for me to be able to keep track of everything on one
+  page. */
+
   // Pizza Order Component
   function Order(props) {
     const [order, setOrder] = useState(0);
