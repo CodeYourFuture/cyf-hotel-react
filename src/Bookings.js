244f8ac51379fa8b3774386b4b4bcfcefdf58a6b
Monika39-maker <dangolmonika@yahoo.com>
2022-04-20 20:47:48 +0100

    correction for netlify

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./searchResults.js";
-import BookingForm from "./bookingForm.js";
+// import BookingForm from "./bookingForm.js";
 
 const Bookings = () => {
   let [bookingData, setBookingData] = useState([]);
