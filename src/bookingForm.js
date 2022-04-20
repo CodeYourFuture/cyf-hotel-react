7b26ab6e065601fcb2eba6e538e1aba1a12de454
Monika39-maker <dangolmonika@yahoo.com>
2022-04-20 21:00:16 +0100

    manually added the files

---
+++ src/bookingForm.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+
+function BookingForm({ addUser }) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [roomId, setRoomId] = useState("");
+  const [title, setTitle] = useState("");
+  const [checkInDate, setCheckInDate] = useState("");
+  const [checkOutDate, setCheckOutDate] = useState("");
+  const [newUsers, setNewUsers] = useState({});
+
+  const handleFirstName = event => {
+    event.preventDefault();
+    setFirstName(event.target.value);
+  };
+  const handleLastName = event => {
+    event.preventDefault();
+    setLastName(event.target.value);
+  };
+  const handleEmail = event => {
+    event.preventDefault();
+    setEmail(event.target.value);
+  };
+  const handleRoomId = event => {
+    event.preventDefault();
+    setRoomId(event.target.value);
+  };
+  const handleCheckInDate = event => {
+    event.preventDefault();
+    setCheckInDate(event.target.value);
+  };
+  const handleHandleCheckOutDate = event => {
+    event.preventDefault();
+    setCheckOutDate(event.target.value);
+  };
+  const handleTitle = event => {
+    setTitle(event.target.value);
+  };
+  function addNewUser() {
+    setNewUsers({
+      firstName: firstName,
+      surname: lastName,
+      email: email,
+      title: title,
+      roomId: roomId,
+      checkInDate: checkInDate,
+      checkOutDate: checkOutDate
+    });
+    addUser(newUsers);
+    console.log(newUsers);
+  }
+
+  return (
+    <form onSubmit={addNewUser} className="register-form">
+      <h3 className="tc">Registration Form</h3>
+      <input placeholder="enter your first name" onChange={handleFirstName} />
+      <br />
+      <input placeholder="enter your last name" onChange={handleLastName} />
+      <br />
+      <input placeholder="enter your title" onChange={handleTitle} />
+      <br />
+      <input placeholder="enter your email" onChange={handleEmail} />
+      <br />
+      <input placeholder="enter your room id" onChange={handleRoomId} />
+      <input
+        placeholder="enter your check in date"
+        onChange={handleCheckInDate}
+      />
+      <br />
+      <input placeholder="check out date" onChange={handleHandleCheckOutDate} />
+      <br />
+
+      <button>Submit</button>
+    </form>
+  );
+}
+
+export default BookingForm;
