364af1de21f4908a99156ba0e556e44b43e37b26
mohammed <mohammedalabdulhamid@gmail.com>
2022-04-20 01:22:05 +0100

    adding logo

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import logo from "./logo.jpg";
 
 function Header() {
-  return <header className="App-header">CYF Hotel</header>;
+  return (
+    <header className="App-header">
+      <p>CYF</p>
+      <img src={logo} width="75px" />
+    </header>
+  );
 }
 export default Header;
