777a08e424f01f9557e050cbb54a2e6dcbdd65ce
lalehkoupa <lalehkoupa@gmail.com>
2022-04-21 22:47:26 +0100

    up to Stretch goal completed

--- src/Components/Bookings.js
+++ src/Components/Bookings.js
@@ -5,14 +5,26 @@ import SearchResults from "./SearchResults.js";
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
   const [filterBookings, setFilterBookings] = useState([]);
+  const [isDataLoaded, setIsDataLoaded] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    fetch(`https://cyf-react.glitch.me/`)
-      .then(res => res.json())
+    //fetch(`https://cyf-react.glitch.me/`)
+    fetch(`https://cyf-react.glitch.me/delayed`)
+      //fetch(`https://cyf-react.glitch.me/error`)
+      .then(res => {
+        if (res.status === 500) {
+          throw new Error(res.status);
+        } else {
+          return res.json();
+        }
+      })
       .then(data => {
         setBookings(data);
         setFilterBookings(data);
-      });
+        setIsDataLoaded(false);
+      })
+      .catch(error => setError(true));
   }, []);
 
   const search = searchVal => {
@@ -32,7 +44,13 @@ const Bookings = () => {
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults bookings={filterBookings} />
+        {!isDataLoaded ? (
+          <SearchResults bookings={filterBookings} />
+        ) : !error ? (
+          <p>Loading..... Please Wait</p>
+        ) : (
+          <p>Error..... Content not found</p>
+        )}
       </div>
     </div>
   );
