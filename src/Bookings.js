2fc6d9bff60b7d3c0700160a818c64a4af504de5
Monika39-maker <dangolmonika@yahoo.com>
2022-04-22 21:23:24 +0100

    added the user registration form

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./searchResults.js";
-// import BookingForm from "./bookingForm.js";
 
 const Bookings = () => {
+  const [person, setPerson] = useState({
+    firstName: "",
+    surname: "",
+    email: "",
+    title: "",
+    roomId: "",
+    checkInDate: "",
+    checkOutDate: ""
+  });
   let [bookingData, setBookingData] = useState([]);
+
   let [searchVal, setSearchVal] = useState("");
   let [pending, setPending] = useState(true);
+
   const [error, setError] = useState(null);
   const search = searchVal => {
     setSearchVal(searchVal);
-    console.log(searchVal);
   };
-
-  // const addUser = user => {
-  //   setBookingData([...bookingData, user]);
-  //   console.log(bookingData);
-  // };
 
   useEffect(() => {
     fetch("https://cyf-react.glitch.me") //use url: https://cyf-react.glitch.me/error to check for error catching and 'https://cyf-react.glitch.me/delayed' for pending req
@@ -36,13 +40,98 @@ const Bookings = () => {
         setPending(false);
       });
   }, []);
+
+  const handleChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setPerson({ ...person, [name]: value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (person.firstName && person.surname) {
+      const newPerson = { ...person, id: bookingData.length + 1 };
+      setBookingData([...bookingData, newPerson]);
+      setPerson({
+        firstName: "",
+        surname: "",
+        email: "",
+        title: "",
+        roomId: "",
+        checkInDate: "",
+        checkOutDate: ""
+      });
+    }
+    console.log(person);
+    console.log(bookingData);
+  };
+
   return !pending ? (
     <div className="App-content">
       {error && <h3>{error}</h3>}
       <div className="container">
         <Search search={search} />
         <SearchResults FakeBookings={bookingData} searchVal={searchVal} />
-        {/* <BookingForm FakeBookings={bookingData} addUser={addUser}/> */}
+        <form className="register-form" onSubmit={handleSubmit}>
+          <h3 className="tc">Registration Form</h3>
+          <input
+            type="text"
+            placeholder="enter your first name"
+            name="firstName"
+            value={person.firstName}
+            onChange={handleChange}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="enter your last name"
+            name="surname"
+            value={person.surname}
+            onChange={handleChange}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="enter your title"
+            name="title"
+            value={person.title}
+            onChange={handleChange}
+          />
+          <br />
+          <input
+            type="email"
+            placeholder="enter your email"
+            name="email"
+            value={person.email}
+            onChange={handleChange}
+          />
+          <br />
+          <input
+            type="number"
+            placeholder="enter your room id"
+            name="roomId"
+            value={person.roomId}
+            onChange={handleChange}
+          />
+          <input
+            placeholder="enter your check in date"
+            onChange={handleChange}
+            name="checkInDate"
+            value={person.checkInDate}
+            type="date"
+          />
+          <br />
+          <input
+            type="date"
+            placeholder="check out date"
+            name="checkOutDate"
+            value={person.checkOutDate}
+            onChange={handleChange}
+          />
+          <br />
+
+          <button>Submit</button>
+        </form>
       </div>
     </div>
   ) : (
--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from "react";
 
 function CustomerProfile({ profile }) {
-  const [user, setUser] = useState("");
+  const [user, setUser] = useState({});
 
   useEffect(() => {
     fetch(`https://cyf-react.glitch.me/customers/${profile}`)
       .then(resp => resp.json())
-      .then(data => setUser(data));
+      .then(data => {
+        setUser(data);
+        console.log(user);
+      });
   }, [profile]);
 
   return profile ? (
     <div className="user-profile-box">
-      <p>ID: {profile}</p>
+      <p>ID: {user.id}</p>
       <p>Email: {user.email}</p>
       <p>Vip: {user.vip}</p>
       <p>Phone No: {user.phoneNumber}</p>
