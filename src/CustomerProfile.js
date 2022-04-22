64dba7938d655b3d85f790dd005b80433f73a332
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-04-22 21:47:25 +0100

    Display a customer profile - step 2

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const CustomerProfile = props => {
+  const [customerProfile, setCustomerProfile] = useState({});
+  const URL = "https://cyf-react.glitch.me/customers/";
+
+  useEffect(() => {
+    fetch(`${URL}${props.id}`)
+      .then(res => res.json())
+      .then(data => setCustomerProfile(data));
+  }, [props.id]);
+
   return (
     <div>
-      <h1>Showing Guest {props.id} profile</h1>
+      <h1>Showing Guest {customerProfile.id} profile</h1>
+      <ul className="list-group">
+        <li className="list-group-item">
+          <p>Guest Id:</p>
+          {customerProfile.id}
+        </li>
+        <li className="list-group-item">
+          <p>Email:</p>
+          {customerProfile.email}
+        </li>
+        <li className="list-group-item">
+          <p>Phone No:</p>
+          {customerProfile.phoneNumber}
+        </li>
+        <li className="list-group-item">
+          <p>VIP:</p>
+        </li>
+      </ul>
     </div>
   );
 };
