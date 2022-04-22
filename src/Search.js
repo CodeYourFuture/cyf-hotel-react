6ef2463fa439fc3263b7f1b77e056267535dc80e
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-04-22 12:28:02 +0100

    Trigger search function when submitting the form

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import SearchButton from "./SearchButton";
+// import Bookings from "./Bookings";
 
-const Search = () => {
+const Search = props => {
   const [searchInput, setSearchInput] = useState("");
 
   const handleSearchInput = event => {
     setSearchInput(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    props.search(searchInput);
   };
 
   return (
@@ -15,7 +21,7 @@ const Search = () => {
       </div>
       <div className="row search-wrapper">
         <div className="col">
-          <form className="form-group search-box">
+          <form className="form-group search-box" onSubmit={handleSubmit}>
             <label htmlFor="customerName">Customer name</label>
             <div className="search-row">
               <input
