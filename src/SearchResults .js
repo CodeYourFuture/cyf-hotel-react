e746a36a50f8b6c80108060b05009e7a02bea40f
Abd@94 <45979612+Abdalrazaq94@users.noreply.github.com>
2022-04-24 16:45:18 +0100

    stesp 6 and 7

--- src/SearchResults .js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function SearchResults(params) {
-  return <div />;
-}
-
-export default SearchResults;
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,6 +1,46 @@
+import moment from "moment";
 import React from "react";
-function SearchResults(params) {
-  return <div />;
+import Data from "./data/fakeBookings.json";
+
+function SearchResults() {
+  const DisplayData = Data.map(info => {
+    return (
+      <tr>
+        <td>{info.id}</td>
+        <td>{info.title}</td>
+        <td>{info.firstName}</td>
+        <td>{info.surname}</td>
+        <td>{info.email}</td>
+        <td>{info.roomId}</td>
+        <td>{info.checkInDate}</td>
+        <td>{info.checkOutDate}</td>
+        <td>
+          {moment(info.checkOutDate).diff(moment(info.checkInDate), "days")}
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Tittle</th>
+            <th>first name</th>
+            <th>surname</th>
+            <th>Email</th>
+            <th>Room id</th>
+            <th>check in date</th>
+            <th>check out id</th>
+            <th>night left</th>
+          </tr>
+        </thead>
+        <tbody>{DisplayData}</tbody>
+      </table>
+    </div>
+  );
 }
 
 export default SearchResults;
