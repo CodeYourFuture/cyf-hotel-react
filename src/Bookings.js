0e5b4eb2bc5e8f39eb0f0a3984119ae4d5802da4
DeagoBrowne <89912823+DeagoBrowne@users.noreply.github.com>
2022-04-26 13:29:45 +0100

    fecthing bookings from api, storing, and triggering search submission. commencing implenetation of search functionality

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
+// import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
+  useEffect(() => {
+    console.log("meow");
+
+    fetch("https://cyf-react.glitch.me")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        setBookings(data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
 
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
 
   return (
     <div className="App-content">
