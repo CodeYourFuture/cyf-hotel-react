262b1f6bf9b4c2c2aba8cc1cc6c8e774e97359dc
Ika Khoza <ikaneng.khoza@gmail.com>
2022-04-27 10:35:44 +0100

    Lesson 21 completed
    Lesson 21 completed

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -1,13 +1,25 @@
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const CustomerProfile = props => {
+  const [profile, setProfile] = useState([]);
+
   useEffect(() => {
     fetch(`https://cyf-react.glitch.me/customers/${props.id}`)
       .then(res => res.json())
-      .then(data => console.log(data));
+      .then(data => setProfile(data));
   }, [props.id]);
 
-  return `Customer ${props.id} Profile,`;
+  return (
+    <div className="customer-profile">
+      <ul>
+        <h2>Customer {props.id} Profile </h2>
+        <li>email: {profile.email}</li>
+        <li>number:{profile.phoneNumber}</li>
+        {profile.vip ? <li>VIP: {profile.vip.toString()}</li> : ""}
+        {profile.vip ? <li>{profile.phoneNumber}</li> : ""}
+      </ul>
+    </div>
+  );
 };
 
 export default CustomerProfile;
