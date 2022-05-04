d2429b70ff2315a34b335484b88e14c26e2114a1
teniolao <Tennybetty100@gmail.com>
2022-05-04 01:25:26 +0100

    tourist info-cards css

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -4,7 +4,7 @@ import Order from "./Order";
 const Restaurant = () => {
   return (
     <div className="restaurant">
-      <h3>Restaurant Orders</h3>
+      <h4>Restaurant Orders</h4>
       <ul>
         <Order orderType={"Pizzas"} />
         <Order orderType={"Salads"} />
--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -2,27 +2,10 @@ import React from "react";
 
 function TouristInfoCards() {
   return (
-    <div className="card-container">
+    <div className="card-container text-center">
       <div className="card text-center">
         <img
-          src="https://media.timeout.com/images/105400989/750/422/image.jpg"
-          alt=""
-          className="card-img-top"
-        />
-        <div className="card-body">
-          <p>
-            The city is also rich in artists and creators which makes for a
-            thriving and exciting independent shopping scene.
-          </p>
-          <a href="http://peoplemakeglasgow.com" className="btn btn-info">
-            Glasgow
-          </a>
-        </div>
-      </div>
-
-      <div className="card text-center">
-        <img
-          src="https://media.timeout.com/images/105798074/750/422/image.jpg"
+          src="https://www.itpr.co.uk/hubfs/Manchester-skyline-.jpg"
           alt=""
           className="card-img-top"
         />
@@ -52,6 +35,91 @@ function TouristInfoCards() {
           </p>
           <a href="http://visitlondon.com" className="btn btn-info">
             London
+          </a>
+        </div>
+      </div>
+
+      <div className="card text-center">
+        <div>
+          <img
+            src="https://cdn.getyourguide.com/img/location/5d0a6b6c59a42.jpeg/88.jpg
+          "
+            alt=""
+            className="card-img-top"
+          />
+        </div>
+
+        <div className="card-body">
+          <p>
+            Welcome to London! Visit London, from iconic sightseeing spots and
+            fun-filled days out to top restaurants, theatre and unmissable
+            London events.
+          </p>
+          <a
+            href="https://www.visitbritain.com/gb/en/england/south-west/bristol"
+            className="btn btn-info"
+          >
+            Bristol
+          </a>
+        </div>
+      </div>
+
+      <div className="card text-center">
+        <img
+          src="https://www.nottingham.ac.uk/Careers/Images-Multimedia/1-Students/International-students/WorkingUK-714x335.jpg"
+          alt=""
+          className="card-img-top"
+        />
+        <div className="card-body">
+          <p>
+            Welcome to London! Visit London, from iconic sightseeing spots and
+            fun-filled days out to top restaurants, theatre and unmissable
+            London events.
+          </p>
+          <a
+            href="https://www.visit-nottinghamshire.co.uk/"
+            className="btn btn-info"
+          >
+            Nottingham
+          </a>
+        </div>
+      </div>
+
+      <div className="card text-center">
+        <img
+          src="https://eus-www.sway-cdn.com/s/RXKsAGElo0fpKh08/images/n4zFwrC5o12bqN?quality=1920&allowAnimation=true"
+          alt=""
+          className="card-img-top"
+        />
+        <div className="card-body">
+          <p>
+            Welcome to London! Visit London, from iconic sightseeing spots and
+            fun-filled days out to top restaurants, theatre and unmissable
+            London events.
+          </p>
+          <a
+            href="https://visitbirmingham.com/inspire-me/explore-the-west-midlands/wolverhampton"
+            className="btn btn-info"
+          >
+            Wolverhampton
+          </a>
+        </div>
+      </div>
+
+      <div className="card text-center">
+        <img
+          src="https://a.cdn-hotels.com/gdcs/production131/d1170/e7199a50-080e-4dab-87d5-6d15ddada00d.jpg"
+          alt=""
+          className="card-img-top"
+        />
+        <div className="card-body">
+          <p>
+            Welcome to London! Visit London, from iconic sightseeing spots and
+            fun-filled days out to top restaurants, theatre and unmissable
+            London events.
+          </p>
+          <a href="https://visitbirmingham.com/" className="btn btn-info">
+            Birmingham
           </a>
         </div>
       </div>
