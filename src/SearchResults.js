be92bd5a496fe4b31fcebb53b29d8625e1ab1859
Abd@94 <45979612+Abdalrazaq94@users.noreply.github.com>
2022-05-08 22:17:38 +0100

    costmer profile

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,56 +1,164 @@
+import React, { useState } from "react";
 import moment from "moment";
-import React, { useState, useEffect } from "react";
-// import Data from "./data/fakeBookings.json";
+import CustomerProfile from "./components/CustomerProfile";
+import NewBooking from "./components/NewBooking";
 
-function SearchResults() {
-  const [fetchedData, setFetchedData] = useState([]);
-  useEffect(() => {
-    fetch("https://cyf-react.glitch.me")
-      .then(res => res.json())
-      .then(result => {
-        setFetchedData(result);
-      });
-    console.log(fetchedData);
-  }, []);
-  // const [bookings, setBokking] = useState(Data);
-  const DisplayData = fetchedData.map(info => {
-    return (
-      <tr>
-        <td>{info.id}</td>
-        <td>{info.title}</td>
-        <td>{info.firstName}</td>
-        <td>{info.surname}</td>
-        <td>{info.email}</td>
-        <td>{info.roomId}</td>
-        <td>{info.checkInDate}</td>
-        <td>{info.checkOutDate}</td>
-        <td>
-          {moment(info.checkOutDate).diff(moment(info.checkInDate), "days")}
-        </td>
-      </tr>
-    );
-  });
+const itemsTitle = [
+  <thead key={11} className="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Title</th>
+      <th scope="col">First Name</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Room Number</th>
+      <th scope="col">Check in</th>
+      <th scope="col">Check Out</th>
+      <th scope="col">Nights</th>
+      <th scope="col">Profile</th>
+    </tr>
+  </thead>
+];
+
+const SearchResults = props => {
+  const [id, setId] = useState(null);
+  const [selectedRow, setSelectedRow] = useState([]);
+  const { results, setBookings } = props;
+
+  function Highlighter(id) {
+    if (selectedRow.includes(id)) {
+      // console.log(id);
+      setSelectedRow(selectedRow.filter(el => el !== id));
+    } else {
+      setSelectedRow(selectedRow.concat(id));
+    }
+  }
 
   return (
-    <div>
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Tittle</th>
-            <th>first name</th>
-            <th>surname</th>
-            <th>Email</th>
-            <th>Room id</th>
-            <th>check in date</th>
-            <th>check out id</th>
-            <th>night left</th>
-          </tr>
-        </thead>
-        <tbody>{DisplayData}</tbody>
+    <div className="container">
+      <table className="table">
+        {itemsTitle}
+        {results.map(booking => {
+          // console.log(booking.id);
+          return (
+            <tbody key={booking.id}>
+              <tr
+                key={booking.id}
+                style={
+                  selectedRow.includes(booking.id)
+                    ? { backgroundColor: "#f1f2f6" }
+                    : { backgroundColor: "#fff" }
+                }
+                onClick={() => Highlighter(booking.id)}
+              >
+                <th scope="row">{booking.id}</th>
+                <td>{booking.title}</td>
+                <td>{booking.firstName}</td>
+                <td>{booking.surname}</td>
+                <td>{booking.email}</td>
+                <td>{booking.roomId}</td>
+                <td>{booking.checkInDate}</td>
+                <td>{booking.checkOutDate}</td>
+                <td>
+                  {moment(booking.checkOutDate).diff(
+                    moment(booking.checkInDate),
+                    "days"
+                  )}
+                </td>
+                <td>
+                  <button
+                    type="button"
+                    data-toggle="modal"
+                    data-target="#staticBackdrop"
+                    onClick={() => {
+                      setId(booking.id);
+                    }}
+                    className="btn btn-outline-success"
+                  >
+                    Show Profile
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          );
+        })}
       </table>
+      <div
+        className="modal fade"
+        id="staticBackdrop"
+        data-backdrop="static"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="staticBackdropLabel">
+                Customer Profile
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            {results ? <CustomerProfile key={id} id={id} /> : null}
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button
+        type="button"
+        data-toggle="modal"
+        data-target="#staticBackdrops"
+        className="btn btn-success"
+      >
+        {" "}
+        + Add Booking
+      </button>
+      <div
+        className="modal fade"
+        id="staticBackdrops"
+        data-backdrop="static"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog " role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title " id="staticBackdropLabel">
+                Add Booking
+              </h4>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <NewBooking setBookings={setBookings} results={results} />
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default SearchResults;
