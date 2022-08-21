cb21d329a99b887ff7fac3f5bc9a34a493ebc1a5
Baz Murphy <bazmurphy@gmail.com>
2022-08-21 10:29:56 +0100

    added loading and error handling

--- src/Bookings.js
+++ src/Bookings.js
@@ -6,14 +6,44 @@ import CustomerProfile from "./CustomerProfile.js";
 
 const Bookings = () => {
   const [bookings, setBookings] = React.useState([]);
+  // ^store the API data in an Array
 
   const [customerId, setCustomerId] = React.useState(null);
   // ^create state here so that SearchResultSingular button can use it... 2 levels deeper
 
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  const [errorOccurred, setErrorOccurred] = React.useState({
+    ok: true,
+    status: "",
+    statusText: ""
+  });
+
   React.useEffect(() => {
-    fetch(`https://cyf-react.glitch.me`)
-      .then(res => res.json())
-      .then(data => setBookings(data));
+    fetch(`https://cyf-react.glitch.me/delayed`)
+      .then(res => {
+        // console.log(res)
+        if (!res.ok) {
+          // console.log(res.status)
+          setErrorOccurred({
+            ok: res.ok,
+            status: res.status,
+            statusText: res.statusText
+          });
+          throw new Error(res);
+        } else {
+          return res.json();
+        }
+      })
+      .then(data => {
+        setBookings(data);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.log(error);
+        // something is lost here when the (res) is sent as the thrown error
+        // cannot access the ok/status/statusText key/values;
+      });
   }, []);
 
   const search = searchVal => {
@@ -33,7 +63,32 @@ const Bookings = () => {
   return (
     <div className="bookings-container">
       <Search search={search} />
-      <SearchResults results={bookings} setCustomerId={setCustomerId} />
+      {errorOccurred.ok === false ? (
+        <div className="search-results-error-container">
+          <img
+            className="search-results-error-image"
+            src="../images/error.png"
+            alt="loading icon"
+          />
+          <span className="search-results-error-text">
+            An Error occured when fetching the Bookings Data :{" "}
+            {errorOccurred.status} {errorOccurred.statusText}
+          </span>
+        </div>
+      ) : isLoading === true ? (
+        <div className="search-results-loading-container">
+          <img
+            className="search-results-loading-image"
+            src="../images/loading.gif"
+            alt="loading icon"
+          />
+          <span className="search-results-loading-text">
+            The Bookings data is loading, please wait...
+          </span>
+        </div>
+      ) : (
+        <SearchResults results={bookings} setCustomerId={setCustomerId} />
+      )}
       {customerId && <CustomerProfile id={customerId} />}
     </div>
   );
