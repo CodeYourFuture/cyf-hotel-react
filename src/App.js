121b9ed9eff92c2708afdcc4d190a799cca17eab
Anthony Mogotlane <almogotlane@gmail.com>
2022-10-13 20:58:24 +0200

    Created Footer component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Heading from "./Heading";
+import Footer from "./Footer";
 import TouristInfoCards from "./TouristInfoCards";
 import Bookings from "./Bookings";
 import "./App.css";
+
+const arr = [
+  "123 Fake Street, London, E1 4UD",
+  "hello@fakehotel.com",
+  "0123 456789"
+];
 
 const App = () => {
   return (
@@ -10,6 +17,7 @@ const App = () => {
       <Heading />
       <TouristInfoCards />
       <Bookings />
+      <Footer details={arr} />
     </div>
   );
 };
