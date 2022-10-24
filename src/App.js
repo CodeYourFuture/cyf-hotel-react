69555f2431ddfb9d7ab98f847bcaa0617b9b3839
EleMashige <elekanyanimashige@yahoo.com>
2022-10-24 11:36:46 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Heading from "./Heading";
 import TouristInfoCards from "./TouristInfoCards";
-
 import Bookings from "./Bookings";
 import "./App.css";
 
---
+++ src/TouristInfoCards.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TouristInfoCards = () => {
+  return (
+    <div>
+      <h4>How to use CardComponent in ReactJS?</h4>
+    </div>
+  );
+};
+
+export default TouristInfoCards;
