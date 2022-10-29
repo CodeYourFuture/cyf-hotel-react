d2674cdd0786b7704553129bb1c2724e3fb8935a
shafiekdavids <shafiekd@gmail.com>
2022-10-29 21:58:37 +0200

    Lesson 1
    Exercise 1, 2 and 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import Search from "./Search.js";
+import Heading from "./Heading.js";
+import TouristInfoCard from "./TouristInfoCards.js";
 
-import Bookings from "./Bookings";
+import Bookings from "./Bookings.js";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      {/* <header className="App-header">CYF Hotel</header> */}
+
+      <Heading />
+      <TouristInfoCard />
       <Bookings />
+      <Search />
     </div>
   );
 };
---
+++ src/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <img
+        src="https://drive.google.com/uc?export=view&id=1RoX-aJ6ayIv-5-I5MkRj8Tj_0UPIrx32"
+        alt=""
+      />
+      CYF Hotel
+    </header>
+  );
+};
+
+export default Heading;
