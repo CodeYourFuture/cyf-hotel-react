18df064a50b701518beb554e1259a6d9d6d39461
mfuke christian kambulu <60177462+mfukechristian@users.noreply.github.com>
2022-10-30 22:21:58 +0200

    ZA2-Christian-Mfuke-cyf-hotel-react Lesson 2
    10. Add more pizzas (completed)

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -3,12 +3,19 @@ import React, { useState } from "react";
 const Restaurant = () => {
   const [countPizza, setCountPizza] = useState(0);
 
+  function orderOne() {
+    setCountPizza(countPizza + 1);
+  }
+
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {countPizza} <button className="btn btn-primary">Add</button>
+          Pizzas: {countPizza}{" "}
+          <button className="btn btn-primary" onClick={orderOne}>
+            Add
+          </button>
         </li>
       </ul>
     </div>
