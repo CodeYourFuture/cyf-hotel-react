865b9c1e4a9b42acd34465634f4f1c650949117f
mfuke christian kambulu <60177462+mfukechristian@users.noreply.github.com>
2022-10-30 22:14:08 +0200

    ZA2-Christian-Mfuke-cyf-hotel-react Lesson 2
    9. Preparing to add more pizzas (completed)

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Restaurant = () => {
-  const pizzas = 0;
+  const [countPizza, setCountPizza] = useState(0);
+
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
+          Pizzas: {countPizza} <button className="btn btn-primary">Add</button>
         </li>
       </ul>
     </div>
