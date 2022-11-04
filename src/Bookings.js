88b82233ad9a665d6ac93e5def49ab29fac11e22
chandredewet <chandredewet@gmail.com>
2022-11-04 22:08:53 +0200

    completed fetch and partial form

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,20 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
 
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    console.log("Fetching data...");
+
+    fetch(`https://cyf-react.glitch.me`)
+      .then(res => res.json())
+      .then(data => {
+        setBookings(data);
+      });
+  }, []);
+
   return (
-    <div className="App-content">
-      <div className="container">
-        <Search search={search} />
-        <SearchResults results={bookings} />
-      </div>
+    <div>
+      {bookings ? (
+        <div className="App-content">
+          <div className="container">
+            <Search search={search} />
+            <SearchResults results={bookings} />
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
