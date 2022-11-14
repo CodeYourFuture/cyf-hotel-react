9a5ec413ef188eb9fd5fee6a27c3ac161be50494
Alex Phillip <alexjoelphillip@gmail.com>
2022-11-14 22:16:36 +0000

    added london card

--- src/TouristInfoCards.jsx
+++ src/TouristInfoCards.jsx
@@ -6,8 +6,8 @@ const TouristInfoCards = () => {
       <div className="Card">
         <img
           className="CardImage"
-          alt="photo of Glasgow"
-          src="https://i0.wp.com/www.stephaniefox.co.uk/wp-content/uploads/2020/09/4FE78B9D-C5C3-4F31-A87F-423C71EB984B.jpeg?fit=800%2C706&ssl=1"
+          alt="glasgow street art"
+          src="https://www.scotsmagazine.com/wp-content/uploads/sites/7/2020/08/Screenshot-2020-08-28-at-14.15.33.png"
           width="250"
           height="250"
         />
@@ -30,7 +30,7 @@ const TouristInfoCards = () => {
       <div className="Card">
         <img
           className="CardImage"
-          alt="photo of Glasgow"
+          alt="manchester street art"
           src="http://jenikya.com/blog/manchester-streetart2019-12.jpg"
           width="250"
           height="250"
@@ -47,6 +47,29 @@ const TouristInfoCards = () => {
             target="_blank"
           >
             Visit Manchester
+          </a>
+        </div>
+      </div>
+      <div className="Card">
+        <img
+          className="CardImage"
+          alt="london street art"
+          src="https://thesanetravel.com/images/Travels/_1-London-Street-art/26street-art-london-thesanetravel.com-1650418.jpg"
+          width="250"
+          height="250"
+        />
+        <div className="CardBody">
+          <h1>London</h1>
+          <p>
+            London is by far the UK's largest metropolis, and also serves as its
+            economic, transportation, and cultural centre.
+          </p>
+          <a
+            href="https://www.visitlondon.com/"
+            className="LondonButton"
+            target="_blank"
+          >
+            Visit London
           </a>
         </div>
       </div>
