99f2957c02413c6b1886538ec013faa31b4c83f0
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-11-14 15:31:14 +0000

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,6 @@
 import React from "react";
+import SearchButton from "./SearchButton";
+
 
 const Search = () => {
   return (
@@ -17,7 +19,7 @@ const Search = () => {
                 className="form-control"
                 placeholder="Customer name"
               />
-              <button className="btn btn-primary">Search</button>
+              <SearchButton />
             </div>
           </form>
         </div>
