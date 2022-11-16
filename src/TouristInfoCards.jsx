02eaf2d3264e3da73183d2ba66ccaae7a5ca668b
Pakize Bozkurt <pakiozihak@gmail.com>
2022-11-16 23:51:16 +0000

    Changed cards

--- src/TouristInfoCards.jsx
+++ src/TouristInfoCards.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 function TouristInfoCards() {
   return (
     <div>
-      <a href="https://peoplemakeglasgow.com/" />
-      <a href="https://visitmanchester.com/" />
-      <a href="https://visitlondon.com/" />
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Glasgow</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">More Information</Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
