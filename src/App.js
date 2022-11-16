66bc146862a1911822592eadf7941d053886d402
Pakize Bozkurt <pakiozihak@gmail.com>
2022-11-16 23:27:50 +0000

    Created cards

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const App = () => {
   return (
     <div className="App">
       <Heading />
+      <TouristInfoCards />
       <Bookings />
     </div>
   );
---
+++ src/TouristInfoCards.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TouristInfoCards() {
+  return (
+    <div>
+      <a href="https://peoplemakeglasgow.com/" />
+      <a href="https://visitmanchester.com/" />
+      <a href="https://visitlondon.com/" />
+    </div>
+  );
+}
+
+export default TouristInfoCards;
