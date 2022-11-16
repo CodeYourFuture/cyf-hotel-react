7220ca1f569e9cb96d5404df6f964408c0feaba0
Pakize Bozkurt <pakiozihak@gmail.com>
2022-11-16 23:17:10 +0000

    Added hotel's logo

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -4,6 +4,10 @@ function Heading() {
   return (
     <div>
       <header className="App-header">CYF Hotel</header>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9TlnMzjBc2fVKB68tbpwgHcNVWjFECEU2Yw&usqp=CAU"
+        alt="Logo"
+      />
     </div>
   );
 }
