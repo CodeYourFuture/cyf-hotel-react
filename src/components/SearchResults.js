55aadb6ee1d54fcea923d95af508a94a518e7e7e
SaadiaElf <saadiaelfekak@gmail.com>
2022-11-17 12:34:36 +0000

    Added number of nights

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,19 +1,24 @@
 import React from "react";
+import moment from "moment";
 
 const SearchResults = props => {
   return (
     <table>
       <tr>
-        <th>id</th>
-        <th>title</th>
-        <th>first name</th>
-        <th>surname</th>
-        <th>email</th>
-        <th>room id</th>
-        <th>check in date</th>
-        <th>check out date</th>
+        <th>Id</th>
+        <th>Title</th>
+        <th>First name</th>
+        <th>Surname</th>
+        <th>Email Address</th>
+        <th>Room id</th>
+        <th>Check in date</th>
+        <th>Check out date</th>
+        <th>Number of nights</th>
       </tr>
       {props.results.map((booking, index) => {
+        let inDate = moment(booking.checkInDate);
+        let outDate = moment(booking.checkOutDate);
+        let numOfNights = outDate.diff(inDate, "days"); // 1
         return (
           <tr key={index}>
             <td>{booking.id}</td>
@@ -23,7 +28,8 @@ const SearchResults = props => {
             <td>{booking.email}</td>
             <td>{booking.roomId}</td>
             <td>{booking.checkInDate}</td>
-            <td>{booking.heckOutDate}</td>
+            <td>{booking.checkOutDate}</td>
+            <td>{numOfNights}</td>
           </tr>
         );
       })}
