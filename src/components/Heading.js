e45631323c426a301f31e34c5f175dfaca86110b
SaadiaElf <saadiaelfekak@gmail.com>
2022-11-17 09:37:05 +0000

    Added logo

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,7 +1,13 @@
 import React from "react";
+import logo from "../img/logo.png";
 
 const Heading = () => {
-  return <header className="App-header">CYF Hotel</header>;
+  return (
+    <header className="App-header">
+      <img className="App-logo" src={logo} alt="logo" />
+      <h1 className="App-title">CYF Hotel</h1>
+    </header>
+  );
 };
 
 export default Heading;
