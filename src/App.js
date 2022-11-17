50561a93cd8c8bbc88d99354762f2e7f8a1a3066
SaadiaElf <saadiaelfekak@gmail.com>
2022-11-17 10:29:08 +0000

    Styled cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Bookings from "./components/Bookings";
 import Heading from "./components/Heading";
+import TouristInfoCards from "./components/TouristInfoCards";
 import "./App.css";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div className="App">
       <Heading />
       <Bookings />
+      <TouristInfoCards />
     </div>
   );
 };
