25ca56da0c53a5a572962bdae5e84dc260b25976
Dewayne Sewell <dewaynesewell1@gmail.com>
2022-11-18 15:57:19 +0000

    img alt

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -7,6 +7,7 @@ function Heading() {
       <img
         id="logo"
         src="https://cdn-icons-png.flaticon.com/512/3009/3009487.png"
+        alt="hotel logo"
       />
       CYF Hotel
     </header>
