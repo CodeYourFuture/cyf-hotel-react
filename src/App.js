96758b8b4a25235df7b302fef2645e08be843648
ozibozkurt <ozipaki@gmail.com>
2022-11-18 12:47:30 +0000

    Lesson1.4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TouristInfoCards from "./components/TouristInfoCards";
 import Bookings from "./Bookings";
 import "./App.css";
 import Heading from "./components/Heading";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -10,6 +11,13 @@ const App = () => {
       <Heading />
       <TouristInfoCards />
       <Bookings />
+      <Footer
+        array={[
+          "123 Fake Street, London, E1 4UD",
+          "hello@fakehotel.com",
+          "0123 456789"
+        ]}
+      />
     </div>
   );
 };
