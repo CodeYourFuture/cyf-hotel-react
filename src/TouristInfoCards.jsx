c3591142931ff9520f87e2751d83b478aac041b9
Dewayne Sewell <dewaynesewell1@gmail.com>
2022-11-18 15:58:36 +0000

    alt add

--- src/TouristInfoCards.jsx
+++ src/TouristInfoCards.jsx
@@ -7,6 +7,7 @@ function TouristInfoCards() {
         <img
           src="https://www.visitbritain.com/sites/default/files/consumer/paragraphs-bundles/image-header-with-text/glasgow_clyde_arc_vb34161123.jpg"
           className="card-img-top"
+          alt="city image"
         />
         <div className="card-body">
           <a
@@ -22,6 +23,7 @@ function TouristInfoCards() {
         <img
           src="https://media.architecturaldigest.com/photos/56fd9d65ecd154e0329c1627/4:3/w_768/london-travel-guide-lede.jpg"
           className="card-img-top"
+          alt="city image"
         />
         <div className="card-body">
           <a href="https://www.visitlondon.com" className="btn btn-primary">
@@ -34,6 +36,7 @@ function TouristInfoCards() {
         <img
           src="https://www.centreforcities.org/wp-content/uploads/2014/08/Manchester_x1650.jpg"
           className="card-img-top"
+          alt="city image"
         />
         <div className="card-body">
           <a href="https://www.visitmanchester.com" className="btn btn-primary">
