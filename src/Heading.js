0ccab4c3b0f1297341530ec094318913da1cd353
jaspreet kaur <kaurjas1928@yahoo.com>
2022-11-18 17:44:52 +0000

    addded search button and heading

---
+++ src/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <p className="LogoText">CYF Hotel</p>
+      <img
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        alt="logo"
+        className="logo"
+      />
+    </header>
+  );
+};
+export default Heading;
