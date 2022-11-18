851c6dadb34c570013752930ad74d3101abb6594
chandredewet <chandredewet@gmail.com>
2022-11-19 01:48:02 +0200

    completed level 22 implemented loading error

--- src/Bookings.js
+++ src/Bookings.js
@@ -3,7 +3,14 @@ import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 
 const Bookings = () => {
+  const initialError = {
+    errorExists: false,
+    errorStatus: 0,
+    errorMessage: ""
+  };
   const [bookings, setBookings] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [errorInfo, setErrorInfo] = useState(initialError);
 
   const search = searchVal => {
     console.info("TO DO!", searchVal);
@@ -16,24 +23,50 @@ const Bookings = () => {
 
   useEffect(() => {
     console.log("Fetching data...");
-
-    fetch(`https://cyf-react.glitch.me`)
-      .then(res => res.json())
+    // fetch(`https://cyf-react.glitch.me`);
+    //fetch(`https://cyf-react.glitch.me/delayed`)
+    fetch(`https://cyf-react.glitch.me/error`)
+      .then(res => {
+        if (res.status >= 300) {
+          setErrorInfo(errorInfo => ({ ...errorInfo, errorExists: true }));
+          setErrorInfo(errorInfo => ({
+            ...errorInfo,
+            errorStatus: res.status
+          }));
+          setErrorInfo(errorInfo => ({
+            ...errorInfo,
+            errorMessage: res.statusText
+          }));
+          console.log(res.status, errorInfo.errorStatus);
+        } else {
+          return res.json();
+        }
+      })
       .then(data => {
-        setBookings(data);
+        if (data) {
+          setBookings(data);
+          setLoaded(true);
+        }
       });
   }, []);
 
   return (
     <div>
-      {bookings ? (
+      {errorInfo.errorExists ? (
+        <span>
+          Encountered something unexpected: {errorInfo.errorStatus}{" "}
+          {errorInfo.errorMessage}
+        </span>
+      ) : loaded ? (
         <div className="App-content">
           <div className="container">
             <Search search={search} />
             <SearchResults results={bookings} />
           </div>
         </div>
-      ) : null}
+      ) : (
+        <span>Loading...</span>
+      )}
     </div>
   );
 };
