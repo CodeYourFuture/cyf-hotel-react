117f118f6724f0871782e799c6e27e7635e722d4
Kawa MANMI <kawamam2007@gmail.com>
2022-11-19 11:55:37 +0000

    Step 12 in Lesson 2 has been done

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -3,10 +3,7 @@ import { useState } from "react";
 
 const Restaurant = () => {
   // const pizzas = 0;
-  const [pizzas, setPizzas] = useState(0);
-  const orderOne = () => {
-    setPizzas(pizzas + 1);
-  };
+
   const RestaurantButton = props => {
     return (
       <button className="btn btn-primary" onClick={props.pizzaFun}>
@@ -14,14 +11,24 @@ const Restaurant = () => {
       </button>
     );
   };
+  const Order = () => {
+    const [pizzas, setPizzas] = useState(0);
+    const orderOne = () => {
+      setPizzas(pizzas + 1);
+    };
+    return (
+      <li>
+        Pizzas: {pizzas}
+        <RestaurantButton pizzaFun={orderOne} />
+      </li>
+    );
+  };
+
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
-        <li>
-          Pizzas: {pizzas}
-          <RestaurantButton pizzaFun={orderOne} />
-        </li>
+        <Order />
       </ul>
     </div>
   );
