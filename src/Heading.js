d7fe716d8956626bfebefd6719d43b9028ffeae8
micheale2021 <andemariammicheale@gmail.com>
2022-11-20 21:24:43 +0000

    update heading.js

--- src/Heading.js
+++ src/Heading.js
@@ -1,18 +1,10 @@
 import React from "react";
 import "./App.css";
-import HotelLogo from "./HotelLogo.png"
-
-// const Logo = () => {
-//   return (
-//     <img src="https://image.flaticon.com/icons/svg/139/139899.svg" alt="Logo" />
-//   );
-// };
+import HotelLogo from "./HotelLogo.png";
 
 const Heading = () => {
   return (
     <header className="App-header">
-      {/* <Logo /> */}
-      {/* <header className="App-header">CYF Hotel</header> */}
       <img className="App-logo" src={HotelLogo} alt="Logo" />
       CYF Hotel
     </header>
