2659393f2c25638a6d5e68a93e78cc6992afb5c3
Pakize Bozkurt <pakiozihak@gmail.com>
2022-11-21 19:59:16 +0000

    Completed 15

--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
-import moment from "moment";
+//import SearchResultRow from "./SearchResultRow.jsx";
 
 function SearchResults(props) {
   return (
@@ -21,19 +21,7 @@ function SearchResults(props) {
         </thead>
         <tbody>
           {props.results.map(dt => {
-            return (
-              <tr>
-                <td>{dt.id}</td>
-                <td>{dt.title}</td>
-                <td>{dt.firstName}</td>
-                <td>{dt.surname}</td>
-                <td>{dt.email}</td>
-                <td>{dt.roomId}</td>
-                <td>{dt.checkInDate}</td>
-                <td>{dt.checkOutDate}</td>
-                <td>{moment(dt.checkOutDate).diff(dt.checkInDate, "days")}</td>
-              </tr>
-            );
+            return <SearchResultsRow dt={dt} />;
           })}
         </tbody>
       </Table>
