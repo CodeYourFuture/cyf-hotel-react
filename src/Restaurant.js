c25e81f0b4261958914fb274a8697ebeed354db3
Pakize Bozkurt <pakiozihak@gmail.com>
2022-11-21 19:07:24 +0000

    Completed 9- 10

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,33 +1,24 @@
-import React from "react";
 import React, { useState } from "react";
 
 const Restaurant = () => {
-  const pizzas = 0;
+  const [orders, setOrders] = useState(0);
+  function orderOne() {
+    setOrders(orders + 1);
+  }
+
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
+          Pizzas: {orders}{" "}
+          <button onClick={orderOne} className="btn btn-primary">
+            Add
+          </button>
         </li>
       </ul>
     </div>
   );
 };
-
-function Orders() {
-  const [orders, setCount] = useState(0);
-
-  function incrementOrder() {
-    setCount(1);
-  }
-
-  return (
-    <div>
-      <button onClick={incrementOrder}>Click me</button>
-      <p>You clicked {orders} times</p>
-    </div>
-  );
-}
 
 export default Restaurant;
