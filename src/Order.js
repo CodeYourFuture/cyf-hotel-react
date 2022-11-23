62ecfb6bf032f4764ebd609abf84e89df508920f
simeonbikov <bikovsem@gmail.com>
2022-11-23 15:07:00 +0000

    Les. 2, ex. 12

---
+++ src/Order.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import RestaurantButton from "./RestaurantButton";
+
+const Order = () => {
+  const [orders, setOrders] = useState(0);
+
+  function orderOne() {
+    setOrders(order => {
+      return order + 1;
+    });
+  }
+
+  return (
+    <li>
+      Pizzas: {orders}
+      <RestaurantButton handleClick={orderOne} />
+    </li>
+  );
+};
+
+export default Order;
--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,22 +1,12 @@
-import React, { useState } from "react";
-import RestaurantButton from "./RestaurantButton";
+import React from "react";
+import Order from "./Order";
 
 const Restaurant = () => {
-  const [orders, setOrders] = useState(0);
-
-  function orderOne() {
-    setOrders(order => {
-      return order + 1;
-    });
-  }
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
-        <li>
-          Pizzas: {orders}
-          <RestaurantButton handleClick={orderOne} />
-        </li>
+        <Order />
       </ul>
     </div>
   );
