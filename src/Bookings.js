e2c3a121e24deee674d822f7f4b8a70ea04592d9
Pakize Bozkurt <pakiozihak@gmail.com>
2022-11-25 19:06:42 +0000

    useEffect

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.jsx";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+  useEffect(() => {
+    setBookings(FakeBookings);
+  }, []);
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
