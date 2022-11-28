75e83140f75c36974d7478a6dab01ec1ac1afcc9
Kawa MANMI <kawamam2007@gmail.com>
2022-11-28 09:03:33 +0000

    Some more buttons for add and romve orders have been added

--- src/Heading.js
+++ src/Heading.js
@@ -1,10 +1,10 @@
 import React from "react";
-import hotelLogo from "./images/logo.png";
+import hotelLogo from "./images/logo.jpg";
 import "./App.css";
 
 const Heading = () => {
   return (
-    <header className="App-header bg-dark text-warning">
+    <header className="App-header text-warning">
       <img className="hotelLogo" src={hotelLogo} alt="Logo CYF" />
       Claridge's Hotel
     </header>
