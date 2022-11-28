1d62383f623d0572cda3bce598d8ac6a1dd54f99
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-11-28 13:50:20 +0000

    Update Bookings.js

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,20 +1,28 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
 
-const Bookings = () => {
+const Bookings = () =>
+{
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
 
-  const [bookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() =>
+  {
+    fetch("https://cyf-react.glitch.me")
+      .then(response => response.json())
+      .then(data => setBookings(data))
+  }, []);
 
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search}/>
-        {<SearchResults results={bookings}/>}
+        <SearchResults results={bookings}/>
       </div>
     </div>
   );
