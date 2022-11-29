88883bdc48f6774bea02abffb1c76082947d5859
Biruk Kebede <biruk.key@gmail.com>
2022-11-29 20:52:22 +0000

    modified customer profile

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -1,31 +1,26 @@
 import React, { useState, useEffect } from "react";
 
-const CustomerProfile = props => {
-  const [details, setDetails] = useState({});
-  const [controller, setController] = useState(false);
+const CustomerProfile = ({ id }) => {
+  const [customerProfile, setCustomerProfile] = useState({});
+  const [customerId, setCustomerId] = useState(null);
   const [show, setShow] = useState(true);
 
-  const handleProfile = e => {
-    e.preventDefault();
-    if (details !== props.id) {
-      setDetails({ id: props.id });
-      show ? setShow(false) : setShow(true);
-    }
+  const handleProfile = () => {
+    setCustomerId(id);
+    show ? setShow(false) : setShow(true);
   };
 
   useEffect(() => {
-    if (details.id && !controller) {
-      fetch(`https://hotel-cyf-server.onrender.com/bookings/${details.id}`)
+    if (customerId) {
+      fetch(`https://hotel-cyf-server.onrender.com/bookings/${customerId}`)
         .then(res => res.json())
         .then(data => {
-          if (details.id === props.id) {
-            setDetails(data);
-            setController(true);
-          }
+          console.log(data);
+          setCustomerProfile(data);
         })
         .catch(err => console.log(err));
     }
-  }, [details, props.id, controller]);
+  }, [customerId]);
 
   return (
     <>
@@ -33,13 +28,13 @@ const CustomerProfile = props => {
         {show ? "Show Profile" : "Hide Profile"}
       </button>
       <ul className={show ? "d-none" : "d-block"}>
-        <li>id: {details.id}</li>
-        <li>title: {details.title}</li>
-        <li>first name: {details.firstName}</li>
-        <li>surname: {details.surname}</li>
-        <li>email: {details.email}</li>
-        <li>phone number: {details.phoneNumber}</li>
-        <li>vip: {`${details.vip}`}</li>
+        <li>ID: {customerProfile.id}</li>
+        <li>Title: {customerProfile.title}</li>
+        <li>First Name: {customerProfile.firstName}</li>
+        <li>Surname: {customerProfile.surname}</li>
+        <li>Email: {customerProfile.email}</li>
+        <li>Phone Number: {customerProfile.phoneNumber}</li>
+        {customerProfile.vip && <li style={{ color: "purple" }}> VIP </li>}
       </ul>
     </>
   );
