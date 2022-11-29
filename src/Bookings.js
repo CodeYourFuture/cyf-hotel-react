d03c353fa8d27c8884780910c82666b697a2717a
Abdirahim Hussein <drahimhussein@gmail.com>
2022-11-29 19:57:33 +0000

    update bookings.js

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
@@ -9,6 +9,14 @@ const Bookings = () => {
   };
   // eslint-disable-next-line
   const [bookings, setBookings] = useState(FakeBookings);
+
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      });
+  }, []);
   return (
     <div className="App-content">
       <div className="container">
