260891e9f55919a8ed0d90a6adae1a0141e89bb0
SaadiaElf <saadiaelfekak@gmail.com>
2022-11-29 13:37:15 +0000

    Display a customer profile - step 2

--- src/components/CustomerProfile.js
+++ src/components/CustomerProfile.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const CustomerProfile = props => {
+  const [customer, setCustomer] = useState({});
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me/customers/${props.id}`)
+      .then(res => res.json())
+      .then(data => setCustomer(data));
+  }, [props.id]);
+  console.log(customer);
   return (
-    <div>
-      <p>"Customer {props.id} Profile"</p>
-    </div>
+    <>
+      {customer.vip ? (
+        <div>
+          <h1>Customer {props.id} Profile</h1>
+          <ul>
+            <li>{customer.email}</li>
+            <li>{customer.phoneNumber}</li>
+          </ul>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 
