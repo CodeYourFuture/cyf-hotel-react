c371bcd9ce87dc86ee6c55da2ec75532d40fdadd
Kawa MANMI <kawamam2007@gmail.com>
2022-11-29 12:12:30 +0000

    Lesson3, 21. Display a customer profile - step 2, vip part mantep2 , vip part manged by inserting some condition

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -14,12 +14,35 @@ export default function CustomerProfile(props) {
   return (
     <div>
       <p>Customer {props.id} Profiles </p>
-      <p>
+      {/* <p>
         {Object.entries(customerDetails).map(elm => (
           <li>
             {elm[0].charAt(0).toUpperCase() + elm[0].slice(1)}: {String(elm[1])}
           </li>
         ))}
+      </p> */}
+
+      {/* {customerDetails.vip == true ? (
+        <div>
+          <p>customer Id: {customerDetails.id} </p>
+          <p> customer Email: {customerDetails.email} </p>
+          <p>customer Phone: {customerDetails.phoneNumber}</p>
+        </div>
+      ) : (
+        <div>
+          {Object.entries(customerDetails).map((elm) => (
+            <p>
+              {elm[0].charAt(0).toUpperCase() + elm[0].slice(1)}:{" "}
+              {String(elm[1])}
+            </p>
+          ))}
+        </div>
+      )} */}
+      <p>customer Id: {customerDetails.id} </p>
+      <p> customer Email: {customerDetails.email} </p>
+      <p>customer Phone: {customerDetails.phoneNumber}</p>
+      <p>
+        Customer Base: {Object.values(customerDetails)[5] ? "VIP" : "Standard"}
       </p>
     </div>
   );
