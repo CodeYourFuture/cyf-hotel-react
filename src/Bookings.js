96e5effbf0b6106c6772cda0e9873d16c276aedc
RobCso <robcso@DESKTOP-AA47BN0.localdomain>
2022-11-30 13:09:14 +0000

    complete level19

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,13 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
+  useEffect(() => {
+    console.log("this is a test");
+    fetch("https://cyf-react.glitch.me")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setBookings(data);
+      });
+  }, []);
+
   const search = searchVal => {
-    console.info("TO DO!", searchVal);
+    //console.info("TO DO!", searchVal);
+    const result = bookings.filter(item => {
+      return (
+        item.firstName.includes(searchVal) || item.surname.includes(searchVal)
+      );
+    });
+    //console.log(result)
+    setBookings(result);
   };
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
   console.log(setBookings);
   return (
     <div className="App-content">
