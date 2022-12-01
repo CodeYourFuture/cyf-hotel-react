4e7a222a78f1526f55aeae1752ca40509742e029
leilafaez <leila.faez@gmail.com>
2022-12-01 13:51:35 +0000

    Add SearchButton and Heading components
    add logo to Heading and style in App.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
+import ReactDOM from "react";
+import Heading from "./Heading";
 
-import Bookings from "./Bookings";
+import Search from "./Search";
+import SearchButton from "./SearchButton";
+
 import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">CYF Hotel</header>
-      <Bookings />
+    <div>
+      <Heading />
+
+      <Search />
+      <SearchButton />
     </div>
+    // <div className="App">
+    //   <header className="App-header">CYF Hotel</header>
+    //   <Bookings />
+    // </div>
   );
 };
 
--- src/Search.js
+++ src/Search.js
@@ -17,7 +17,7 @@ const Search = () => {
                 className="form-control"
                 placeholder="Customer name"
               />
-              <button className="btn btn-primary">Search</button>
+              {/* <button className="btn btn-primary">Search</button> */}
             </div>
           </form>
         </div>
