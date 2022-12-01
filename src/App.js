e61ef81101b6aef1591cdab085202f749bd02c09
4maziar <53979524+8maziar@users.noreply.github.com>
2022-12-01 19:54:38 +0000

    footer done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 
 import Bookings from "./Bookings";
+import Heading from "./Heading";
+import TouristInfoCards from "./TouristInfoCards";
+import Footer from "./Footer";
+
 import "./App.css";
+
+const data = [
+  "123 Fake Street, London, E1 4UD",
+  "hello@fakehotel.com",
+  "0123 456789"
+];
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Heading />
+      <TouristInfoCards />
       <Bookings />
+      <Footer data={data} />
     </div>
   );
 };
