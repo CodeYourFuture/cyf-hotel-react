33a466da232f1afa9bebb5545074e3ccbfd50bc4
azadehroshan <105980239+azadehroshan@users.noreply.github.com>
2022-12-02 21:02:58 +0000

    show the number of nights

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,4 +1,12 @@
 import React from "react";
+import Moment from "moment";
+
+let checkDiff = (InDate, OutDate) => {
+  const checkInDate = Moment(InDate, "YYYY-MM-DD");
+  const checkOutDate = Moment(OutDate, "YYYY-MM-DD");
+  const diff = checkOutDate.diff(checkInDate, "days");
+  return diff;
+};
 
 const SearchResults = props => {
   return (
@@ -12,6 +20,7 @@ const SearchResults = props => {
         <th scope="col">room id</th>
         <th scope="col">check in date</th>
         <th scope="col">check out date</th>
+        <th scope="col">number of nights</th>
       </tr>
       {/* props.results => fakeBooking.json */}
       {props.results.map(result => (
@@ -24,6 +33,7 @@ const SearchResults = props => {
           <td>{result.roomId}</td>
           <td>{result.checkInDate}</td>
           <td>{result.checkOutDate}</td>
+          <td>{checkDiff(result.checkInDate, result.checkOutDate)}</td>
         </tr>
       ))}
     </table>
