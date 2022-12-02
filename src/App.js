9e32ff393e31035062097fd11fec44c0235fd70a
Miladepour <epour.milad@gmail.com>
2022-12-02 14:04:42 +0000

    4. Create a Footer component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
-
 import Bookings from "./Bookings";
 import "./App.css";
 import Header from "./Header";
 import TouristInfoCards from "./TouristInfoCards";
+import Footer from "./Footer";
 
 const App = () => {
+  const info = ["123 Fake Street, London, E1 4UD", "hello@fakehotel.com", "0123 456789"]
+
   return (
     <div className="App">
       <Header />
       <TouristInfoCards />
       <Bookings />
+      <Footer footerInfo={info} />
     </div>
   );
 };
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = ({footerInfo, }) => {
+ console.log(footerInfo)   
+return (
+    <ul className="d-flex justify-content-around">
+    {footerInfo.map((info) => {
+    return <li key = {info}>{info}</li>;
+      })}
+    </ul>
+)
+}
+
+export default Footer;
