12db8b1ebaf6c7d875ff46ec9814c36e3ea97d54
levilu800b <levilu800b@gmail.com>
2022-12-03 15:20:05 +0000

    Added heading and searchButton

---
+++ src/heading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const heading = () => {
+  return (
+    <div>
+      <header className="App-header">CYF Hotel</header>
+    </div>
+  );
+};
+
+export default heading;
---
+++ src/searchButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const searchButton = () => {
+  return (
+    <div>
+      <button className="btn btn-primary">Search</button>
+    </div>
+  );
+};
+
+export default searchButton;
