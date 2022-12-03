fc8d126355541f99f42b0ef5fa59ec1cfc035810
Doris-Siu <doris.siu.my@gmail.com>
2022-12-03 13:57:17 +0000

    Complete Lesson 1 #1,2

---
+++ src/Heading.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function() {
+  return (
+    <div>
+      <img src="https://image.flaticon.com/icons/svg/139/139899.svg" />
+      <header className="App-header">CYF Hotel</header>
+    </div>
+  );
+}
