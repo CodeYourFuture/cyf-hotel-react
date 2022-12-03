c7133c3a137e58be0380a2bf595b7a8b8182b311
clav00cl <clav00cl@hotmail.com>
2022-12-03 15:42:12 +0000

    SearchButton.add

---
+++ src/component/SearchButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SearchButton = () => {
+  return (
+    <div>
+      {" "}
+      <button className="btn btn-primary">Search</button>{" "}
+    </div>
+  );
+};
+
+export default SearchButton;
