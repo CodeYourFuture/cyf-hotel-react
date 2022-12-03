2236fb69cc5d4544b6a075fdbf579045c0551b68
fatih-celebi <celebifatih.c1@gmail.com>
2022-12-03 23:50:11 +0000

    Complete Lesson 1 - Task 1&2

---
+++ src/Heading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div class="div">
+      <header className="App-header">CYF Hotel</header>
+      <img src="https://images.unsplash.com/photo-1615460549969-36fa19521a4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGhvdGVsfGVufDB8fDB8fA%3D%3D&w=1000&q=80" />
+    </div>
+  );
+};
+
+export default Heading;
