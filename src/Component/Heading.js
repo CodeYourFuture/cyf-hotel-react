2c9e13a134b8303b35e9705f920e1c556e4e6580
sasarkarimi <sahar.karimi90@outlook.com>
2022-12-03 15:42:39 +0000

    button/image done

---
+++ src/Component/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div className="App-header-section">
+      <img
+        src="https://cdn-icons-png.flaticon.com/128/1475/1475996.png"
+        alt=""
+      />
+      <header className="App-header">CYF Hotel</header>
+    </div>
+  );
+};
+
+export default Heading;
