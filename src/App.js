0b79782759f934c8ce4c62fd2432dd3dbdf595ff
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2022-12-04 19:48:18 +0000

    up to exercise 3
    up to exercise 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import SearchButton from "./SearchButton";
 
 import Bookings from "./Bookings";
 import "./App.css";
+import Heading from "./Heading";
+import TouristInfoCards from "./TouristInfoCards";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      {/* <header className="App-header">CYF Hotel</header> */}
+      <Heading />
       <Bookings />
+      <SearchButton />
     </div>
   );
 };
---
+++ src/Heading.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      {" "}
+      <img
+        src="https://i.pinimg.com/736x/10/ff/aa/10ffaadab6bc3c4c1dd4a3e44bf6d5ad.jpg"
+        width="25px"
+        height="25px"
+        alt="Logotop"
+      />{" "}
+      CYF Hotel
+    </header>
+  );
+};
+
+export default Heading;
