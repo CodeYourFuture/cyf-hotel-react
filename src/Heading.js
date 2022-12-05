49c2792bd66574d9688891313000cffeaed6b305
Doris-Siu <doris.siu.my@gmail.com>
2022-12-05 18:12:46 +0000

    Install Moment and update CSS

--- src/Heading.js
+++ src/Heading.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function() {
   return (
     <div>
-      <img src="https://images.freeimages.com/images/previews/09e/moon-art-1641879.png" />
+      <img
+        id="logo"
+        src="https://images.freeimages.com/images/previews/09e/moon-art-1641879.png"
+      />
       <header className="App-header">CYF Hotel</header>
     </div>
   );
