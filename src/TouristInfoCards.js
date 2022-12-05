4a3b9ab6d0b353b753f763da0d062ea768ad0fef
ShimenAfshar <afsharshimen@gmail.com>
2022-12-05 23:48:23 +0000

    exercises 5,6,7 done

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -8,9 +8,9 @@ const HotelCard = props => {
         <h2>{props.name}</h2>
         <p>{props.text}</p>
       </div>
-      <button a href={props.link} className="btn btn-primary">
-        More information
-      </button>
+      <a href={props.link} className="btn btn-primary">
+        <button>More information</button>
+      </a>
     </div>
   );
 };
@@ -25,6 +25,7 @@ const TouristInfoCards = () => {
         }
         link="https://visitmanchester.com"
         image="https://thumbs.dreamstime.com/b/manchester-canal-uk-england-skyline-shot-deansgate-108118828.jpg"
+        alt=""
       />
       <HotelCard
         name={"London"}
@@ -33,14 +34,16 @@ const TouristInfoCards = () => {
         }
         link="https://visitlondon.com"
         image="https://media.istockphoto.com/id/1294454411/photo/london-symbols-with-big-ben-double-decker-buses-and-red-phone-booth-in-england-uk.jpg?b=1&s=170667a&w=0&k=20&c=SR5FBt8VvpZEOIxZcTmoEf9XxU0oiSnbobD3E6ZB358="
+        alt=""
       />
       <HotelCard
         name={"Glasgow"}
         text={
-          "You’re guaranteed to find accommodation in Glasgow which suits your taste and budget. Whether you’re looking for something uniquely Glaswegian, a trendy hotel, a vibrant hostel or a comfortable city centre apartment, you can be assured of a warm welcome."
+          "Youe guaranteed to find accommodation in Glasgow which suits your taste and budget. Whether you’re looking for something uniquely Glaswegian, a trendy hotel, a vibrant hostel or a comfortable city centre apartment, you can be assured of a warm welcome."
         }
         link="https://peoplemakeglasgow.com"
         image="https://media.cntraveler.com/photos/5ad4ece4379a3a44feb968b6/master/w_3604,h_2574,c_limit/Colosseum-GettyImages-537714366.jpg"
+        alt=""
       />
     </div>
   );
