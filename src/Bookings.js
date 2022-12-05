1208162f83424c79a49eca5394965fff76c7091f
Rabie AL-Ashmali <rabiealashmali@gmail.com>
2022-12-05 18:40:19 +0000

    loading all the bookings remotely

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
-// import SearchResults from "./SearchResults.js";
-// import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
-  console.log(bookings);
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me/")
+      .then(res => res.json())
+      .then(data => setBookings(data));
+  }, []);
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
