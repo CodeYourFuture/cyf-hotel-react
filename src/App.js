50413a8ac983fbdaeb664d64f35c51d7b0debaa5
ShimenAfshar <afsharshimen@gmail.com>
2022-12-05 21:55:13 +0000

    searchbutton added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Heading from "./Heading";
 import Bookings from "./Bookings";
-import "./App.css";
-import SearchButton from "./SearchButton";
+// import "./App.css";
+// import SearchButton from "./SearchButton";
 import TouristInfoCards from "./TouristInfoCards";
+import Footer from "./Footer";
+// import SearchResults from "./SearchResults.JS";
+
+const data = [
+  "123 Fake Street, London, E1 4UD",
+  "hello@fakehotel.com",
+  "0123 456789"
+];
 
 const App = () => {
   return (
@@ -11,8 +19,8 @@ const App = () => {
       {/* <header className="App-header">CYF Hotel</header> */}
       <Heading />
       <Bookings />
-      <SearchButton />
       <TouristInfoCards />
+      <Footer data={data} />
     </div>
   );
 };
