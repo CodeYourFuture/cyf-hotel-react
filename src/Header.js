ffcee78f7a176114add5517f48e73987818d6f20
malhallaq <malhallaq@gmail.com>
2022-12-06 13:55:18 +0000

    resolving Header deplyment error

--- src/Header.js
+++ src/Header.js
@@ -25,19 +25,19 @@ const Header = () => {
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="/#">
               Home <span class="sr-only">(current)</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="/#">
               Link
             </a>
           </li>
           <li class="nav-item dropdown">
             <a
               class="nav-link dropdown-toggle"
-              href="#"
+              href="/#"
               id="navbarDropdown"
               role="button"
               data-toggle="dropdown"
@@ -47,20 +47,20 @@ const Header = () => {
               Dropdown
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">
+              <a class="dropdown-item" href="/#">
                 Action
               </a>
-              <a class="dropdown-item" href="#">
+              <a class="dropdown-item" href="/#">
                 Another action
               </a>
               <div class="dropdown-divider" />
-              <a class="dropdown-item" href="#">
+              <a class="dropdown-item" href="/#">
                 Something else here
               </a>
             </div>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled" href="#">
+            <a class="nav-link disabled" href="/#">
               Disabled
             </a>
           </li>
