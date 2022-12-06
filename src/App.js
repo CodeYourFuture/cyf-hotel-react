fc2d3a818153e0f7514b6d817e269f5d7cbfdc1f
Ekrem <ekremteke.uk@gmail.com>
2022-12-06 17:06:06 +0000

    some small changes was added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Search from "./Search";
 import Bookings from "./Bookings";
 import "./App.css";
 import InfoCard from "./TouristInfoCards";
--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -3,7 +3,11 @@ import React from "react";
 const InfoCard = props => {
   return (
     <div className="card">
-      <img src={props.imageSource} className="card-img-top" />
+      <img
+        src={props.imageSource}
+        alt="imageOfCities"
+        className="card-img-top"
+      />
       <div className="card-body">
         <h4 className="card-title"> {props.title}</h4>
         <p className="card-text">{props.paragraph}</p>
