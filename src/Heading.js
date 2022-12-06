bf0cde57a538adc1e3f2b6c53b2cd2564f3868d2
Sandra-Duarte <106591234+Sandra-Duarte@users.noreply.github.com>
2022-12-06 16:38:21 +0000

    Start exercises

---
+++ src/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <div>
+      <header className="App-header"> </header>
+      <img
+        className="logo"
+        src="https://png.pngtree.com/png-clipart/20200709/original/pngtree-luxury-hotel-logo-design-template-png-image_3595577.jpg"
+      />
+    </div>
+  );
+}
+
+export default Heading;
