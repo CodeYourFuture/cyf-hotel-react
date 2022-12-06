5e427646f97472168960e79cdc990830ddecf91e
clav00cl <clav00cl@hotmail.com>
2022-12-06 23:13:51 +0000

    lesson-one-done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 
 import Bookings from "./Bookings";
 import "./App.css";
-
+import Heading from "./component/Heading";
+import TouristInfoCards from "./component/TouristInfoCards";
+import Footer from "./component/Footer";
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Heading />
+      {/* <header className="App-header">CYF Hotel</header> */}
+      <TouristInfoCards />
       <Bookings />
+
+      <Footer />
     </div>
   );
 };
---
+++ src/component/Heading.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Heading = () => {
+  return (
+    <div className="App-header-section">
+      <img
+        className="hotel-logo"
+        src="https://img.freepik.com/premium-vector/beach-house-logo-design-template-beach-resort-villa-beach-hotel-logo_98702-711.jpg?w=2000"
+        alt=""
+      />
+      <header className="App-header"> cyf hotel</header>
+    </div>
+  );
+};
+export default Heading;
