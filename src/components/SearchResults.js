4951f81f78024034a78de4d9d9cf08fe97feb797
SaadiaELF <saadiaelfekak@gmail.com>
2022-12-07 23:48:40 +0000

    Added sorting functions

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -12,36 +12,62 @@ const SearchResults = props => {
     setCustomerId(e.target.value);
   }
 
-  function orderColumn() {
+  function orderNumber(e) {
+    let att = e.target.abbr;
+
     if (order) {
       results.sort((a, b) => {
-        return b.id - a.id;
+        return b[att] - a[att];
       });
       setResults(results);
       setOrder(false);
     } else {
       results.sort((a, b) => {
-        return a.id - b.id;
+        return a[att] - b[att];
       });
       setResults(results);
       setOrder(true);
     }
   }
 
+  function orderName(e) {
+    e.stopPropagation();
+    let att = e.target.abbr;
+
+    if (order) {
+      results.sort((a, b) => {
+        let nA = a[att].toLowerCase();
+        let nB = b[att].toLowerCase();
+        return nA < nB ? -1 : nA > nB ? 1 : 0;
+      });
+      setResults(results);
+      setOrder(false);
+    } else {
+      results.reverse();
+      setResults(results);
+      setOrder(true);
+    }
+  }
   return (
     <div className="row d-flex justify-content-between">
       <div className="col-sm">
         <table className="table table-sm table-bordered border-primary">
           <thead>
             <tr className="table-primary">
-              <th scope="col" onClick={orderColumn}>
+              <th scope="col" abbr="id" onClick={orderNumber}>
                 Id
               </th>
               <th scope="col">Title</th>
-              <th scope="col">First name</th>
-              <th scope="col">Surname</th>
+              <th scope="col" abbr="firstName" onClick={orderName}>
+                First name
+              </th>
+              <th scope="col" abbr="surname" onClick={orderName}>
+                Surname
+              </th>
               <th scope="col">Email Address</th>
-              <th scope="col">Room id</th>
+              <th scope="col" abbr="roomId" onClick={orderNumber}>
+                Room id
+              </th>
               <th scope="col">Check in date</th>
               <th scope="col">Check out date</th>
               <th scope="col">Number of nights</th>
