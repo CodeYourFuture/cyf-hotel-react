785479e9230a6d8fc01706a265a8fe14f78499d0
youweb3 <yousef.shaikhan@gmail.com>
2022-12-07 03:35:01 +0000

    lesson 1

--- src/Lesson1/TouristInfoCards.js
+++ src/Lesson1/TouristInfoCards.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Glasgow from "../image/Glasgow.jpg";
+import Glasgow from "../image/glasgow.jpg";
 import Manchester from "../image/manchester.jpg";
-import London from "../image/london.jpeg";
+import London from "../image/london.jpg";
 
 export default function TouristInfoCards() {
   return (
@@ -30,10 +30,7 @@ export default function TouristInfoCards() {
             Manchester is a major city in the northwest of England with a rich
             industrial heritage. The Castlefield conservation area’s
             18th-century canal system recalls the city’s days as a textile
-            powerhouse, and visitors can trace this history at the interactive
-            Museum of Science & Industry. The revitalised Salford Quays
-            dockyards now house the Daniel Libeskind-designed Imperial War
-            Museum North and the Lowry cultural centre
+            powerhouse.
           </p>
           <a href="http://visitmanchester.com" className="btn btn-primary">
             More Informations
@@ -46,13 +43,9 @@ export default function TouristInfoCards() {
         <div className="card-body">
           <h4>London</h4>
           <p>
-            London, the capital of England and the United Kingdom, is a
-            21st-century city with history stretching back to Roman times. At
-            its centre stand the imposing Houses of Parliament, the iconic ‘Big
-            Ben’ clock tower and Westminster Abbey, site of British monarch
-            coronations. Across the Thames River, the London Eye observation
-            wheel provides panoramic views of the South Bank cultural complex,
-            and the entire city
+            London, the capital of England and the United Kingdom, At its centre
+            stand the imposing Houses of Parliament, the iconic ‘Big Ben’ clock
+            tower and Westminster Abbey.
           </p>
           <a href="http://visitlondon.com" className="btn btn-primary">
             More Informations
