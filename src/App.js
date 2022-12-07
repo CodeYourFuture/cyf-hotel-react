c27e5f8a0d9f64b1ef030b8bb7c475592c0d0ab3
Anosh Malik <anoshmalik.am@gmail.com>
2022-12-07 20:54:05 +0000

    Done lesson 1 - Part 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-// npm import React from "react";
+import React from "react";
 
 import Bookings from "./Bookings";
+import Heading from "./Heading";
 import "./App.css";
-import Logo from "./Logo";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      {/* <header className="App-header">CYF Hotel</header> */}
+      <Heading />
       <Bookings />
-      <Logo />
     </div>
   );
 };
 
-const CaughtPokemon = () => {
-  let newDate = new Date().toLocaleDateString();
-  return <p>Caught 0 Pokemon on {newDate}</p>;
-};
 export default App;
