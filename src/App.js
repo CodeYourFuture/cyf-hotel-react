1c93c6cc421005482bd5d4100e814bed9ccccce1
EABarton123 <83503987+EABarton123@users.noreply.github.com>
2022-12-08 16:49:12 +0000

    0-7 completed-having issues with moment

--- src/App.js
+++ src/App.js
@@ -12,76 +12,12 @@ const footerInformation = [
   "0123 456789"
 ];
 
-const bookingInformation = [
-  {
-    id: "001",
-    title: "Miss",
-    firstName: "Kimberley",
-    surname: "Kangur",
-    email: "kim.kangur@hotmail.co.uk",
-    roomId: "34",
-    checkInDate: "22/12/2022",
-    checkOutDate: "27/12/2022"
-  },
-  {
-    id: "002",
-    title: "Mr",
-    firstName: "Maziar",
-    surname: "Mansouri",
-    email: "m.mansouri@googlemail.com",
-    roomId: "25",
-    checkInDate: "28/12/2022",
-    checkOutDate: "01/01/2023"
-  },
-  {
-    id: "003",
-    title: "Mrs",
-    firstName: "Emily",
-    surname: "Barton",
-    email: "emkangur@hotmail.co.uk",
-    roomId: "19",
-    checkInDate: "22/12/2022",
-    checkOutDate: "28/12/2022"
-  },
-  {
-    id: "004",
-    title: "Mr",
-    firstName: "Zaw",
-    surname: "Manx",
-    email: "z.manx@hotmail.co.uk",
-    roomId: "15",
-    checkInDate: "22/12/2022",
-    checkOutDate: "23/12/2022"
-  },
-  {
-    id: "004",
-    title: "Miss",
-    firstName: "Amalia",
-    surname: "Bronker",
-    email: "Ams234Bron@hotmail.co.uk",
-    roomId: "10",
-    checkInDate: "12/12/2022",
-    checkOutDate: "15/12/2022"
-  },
-  {
-    id: "005",
-    title: "Mr",
-    firstName: "Sam",
-    surname: "Meadows",
-    email: "sammeadows25@gmail.com",
-    roomId: "12",
-    checkInDate: "04/12/2022",
-    checkOutDate: "05/12/2022"
-  }
-];
-
 const App = () => {
   return (
     <div className="App">
       <Heading />
       <TouristInfoCards />
       <Bookings />
-      <SearchResults />
       <Footer footerInformation={footerInformation} />
     </div>
   );
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,7 +1,51 @@
 import React from "react";
+{
+  /*import Moment from "moment";*/
+}
 
-const SearchResults = () => {
-  return <table />;
-};
+{
+  /*function numberOfDays(checkIn, checkOut) {
+  let date1 = moment(checkIn);
+  let date2 = moment(checkOut);
+  return date1.diff(date2, "days");
+}*/
+}
+
+function SearchResults(props) {
+  return (
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Number</th>
+          <th scope="col">Title</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Surname</th>
+          <th scope="col">E-mail</th>
+          <th scope="col">Room Id</th>
+          <th scope="col">Check in Date</th>
+          <th scope="col">Check out Date</th>
+          <th scope="col">Number Of Days</th>
+        </tr>
+      </thead>
+      {props.fakeBookings.map(booking => {
+        return (
+          <tbody>
+            <tr>
+              <td>{booking.id}</td>
+              <td>{booking.title}</td>
+              <td>{booking.firstName}</td>
+              <td>{booking.surname}</td>
+              <td>{booking.email}</td>
+              <td>{booking.roomId}</td>
+              <td> {booking.checkInDate}</td>
+              <td>{booking.checkOutDate}</td>
+              <td>{booking.checkInDate}</td>
+            </tr>
+          </tbody>
+        );
+      })}
+    </table>
+  );
+}
 
 export default SearchResults;
