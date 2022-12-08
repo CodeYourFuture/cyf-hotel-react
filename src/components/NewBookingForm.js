4c8793561bba9afec6961c29d462d08cc1711d0c
SaadiaELF <saadiaelfekak@gmail.com>
2022-12-08 14:44:20 +0000

    refactored code

--- src/components/NewBookingForm.js
+++ src/components/NewBookingForm.js
@@ -1,25 +1,35 @@
 import React from "react";
 
 const NewBookingForm = () => {
+  //   const [customerData, setCustomerData] = useState({
+  //     firstName: "",
+  //     surname: "",
+  //     title: "",
+  //     roomId: "",
+  //   });
   return (
     <form className="m-5 p-5">
       <h3>New Booking Form</h3>
-      <div className="row">
-        <div className="form-group col mb-2">
+      <div className="row mb-2">
+        <div className="form-group col-sm-2 mb-2">
           <label htmlFor="title">Title</label>
           <input type="text" className="form-control" id="title" />
         </div>
-        <div className="form-group col mb-2">
+        <div className="form-group col-sm-3 mb-2">
           <label htmlFor="first-name">First Name</label>
           <input type="text" className="form-control" id="first-name" />
         </div>
-        <div className="form-group col mb-2">
+        <div className="form-group col-sm-3 mb-2">
           <label htmlFor="surname">Surname</label>
           <input type="text" className="form-control" id="surname" />
         </div>
+        <div className="form-group col mb-2">
+          <label htmlFor="email">Email</label>
+          <input type="email" className="form-control" id="email" />
+        </div>
       </div>
       <div className="row">
-        <div className="form-group col">
+        <div className="form-group col-sm-2">
           <label htmlFor="room-id">Room Id</label>
           <input type="number" className="form-control" id="room-id" />
         </div>
