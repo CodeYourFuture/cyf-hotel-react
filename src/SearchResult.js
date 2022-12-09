e8063f44b96a74f0b7536aee573745b39d6ee533
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2022-12-09 20:02:28 +0000

    15. Highlight booking row when clicked

---
+++ src/SearchResult.js
@@ -0,0 +1,36 @@
+import Moment from "react-moment";
+import { useState } from "react";
+function SearchResult({ item }) {
+  const [select, setSelect] = useState(true);
+  const toggleSelecter = () => {
+    setSelect(select => !select);
+  };
+  return (
+    <tr
+      onClick={toggleSelecter}
+      style={{
+        backgroundColor: select ? "white" : "blue",
+        color: select ? "black" : "white"
+      }}
+    >
+      <th scope="row">{item.id}</th>
+      <td>{item.title}</td>
+      <td>{item.firstName}</td>
+      <td>{item.surname}</td>
+      <td>{item.email}</td>
+      <td>{item.roomId}</td>
+      <td>
+        <Moment format="DD-MM-YYYY">{item.checkInDate}</Moment>
+      </td>
+      <td>
+        <Moment format="DD-MM-YYYY">{item.checkOutDate}</Moment>
+      </td>
+      <td>
+        <Moment diff={item.checkInDate} unit="days">
+          {item.checkOutDate}
+        </Moment>
+      </td>
+    </tr>
+  );
+}
+export default SearchResult;
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,5 +1,7 @@
 import React from "react";
-import Moment from "react-moment";
+
+import SearchResult from "./SearchResult";
+
 function SearchResults({ results }) {
   return (
     <div id="searchResults">
@@ -18,29 +20,9 @@ function SearchResults({ results }) {
           </tr>
         </thead>
         <tbody>
-          {results.map((item, i) => {
-            return (
-              <tr key={i}>
-                <th scope="row">{item.id}</th>
-                <td>{item.title}</td>
-                <td>{item.firstName}</td>
-                <td>{item.surname}</td>
-                <td>{item.email}</td>
-                <td>{item.roomId}</td>
-                <td>
-                  <Moment format="DD-MM-YYYY">{item.checkInDate}</Moment>
-                </td>
-                <td>
-                  <Moment format="DD-MM-YYYY">{item.checkOutDate}</Moment>
-                </td>
-                <td>
-                  <Moment diff={item.checkInDate} unit="days">
-                    {item.checkOutDate}
-                  </Moment>
-                </td>
-              </tr>
-            );
-          })}
+          {results.map((item, i) => (
+            <SearchResult key={i} item={item} />
+          ))}
         </tbody>
       </table>
     </div>
