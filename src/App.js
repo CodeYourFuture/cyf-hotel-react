2bc83a69a34a5167e5b5c3245dc9c9c6ca3c9bc7
Miladepour <epour.milad@gmail.com>
2022-12-09 19:04:38 +0000

    Lesson 2.8
    -Render the Restaurant component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ import Header from "./Header";
 import TouristInfoCards from "./TouristInfoCards";
 import Footer from "./Footer";
 import SearchResults from "./SearchResults";
+import Restaurant from "./Restaurant";
 
 const App = () => {
-  const info = ["123 Fake Street, London, E1 4UD", "hello@fakehotel.com", "0123 456789"]
+  const info = [
+    "123 Fake Street, London, E1 4UD",
+    "hello@fakehotel.com",
+    "0123 456789"
+  ];
 
   return (
     <div className="App">
@@ -15,6 +20,7 @@ const App = () => {
       <TouristInfoCards />
       <Bookings />
       <SearchResults />
+      <Restaurant />
       <Footer footerInfo={info} />
     </div>
   );
