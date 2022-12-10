dbb0cc008300f5680a149bda91544fbbf631159a
Miladepour <epour.milad@gmail.com>
2022-12-10 16:02:18 +0000

    Lesson 2:15
    -Highlight booking row when clicked

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import FakeBooking from "./data/fakeBookings.json";
 import Moment from "react-moment";
 
@@ -28,26 +28,40 @@ const SearchTable = ({ searchTableBooking }) => {
   return (
     <tbody>
       {searchTableBooking.map((item, index) => (
-        <tr key={index}>
-          <td>{item.title}</td>
-          <td>{item.firstName}</td>
-          <td>{item.surname}</td>
-          <td>{item.email}</td>
-          <td>{item.id}</td>
-          <td>
-            <Moment format="DD-MM-YYYY">{item.checkInDate}</Moment>
-          </td>
-          <td>
-            <Moment format="DD-MM-YYYY">{item.checkOutDate}</Moment>
-          </td>
-          <td>
-            <Moment diff={item.checkInDate} unit="days">
-              {item.checkOutDate}
-            </Moment>
-          </td>
-        </tr>
+        <BookingRow key={index} item={item} />
       ))}
     </tbody>
+  );
+};
+
+const BookingRow = ({ item, index }) => {
+  const [active, setActive] = useState(false);
+  function makeItActive() {
+    setActive(!active);
+  }
+  return (
+    <tr
+      key={index}
+      className={active ? "activeRow" : ""}
+      onClick={makeItActive}
+    >
+      <td>{item.title}</td>
+      <td>{item.firstName}</td>
+      <td>{item.surname}</td>
+      <td>{item.email}</td>
+      <td>{item.id}</td>
+      <td>
+        <Moment format="DD-MM-YYYY">{item.checkInDate}</Moment>
+      </td>
+      <td>
+        <Moment format="DD-MM-YYYY">{item.checkOutDate}</Moment>
+      </td>
+      <td>
+        <Moment diff={item.checkInDate} unit="days">
+          {item.checkOutDate}
+        </Moment>
+      </td>
+    </tr>
   );
 };
 
