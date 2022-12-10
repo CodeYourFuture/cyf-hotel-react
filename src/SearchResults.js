1fb5cb067080f324b76a75ba2bad557091139e3e
ShimenAfshar <afsharshimen@gmail.com>
2022-12-10 16:23:31 +0000

    add SearchResults file

---
+++ src/SearchResults.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Moment from "moment";
+import SearchResult from "./SearchResult";
+
+function SearchResults({ results }) {
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">title</th>
+            <th scope="col">firstName</th>
+            <th scope="col">surName</th>
+            <th scope="col">email</th>
+            <th scope="col">roomId</th>
+            <th scope="col">checkInDate</th>
+            <th scope="col">checkOutDate</th>
+            <th scope="col">numberOfNight</th>
+          </tr>
+        </thead>
+        <tbody>
+          {results.map((item, i) => {
+            return <SearchResult key={i} item={item} checkDiff={checkDiff} />;
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+let checkDiff = (InDate, OutDate) => {
+  const checkInDate = Moment(InDate, "YYYY-MM-DD");
+  const checkOutDate = Moment(OutDate, "YYYY-MM-DD");
+  const diff = checkOutDate.diff(checkInDate, "days");
+  return diff;
+};
+export default SearchResults;
