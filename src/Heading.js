925c88935da0e117b389ae2b34b50bcff903766b
hayatu4islam <hayatu4islam@yahoo.com>
2022-12-10 00:34:14 +0000

    updated component

---
+++ src/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div>
+      {/* <header className="App-header">CYF Hotel</header> */}
+      <img
+        id="hotel-logo"
+        src="https://thumbs.dreamstime.com/z/hotel-logo-template-background-58362974.jpg"
+        alt="Hotel header"
+      />
+    </div>
+  );
+};
+export default Heading;
