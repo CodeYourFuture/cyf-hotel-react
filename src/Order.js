b35b394ff2c2b3e0e2aecdbb7ff65c9cbc06e3c4
Milad E-Pour <104828541+Miladepour@users.noreply.github.com>
2022-12-10 12:26:03 +0000

    Update src/Order.js
    Changes Applied.
    
    Co-authored-by: MitchLloyd <mitchelllloyd154@gmail.com>

--- src/Order.js
+++ src/Order.js
@@ -1,4 +1,4 @@
-import react, { useState } from "react";
+import React, { useState } from "react";
 import RestaurantButton from "./RestaurantButton";
 const Order = () => {
   const [order, setOrder] = useState(0);
