8027b65bf5626bdfed4331e236722a2e48966e30
Miladepour <epour.milad@gmail.com>
2022-12-16 08:40:41 +0000

    Update CSS and some classes

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -4,7 +4,7 @@ import Order from "./Order";
 const Restaurant = () => {
   return (
     <div>
-      <h3>Restaurant Orders</h3>
+      <h3 className="container">Restaurant Orders</h3>
       <ul className="d-flex justify-content-around">
         <Order orderType={"Pizzas"} />
         <Order orderType={"Salads"} />
