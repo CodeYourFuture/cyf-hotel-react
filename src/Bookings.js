c223137dd88bb77f77751e9f2a2b8b6b557879b3
Doris-Siu <doris.siu.my@gmail.com>
2022-12-16 21:52:49 +0000

    Complete Lesson3 #16

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
@@ -8,8 +8,14 @@ const Bookings = () => {
     console.info("TO DO!", searchVal);
   };
 
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
 
+  useEffect(() => {
+    console.log("use effect...");
+    fetch("https://cyf-react.glitch.me")
+      .then(res => res.json())
+      .then(data => setBookings(data));
+  }, []);
   return (
     <div className="App-content">
       <div className="container">
