eea0a4f3db030cc339248261c446d917f5b8e612
Doris-Siu <doris.siu.my@gmail.com>
2022-12-16 22:03:21 +0000

    Complete Lesson3 #17

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchButton from "./SearchButton";
 
 const Search = () => {
+  const [searchInput, setsearchInput] = useState("");
+
+  function handleSearchInput(e) {
+    setsearchInput(e.target.value);
+    console.log(e.target.value);
+  }
+
   return (
     <div className="search">
       <div className="page-header">
@@ -17,6 +24,8 @@ const Search = () => {
                 id="customerName"
                 className="form-control"
                 placeholder="Customer name"
+                value={searchInput}
+                onChange={handleSearchInput}
               />
               {/* <button className="btn btn-primary">Search</button> */}
               <SearchButton />
