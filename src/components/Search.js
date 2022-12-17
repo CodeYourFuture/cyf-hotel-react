ec0e1326b27179a0bef4aa902ee12d21225e505b
levilu800b <levilu800b@gmail.com>
2022-12-17 16:10:24 +0000

    fixed seach errors

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchButton from "../components/searchButton";
 
-const Search = () => {
+const Search = ({ search }) => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (searchInput) {
+      search(searchInput);
+    }
+  };
+
   return (
     <div className="search">
       <div className="page-header">
@@ -9,7 +18,7 @@ const Search = () => {
       </div>
       <div className="row search-wrapper">
         <div className="col">
-          <form className="form-group search-box">
+          <form className="form-group search-box" onSubmit={handleSubmit}>
             <label htmlFor="customerName">Customer name</label>
             <div className="search-row">
               <input
@@ -17,6 +26,8 @@ const Search = () => {
                 id="customerName"
                 className="form-control"
                 placeholder="Customer name"
+                onChange={e => setSearchInput(e.target.value)}
+                value={searchInput}
               />
               <SearchButton />
             </div>
