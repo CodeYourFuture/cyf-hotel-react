e521a89778357ff2f83e7997c86cfefb15a81697
levilu800b <levilu800b@gmail.com>
2022-12-17 16:56:30 +0000

    changes

--- src/components/TouristInfoCards.js
+++ src/components/TouristInfoCards.js
@@ -5,7 +5,7 @@ const cardData = [
     id: 1,
     title: "Glasgow",
     img:
-      "https://images.unsplash.com/photo-1621519604512-85ea63c15ca2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2xhc2dvd3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      "https://images.unsplash.com/photo-1594309140264-5d7a7787f0c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTgyfHxnbGFzZ293fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     desc:
       "Glasgow is the largest city in Scotland, and third largest in the United Kingdom.",
     link: "https://peoplemakeglasgow.com/"
@@ -15,7 +15,7 @@ const cardData = [
     id: 2,
     title: "Manchester",
     img:
-      "https://images.unsplash.com/photo-1597740049284-388659a41286?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFuY2hlc3RlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      "https://images.unsplash.com/photo-1618165987743-825267de36fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTkwfHxtYW5jaGVzdGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     desc:
       "Manchester is a city and metropolitan borough in Greater Manchester, England, with a population of 530,300 as of 2019.",
     link: "https://visitmanchester.com/"
@@ -25,7 +25,7 @@ const cardData = [
     id: 3,
     title: "London",
     img:
-      "https://images.unsplash.com/photo-1529655683826-aba9b3e77383?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bG9uZG9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      "https://images.unsplash.com/photo-1534374950034-3644ddb72710?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGxvbmRvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
     desc:
       "London is the capital and largest city of England and the United Kingdom.",
     link: "https://visitlondon.com/"
