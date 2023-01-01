a98276cea41c0f4b25134cc900b93faa08d8ee36
levilu800b <levilu800b@gmail.com>
2023-01-01 20:46:40 +0000

    color changes

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -7,6 +7,7 @@ const Restaurant = () => {
       <ul>
         <Order orderType={"Pasta"} />
         <Order orderType={"Salads"} />
+        <Order orderType={"Chocolate cake"} />
         <Order orderType={"Chicken"} />
         <Order orderType={"Vanilla Ice cream"} />
       </ul>
