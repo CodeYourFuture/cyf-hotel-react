3bdb2e9f79857891f072fd8c01ee0fdc0f451bc4
azadehroshan <105980239+azadehroshan@users.noreply.github.com>
2023-01-06 19:55:11 +0000

    Update Bookings.js

--- src/Bookings.js
+++ src/Bookings.js
@@ -6,22 +6,21 @@ import SearchResults from "./components/SearchResults.js";
 const Bookings = () => {
   // const [bookings, setBookings] = useState(FakeBookings);
   const [bookings, setBookings] = useState([]);
-  // const [booksStatus, setBooksStatus] = useState(false);
   const [loading, setLoading] = useState(true);
   // const [error, setError] = useState(false);
 
   useEffect(() => {
     fetch("https://cyf-react.glitch.me/delayed")
       .then(response => {
+        console.log(response);
         if (response.status >= 200 && response.status < 200) {
           throw new Error("this site is not avaliable for now");
         }
-        return Response.json();
+        return response.json();
       })
       .then(data => {
         setBookings(data);
         setLoading(false);
-        // setBooksStatus(true);
       })
       .catch(error => console.log(error));
   }, []);
@@ -39,8 +38,6 @@ const Bookings = () => {
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        {/* <SearchResults results={FakeBookings} /> */}
-        {/* ternary operator */}
         {loading ? "loading" : <SearchResults results={bookings} />}
       </div>
     </div>
