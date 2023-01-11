aa7d5a20c2325baae27746f717518438c00b2b39
OmidOmar <a.omidomari@gmail.com>
2023-01-11 15:45:08 +0000

    build 1.3

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = process.env.PORT || 3001;
+
+app.use(express.static(path.resolve(__dirname, "../build")));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello From Server" });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../build", "index.html"));
 });
 
 app.listen(port, () => {
