0058e1cddad3ffd8c11d29056586554c4c13eb0b
OmidOmar <a.omidomari@gmail.com>
2023-01-13 16:40:54 +0000

    create a new booking done!

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 3001;
 const bookingData = require("./booking.json");
+const fs = require("fs");
+
+let data = JSON.parse(fs.readFileSync(__dirname + "/booking.json", "utf-8"));
+let maxID = Math.max(...data.map(c => c.id));
 
 app.use(express.static(path.resolve(__dirname, "../build")));
 app.use(express.urlencoded({ extended: true }));
@@ -10,16 +14,37 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 app.get("/api/delayed", async (req, res) => {
   await sleep(500);
-  res.json(bookingData);
+  res.json(data);
 });
 
-app.post("/guest", async (req, res) => {
+app.get("/", (req, res) => {
+  res.json(data);
+});
+
+app.post("/guest/registration", async (req, res) => {
   await sleep(1000);
-  console.log(req.body);
+  const newRegistration = {
+    id: ++maxID,
+    title: req.body.title,
+    firstName: req.body.firstName,
+    surname: req.body.surName,
+    email: req.body.email,
+    roomId: req.body.roomID,
+    checkInDate: req.body.checkInDate,
+    checkOutDate: req.body.checkOutDate
+  };
+  data.push(newRegistration);
+  save();
+  res.status(200).sendFile(path.resolve(__dirname, "../build", "index.html"));
+
   //res.sendFile(path.resolve(__dirname, "../build", "index.html"));
 });
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../build", "index.html"));
 });
+
+const save = () => {
+  fs.writeFileSync(__dirname + "/booking.json", JSON.stringify(data, null, 2));
+};
 
 app.listen(port);
--- src/GuestsBooking.js
+++ src/GuestsBooking.js
@@ -3,8 +3,14 @@ import React from "react";
 const GuestsBooking = () => {
   return (
     <div>
-      <form method="POST" action="/guest">
-        <input type="text" name="userName" />
+      <form method="POST" action="/guest/registration">
+        <input type="text" name="title" placeholder="title" />
+        <input type="text" name="firstName" placeholder="First Name" />
+        <input type="text" name="surName" placeholder="Last Name" />
+        <input type="text" name="email" placeholder="Email" />
+        <input type="text" name="roomID" placeholder="Room ID" />
+        <input type="date" name="checkInDate" placeholder="Check in Date" />
+        <input type="date" name="checkOutDate" placeholder="CHeck out Date" />
         <input type="submit" class="btn btn-primary" />
       </form>
     </div>
