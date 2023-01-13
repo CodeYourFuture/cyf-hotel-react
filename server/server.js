179bd8dff335bf4bc66ad36cd29cef83e856d170
OmidOmar <a.omidomari@gmail.com>
2023-01-13 18:24:21 +0000

    level 1 done!

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = process.env.PORT || 3001;
-const bookingData = require("./booking.json");
+
 const fs = require("fs");
 
-let data = JSON.parse(fs.readFileSync(__dirname + "/booking.json", "utf-8"));
-let maxID = Math.max(...data.map(c => c.id));
+let bookings = JSON.parse(
+  fs.readFileSync(__dirname + "/booking.json", "utf-8")
+);
+let maxID = Math.max(...bookings.map(c => c.id));
 
 app.use(express.static(path.resolve(__dirname, "../build")));
 app.use(express.urlencoded({ extended: true }));
@@ -14,29 +16,46 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 app.get("/api/delayed", async (req, res) => {
   await sleep(500);
-  res.json(data);
+  res.json(bookings);
 });
 
-app.get("/", (req, res) => {
-  res.json(data);
+app.get("/bookings", (req, res) => {
+  res.json(bookings);
+});
+
+app.post("/validation", (req, res) => {
+  //console.log(Object.keys(req.body).every((e) => req.body[e]));
+  //const date = new Date().toLocaleDateString();
+  console.log(req.body);
+});
+
+app.get("/bookings/:id", (req, res) => {
+  if (!parseInt(req.params.id)) {
+    res.status(404).send({ message: "Not Found" });
+    return;
+  }
+  const searchByID = bookings.filter(e => e.id == req.params.id);
+  if (searchByID.length > 0) res.status(200).json(searchByID);
+  else res.status(404).send({ message: "Not Found" });
 });
 
 app.post("/guest/registration", async (req, res) => {
   await sleep(1000);
-  const newRegistration = {
-    id: ++maxID,
-    title: req.body.title,
-    firstName: req.body.firstName,
-    surname: req.body.surName,
-    email: req.body.email,
-    roomId: req.body.roomID,
-    checkInDate: req.body.checkInDate,
-    checkOutDate: req.body.checkOutDate
-  };
-  data.push(newRegistration);
-  save();
-  res.status(200).sendFile(path.resolve(__dirname, "../build", "index.html"));
-
+  if (Object.keys(req.body).every(e => req.body[e])) {
+    const newRegistration = {
+      id: ++maxID,
+      title: req.body.title,
+      firstName: req.body.firstName,
+      surname: req.body.surName,
+      email: req.body.email,
+      roomId: req.body.roomID,
+      checkInDate: req.body.checkInDate,
+      checkOutDate: req.body.checkOutDate
+    };
+    bookings.push(newRegistration);
+    save();
+    res.status(200).sendFile(path.resolve(__dirname, "../build", "index.html"));
+  } else res.status(400).json({ message: "Bad Request" });
   //res.sendFile(path.resolve(__dirname, "../build", "index.html"));
 });
 app.get("*", (req, res) => {
@@ -44,7 +63,10 @@ app.get("*", (req, res) => {
 });
 
 const save = () => {
-  fs.writeFileSync(__dirname + "/booking.json", JSON.stringify(data, null, 2));
+  fs.writeFileSync(
+    __dirname + "/booking.json",
+    JSON.stringify(bookings, null, 2)
+  );
 };
 
 app.listen(port);
--- src/GuestsBooking.js
+++ src/GuestsBooking.js
@@ -1,17 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import moment from "moment";
 
 const GuestsBooking = () => {
+  const date = moment().format("YYYY-MM-DD");
+
+  const [checkOutDate, setCheckOutDate] = useState(date);
+
+  const handleDate = e => {
+    setCheckOutDate(
+      moment(e.target.value, "YYYY-MM-DD")
+        .add(1, "days")
+        .format("YYYY-MM-DD")
+    );
+  };
   return (
-    <div>
+    <div className="border border-info">
+      <h1 className="lead">Guests Booking Form</h1>
       <form method="POST" action="/guest/registration">
-        <input type="text" name="title" placeholder="title" />
-        <input type="text" name="firstName" placeholder="First Name" />
-        <input type="text" name="surName" placeholder="Last Name" />
-        <input type="text" name="email" placeholder="Email" />
-        <input type="text" name="roomID" placeholder="Room ID" />
-        <input type="date" name="checkInDate" placeholder="Check in Date" />
-        <input type="date" name="checkOutDate" placeholder="CHeck out Date" />
-        <input type="submit" class="btn btn-primary" />
+        <select className="form-control" name="title">
+          <option>Mr.</option>
+          <option>Ms.</option>
+          <option>Mrs.</option>
+          <option>Dr.</option>
+        </select>
+        {/*action="/guest/registration">*/}
+        <input
+          className="form-control"
+          type="text"
+          name="firstName"
+          placeholder="First Name"
+        />
+        <input
+          className="form-control"
+          type="text"
+          name="surName"
+          placeholder="Last Name"
+        />
+        <input
+          className="form-control"
+          type="email"
+          name="email"
+          placeholder="Email"
+        />
+        <input
+          className="form-control"
+          type="text"
+          name="roomID"
+          placeholder="Room ID"
+        />
+        <input
+          className="form-control"
+          type="date"
+          min={date}
+          name="checkInDate"
+          onChange={handleDate}
+          placeholder="Check in Date"
+        />
+        <input
+          className="form-control"
+          type="date"
+          min={checkOutDate}
+          name="checkOutDate"
+          placeholder="CHeck out Date"
+        />
+        <input type="submit" className="btn btn-primary form-control" />
       </form>
     </div>
   );
