29b7c4ddd0de15bd7f089cefa069d1ed6750cff7
Anosh Malik <anoshmalik.am@gmail.com>
2023-01-23 16:31:34 +0000

    aligned cards horizontally using bootstrap row and col classes

--- src/TouristInfoCards.jsx
+++ src/TouristInfoCards.jsx
@@ -56,51 +56,65 @@ class TouristInfoCards extends React.Component {
 
   render() {
     return (
-      <div>
-        <div className="card">
-          <img
-            alt="glasgow"
-            src="https://webunwto.s3.eu-west-1.amazonaws.com/s3fs-public/2022-03/the-glasgow-declaration-on-climate-action-in-tourism-sm.jpg"
-            className="card-img-top"
-          />
-          <div className="card-body">
-            <a
-              href="http://www.peoplemakeglasgow.com"
-              className="btn btn-primary"
-            >
-              Visit Glasgow
-            </a>
-          </div>
-        </div>
+      <div className="container">
+        <hr></hr>
+        <div className="row">
+          <div className="col">
+            <div className="card">
+              <img
+                alt="glasgow"
+                src="https://webunwto.s3.eu-west-1.amazonaws.com/s3fs-public/2022-03/the-glasgow-declaration-on-climate-action-in-tourism-sm.jpg"
+                className="card-img-top"
+              />
 
-        <div className="card">
-          <img
-            alt="manchester"
-            src="https://www.countrysidepartnerships.com/sites/default/files/styles/hero_mobile/public/media/2019-10/AdobeStock_78727861.jpeg.webp?itok=fSffe8ay&cache-buster=a684714d75&coords_hash=cec3070d8e"
-            className="card-img-top"
-          />
-          <div className="card-body">
-            <a
-              href="http://www.visitmanchester.com"
-              className="btn btn-primary"
-            >
-              Visit Manchester
-            </a>
+              <div className="card-body">
+                <a
+                  href="http://www.peoplemakeglasgow.com"
+                  className="btn btn-primary"
+                >
+                  Visit Glasgow
+                </a>
+              </div>
+            </div>
           </div>
-        </div>
+          <div className="col">
+            <div className="card">
+              <img
+                alt="manchester"
+                src="https://www.countrysidepartnerships.com/sites/default/files/styles/hero_mobile/public/media/2019-10/AdobeStock_78727861.jpeg.webp?itok=fSffe8ay&cache-buster=a684714d75&coords_hash=cec3070d8e"
+                className="card-img-top"
+              />
 
-        <div className="card">
-          <img
-            alt="london"
-            src="https://image.arrivalguides.com/500x500/09/1dd23cc06c31c31ba7df72f2c74db5bc.jpg"
-            className="card-img-top"
-          />
-          <div className="card-body">
-            <a href="http://www.visitlondon.com" className="btn btn-primary">
-              Visit London
-            </a>
+              <div className="card-body">
+                <a
+                  href="http://www.visitmanchester.com"
+                  className="btn btn-primary"
+                >
+                  Visit Manchester
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col">
+            <div className="card">
+              <img
+                alt="london"
+                src="https://image.arrivalguides.com/500x500/09/1dd23cc06c31c31ba7df72f2c74db5bc.jpg"
+                className="card-img-top"
+              />
+              <div className="card-body">
+                <a
+                  href="http://www.visitlondon.com"
+                  className="btn btn-primary"
+                >
+                  Visit London
+                </a>
+              </div>
+            </div>
           </div>
         </div>
+        <hr></hr>
       </div>
     );
   }
