30bffa3a093249f8d24ea87bf83594eaad8e4d0e
Anosh Malik <anoshmalik.am@gmail.com>
2023-01-24 11:41:05 +0000

    16 Finished

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.jsx";
 import FakeBookings from "./data/fakeBookings.json";
@@ -8,13 +8,22 @@ const Bookings = () => {
     console.info("TO DO!", searchVal);
   };
 
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data);
+        setBookings(data);
+      });
+  });
 
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults results={bookings} meh={[1, 2, 4, 5888]} />
+        <SearchResults results={bookings} />
       </div>
     </div>
   );
