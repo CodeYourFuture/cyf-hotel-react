8859f9a79a0c24d5d30e3b93975d9d4227dc8903
Anosh Malik <anoshmalik.am@gmail.com>
2023-02-07 14:06:04 +0000

    Section 21 fully functional

--- src/CustomerProfile.jsx
+++ src/CustomerProfile.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const CustomerProfile = props => {
   // console.log(props.id);
 
-  return <p>Customer {props.id} Profile</p>;
+  return (
+    <div>
+      <p>Customer {props.name}</p>
+      <p>Email - {props.email}</p>
+      <p>Status - {props.vip}</p>
+      <p>Contact - {props.phone}</p>
+    </div>
+  );
 };
 
 export default CustomerProfile;
--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -2,94 +2,18 @@ import React, { useState, useEffect } from "react";
 import moment from "moment";
 import CustomerProfile from "./CustomerProfile";
 
-// class SearchResults extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       id: ""
-//     };
-//   }
-
-//   //   useEffect(() => {
-//   //     fetch(`https://cyf-react.glitch.me/customers/${this.state.id}`)
-//   //      .then(res => res.json())
-//   //       .then(data => {
-//   //         // console.log(data);
-//   //         setId(data);
-//   //       });
-//   // }, []);
-
-//   render() {
-//     return (
-//       <div>
-//         <table className="table">
-//           <thead>
-//             <tr>
-//               <th scope="col">ID</th>
-//               <th scope="col">Title</th>
-//               <th scope="col">Firstname</th>
-//               <th scope="col">Surname</th>
-//               <th scope="col">Email</th>
-//               <th scope="col">Room ID</th>
-//               <th scope="col">Check-in Date</th>
-//               <th scope="col">Check-out Date</th>
-//               <th scope="col">Nights</th>
-//               <th scope="col">Profile</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {this.props.results.map((result, index) => {
-//               return (
-//                 <tr key={index}>
-//                   <th>{result.id}</th>
-//                   <td>{result.title}</td>
-//                   <td>{result.firstName}</td>
-//                   <td>{result["surname"]}</td>
-//                   <td>{result["email"]}</td>
-//                   <td>{result.roomId}</td>
-//                   <td>{result["checkInDate"]}</td>
-//                   <td>{result["checkOutDate"]}</td>
-
-//                   <td>
-//                     {moment(result["checkOutDate"]).diff(
-//                       moment(result["checkInDate"]),
-//                       "days"
-//                     )}
-//                   </td>
-//                   <td>
-//                     <button
-//                       className={"btn btn-primary"}
-//                       onClick={() => this.setState({ id: result.id })}
-//                     >
-//                       Show Profile
-//                     </button>
-//                   </td>
-//                 </tr>
-//               );
-//             })}
-//           </tbody>
-//         </table>
-//         <CustomerProfile id={this.state.id} />
-//       </div>
-//     );
-//   }
-// }
-
-// export default SearchResults;
-
 const SearchResults = props => {
   const [id, setId] = useState(1);
-  const [data, setData] = useState("");
-  const [other, setOther] = useState("data");
+  // const [vip, setVip] = useState("");
+  // const [email, setEmail] = useState("");
+  const [datas, setDatas] = useState("");
 
   useEffect(() => {
     fetch(`https://cyf-react.glitch.me/customers/${id}`)
       .then(res => res.json())
       .then(data => {
         console.log(data);
-        setOther(data);
-        setData(data);
+        setDatas(data);
       });
   }, [id]);
 
@@ -142,7 +66,13 @@ const SearchResults = props => {
           })}
         </tbody>
       </table>
-      <CustomerProfile id={id} />
+      <CustomerProfile
+        name={`${datas.firstName} ${datas.surname}`}
+        id={datas.id}
+        email={datas.email}
+        vip={datas.vip ? "VIP" : "NOT VIP"}
+        phone={datas.phoneNumber}
+      />
     </div>
   );
 };
