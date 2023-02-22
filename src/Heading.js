5ab61da26eed8b3866dad930d3f38ba5e19b1de1
Bahare <itxbb09@gmail.com>
2023-02-22 13:05:44 +0000

    exe1,exe2 done

---
+++ src/Heading.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Heading = () => {
+  return (
+    <header>
+      <img
+        src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
+        alt="logo"
+      />
+    </header>
+  );
+};
+export default Heading;
---
+++ src/SearchButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SearchButton = () => {
+  return (
+    <div>
+      <button>Search</button>
+    </div>
+  );
+};
+export default SearchButton;
