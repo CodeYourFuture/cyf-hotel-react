d42c803ce56a0efe69d6e2d33e9d821230e30822
nataliizab <nataliia.zab@gmail.com>
2023-02-23 23:14:35 +0000

    Extracted the header

---
+++ src/Heading.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div>
+      <header className="App-header">CYF Hotel</header>
+      <img
+        className="App-logo"
+        src="https://syllabus.codeyourfuture.io/img/logo.png"
+        alt="CYF logo"
+        // style={{ width: "100px" }}
+      />
+    </div>
+  );
+};
+
+export default Heading;
