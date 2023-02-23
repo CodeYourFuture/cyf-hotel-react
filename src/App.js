dd5985cf07fd6581487cd1bd526e2f7ad1a1c378
Shayan Mahnam <shayan.mahnam.uk@gmail.com>
2023-02-23 17:15:28 +0000

    footer and cards added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Heading from "./Heading";
+import TouristInfoCards from "./TouristInfoCards";
 import Bookings from "./Bookings";
+import Footer from "./Footer";
 import "./App.css";
 
 const App = () => {
+  const contactDetails = [
+    "123 Fake Street, London, E1 4UD",
+    "hello@fakehotel.com",
+    "0123 456789"
+  ];
+
   return (
     <div className="App">
       <Heading />
+      <TouristInfoCards />
       <Bookings />
+      <Footer contactDetails={contactDetails} />
     </div>
   );
 };
