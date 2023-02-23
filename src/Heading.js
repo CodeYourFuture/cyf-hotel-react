979aadab8302c984d0538c86111e438f12638988
tony-dev-wm <ta.devopsuk@gmail.com>
2023-02-23 11:18:26 +0000

    step 2

---
+++ src/Heading.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div className="App">
+      <header className="App-header">tony Hotel</header>
+    </div>
+  );
+};
+export default Heading;
