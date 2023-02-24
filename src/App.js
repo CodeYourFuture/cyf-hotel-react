bb2818591b58444052f6929cbf218b2ab96b0451
Mo Mahboobian <momahboobian@gmail.com>
2023-02-24 22:57:31 +0000

    exercise 5

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
-
+import Heading from "./Heading";
+import Search from "./Search";
 import Bookings from "./Bookings";
+import TouristInfoCards from "./TouristInfoCards";
+import Footer from "./Footer";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Heading />
       <Bookings />
+      <TouristInfoCards />
+      <Footer />
     </div>
   );
 };
