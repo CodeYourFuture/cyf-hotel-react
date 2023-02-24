874ad6f0b5602b2a75b519867e5f634207eb02a7
nataliizab <nataliia.zab@gmail.com>
2023-02-24 22:11:36 +0000

    Added basic CSS to header

--- src/Heading.js
+++ src/Heading.js
@@ -2,13 +2,12 @@ import React from "react";
 
 const Heading = () => {
   return (
-    <div>
+    <div className="header-container">
       <header className="App-header">CYF Hotel</header>
       <img
         className="App-logo"
         src="https://syllabus.codeyourfuture.io/img/logo.png"
         alt="CYF logo"
-        // style={{ width: "100px" }}
       />
     </div>
   );
