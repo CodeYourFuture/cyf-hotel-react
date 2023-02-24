3bb9f2c462e2b5172df9aa9e024c87d00d41b1dd
farzaneh-haghani <farzaneh.haghani@gmail.com>
2023-02-24 02:18:48 +0000

    up to 3 done

---
+++ src/TouristInfoCards.js
@@ -0,0 +1,18 @@
+const TouristInfoCards = props => (
+  <div className="card-container">
+    {props.cities.map(eachCard => (
+      <div className="card">
+        <img src={eachCard.image} className="card-img card-img-top" />
+        <div className="card-body">
+          <h1>{eachCard.name}</h1>
+          <p>{eachCard.description}</p>
+          <a href={eachCard.information} className="btn btn-primary">
+            More Information
+          </a>
+        </div>
+      </div>
+    ))}
+  </div>
+);
+
+export default TouristInfoCards;
