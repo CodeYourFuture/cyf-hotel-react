b39878e7980ff121b226978e6fdf43a36cf1d9f4
elahemortazavi <emor5680@gmail.com>
2023-02-24 13:59:00 +0000

    app.css

--- src/Heading.js
+++ src/Heading.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from "./image/logo.jpg"
 
 const Heading = () => {
     return (
         <div>
              <header className="App-header">
                 <h1>CYF Hotel</h1>
-                <img className ="logo" alt = "logo" src = "../alex-seinet-8dmtbpJJJ7k-unsplash.jpg" />
+                <img className ="logo" src = {logo} alt = "logo"  />
              </header>
         </div>
     );
