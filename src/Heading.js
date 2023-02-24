e96646019e65db0253d336ab57b2e6144f3bb71d
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-02-24 08:16:35 +0000

    Adding logo and extracting searchButton

---
+++ src/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div className="App-header-image">
+      <header className="App-header">CYF Hotel</header>
+      <img
+        src="https://static.vecteezy.com/system/resources/previews/003/543/093/original/eagle-head-logo-template-on-black-background-vector.jpg"
+        alt="logo"
+      />
+    </div>
+  );
+};
+
+export default Heading;
