9dc629793b71d44646f3e8376a28252489f0f46d
elahemortazavi <emor5680@gmail.com>
2023-02-24 14:40:53 +0000

    exercise 3

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import london from "./image/london.jpg";
+import glasgow from "./image/glasgow.jpg";
+import manchester from "./image/manchester.jpg";
 
 const TouristInfoCards = () => {
     return (
-    <div className="card">
-	  <img src="..." className="card-img-top" />
-	     <div className="card-body">
-		   <a href="#" className="btn btn-primary">Go somewhere</a>
-	     </div>
+      <React.Fragment>
+     <div className="card">
+	      <img src = {london} className="card-img-top" />
+	      <div className="card-body">
+          <h1>LONDON</h1>
+          <p>London, the capital of England and the United Kingdom, is a 21st-century city with history stretching back to Roman times. At its centre stand the imposing Houses of Parliament, the iconic ‘Big Ben’ clock tower and Westminster Abbey, site of British monarch coronations.</p>
+		      <a href = "visitlondon.com" className="btn btn-primary">More Information</a>
+	      </div>
       </div>
+
+       <div className="card">
+	      <img src = {glasgow} className="card-img-top" />
+	      <div className="card-body">
+          <h1>GLASGOW</h1>
+          <p>Glasgow is a port city on the River Clyde in Scotland's western Lowlands. It's famed for its Victorian and art nouveau architecture, a rich legacy of the city's 18th–20th-century prosperity due to trade and shipbuilding.</p>
+		      <a href = "peoplemakeglasgow.com" className="btn btn-primary">More Information</a>
+	      </div>
+      </div>
+
+       <div className="card">
+	      <img src = {manchester} className="card-img-top" />
+	      <div className="card-body">
+          <h1>MANCHESTER</h1>
+          <p>Manchester is a major city in the northwest of England with a rich industrial heritage. The Castlefield conservation area’s 18th-century canal system recalls the city’s days as a textile powerhouse, and visitors can trace this history at the interactive Museum of Science & Industry.</p>
+		      <a href = "visitmanchester.com" className="btn btn-primary">More Information</a>
+	      </div>
+      </div>
+      </React.Fragment>
     );
 };
 
