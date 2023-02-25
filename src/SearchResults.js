3547e0a132ff91b365644d5ded3472d5e467d47d
MohammadSefidgar <108987757+MohammadSefidgar@users.noreply.github.com>
2023-02-25 16:12:26 +0000

    London 9 - Lovelace - Mohammad Sefidgar - React - Week 1
    Lesson 1

---
+++ src/SearchResults.js
@@ -0,0 +1,46 @@
+import SearchResult from "./SearchResult";
+import moment from "moment";
+
+const SearchResults = (props) => {
+  return (
+    <>
+      <table class="table">
+        <thead class="thead-dark">
+          <tr className="text-center">
+            <th scope="col">ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Room ID</th>
+            <th scope="col">Check in Date</th>
+            <th scope="col">Check out Date</th>
+            <th scope="col">Total Nights</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.results.map((user) => {
+            let a = moment(user.checkInDate, "YYYY-MM-DD");
+            let b = moment(user.checkOutDate, "YYYY-MM-DD");
+            return (
+              <SearchResult
+                key={user.id}
+                id={user.id}
+                title={user.title}
+                firstName={user.firstName}
+                surname={user.surname}
+                email={user.email}
+                roomId={user.roomId}
+                checkInDate={user.checkInDate}
+                checkOutDate={user.checkOutDate}
+                diff={b.diff(a, "days")}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default SearchResults;
---
+++ src/SearchRouls.js
@@ -0,0 +1,19 @@
+const SearchResult = (props) => {
+  return (
+    <>
+      <tr className="text-center">
+        <th scope="row">{props.id}</th>
+        <td>{props.title}</td>
+        <td>{props.firstName}</td>
+        <td>{props.surname}</td>
+        <td>{props.email}</td>
+        <td>{props.roomId}</td>
+        <td>{props.checkInDate}</td>
+        <td>{props.checkOutDate}</td>
+        <td>{props.diff}</td>
+      </tr>
+    </>
+  );
+};
+
+export default SearchResult;
