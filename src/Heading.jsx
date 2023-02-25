0583f79c9d38da94ee6511227e4b5c67cb8e3e94
Lorena <lorenacapraru@gmail.com>
2023-02-25 16:17:36 +0000

    Lesson 1 ->> T1, T2, T3

---
+++ src/Heading.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+
+export default function Heading() {
+  return (
+    <>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyGBQSH7WeH4J0CI96lrRMn8Y86JiCFeTVdA&usqp=CAU"
+        alt="logo"
+        className="imgLogo"
+      />
+      <header className="App-header">CYF Hotel</header>
+    </>
+  );
+}
