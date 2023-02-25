56c07a9e10587bd1c582692a9a06fca2998fc376
George <108883931+GeorgePrimentas@users.noreply.github.com>
2023-02-25 06:11:46 +0000

    Exercises 1 & 2

---
+++ src/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/201/201426.png"
+        alt="logo"
+      />
+      CYF Hotel
+    </header>
+  );
+};
+
+export default Heading;
---
+++ src/TouristInfoCards.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TouristInfoCards = props => {
+  return (
+    <div className="card">
+      <img src="..." className="card-img-top" />
+      <div className="card-body">
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
+};
