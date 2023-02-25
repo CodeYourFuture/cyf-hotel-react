4cdb86f67cc4a41590eef85af25a41ffdac41840
LexiisYing <nyxxing0824@gmail.com>
2023-02-25 16:03:26 +0000

    exercise 2 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-
+import Heading from "./Heading";
+import TouristInfoCards from "./TouristInfoCards";
 import Bookings from "./Bookings";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <TouristInfoCards />
       <Bookings />
+      <Heading />
     </div>
   );
 };
