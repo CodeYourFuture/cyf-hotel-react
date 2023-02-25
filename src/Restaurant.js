52c39407223ea117d134de16a4c574ebc3137c54
Inna Poliakova <polyakova1081@gmail.com>
2023-02-25 13:21:32 +0000

    #### 9. Preparing to add more pizzas

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Restaurant = () => {
-  const pizzas = 0;
+ const [orders, setOrders] = useState(0);
   return (
     <div>
       <h4>Restaurant Orders</h4>
       <ul>
         <li>
-          Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
+          Pizzas: {orders} <button className="btn btn-primary">Add</button>
         </li>
       </ul>
     </div>
   );
 };
+
 
 export default Restaurant;
