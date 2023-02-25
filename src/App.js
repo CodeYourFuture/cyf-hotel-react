c85b01a5199a53f1fe3e08ee4329a3fbf62d9706
MacOS <macos@blessing.local>
2023-02-25 16:51:22 +0000

    feat: adding footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Heading from "./Heading";
 import TouristInfoCards from "./TouristInfoCards";
+import Footer from "./Footer"
 
 import Bookings from "./Bookings";
 import "./App.css";
 
 const App = () => {
+  const addressArray = ["123 Fake Street, London, E1 4UD", "hello@fakehotel.com", "0123 456789"];
   return (
     <div className="App">
       <Heading />
       <TouristInfoCards />
       <Bookings />
+      <Footer address = {addressArray}/>
     </div>
   );
 };
