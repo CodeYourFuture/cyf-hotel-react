32553f1fcbe888058e0e63a32dacdc90a3a36684
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-02-26 19:31:14 +0000

    Adding 2 more orders to page

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,18 +1,14 @@
 import React, { useState } from "react";
-import RestaurantButton from "./RestaurantButton";
+import Order from "./Order";
 
 const Restaurant = () => {
-  const [orders, setOrders] = useState(0);
-  const orderOne = () => {
-    setOrders(orders + 1);
-  };
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
-        <li>
-          Pizzas: {orders} <RestaurantButton order={orderOne} />
-        </li>
+        <Order orderType="Pizzas" />
+        <Order orderType="Salads" />
+        <Order orderType="Chocolate cake" />
       </ul>
     </div>
   );
