6187c7edc98961be0f9d32ed1fa9d5793829ba08
Gayle Ngozi <gaylengozi@gmail.com>
2023-02-27 13:20:04 +0000

    added highlighter-exercise 15 complete

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,11 +1,16 @@
-import React, { useState} from "react";
+import React, { useState, useEffect} from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
   const [bookings, setBookings] = useState(FakeBookings);
-  const search = searchVal => {
+
+  //  function allbookings() {
+  //    setBookings(FakeBookings + 1);
+  //  }
+
+  const search = (searchVal) => {
     console.info("TO DO!", searchVal);
   };
 
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,40 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 
+function SearchResults(props) {
+  const [selectedRows, setSelectedRows] = useState([]);
 
-function SearchResults(props){
+  const clickedRows = (bookingDetails) => {
+    const newSelectedRows = [...selectedRows];
 
-    return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">id</th>
-            <th scope="col">Title</th>
-            <th scope="col">First Name</th>
-            <th scope="col">Surname</th>
-            <th scope="col">Email</th>
-            <th scope="col">Room id</th>
-            <th scope="col">Check-in Date</th>
-            <th scope="col">Check-out Date</th>
-            <th scope="col">Total Num of Nights</th>
+    if (newSelectedRows.includes(bookingDetails)) {
+      setSelectedRows(
+        newSelectedRows.filter((eachRow) => eachRow !== bookingDetails)
+      );
+    } else {
+      newSelectedRows.push(bookingDetails);
+      setSelectedRows(newSelectedRows);
+    }
+  };
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">id</th>
+          <th scope="col">Title</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Surname</th>
+          <th scope="col">Email</th>
+          <th scope="col">Room id</th>
+          <th scope="col">Check-in Date</th>
+          <th scope="col">Check-out Date</th>
+          <th scope="col">Total Num of Nights</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.bookingResults.map((result, index) => (
+          <tr
+            key={result.id}
+            onClick={() => clickedRows(index)}
+            className={selectedRows.includes(index) ? "selected" : ""}
+          >
+            <th scope="row">{result.id}</th>
+            <td>{result.title}</td>
+            <td>{result.firstName}</td>
+            <td>{result.surname}</td>
+            <td>{result.email}</td>
+            <td>{result.roomId}</td>
+            <td>{result.checkInDate}</td>
+            <td>{result.checkOutDate}</td>
+            <td>
+              {moment(result.checkOutDate).diff(result.checkInDate, "days")}
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          {props.bookingResults.map((result) => (
-            <tr key={result.id}>
-              <th scope="row">{result.id}</th>
-              <td>{result.title}</td>
-              <td>{result.firstName}</td>
-              <td>{result.surname}</td>
-              <td>{result.email}</td>
-              <td>{result.roomId}</td>
-              <td>{result.checkInDate}</td>
-              <td>{result.checkOutDate}</td>
-              <td>{moment(result.checkOutDate).diff((result.checkInDate),"days")}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
+        ))}
+      </tbody>
+    </table>
+  );
 }
+
 export default SearchResults;
