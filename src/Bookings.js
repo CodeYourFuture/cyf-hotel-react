22ec0e03464e8afe7c040ebc0a3652fa240f7af8
elahemortazavi <emor5680@gmail.com>
2023-02-27 14:51:35 +0000

    searchresult

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Search from "./Search.js";
-import SearchResult from "./SearchResult.js";
+import SearchResult from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
