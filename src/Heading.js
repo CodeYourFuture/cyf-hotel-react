c54778a1df780d716c0bd425b711a9ce7e52d715
Olena Reukova <kronverk01@gmail.com>
2023-02-27 03:14:20 +0000

    2. Extract the header in its own component

---
+++ src/Heading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <p>CYF Hotel</p>
+      <img src="https://uploads.turbologo.com/uploads/design/hq_preview_image/789526/draw_svg20210507-22909-nk3awt.svg.png" />
+    </header>
+  );
+};
+
+export default Heading;
