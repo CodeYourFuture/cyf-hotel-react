e4bd0ffde6236e125e592ea2d6de60cbbad9bc93
Pamelalun <105378916+Pamelalun@users.noreply.github.com>
2023-02-28 20:36:12 +0000

    Head Style_update

--- src/Heading.js
+++ src/Heading.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 function Heading () {
-return  <><header className="App-header">CYF Hotel</header>
+return  <><header className="App-header">CYF Hotel
 <img src= "https://image.flaticon.com/icons/svg/139/139899.svg"></img>
+</header>
 </>
-
 }
 
 export default Heading;
