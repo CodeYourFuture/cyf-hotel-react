429bd2ae8b91522fb35225dc2a3e9625ad307ba5
Oleh <Oleh@progressive-id.co.uk>
2023-02-28 10:14:47 +0000

    feat: creating dynamically cards  info

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,23 +1,22 @@
 import React from "react";
 
 function SearchResults(props) {
+  // var a = moment([2007, 0, 29]);
+  // var b = moment([2007, 0, 28]);
+  // a.diff(b, 'days') // 1
 
-    // var a = moment([2007, 0, 29]);
-    // var b = moment([2007, 0, 28]);
-    // a.diff(b, 'days') // 1 
-
-    let tr = props.results.map((element)=>
+  let tr = props.results.map((element) => (
     <tr className="tableRow">
-    <td>{element.id}</td>
-    <td>{element.title}</td>
-    <td>{element.firstName}</td>
-    <td>{element.surname}</td>
-    <td>{element.email}</td>
-    <td>{element.roomId}</td>
-    <td>{element.checkInDate}</td>
-    <td>{element.checkOutDate}</td>
+      <td>{element.id}</td>
+      <td>{element.title}</td>
+      <td>{element.firstName}</td>
+      <td>{element.surname}</td>
+      <td>{element.email}</td>
+      <td>{element.roomId}</td>
+      <td>{element.checkInDate}</td>
+      <td>{element.checkOutDate}</td>
     </tr>
-    )
+  ));
 
   return (
     <table class="table">
@@ -34,9 +33,7 @@ function SearchResults(props) {
           <th scope="col">Nigths</th>
         </tr>
       </thead>
-      <tbody>
-        {tr}
-      </tbody>
+      <tbody>{tr}</tbody>
     </table>
   );
 }
--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -1,47 +1,30 @@
 import React from "react";
 
-function TouristInfoCards() {
+function TouristInfoCards(props) {
+
+  const card = props.cards.map((element) => (
+    <div className="card">
+      <img
+        src={element.image}
+        className="card-img-top"
+      />
+      <h1>{element.city}</h1>
+      <p>{element.description}</p>
+      <div className="card-body">
+        <a
+          href={element.buttonLink}
+          className="btn btn-primary"
+          target="_blank"
+        >
+          More Information
+        </a>
+      </div>
+    </div>
+  ));
+
   return (
     <div className="cards-wrapper">
-      <div className="card">
-        <img
-          src="https://media.timeout.com/images/105400989/750/422/image.jpg"
-          className="card-img-top"
-        />
-        <h1>Glasgow</h1>
-        <p>This is a description</p>
-        <div className="card-body">
-          <a href="https://peoplemakeglasgow.com" className="btn btn-primary" target="_blank">
-          More Information
-          </a>
-        </div>
-      </div>
-      <div className="card">
-        <img
-          src="https://cdn.citybaseapartments.com/blog/cba-media/2016-10/shutterstock_1072210547.jpg"
-          className="card-img-top"
-        />
-        <h1>Manchester</h1>
-        <p>This is a description</p>
-        <div className="card-body">
-          <a href="https://visitmanchester.com" className="btn btn-primary" target="_blank">
-          More Information
-          </a>
-        </div>
-      </div>
-      <div className="card">
-        <img
-          src="https://cdn.londonandpartners.com/visit/london-organisations/houses-of-parliament/63950-640x360-london-icons2-640.jpg"
-          className="card-img-top"
-        />
-        <h1>London</h1>
-        <p>This is a description</p>
-        <div className="card-body">
-          <a href="https://visitlondon.com" className="btn btn-primary" target="_blank">
-            More Information
-          </a>
-        </div>
-      </div>
+      {card}
     </div>
   );
 }
