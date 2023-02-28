bce50d534710bcc0b9989deb8832eadab608991b
Pamelalun <105378916+Pamelalun@users.noreply.github.com>
2023-02-28 18:07:42 +0000

    7. Calculate and show the number of nights for each booking

--- src/Heading.js
+++ src/Heading.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 function Heading () {
-return <header className="App-header">CYF Hotel</header>;
+return  <><header className="App-header">CYF Hotel</header>
 <img src= "https://image.flaticon.com/icons/svg/139/139899.svg"></img>
+</>
 
 }
 
