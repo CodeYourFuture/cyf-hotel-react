d9da6d6fee056e330104ebd1c56d7d859d2e53b8
Oleh <Oleh@progressive-id.co.uk>
2023-03-01 11:10:53 +0000

    feat: solving 9 level for Restaurant

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,28 +1,22 @@
 import React, {useState} from "react";
 
 const Restaurant = () => {
-  // const pizzas = 0;
-  let orders = 0
 
-  const setOrders = () => {
-    orders = useState(0)
-  }
+    const [orders, setCount] = useState(0);
+    const orderOne = () => {
+      setCount(orders+1);
+    }
 
   return (
     <div className="restaurantOrders">
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {orders} <button className="btn btn-primary">Add</button>
+          Pizzas: {orders} <button onClick = {orderOne} className="btn btn-primary">Add</button>
         </li>
       </ul>
     </div>
   );
 };
-
-const orderOne = () =>{
-  onclick (setOrders+=1) 
-}
-
 
 export default Restaurant;
