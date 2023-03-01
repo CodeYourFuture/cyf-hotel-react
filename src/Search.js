9ffc43f1611f5d52970237055dbb70a3f72f15cb
Gayle Ngozi <gaylengozi@gmail.com>
2023-03-01 17:53:34 +0000

    Exercise 17 completed. Search input stored in a state.

--- src/Search.js
+++ src/Search.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchButton from "./SearchButton";
 
 
-const Search = () => {
+// const Search = () => {
+
+  function Search() {
+  const [searchInput, setSearchInput] = useState('');
+
+  function handleSearchInput(event) {
+    const value = event.target.value;
+    setSearchInput(value);
+     console.log(value); 
+  }
+
   return (
     <div className="search">
       <div className="page-header">
-        <h4 className="text-left">Search Bookings</h4>
+        <h4 className="text-center">Search Bookings</h4>
       </div>
       <div className="row search-wrapper">
         <div className="col">
           <form className="form-group search-box">
-            <label htmlFor="customerName">Customer name</label>
+            {/* <label htmlFor="customerName">Customer name</label> */}
             <div className="search-row">
               <input
+              value={searchInput}
                 type="text"
                 id="customerName"
                 className="form-control"
                 placeholder="Customer name"
+                onChange={handleSearchInput}
               />
               <SearchButton />
             </div>
@@ -27,7 +39,5 @@ const Search = () => {
     </div>
   );
 };
-
-
 
 export default Search;
