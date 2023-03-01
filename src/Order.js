04cb09b8241f2ab7e7c2f0d007d772c8825bd285
elahemortazavi <emor5680@gmail.com>
2023-03-01 23:03:05 +0000

    order.js

---
+++ src/Order.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Order = () => {
+    return (
+        <div>
+            <ul>
+                
+            </ul>
+        </div>
+    );
+};
+
+export default Order;
--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,15 +1,38 @@
-import React from "react";
+// import React from "react";
+
+// const Restaurant = () => {
+//   const pizzas = 0;
+//   return (
+//     <div>
+//       <h3>Restaurant Orders</h3>
+//       <ul>
+//         <li>
+//           Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Restaurant;
+
+
+import React, {useState} from 'react';
 
 const Restaurant = () => {
-  const pizzas = 0;
+
+  const [number, setNumber] = useState(0);
+  
+  const clickHandler = () => {
+    setNumber(number + 1)
+  }
+
   return (
     <div>
-      <h3>Restaurant Orders</h3>
-      <ul>
-        <li>
-          Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
-        </li>
-      </ul>
+      <h3>Restaurant Orders:</h3>
+      
+      <h1>{number}</h1>
+      <button onClick = {clickHandler}>Add</button>
     </div>
   );
 };
