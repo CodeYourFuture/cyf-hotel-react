4506ac0416a25f10178143a8bf4d169029bf3470
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-03-03 16:04:05 +0000

    update TouristInfoCards

--- src/App.js
+++ src/App.js
@@ -14,9 +14,26 @@ const App = () => {
   return (
     <div className="App">
       <Heading />
-      <TouristInfoCards />
+      <TouristInfoCards
+        image=""
+        title="Glasgow"
+        link="href=https://peoplemakeglasgow.com"
+        description="Is the most populous city in Scotland and the fourth-most populous city in the United Kingdom, as well as being the 27th largest city by population in Europe"
+      />
+      <TouristInfoCards
+        image=""
+        title="Manchester"
+        link="href=https://visitmanchester.com"
+        description="Is a major city in the northwest of England with a rich industrial heritage. The Castlefield conservation area’s 18th-century canal system recalls the city’s days as a textile powerhouse, and visitors can trace this history at the interactive Museum of Science & Industry."
+      />
+      <TouristInfoCards
+        image=""
+        title="London"
+        link="href=https://visitlondon.com"
+        description="The capital of England and the United Kingdom, is a 21st-century city with history stretching back to Roman times. At its centre stand the imposing Houses of Parliament, the iconic ‘Big Ben’ clock tower and Westminster Abbey, site of British monarch coronations"
+      />
       <Bookings />
-      
+
       <Footer contact={contact} />
     </div>
   );
