786f04524afad4da9a07d9292d2a9f92bc53769e
nataliizab <nataliia.zab@gmail.com>
2023-03-03 20:01:26 +0000

    14. Passed bookings from a state variable

--- src/Bookings.jsx
+++ src/Bookings.jsx
@@ -1,18 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./Search.jsx";
 import SearchResults from "./SearchResults.jsx";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const search = (searchVal) => {
-    console.info("TO DO!", searchVal);
-  };
+  const [bookings, setBookings] = useState(FakeBookings);
 
   return (
     <div className="App-content">
       <div className="container">
-        <Search search={search} />
-        <SearchResults results={FakeBookings} list = {FakeBookings} />
+        <Search search={Search} />
+        <SearchResults results={bookings} list={bookings} />
       </div>
     </div>
   );
