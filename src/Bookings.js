9259f5ac14771b5edf32abb5dcc1c79f24caba15
Lorena <lorenacapraru@gmail.com>
2023-03-04 12:08:44 +0000

    Lesson 3 T16

--- src/Bookings.js
+++ src/Bookings.js
@@ -2,13 +2,25 @@ import React, { useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.jsx";
 import FakeBookings from "./data/fakeBookings.json";
+import { useEffect } from "react";
 
 const Bookings = () => {
+  //console.log(useEffect);
   const search = (searchVal) => {
     console.info("TO DO!", searchVal);
   };
+  const [bookings, setBookings] = useState([]);
 
-  const [bookings, setBooking] = useState(FakeBookings);
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then((res) => res.json())
+      .then((data) => {
+        setBookings(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className="App-content">
