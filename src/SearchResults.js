68040f707a5e2ea22877f968ac1e60c102394615
Howard <howardsoon@gmail.com>
2023-03-06 20:18:23 +0000

    complete task 15

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,7 +1,18 @@
-import React,{ useState }from "react";
+import React, { useState } from "react";
 import moment from "moment";
 
 const SearchResults = () => {
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  const handleRowClick = (bookingId) => {
+    setSelectedRows((prevState) => {
+      if (prevState.includes(bookingId)) {
+        return prevState.filter((id) => id !== bookingId);
+      } else {
+        return [...prevState, bookingId];
+      }
+    });
+  };
   // Example booking data
   const bookings = [
     {
@@ -54,11 +65,46 @@ const SearchResults = () => {
       checkInDate: "2023-04-09",
       checkOutDate: "2023-04-15",
     },
-];
+  ];
 
+  const renderBookingRows = () => {
+    return bookings.map((booking) => {
+      const {
+        id,
+        title,
+        firstName,
+        surname,
+        email,
+        roomId,
+        checkInDate,
+        checkOutDate,
+      } = booking;
+      const numNights = moment(checkOutDate).diff(moment(checkInDate), "days");
+
+      // Check if the current row is selected
+      const isSelected = selectedRows.includes(id);
+
+      // Set the row class based on the selected state
+      const rowClass = isSelected ? "table-active" : "";
+
+      return (
+        <tr key={id} className={rowClass} onClick={() => handleRowClick(id)}>
+          <td>{id}</td>
+          <td>{title}</td>
+          <td>{firstName}</td>
+          <td>{surname}</td>
+          <td>{email}</td>
+          <td>{roomId}</td>
+          <td>{checkInDate}</td>
+          <td>{checkOutDate}</td>
+          <td>{numNights}</td>
+        </tr>
+      );
+    });
+  };
 
   return (
-    <div className="table-responsive">
+    <div className="container">
       <table className="table">
         <thead>
           <tr>
@@ -68,34 +114,28 @@ const SearchResults = () => {
             <th>Surname</th>
             <th>Email</th>
             <th>Room ID</th>
-            <th>Check-In Date</th>
-            <th>Check-Out Date</th>
-            <th>Nights</th>
+            <th>Check In Date</th>
+            <th>Check Out Date</th>
+            <th># Nights</th>
           </tr>
         </thead>
-        <tbody>
-          {bookings.map((booking) => {
-            const checkIn = moment(booking.checkInDate);
-            const checkOut = moment(booking.checkOutDate);
-            const nights = checkOut.diff(checkIn, "days");
-            return (
-              <tr key={booking.id}>
-                <td>{booking.id}</td>
-                <td>{booking.title}</td>
-                <td>{booking.firstName}</td>
-                <td>{booking.surname}</td>
-                <td>{booking.email}</td>
-                <td>{booking.roomId}</td>
-                <td>{booking.checkInDate}</td>
-                <td>{booking.checkOutDate}</td>
-                <td>{nights}</td>
-              </tr>
-            );
-          })}
-        </tbody>
+        <tbody>{renderBookingRows()}</tbody>
       </table>
     </div>
   );
 };
 
 export default SearchResults;
+
+
+/**
+ * added a new state variable selectedRows using the useState hook. This variable is an array of 
+ * booking IDs representing the currently selected rows.
+ * 
+ * Also added a new handleRowClick function which toggles the selected state of a row when it is clicked. 
+ * It does this by checking if the booking ID is already in the selectedRows array. If it is, it removes 
+ * the ID from the array. If it is not, it adds the ID to the array.
+ * 
+ * I have updated the renderBookingRows function to set the className prop of each row based on whether
+
+ */
