801ffbf52e5b9e581a0d49663838968542227950
BoshraM <gbboshra@gmail.com>
2023-03-06 12:40:27 +0000

    add style

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -3,7 +3,7 @@ import { Order } from "./Order";
 
 const Restaurant = () => {
   return (
-    <div>
+    <div className="restaurant-menue">
       <h3>Restaurant Orders</h3>
       <ul>
         <Order orderType={"Pizzas"} />
