c68a8830003b0ba2c164f33634934b7a53e3a831
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-03-06 22:08:39 +0000

    Adding html and styling HotelImages component

--- src/HotelImages.js
+++ src/HotelImages.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "./HotelImages.css";
 
 const HotelImages = () => {
-  return <p> Hello images</p>;
+  return (
+    <div class="image-slider">
+      <div class="row-slider">
+        <div class="frame">
+          <div class="boxes">
+            <div class="box box-1"></div>
+            <div class="box box-2"></div>
+            <div class="box box-3"></div>
+            <div class="box box-4"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HotelImages;
