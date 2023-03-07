fd12bc12f289e6d15d0c7c1d1b209fbe3e8fe347
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-03-07 23:02:49 +0000

    Passing bookings from a state variable

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
+  const [bookings, setBookings]= useState(FakeBookings)
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
