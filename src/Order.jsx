5306ff20f8eb4f6c48e87051e53c373854463f4f
Mo Mahboobian <momahboobian@gmail.com>
2023-03-07 18:42:24 +0000

    minor update

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { RestaurantButton } from "./RestaurantButton";
+import RestaurantButton from "./RestaurantButton";
 
 const Order = ({ orderType }) => {
   const [orders, setOrders] = useState(0);
--- src/RestaurantButton.jsx
+++ src/RestaurantButton.jsx
@@ -1,5 +1,7 @@
-export const RestaurantButton = ({ onClick }) => (
+const RestaurantButton = ({ onClick }) => (
   <button className="btn btn-primary" onClick={onClick}>
     Add
   </button>
 );
+
+export default RestaurantButton;
