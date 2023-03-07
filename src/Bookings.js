7d95daf293e8442def441dd391a40994b6a95aa5
MacOS <macos@blessing.home>
2023-03-07 22:50:35 +0000

    feat: Load bookings remotely

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,19 +1,25 @@
-import React, {useState} from "react";   
+import React, {useState, useEffect} from "react";   
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
+// import FakeBookings from "./data/fakeBookings.json";
 
 
 const Bookings = () => {
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
-  const [bookings, setBookings] = useState(FakeBookings)
-
-  const Bookings = () => {
-    setBookings(FakeBookings);
-  }
+  const [bookings, setBookings] = useState([])
   
+  useEffect(() => {
+    fetch(
+      `https://cyf-react.glitch.me`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setBookings(data);
+      });
+  }, []);
+
   return (
     <div className="App-content">
       <div className="container">
