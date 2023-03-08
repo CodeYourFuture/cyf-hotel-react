2a8951dfc36433d3b9c84c5af37e507b808b146a
Lorena <lorenacapraru@gmail.com>
2023-03-08 11:23:17 +0000

    Lesson 2 T20

---
+++ src/CustomerProfile.jsx
@@ -0,0 +1,5 @@
+import react from "react";
+
+export default function CustomerProfile(props) {
+  return <span>Customer {props.id} Profile</span>;
+}
--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -1,20 +1,17 @@
 import React, { useState } from "react";
 import moment from "moment";
+import CustomerProfile from "./CustomerProfile";
 
 export default function SearchResults(props) {
-  const [selectedRow, setSelectedRow] = useState(false);
-
-  function selectRow() {
-    setSelectedRow((selectedRow) =>
-      selectedRow === false ? "table-dark" : false
-    );
+  const [selectedRow, setSelectedRow] = useState("");
+  function selectRow(id) {
+    id === selectedRow ? setSelectedRow("") : setSelectedRow(id);
   }
 
   const bookingList = props.results.map((element) => (
     <tr
       key={element.id}
       className={element.id === selectedRow ? "table-primary" : ""}
-      onClick={() => selectRow(element.id)}
     >
       <td>{element.id}</td>
       <td>{element.title}</td>
@@ -27,25 +24,32 @@ export default function SearchResults(props) {
       <td>
         {moment(element.checkOutDate).diff(moment(element.checkInDate), "days")}
       </td>
+      <td scope="col">
+        <button onClick={() => selectRow(element.id)}>Show Profile</button>
+      </td>
     </tr>
   ));
 
   return (
-    <table class="table table-borderless ">
-      <thead class="thead-light">
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Title</th>
-          <th scope="col">First Name</th>
-          <th scope="col">Surname</th>
-          <th scope="col">Email</th>
-          <th scope="col">Room ID</th>
-          <th scope="col">Check-in Date</th>
-          <th scope="col">Check-out Date</th>
-          <th scope="col">No. of Nights</th>
-        </tr>
-      </thead>
-      {bookingList}
-    </table>
+    <>
+      <table class="table table-borderless ">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Surname</th>
+            <th scope="col">Email</th>
+            <th scope="col">Room ID</th>
+            <th scope="col">Check-in Date</th>
+            <th scope="col">Check-out Date</th>
+            <th scope="col">No. of Nights</th>
+            <th scope="col">Profiles</th>
+          </tr>
+        </thead>
+        {bookingList}
+      </table>
+      <CustomerProfile id={selectedRow} />
+    </>
   );
 }
