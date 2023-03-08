1d318f48733f94312c458107434f4fdbd497ff0f
Bahare <itxbb09@gmail.com>
2023-03-08 14:25:27 +0000

    exe-22 done

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
+//import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    fetch(`https://cyf-react.glitch.me`)
+    fetch(`https://cyf-react.glitch.me/delayed`)
       .then(res => res.json())
       .then(data => {
         setBookings(data);
+        setIsLoading(false);
       });
   }, []);
 
@@ -18,18 +20,22 @@ const Bookings = () => {
     setBookings(
       bookings.filter(
         booking =>
-          booking.firstName.toLowerCase() === searchVal.toLowerCase() ||
-          booking.surname.toLowerCase() === searchVal.toLowerCase()
+          booking.firstName.toLowerCase().includes(searchVal.toLowerCase()) ||
+          booking.surname.toLowerCase().includes(searchVal.toLowerCase())
       )
     );
   };
 
   return (
     <div className="App-content">
-      <div className="container">
-        <Search search={search} />
-        <SearchResults bookings={bookings} />
-      </div>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <div className="container">
+          <Search search={search} />
+          <SearchResults bookings={bookings} />
+        </div>
+      )}
     </div>
   );
 };
