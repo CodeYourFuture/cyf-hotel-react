14ea4670a89893eee679ab6488cae53f85ecb3aa
Bahare <itxbb09@gmail.com>
2023-03-08 12:08:47 +0000

    exe-20 done

---
+++ src/CustomerProfile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CustomerProfile = props => {
+  return (
+    <div>
+      <h2>Customer {props.b} Profile</h2>
+    </div>
+  );
+};
+
+export default CustomerProfile;
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,52 +1,73 @@
 import moment from "moment";
 import React, { useState } from "react";
+import CustomerProfile from "./CustomerProfile";
 
 const SearchResults = props => {
+  const { bookings } = props;
+
   const [selectedRowId, setSelectedRowId] = useState(null);
 
   const handlerRowClick = id => {
     id === selectedRowId ? setSelectedRowId(null) : setSelectedRowId(id);
   };
-  const { bookings } = props;
+
+  const [showProfile, setShowProfile] = useState(0);
+
+  const handlerShowProfileClick = id => {
+    setShowProfile(id);
+  };
+  console.log(selectedRowId);
+
   return (
-    <table className="table table-striped">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Title</th>
-          <th>First Name</th>
-          <th>Surname</th>
-          <th>Email</th>
-          <th>Room ID</th>
-          <th>Check in Date</th>
-          <th>Check out Date</th>
-          <th>Nights of Stay</th>
-        </tr>
-      </thead>
-      <tbody>
-        {bookings.map(booking => (
-          <tr
-            key={booking.id}
-            onClick={() => handlerRowClick(booking.id)}
-            style={{
-              backgroundColor: selectedRowId === booking.id ? "red" : "white"
-            }}
-          >
-            <td>{booking.id}</td>
-            <td>{booking.title}</td>
-            <td>{booking.firstName}</td>
-            <td>{booking.surname}</td>
-            <td>{booking.email}</td>
-            <td>{booking.roomId}</td>
-            <td>{booking.checkInDate}</td>
-            <td>{booking.checkOutDate}</td>
-            <td>
-              {moment(booking.checkOutDate).diff(booking.checkInDate, "days")}
-            </td>
+    <div>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>First Name</th>
+            <th>Surname</th>
+            <th>Email</th>
+            <th>Room ID</th>
+            <th>Check in Date</th>
+            <th>Check out Date</th>
+            <th>Nights of Stay</th>
+            <th>
+              <button>Show profile</button>
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {bookings.map(booking => (
+            <tr
+              key={booking.id}
+              onClick={() => handlerRowClick(booking.id)}
+              style={{
+                backgroundColor: selectedRowId === booking.id ? "red" : "white"
+              }}
+            >
+              <td>{booking.id}</td>
+              <td>{booking.title}</td>
+              <td>{booking.firstName}</td>
+              <td>{booking.surname}</td>
+              <td>{booking.email}</td>
+              <td>{booking.roomId}</td>
+              <td>{booking.checkInDate}</td>
+              <td>{booking.checkOutDate}</td>
+              <td>
+                {moment(booking.checkOutDate).diff(booking.checkInDate, "days")}
+              </td>
+              <td>
+                <button onClick={() => handlerShowProfileClick(booking.id)}>
+                  Show Profile
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {showProfile && <CustomerProfile b={showProfile} />}
+    </div>
   );
 };
 
