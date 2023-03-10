62cf7a62d007f6be528e4999e7d4bd593ba231d3
LexiisYing <nyxxing0824@gmail.com>
2023-03-10 23:20:52 +0000

    updating and fixing issues

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,21 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
-// import SearchResults from "./SearchResults.js";
+import SearchResults from "./SearchResults.js";
 // import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [bookings, setBookings] = useState([]);
+  const [profile, setProfile] = useState(null);
+
+  function setCustomerProfile(id) {
+    const currentProfile = bookings.find(result => result.id === id);
+    setProfile(currentProfile);
+  }
+
+  function handleSearchInput(event) {
+    setSearchInput(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search(searchInput);
+  }
+
   const search = searchVal => {
-    console.info("TO DO!", searchVal);
+    const filteredValue = bookings.filter(value => {
+      return (
+        value.firstName.toLowerCase().includes(searchVal) ||
+        value.surname.toLowerCase().includes(searchVal)
+      );
+    });
+    setBookings(filteredValue, searchVal);
   };
+
+
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then(response => response.json())
+      .then(data => setBookings(data))
+      .catch(error => console.log(error));
+  }, []);
 
   return (
     <div className="App-content">
       <div className="container">
-        <Search search={search} />
-        {/* <SearchResults results={FakeBookings} /> */}
+        <Search
+          searchInput={searchInput}
+          handleSearchInput={handleSearchInput}
+          handleSubmit={handleSubmit}
+        />
+        <SearchResults
+          results={bookings}
+          updateProfile={setProfile}
+          searchInput={searchInput}
+          setCustomerProfile={setCustomerProfile}
+        />
+        {profile && (
+          <ul>
+            <li key={profile.id}>Customer {profile.id} Profile</li>
+            <li key={profile.email}>Customer email: {profile.email}</li>
+            <li key={profile.vip}>
+              VIP customer: {profile.vip ? "VIP" : "Not VIP"}
+            </li>
+            <li key={profile.phoneNumber}>
+              Customer phone number: {profile.phoneNumber}
+            </li>
+          </ul>
+        )}
       </div>
     </div>
+    
   );
 };
+
 
 export default Bookings;
