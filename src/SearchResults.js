b8875fa7398cdc0537081832ef08439c07bd0159
LexiisYing <nyxxing0824@gmail.com>
2023-03-10 23:02:55 +0000

    update

---
+++ src/SearchResults.js
@@ -0,0 +1,44 @@
+import React from "react";
+import SearchDetails from "./SearchDetails";
+
+function SearchResults(props) {
+  const customerProfile = props.setCustomerProfile;
+
+  const searchResults = props.results.filter(value => {
+    return (
+      value.firstName.toLowerCase().includes(props.searchInput.toLowerCase()) ||
+      value.surname.toLowerCase().includes(props.searchInput.toLowerCase())
+    );
+  });
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Booking ID</th>
+          <th scope="col">Title</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Surname</th>
+          <th scope="col">Email</th>
+          <th scope="col">Room ID</th>
+          <th scope="col">Check in date</th>
+          <th scope="col">Check out date</th>
+          <th scope="col">Number of nights</th>
+          <th scope="col">Customer Profile</th>
+        </tr>
+      </thead>
+      <tbody>
+        {searchResults.map(detail => (
+          <SearchDetails
+            key={detail.id}
+            updateProfile={props.updateProfile}
+            detail={detail}
+            customerProfile={customerProfile}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default SearchResults;
