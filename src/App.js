a6bdd69e2ad73b1c13f11af0f78dde6252e15829
ZahraaTayyar <zahraatayyar@gmail.com>
2023-03-10 16:33:33 +0000

    16 completed

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import TouristInfoCards from "./TouristInfoCards";
 import Heading from "./Heading";
 import Bookings from "./Bookings";
 import Footer from "./Footer";
-import SearchResults from "./SearchResults";
-import data from "./data/fakeBookings.json";
-import moment from "moment";
 import "./App.css";
-import TableHeading from "./TableHeading";
 import Restaurant from "./Restaurant";
 import RestaurantButton from "./RestaurantButton";
+import Table from "./Table";
 
-const App = () => {
+function App() {
   const address = [
     "123 Fake Street, London, E1 4UD",
     "hello@fakehotel.com",
     "0123 456789",
   ];
-
-  const [bookings, setBookings] = useState(data);
 
   return (
     <div className="App">
@@ -26,31 +21,7 @@ const App = () => {
       <TouristInfoCards />
       <Bookings/>
       <Restaurant addButton={RestaurantButton} />
-
-      <table className="table">
-        <thead className="thead-dark">
-          <TableHeading />
-        </thead>
-        {bookings.map((client) => {
-          let a = moment(client.checkInDate, "YYYY-MM-DD");
-          let b = moment(client.checkOutDate, "YYYY-MM-DD");
-          return (
-            <SearchResults
-              key={client.id}
-              id={client.id}
-              title={client.title}
-              firstName={client.firstName}
-              surname={client.surname}
-              email={client.email}
-              roomId={client.roomId}
-              checkInDate={client.checkInDate}
-              checkOutDate={client.checkOutDate}
-              numberOfNights={b.diff(a, "days")}
-            />
-          );
-        })}
-      </table>
-
+      <Table />
       <Footer address={address} />
     </div>
   );
---
+++ src/Table.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import SearchResults from "./SearchResults";
+import moment from "moment";
+
+function Table() {
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me`)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        setBookings(data);
+    })
+  })
+  return (
+    <table className="table">
+      <thead className="thead-dark">
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Title</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Room ID</th>
+          <th scope="col">Check in date</th>
+          <th scope="col">Check out date</th>
+          <th scope="col">Number of nights</th>
+        </tr>
+      </thead>
+      {bookings.map((client) => {
+        let a = moment(client.checkInDate, "YYYY-MM-DD");
+        let b = moment(client.checkOutDate, "YYYY-MM-DD");
+        return (
+          <SearchResults
+            key={client.id}
+            id={client.id}
+            title={client.title}
+            firstName={client.firstName}
+            surname={client.surname}
+            email={client.email}
+            roomId={client.roomId}
+            checkInDate={client.checkInDate}
+            checkOutDate={client.checkOutDate}
+            numberOfNights={b.diff(a, "days")}
+          />
+        );
+      })}
+    </table>
+  );
+}
+
+export default Table;
