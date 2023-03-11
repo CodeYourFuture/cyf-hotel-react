3153bea159a6eb56bcf0105d1b795c7be490c634
Inna Poliakova <polyakova1081@gmail.com>
2023-03-11 17:05:33 +0000

    #### 23. Show an error message

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -6,6 +6,8 @@ const SearchResults = ({ results }) => {
   
  const [selectedId, setSelectedId] = useState("");
  const [isLoading, setIsLoading] = useState(true);
+ const [error, setError] = useState(null);
+ const [bookings, setBookings] = useState([]);
 
  const handleRowClick = (id) => {
     setSelectedId(
@@ -17,64 +19,79 @@ const SearchResults = ({ results }) => {
   useEffect(() => {
     setIsLoading(true);
     fetch("https://cyf-react.glitch.me/delayed")
-      .then((response) => response.json())
+    // fetch("https://cyf-react.glitch.me/error")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
       .then((data) => {
         setIsLoading(false);
         setBookings(data);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setError(error);
       });
   }, []);
 
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Failed to load data from the server. Please try again later.</p>;
+  }
+
   return (
     <>
-      {isLoading && <p>Loading...</p>}
-      {!isLoading && (
-        <table className="table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>First Name</th>
-              <th>Surname</th>
-              <th>Email</th>
-              <th>Room ID</th>
-              <th>Check-in Date</th>
-              <th>Check-out Date</th>
-              <th>Numb days</th>
-              <th>Profile</th>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>First Name</th>
+            <th>Surname</th>
+            <th>Email</th>
+            <th>Room ID</th>
+            <th>Check-in Date</th>
+            <th>Check-out Date</th>
+            <th>Numb days</th>
+            <th>Profile</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bookings.map((booking) => (
+            <tr
+              key={booking.id}
+              className=
+              {
+              booking.id === selectedId 
+              ? "selected" 
+              : ""
+              }
+              onClick={() => handleRowClick(booking.id)}
+            >
+              <td>{booking.id}</td>
+              <td>{booking.title}</td>
+              <td>{booking.firstName}</td>
+              <td>{booking.surname}</td>
+              <td>{booking.email}</td>
+              <td>{booking.roomId}</td>
+              <td>{booking.checkInDate}</td>
+              <td>{booking.checkOutDate}</td>
+              <td>
+                {moment(booking.checkOutDate).diff(
+                  booking.checkInDate,
+                  "days"
+                )}
+              </td>
+              <td><button>Show profile</button></td>
             </tr>
-          </thead>
-          <tbody>
-            {results.map((booking) => (
-              <tr
-                key={booking.id}
-                className=
-                {
-                booking.id === selectedId 
-                ? "selected" 
-                : ""
-                }
-                onClick={() => handleRowClick(booking.id)}
-              >
-                <td>{booking.id}</td>
-                <td>{booking.title}</td>
-                <td>{booking.firstName}</td>
-                <td>{booking.surname}</td>
-                <td>{booking.email}</td>
-                <td>{booking.roomId}</td>
-                <td>{booking.checkInDate}</td>
-                <td>{booking.checkOutDate}</td>
-                <td>
-                  {moment(booking.checkOutDate).diff(
-                    booking.checkInDate,
-                    "days"
-                  )}
-                </td>
-                <td><button>Show profile</button></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+          ))}
+        </tbody>
+      </table>
       {selectedId && <CustomerProfile id={selectedId} />}
     </>
   );
