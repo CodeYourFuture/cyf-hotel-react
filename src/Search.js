03d35deb4a8c22ac59059e291ef7236c8f3017a1
hadipournigjeh <108987953+hadipournigjeh@users.noreply.github.com>
2023-03-11 14:34:22 +0000

    I've done exercise 18. Triggering search when submitting the form

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import SearchButton from "./SearchButton";
 
-const Search = () => {
+const Search = (props) => {
   const [searchInput, setSearchInput] = useState("");
 
   const handleSearchInput = (event) => {
@@ -9,35 +9,35 @@ const Search = () => {
     setSearchInput(event.target.value);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.search(searchInput);
+  };
+
   return (
-    <>
-      <input
-        type="text"
-        value={searchInput}
-        onChange={handleSearchInput}
-      ></input>
-      <div className="search">
-        <div className="page-header">
-          <h4 className="text-left">Search Bookings</h4>
-        </div>
-        <div className="row search-wrapper">
-          <div className="col">
-            <form className="form-group search-box">
-              <label htmlFor="customerName">Customer name</label>
-              <div className="search-row">
-                <input
-                  type="text"
-                  id="customerName"
-                  className="form-control"
-                  placeholder="Customer name"
-                />
-                <SearchButton />
-              </div>
-            </form>
-          </div>
+    <div className="search">
+      <div className="page-header">
+        <h4 className="text-left">Search Bookings</h4>
+      </div>
+      <div className="row search-wrapper">
+        <div className="col">
+          <form className="form-group search-box" onSubmit={handleSubmit}>
+            <label htmlFor="customerName">Customer name</label>
+            <div className="search-row">
+              <input
+                type="text"
+                id="customerName"
+                className="form-control"
+                placeholder="Customer name"
+                value={searchInput}
+                onChange={handleSearchInput}
+              />
+              <SearchButton />
+            </div>
+          </form>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
