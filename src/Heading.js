73f8e77a91017be383585bb970ed129e4fbe804d
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-03-13 00:25:33 +0000

    Add a logo to Heading component

--- src/Heading.js
+++ src/Heading.js
@@ -3,10 +3,11 @@ const Heading = () => {
   return (
     <header className="App-header">
       <h1>CYF Hotel</h1>
-      <img
-        src="https://housing.com/news/wp-content/uploads/2022/11/hotel-room-design-compressed-1.jpg"
-        alt="Hotel room"
-        className=""
+      <img 
+      className="App-logo"
+      src="https://images.emojiterra.com/google/noto-emoji/v2.034/512px/1f3e8.png"
+        // src="https://housing.com/news/wp-content/uploads/2022/11/hotel-room-design-compressed-1.jpg"
+      alt="Hotel room"
       />
     </header>
   );
