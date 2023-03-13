3ba67d25984d7398c4b4fbd4c6a19f702d3ca0c7
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-03-13 00:24:18 +0000

    Update Bookings component to load bookings remotely

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,19 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect , useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
+// import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings, setBookings]= useState(FakeBookings)
-  const search = searchVal => {
+  const [bookings, setBookings]= useState([])
+
+
+  useEffect(() => {
+    console.log("Fetching data from FakeBookings");
+
+    fetch("https://cyf-react.glitch.me.")
+      .then((res) => res.json())
+      .then((data) => {
+        setBookings(data);
+      });
+  }, []);
+  const search = (searchVal) => {
     console.info("TO DO!", searchVal);
+    const filteredBooking = bookings.filter((name) =>
+      name.firstName.toLowerCase() === searchVal.toLowerCase() ||
+      name.surname.toLowerCase() === searchVal.toLowerCase() ||
+      name.firstName.toLowerCase().includes(searchVal.toLowerCase()) ||
+      name.surname.toLowerCase().includes(searchVal.toLowerCase())
+        ? setBookings([name])  
+        :-1
+    );
+      console.log(filteredBooking); 
   };
 
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults results ={FakeBookings} />
+        <SearchResults results ={bookings} />
       </div>
     </div>
   );
