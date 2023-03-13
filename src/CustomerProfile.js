a9719ce7f5806d90cdbe2c813be8efffe19f7307
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-03-13 00:20:51 +0000

    Create CustomerProfile component

---
+++ src/CustomerProfile.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function CustomerProfile(props){
+
+    return <div>Customer {props.id} profile</div>
+   }
+
+
+
+
+
+
+
+
+
+export default CustomerProfile;
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,4 +1,5 @@
 import React, { useState }from "react";
+import CustomerProfile from "./CustomerProfile";
 
 
 const SearchResults = (props) => {
@@ -8,40 +9,50 @@ function handleClick(rowId) {
   setSelectedRow(rowId === selectedRow ? null:rowId);
 }
   return (
-    <table className="table table-hover">
-      <thead>
-        <tr>
-          <th scope="col">id</th>
-          <th scope="col">Title</th>
-          <th scope="col">First Name</th>
-          <th scope="col">Sure Name</th>
-          <th scope="col">Email</th>
-          <th scope="col">Room id</th>
-          <th scope="col">Check in</th>
-          <th scope="col">Check out</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.results.map((value, index) => {
-          return (
-            <tr key={index} onClick={() =>handleClick(value.id)}
-            style={{
-backgroundColor: value.id === selectedRow ? "gray":"white",
-            }} >
-              <td>{value.id}</td>
-              <td>{value.title}</td>
-              <td>{value.firstName}</td>
-              <td>{value.surname}</td>
-              <td>{value.email}</td>
-              <td>{value.roomId}</td>
-              <td>{value.checkInDate}</td>
-              <td>{value.checkOutDate}</td>
-              
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div>
+      <table className="table table-hover">
+        <thead>
+          <tr className="table-row-main">
+            <th scope="col">id</th>
+            <th scope="col">Title</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Sure Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Room id</th>
+            <th scope="col">Check in</th>
+            <th scope="col">Check out</th>
+            <th scope="col">Profile</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.results.map((value, index) => {
+            return (
+              <tr
+                key={index}
+                onClick={() => handleClick(value.id)}
+                style={{
+                  backgroundColor: value.id === selectedRow ? "gray" : "white",
+                }}
+              >
+                <td>{value.id}</td>
+                <td>{value.title}</td>
+                <td>{value.firstName}</td>
+                <td>{value.surname}</td>
+                <td>{value.email}</td>
+                <td>{value.roomId}</td>
+                <td>{value.checkInDate}</td>
+                <td>{value.checkOutDate}</td>
+                <td>
+                  <button onClick={()=>handleClick(value.id)}>Show profile</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <CustomerProfile id={selectedRow} />
+    </div>
   );
+ 
 };
 export default SearchResults;
