e3690cf4c97d0a8b9a10a5af44db4475e8e96f07
elahemortazavi <emor5680@gmail.com>
2023-03-14 01:04:56 +0000

    lesson3-22

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from "react";
 import Search from "./Search.js";
-
-// import FakeBookings from "./data/fakeBookings.json";
 import SearchResults from "./SearchResults.js";
 import moment from "moment";
 
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     console.log("we will get and load the bookings from a remote API");
-    fetch("https://cyf-react.glitch.me")
+    setIsLoading(true);
+    fetch("https://cyf-react.glitch.me/delayed")
       .then((response) => response.json())
       .then((data) => {
         setBookings(data);
+        setIsLoading(false);
       });
   }, []);
 
@@ -32,50 +33,52 @@ const Bookings = () => {
   return (
     <div className="App-content">
       <div className="container">
-        <div className="App-content">
-          <div className="container">
-            <div className="tbl-container">
-              <table className="tbl">
-                <thead className="tableHead">
-                  <tr>
-                    <th>id</th>
-                    <th>title</th>
-                    <th> first name</th>
-                    <th> surname</th>
-                    <th> email </th>
-                    <th> room id</th>
-                    <th> check in date</th>
-                    <th>check out date </th>
-                    <th>nights</th>
-                    <th>profile</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {bookings.map((item) => (
-                    <SearchResults
-                      key={item.id}
-                      id={item.id}
-                      title={item.title}
-                      firstName={item.firstName}
-                      surname={item.surname}
-                      email={item.email}
-                      roomId={item.roomId}
-                      checkInDate={item.checkInDate}
-                      checkOutDat={item.checkOutDate}
-                      nights={moment(item.checkOutDate).diff(
-                        moment(item.checkInDate),
-                        "days"
-                      )}
-                      searchValue={searchValue}
-                    />
-                  ))}
-                </tbody>
-
-                {/* <SearchResults result={Bookings} />  */}
-              </table>
+        {isLoading ? (
+          <p>Loading data...</p>
+        ) : (
+          <div className="App-content">
+            <div className="container">
+              <div className="tbl-container">
+                <table className="tbl">
+                  <thead className="tableHead">
+                    <tr>
+                      <th>id</th>
+                      <th>title</th>
+                      <th> first name</th>
+                      <th> surname</th>
+                      <th> email </th>
+                      <th> room id</th>
+                      <th> check in date</th>
+                      <th>check out date </th>
+                      <th>nights</th>
+                      <th>profile</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {bookings.map((item) => (
+                      <SearchResults
+                        key={item.id}
+                        id={item.id}
+                        title={item.title}
+                        firstName={item.firstName}
+                        surname={item.surname}
+                        email={item.email}
+                        roomId={item.roomId}
+                        checkInDate={item.checkInDate}
+                        checkOutDat={item.checkOutDate}
+                        nights={moment(item.checkOutDate).diff(
+                          moment(item.checkInDate),
+                          "days"
+                        )}
+                        searchValue={searchValue}
+                      />
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
