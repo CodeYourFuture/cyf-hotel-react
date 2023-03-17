56262c624b674d1df42bd9ea2ee249ead8623bdb
Zobeir <rigi.zobir@yahoo.com>
2023-03-17 15:34:50 +0000

    iveupdated the customer datails

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+
 const CustomerProfile = (props) => {
-  return props.id;
+  const [customerDetails,setCustomerDetails]=useState([]);
+useEffect(()=>{
+    fetch(`https://cyf-react.glitch.me/customers/${props.id}`)
+    .then(res=>res.json())
+    .then(data=>  setCustomerDetails(data))
+    console.log(customerDetails)
+    
+},[props.id]);
+
+    return (
+      <div>
+        <h2>customer Details : Id = {customerDetails.id} is a {customerDetails.title}</h2>
+        <span>Name : {customerDetails.firstName} </span><span>last Name: {customerDetails.surname}</span>
+        <br></br>
+        <span>Email : {customerDetails.email}</span>
+        <br />
+        <span>{customerDetails.phoneNumber}</span>
+        <p>{customerDetails.vip ? "customer has a Vip place":"customer has an Ordinary place"}</p>
+      </div>
+    );
 };
+
 export default CustomerProfile;
