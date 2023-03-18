68bd12152d073ee78ceb9bacb8b6a0d63d6354d0
ZahraaTayyar <zahraatayyar@gmail.com>
2023-03-18 16:10:34 +0000

    21 completed

--- src/CustomerProfile.jsx
+++ src/CustomerProfile.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function CustomerProfile(props) {
-  return <p>Showing customer with id: {props.id}</p>;
+  const [customerData, setCustomerData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    fetch(`https://cyf-react.glitch.me/customers/${props.id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setCustomerData(data);
+        setLoading(false)
+      });
+  }, [props.id]);
+
+  if (loading) {
+    return (
+        <div>loading...</div>
+    )
+  }
+
+  if (!customerData) {
+    return null;
+  }
+
+  console.log(customerData);
+
+  return (
+    <ul className="customer-profile">
+      <li>Customer ID: {props.id}</li>
+      <li>Email: {customerData.email}</li>
+      <li>VIP status: {customerData.vip ? "VIP" : "Not VIP"}</li>
+      <li>Phone number: {customerData.phoneNumber}</li>
+    </ul>
+  );
 }
 
 export default CustomerProfile;
