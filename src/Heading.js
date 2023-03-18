62383f67bbbf125511d13249d50898d245c65b3b
MohammadSefidgar <108987757+MohammadSefidgar@users.noreply.github.com>
2023-03-18 14:56:11 +0000

    Update

--- src/Heading.js
+++ src/Heading.js
@@ -1,14 +1,11 @@
 import React from "react";
+import logo from "./logo.png";
 
 const Heading = () => {
   return (
     <header className="App-header">
       <h1 className="logo-title">CYF Hotel</h1>
-      <img
-        className="logo-hotel"
-        src="../images/hotel-logo.svg"
-        alt="Hotel Logo"
-      />
+      <img className="logo-hotel" src={logo} alt="Hotel Logo" />
     </header>
   );
 };
