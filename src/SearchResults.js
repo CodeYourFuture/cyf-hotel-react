867a4a334f72e561f6a104d5a6e838ae70b05233
Howard <howardsoon@gmail.com>
2023-03-19 23:48:04 +0000

    fixed bug

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,34 +1,7 @@
 import React, { useState } from "react";
 import SearchRow from "./SearchRow";
 import moment from "moment";
-/**
- * "id": 1,
-    "title": "Mr",
-    "firstName": "John",
-    "surname": "Doe",
-    "email": "johndoe@doe.com",
-    "roomId": 2,
-    "checkInDate": "2017-11-21",
-    "checkOutDate": "2017-11-23"
- */
 
-//will receive props.results which is an array above
-//TODO <<< POPULATE the 
-
-// const SearchResults = (props) => {
-//     return (
-//       <table>
-//             Search Results
-//        <thead>
-//           <tr>
-//               {Object.keys(props.results[0]).map((colHeading) => (
-//              <th key={colHeading}>{colHeading}</th>
-//           ))}
-//           </tr>
-//         </thead>
-//       </table>
-//      );
-//     };
 
 const SearchResults=(props)=> {
   return (
@@ -74,32 +47,7 @@ const SearchResults=(props)=> {
   );
 
 
-//   const tr = props.results.map((element) => (
-//     <>
-//       <SearchRow key={element.id} guest={element} />
-//     </>
-//   ));
-//   return (
-//     <>
-//       <table class="table">
-//         <thead class="thead-dark">
-//           <tr>
-//             <th scope="col">Id</th>
-//             <th scope="col">Title</th>
-//             <th scope="col">First</th>
-//             <th scope="col">Last</th>
-//             <th scope="col">Email</th>
-//             <th scope="col">Room id</th>
-//             <th scope="col">Check in</th>
-//             <th scope="col">Check out</th>
-//             <th scope="col">Nights</th>
-//             <th scope="col">Profile</th>
-//           </tr>
-//         </thead>
-//         <tbody>{tr}</tbody>
-//       </table>
-//     </>
-//   );
+
  }
 
 export default SearchResults;
--- src/SearchRow.js
+++ src/SearchRow.js
@@ -45,45 +45,7 @@ const [customClass, setCustomClass] = useState("");
   );
 
 
-  // const [colour, setColour] = useState("");
-  // function NewColor() {
-  //   setColour(colour ? "" : "newTrColor");
-  // }
-
-  // const [showCustomerProfile, setShowProfile] = useState(false);
-  // function profileHandler() {
-  //   setShowProfile((showProfile) => !showProfile);
-  // }
-
-  // return (
-  //   <>
-  //     <tr className={colour} onClick={NewColor}>
-  //       <td scope="row">{props.guest.id}</td>
-  //       <td>{props.guest.title}</td>
-  //       <td>{props.guest.firstName}</td>
-  //       <td>{props.guest.surname}</td>
-  //       <td>{props.guest.email}</td>
-  //       <td>{props.guest.roomId}</td>
-  //       <td>{props.guest.checkInDate}</td>
-  //       <td>{props.guest.checkOutDate}</td>
-  //       <td>
-  //         {" "}
-  //         {moment(props.guest.checkOutDate).diff(
-  //           moment(props.guest.checkInDate),
-  //           "days"
-  //         )}
-  //       </td>
-  //       <td>
-  //         <button className="button-pointer" onClick={profileHandler}>
-  //           {showCustomerProfile ? "Hide Profile" : "Show Profile"}
-  //         </button>
-  //       </td>
-  //     </tr>
-  //     <td colSpan={5}>
-  //       {showCustomerProfile && <CustomerProfile id={props.guest.id} />}
-  //     </td>
-  //   </>
-  // );
+  
 }
 
 export default SearchRow;
