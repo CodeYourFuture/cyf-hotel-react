0626cde6db5d8d7cc8cac6b7ad9696a8f664b7a3
Joseph Mwanza <jmwanzaj@yahoo.com>
2023-06-03 15:32:08 +0100

    Made a header and centred the image

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 
 import Bookings from "./Bookings";
 import "./App.css";
+import Heading from "./Header";
+
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel DJMS</header>
+      {/* <header className="App-header">CYF Hotel DJMS</header> */}
+      <Heading />
+
       <Bookings />
+      
     </div>
   );
 };
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+
+const Heading = () => {
+    return (<header>
+        <img src = "https://images.unsplash.com/photo-1455587734955-081b22074882?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1920&q=80"></img>
+        </header>
+    )
+}
+
+export default Heading;
