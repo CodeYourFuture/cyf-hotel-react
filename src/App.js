29c6619a57adb1442531c513a02fb54f3ac9acc6
Delnia Alipour <delniaalipur@gmail.com>
2023-06-03 13:21:25 +0100

    Create and use a new component to show info cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import TouristInfoCard from "./TouristInfoCards";
 import Bookings from "./Bookings";
 import "./App.css";
 
@@ -8,6 +8,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">CYF Hotel DJMS</header>
       <Bookings />
+      <TouristInfoCard />
     </div>
   );
 };
