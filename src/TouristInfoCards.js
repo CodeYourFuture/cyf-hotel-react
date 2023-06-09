906dff5ff9e9665ab6a4106333d162790f58f112
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-06-09 16:19:23 +0100

    Created 3 x tourist info cards
    Not added the css yet.

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -1,32 +1,45 @@
 import React from "react";
 
-const touristCardData = [
-  {
-    image:
-      "https://images.pexels.com/photos/12319481/pexels-photo-12319481.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    city: "Glasgow",
-    description:
-      "Ancestor laugh fat lit chair running Uruks master loyal ready. Explanation Dori aside. Frailty within Witch-king greedily. What about second breakfast? ",
-    url: "https://peoplemakeglasgow.com/",
-  },
-  {
-    image:
-      "https://images.pexels.com/photos/11014204/pexels-photo-11014204.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    city: "Manchester",
-    description:
-      "Plant charged flying uprooted Goblin-town weren't? Unto Hobbit's impassable first bolted grass if. Share remove fume they'd Baggins built fox.",
-    url: "https://www.visitmanchester.com/",
-  },
-  {
-    image:
-      "https://images.pexels.com/photos/1906888/pexels-photo-1906888.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    city: "London",
-    description:
-      "Defiling Isildur's om sauteed offer? Stronghold prizewinners names afterwards rob Probably memory seize thank nightfall anger!",
-    url: "https://www.visitlondon.com/",
-  },
-];
+function TouristInfoCards() {
+  const touristCardData = [
+    {
+      image:
+        "https://images.pexels.com/photos/12319481/pexels-photo-12319481.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      city: "Glasgow",
+      description:
+        "Ancestor laugh fat lit chair running Uruks master loyal ready. Explanation Dori aside. Frailty within Witch-king greedily. What about second breakfast? ",
+      url: "https://peoplemakeglasgow.com/",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/11014204/pexels-photo-11014204.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      city: "Manchester",
+      description:
+        "Plant charged flying uprooted Goblin-town weren't? Unto Hobbit's impassable first bolted grass if. Share remove fume they'd Baggins built fox.",
+      url: "https://www.visitmanchester.com/",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/1906888/pexels-photo-1906888.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      city: "London",
+      description:
+        "Defiling Isildur's om sauteed offer? Stronghold prizewinners names afterwards rob Probably memory seize thank nightfall anger!",
+      url: "https://www.visitlondon.com/",
+    },
+  ];
 
-function TouristInfoCards() {}
+  return touristCardData.map((element) => (
+    <div className="card">
+      <div className="card-body">
+        <img src={element.image} className="card-img-top" />
+        <h2 className="card-title">{element.city}</h2>
+        <p className="card-description">{element.description}</p>
+        <a href="#" className="btn btn-primary">
+          Learn More
+        </a>
+      </div>
+    </div>
+  ));
+}
 
 export default TouristInfoCards;
