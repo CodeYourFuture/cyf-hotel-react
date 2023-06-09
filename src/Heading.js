9ab3ecf2227186a1ab492cdb3b98c1983795595a
HereshT <hiresh.turky@gmail.com>
2023-06-09 14:21:52 +0100

    Lesson 1 task 5. Create a table to show hotel bookings is Done.
    I have Created a SearchResults  component that shows hotel bookings in a <table> element. Each booking has an id, title, first name, surname, email, room id, check in date and check out date.

--- src/Heading.js
+++ src/Heading.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import "./Heading.css";
 const HotelIcon = () => (
   <svg
     className="hotel-icon"
---
+++ src/SearchResults.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const SearchResults = ({ results }) => (
+  <table className="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>First Name</th>
+        <th>Surname</th>
+        <th>Email</th>
+        <th>Room ID</th>
+        <th>Check In Date</th>
+        <th>Check Out Date</th>
+      </tr>
+    </thead>
+    <tbody>
+      {results.map((booking) => (
+        <tr key={booking.id}>
+          <td>{booking.id}</td>
+          <td>{booking.title}</td>
+          <td>{booking.firstName}</td>
+          <td>{booking.surname}</td>
+          <td>{booking.email}</td>
+          <td>{booking.roomId}</td>
+          <td>{booking.checkInDate}</td>
+          <td>{booking.checkOutDate}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
+
+export default SearchResults;
