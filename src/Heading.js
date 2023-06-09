91d38a054b7053335a1a361f9ba2de4829935ca3
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-06-09 13:20:39 +0100

    2. Extract the header in its own component

---
+++ src/Heading.js
@@ -0,0 +1,14 @@
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <img
+        src="https://as1.ftcdn.net/v2/jpg/02/21/73/46/1000_F_221734695_OLItP2OWAkRqBLol8esvA4a9PuTV5pgK.jpg"
+        alt="Hotel Icon"
+        id="header-img"
+      />
+      CYF Hotel
+    </header>
+  );
+};
+
+export default Heading;
