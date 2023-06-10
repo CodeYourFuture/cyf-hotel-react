80d612e4bfdbebf3edd14c2d9a71a2637f402c3f
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-06-10 22:36:36 +0100

    Added footer css file and added styling

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,11 @@
+import "./Footer.css";
 import React from "react";
 
 // destructuring props
 function Footer({ address, email, telephone }) {
   return (
-    <footer>
-      <ul>
+    <footer className="footer">
+      <ul className="contact-details">
         <li key={address}>{address}</li>
         <li key={email}>{email}</li>
         <li key={telephone}>{telephone}</li>
