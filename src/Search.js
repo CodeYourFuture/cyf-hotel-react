0654bfbd9b2c23d19aa591882c1f526fd6fe4611
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-06-11 19:33:37 +0100

    create separate new component
    for search button
    and import this button in search JS

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SearchButton from "./SearchButton";
 
 const Search = () => {
   return (
@@ -17,11 +18,11 @@ const Search = () => {
                 className="form-control"
                 placeholder="Customer name"
               />
-              <button className="btn btn-primary">Search</button>
             </div>
           </form>
         </div>
       </div>
+      <SearchButton />
     </div>
   );
 };
