42ce6c9b92a8053b810adc1fcd3cc472fa005412
Delnia Alipour <delniaalipur@gmail.com>
2023-06-12 19:24:46 +0100

    Preparing to add more pizzas

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Restaurant = () => {
-  const pizzas = 0;
+  // const pizzas = 0;
+  const [orders, setOrders] = useState(0);
+  function orderOne() {
+    setOrders(orders + 1);
+  }
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
+          Pizzas: {orders}
+          <button className="btn btn-primary" onClick={orderOne}>
+            Add
+          </button>
         </li>
       </ul>
     </div>
