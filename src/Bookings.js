25367618bf357ca29fa722053b44a8fc087d3619
HereshT <hiresh.turky@gmail.com>
2023-06-12 09:12:48 +0100

    Lesson 2 part 16. Load bookings remotely is Done.
    Lesson 2 part 16. Load bookings remotely, now we will get and load the bookings from a remote API.
    
    I have added Restaurant.css

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,20 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings); // add this line
+  const [bookings, setBookings] = useState([]);
 
   const search = (searchVal) => {
     console.info("TO DO!", searchVal);
   };
 
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then((response) => response.json())
+      .then((data) => setBookings(data));
+  }, []);
+
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults results={bookings} />{" "}
+        <SearchResults results={bookings} />
       </div>
     </div>
   );
