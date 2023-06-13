fe1ec232bc6cdea2ee250140c449d549f397ec0e
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2023-06-13 21:59:46 +0100

    lesson 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-
 import Bookings from "./Bookings";
+import SearchResults from "./SearchResults";
+
 import "./App.css";
 
 const Heading = () => (
@@ -10,11 +11,58 @@ const Heading = () => (
   </div>
 );
 
+const TouristInfoCards = (props) => (
+   (
+    <div className="card">
+      <img src={props.image} className="card-img-top" />
+      <h3>{props.name}</h3>
+      <p>
+        Pellentesque habitant morbi tristique senectus et netus et malesuada
+        fames ac turpis egestas. Ut congue volutpat erat. Suspendisse nec
+        vestibulum risus, at sollicitudin lectus. Sed rhoncus odio ac magna
+        ultrices, in consectetur tortor convallis. Sed sed dui ante. Etiam
+        efficitur lacus velit, eget placerat massa rutrum ut. Integer posuere
+        elit eget imperdiet molestie.
+      </p>
+      <div className="card-body">
+        <a href={props.url} className="btn btn-primary" target="_blank">
+          More information
+        </a>
+      </div>
+    </div>
+  )
+);
+
+const Footer = (props) => (
+  <ul>
+    {props.text.map(item => <li key={item}>{item}</li>)}
+  </ul>
+)
+
 const App = () => {
   return (
     <div className="App">
       <Heading />
+      <div className="InfoCards">
+        <TouristInfoCards
+          name="Glasgow"
+          image="./university-of-glasgow.jpg"
+          url="https://peoplemakeglasgow.com/"
+        />
+        <TouristInfoCards
+          name="Manchester"
+          image="./manchester.webp"
+          url="https://visitmanchester.com"
+        />
+        <TouristInfoCards
+          name="London"
+          image="./london.webp"
+          url="https://visitlondon.com"
+        />
+      </div>
+
       <Bookings />
+      <Footer text={["123 Fake Street, London, E1 4UD", "hello@fakehotel.com", "0123 456789"]} />
     </div>
   );
 };
--- src/Bookings.js
+++ src/Bookings.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Search from "./Search.js";
-// import SearchResults from "./SearchResults.js";
-// import FakeBookings from "./data/fakeBookings.json";
+import SearchResults from "./SearchResults.js";
+import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
-
+  
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        {/* <SearchResults results={FakeBookings} /> */}
+        <SearchResults results={FakeBookings} />
       </div>
     </div>
   );
