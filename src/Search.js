a8b324b5c549775ec81dbfa4311086bf7304c884
BEKIRKSU <bekirkscu@gmail.com>
2023-06-14 09:22:07 -0700

    Header imported

--- src/Search.js
+++ src/Search.js
@@ -1,16 +1,16 @@
 import React from "react";
 import SearchButton from "./SearchButton";
+import Header from "./Header";
 
 const Search = () => {
   return (
-    < Header/>
+    <Header/>
       <div className="row search-wrapper">
         <div className="col">
           <form className="form-group search-box">
             <label htmlFor="customerName">Customer name</label>
-            < SearchButton/>
+            <SearchButton/>
           </form>
-        </div>
       </div>
     </div>
   );
