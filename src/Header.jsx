a8a8b30b48209c2788aa4aa6e04381d36342be59
BEKIRKSU <bekirkscu@gmail.com>
2023-06-14 09:29:33 -0700

    Image of Rome added

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,9 +5,9 @@ const Header = () => {
 <div className="search">
       <div className="page-header">
         <h4 className="text-left">Search Bookings</h4>
+        <img className="imageOfRome" src="https://media.gq-magazine.co.uk/photos/5d13a3a9b6fee91a87c9f87f/16:9/pass/Rome-hp-GQ-24May16_istock_b.jpg" alt="image of Rome" />
       </div>
       </div>
-
       );
       };
 
