bb03e7b8d7e93097784b61f52e56d57d4f205a56
BEKIRKSU <bekirkscu@gmail.com>
2023-06-14 10:16:53 -0700

    App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import TouristInfoCards from "./TouristInfoCards";
 import Bookings from "./Bookings";
+import Footer from "./Footer";
 import "./App.css";
 
 const App = () => {
+  const address = [
+    "123 Fake Street, London, E1 4UD",
+    "hello@fakehotel.com",
+    "0123 456789",
+  ];
+
   return (
     <div className="App">
       <header className="App-header">CYF Hotel</header>
       <TouristInfoCards/>
       <Bookings />
+      <Footer address={address}/>
     </div>
   );
 };
