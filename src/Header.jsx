2622951e90ed86e03cdd8fbd98c58b2a5ac2015b
BEKIRKSU <bekirkscu@gmail.com>
2023-06-14 09:20:04 -0700

    Header

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+<div className="search">
+      <div className="page-header">
+        <h4 className="text-left">Search Bookings</h4>
+      </div>
+      </div>
+
+      );
+      };
+
+export default Header;
