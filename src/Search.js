fa6f1f42dc547fd23b9a5ce6428db1cc20ed9321
BEKIRKSU <bekirkscu@gmail.com>
2023-06-14 09:20:19 -0700

    Header added as component

--- src/Search.js
+++ src/Search.js
@@ -1,24 +1,14 @@
 import React from "react";
+import SearchButton from "./SearchButton";
 
 const Search = () => {
   return (
-    <div className="search">
-      <div className="page-header">
-        <h4 className="text-left">Search Bookings</h4>
-      </div>
+    < Header/>
       <div className="row search-wrapper">
         <div className="col">
           <form className="form-group search-box">
             <label htmlFor="customerName">Customer name</label>
-            <div className="search-row">
-              <input
-                type="text"
-                id="customerName"
-                className="form-control"
-                placeholder="Customer name"
-              />
-              <button className="btn btn-primary">Search</button>
-            </div>
+            < SearchButton/>
           </form>
         </div>
       </div>
