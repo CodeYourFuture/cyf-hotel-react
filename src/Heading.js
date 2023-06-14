25576fa5d6976bf6056a7456c27a7fcb523279ad
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-06-14 19:00:14 +0100

    updated

--- src/Heading.js
+++ src/Heading.js
@@ -2,7 +2,7 @@ import React from "react";
 const Heading = () => {
     const name = "CYF HOTEL"
     return <header className="App-header">
-    <div><img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMiyT0CsbbuRmEjXFYugoingNpNK51BhAOtg&usqp=CAU" className = "logo"alt = "hotel-logo"></img></div>
+    <div><img src ="https://freesvg.org/img/1548468101.png" className = "logo"alt = "hotel-logo"></img></div>
     <div><h2>{name}</h2></div>
 </header>
 }
