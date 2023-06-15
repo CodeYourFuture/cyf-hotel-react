2df1c0ec34e62425fe782d8b90c8e829817bf09c
softacoder <113185323+softacoder@users.noreply.github.com>
2023-06-15 13:39:01 +0100

    update footer

--- src/App.js
+++ src/App.js
@@ -6,11 +6,29 @@ import Footer from "./Footer";
 import Restaurant from "./Restaurant";
 import "./App.css";
 
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <header className="App-header">CYF Hotel</header>
+//       <Bookings />
+//     </div>
+//   );
+// };
+
 const App = () => {
+  const address = [
+    "123 Fake Street, London, E1 4UD",
+    "hello@fakehotel.com",
+    "0123 456789",
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Heading />
+      <TouristInfoCard />
       <Bookings />
+      <Restaurant />
+      <Footer newProp={address} />
     </div>
   );
 };
--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -36,7 +36,7 @@ return = (
 
 </section>
 );
-);
+// );
 
 
 export default TouristInfoCards;
