bf1879b17d81e61f19cc450518f3de3b8657b838
AdrianIlovan <adrian88ilovan@yahoo.com>
2023-06-15 13:28:18 +0100

    ####2 Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-
 import Bookings from "./Bookings";
 import "./App.css";
+import Heading from "./Heading";
+
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Heading />
       <Bookings />
     </div>
   );
---
+++ src/Heading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const Heading = () => {
+    return(
+        <header className="App-header">
+            <img alt="App-logo" className="App-logo" src="https://marketplace.canva.com/EAE8Xyb2xY8/1/0/1600w/canva-gold-exclusive-royal-luxury-hotel-logo-izeElzvImEY.jpg"></img>
+            <h1>CYF Hotel</h1></header>
+    );
+}
+
+export default Heading;
