55b8b8d515492921fdd017c422a08d2de89ff4e7
AdrianIlovan <62564810+AdrianIlovan@users.noreply.github.com>
2023-06-15 16:05:16 +0100

    ####3 Done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Bookings from "./Bookings";
 import "./App.css";
 import Heading from "./Heading";
+import TouristInfoCards from "./TouristInfoCards"
 
 
 const App = () => {
   return (
     <div className="App">
       <Heading />
+      <TouristInfoCards />
       <Bookings />
     </div>
   );
