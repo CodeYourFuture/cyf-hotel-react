57f928774525366a1538065192deb822fec100e3
BEKIRKSU <bekirkscu@gmail.com>
2023-06-15 09:08:03 -0700

    BookingTable made

---
+++ src/BookingTable.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const BookingTable = () => {
+  const bookings = [
+    {
+      id: 1,
+      title: 'Mr',
+      firstName: 'John',
+      surname: 'Doe',
+      email: 'john@example.com',
+      roomId: 101,
+      checkInDate: '2023-06-16',
+      checkOutDate: '2023-06-20',
+    },
+    {
+      id: 2,
+      title: 'Mrs',
+      firstName: 'Jane',
+      surname: 'Smith',
+      email: 'jane@example.com',
+      roomId: 202,
+      checkInDate: '2023-06-18',
+      checkOutDate: '2023-06-23',
+    },
+    // Add more bookings here...
+  ];
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>First Name</th>
+          <th>Surname</th>
+          <th>Email</th>
+          <th>Room ID</th>
+          <th>Check In Date</th>
+          <th>Check Out Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {bookings.map((booking) => (
+          <tr key={booking.id}>
+            <td>{booking.id}</td>
+            <td>{booking.title}</td>
+            <td>{booking.firstName}</td>
+            <td>{booking.surname}</td>
+            <td>{booking.email}</td>
+            <td>{booking.roomId}</td>
+            <td>{booking.checkInDate}</td>
+            <td>{booking.checkOutDate}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default BookingTable;
