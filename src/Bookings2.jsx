8fd2533f53248876f2602b177ff6ef81e95eb7ae
BEKIRKSU <bekirkscu@gmail.com>
2023-06-15 09:17:28 -0700

    Bookings2 made

---
+++ src/Bookings2.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BookingTable from './BookingTable';
+
+const Bookings2 = () => {
+  return (
+    <div>
+      <h1>CYF Hotel Bookings</h1>
+      <BookingTable />
+    </div>
+  );
+};
+
+export default Bookings2;
