e7dacf4c51584c5fa6d36c38cbf4fda7de810170
BEKIRKSU <bekirkscu@gmail.com>
2023-06-15 09:08:28 -0700

    Bookings component made

---
+++ src/Bookings.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BookingTable from './BookingTable';
+
+const Bookings = () => {
+  return (
+    <div>
+      <h1>Hotel Bookings</h1>
+      <BookingTable />
+    </div>
+  );
+};
+
+export default Bookings;
