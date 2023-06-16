e0d41eb4ca7e52f05b693fd0da216ce58765f4e9
RahmaB1 <122676773+RahmaB1@users.noreply.github.com>
2023-06-16 20:08:22 +0100

    header component with style

--- src/Header.js
+++ src/Header.js
@@ -1,2 +1,16 @@
+import React from "react";
+import "./App.css";
 
-// header
+function Heading() {
+  return (
+    <>
+      <header className="App-header">CYF Hotel</header>
+      <img
+        className="hotel-logo"
+        src="https://img.freepik.com/free-vector/golden-elegant-logo-template-with-ornaments_23-2148212128.jpg?w=900&t=st=1686937700~exp=1686938300~hmac=96cba09c7292154b8447b7c54e0d24116ed3c8fc0a2ec07ffc4ac28cab762b20"
+      ></img>
+    </>
+  );
+}
+
+export default Heading;
