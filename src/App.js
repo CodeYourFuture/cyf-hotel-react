70f92f1a92300e05894ee52b9f2bd98819feeddb
AppolinFotso <appolin.s.fotso@gmail.com>
2023-06-17 13:44:41 +0100

    Updated the TouristInfoCards component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import TouristInfoCards from "./TouristInfoCards";
 import Bookings from "./Bookings";
 import "./App.css";
 
@@ -7,6 +7,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">CYF Hotel</header>
+      <TouristInfoCards/>
       <Bookings />
     </div>
   );
