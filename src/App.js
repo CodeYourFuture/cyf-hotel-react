b3ffaae30b795d0bd39ffe27764b90885bc253ae
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-06-18 21:50:41 +0100

    update for exercise 8 done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Heading from "./Heading";
 import Bookings from "./Bookings";
+import Restaurant from "./Restaurant";
 import TouristInfoCards from "./TouristInfoCards";
 import FooterCompo from "./FooterCompo";
 import "./App.css";
@@ -11,6 +12,7 @@ const App = () => {
       <Heading />
       <TouristInfoCards />
       <Bookings name />
+      <Restaurant />
       <FooterCompo
         details={[
           "123 Fake Street, London, E1 4UD",
