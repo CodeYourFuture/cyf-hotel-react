b2cfaf0d2dfa01a43769fbc37b98c55612582792
AppolinFotso <appolin.s.fotso@gmail.com>
2023-06-19 17:37:23 +0100

    Added RestaurantButton to Order component

--- src/Order.js
+++ src/Order.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import RestaurantButton from "./RestaurantButton";
 
-function Order() {}
+function Order() {
+  const [orders, setOrders] = useState(0);
+  function orderOne() {
+    setOrders(orders + 1);
+  }
+  return (
+    <li>
+      Pizzas: {orders}
+      <RestaurantButton order={orderOne} />
+    </li>
+  );
+}
 
 export default Order;
--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,19 +1,13 @@
 import React, { useState } from "react";
 import RestaurantButton from "./RestaurantButton";
+import Order from "./Order";
 
 const Restaurant = () => {
-  const [orders, setOrders] = useState(0);
-  function orderOne() {
-    setOrders(orders + 1);
-  }
   return (
     <div className="center">
       <h3>Restaurant Orders</h3>
       <ul>
-        <span>
-          Pizzas: {orders}
-          <RestaurantButton order={orderOne} />
-        </span>
+        <Order />
       </ul>
     </div>
   );
