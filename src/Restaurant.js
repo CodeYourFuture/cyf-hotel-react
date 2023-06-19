e31faf45d74ef552532dbc8b3fc298d63ab36193
nasir ali <a.nasirali@outlook.com>
2023-06-19 13:22:55 +0100

    orders tallyho

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -2,13 +2,19 @@ import React, { useState } from "react";
 
 const Restaurant = () => {
   const [orders, setOrders] = useState(0);
+  function orderOne() {
+    setOrders(orders + 1);
+  }
   return (
     <div className="center">
       <h3>Restaurant Orders</h3>
       <ul>
-        <li>
-          Pizzas: {orders} <button className="btn btn-primary">Add</button>
-        </li>
+        <span>
+          Pizzas: {orders}{" "}
+          <button className="btn btn-primary" onClick={orderOne}>
+            Add
+          </button>
+        </span>
       </ul>
     </div>
   );
