9504c8938e0234b67877a34e3f809024b2821df4
Afsha10 <unityofbeing1@gmail.com>
2023-06-20 12:54:56 +0100

    7. Calculated and showed the number of nights for each booking
    Added another column to my <SearchResults /> table which shows the number of nights a guest is staying.
    
    Tried installing the moment.js library (by installing it with npm install moment --save) and using the .diff() method to compare dates.
    
    Tested: Each booking in my table and showed the number of nights in a separate column. For example, Mr John Doe has a booking for 2 nights.

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import moment from "moment";
 
 const SearchResults = (props) => {
     return (
@@ -13,23 +14,28 @@ const SearchResults = (props) => {
             <th scope="col">Room id</th>
             <th scope="col">Check in date</th>
             <th scope="col">Check out date</th>
+            <th scope="col">Number of nights</th>
           </tr>
         </thead>
         <tbody>
-        {props.results.map((result, index) => {
+          {props.results.map((result, index) => {
+            const a = moment(result.checkOutDate);
+            const b = moment(result.checkInDate);
+            const days = a.diff(b, "days"); // 1
             return (
-                <tr key={index}>
-                    <td>{result.id}</td>
-                    <td>{result.title}</td>
-                    <td>{result.firstName}</td>
-                    <td>{result.surname}</td>
-                    <td>{result.email}</td>
-                    <td>{result.roomId}</td>
-                    <td>{result.checkInDate}</td>
-                    <td>{result.checkOutDate}</td>
-                </tr>
-            )
-        })}
+              <tr key={index}>
+                <td>{result.id}</td>
+                <td>{result.title}</td>
+                <td>{result.firstName}</td>
+                <td>{result.surname}</td>
+                <td>{result.email}</td>
+                <td>{result.roomId}</td>
+                <td>{result.checkInDate}</td>
+                <td>{result.checkOutDate}</td>
+                <td>{days}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     );
