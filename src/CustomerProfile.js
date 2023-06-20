a198e373489409b464183231a11ef3968a22ec13
malkitbenning <malkitbenning@gmail.com>
2023-06-20 16:29:29 +0100

    Step 21 complete
    Customer profile data now showing based on button click

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -1,15 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 const CustomerProfile = (props) => {
+  let [emailAddress, setEmailAddress] = useState("");
+  let [vipStatus, setVipStatus] = useState(null);
+  let [phoneNumber, setPhoneNumber] = useState("");
+
   useEffect(() => {
-    fetch(`https://cyf-react.glitch.me/customers/${props.customerProfileId}`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      });
+    console.log("profile id", props.customerProfileId);
+    if (props.customerProfileId) {
+      fetch(`https://cyf-react.glitch.me/customers/${props.customerProfileId}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setEmailAddress(data.email);
+          setVipStatus(data.vip ? "Vip Status" : "Not VIP");
+          setPhoneNumber(data.phoneNumber);
+        });
+    }
   }, [props.customerProfileId]);
 
-  return <p>Customer {props.customerProfileId} profile</p>;
+  return (
+    <ul>
+      <li>Customer {props.customerProfileId} profile</li>
+      <li>Email: {emailAddress}</li>
+      <li>VIP: {vipStatus}</li>
+      <li>Phone: {phoneNumber}</li>
+    </ul>
+  );
 };
 
 export default CustomerProfile;
