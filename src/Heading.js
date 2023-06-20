985b317ff88c735b90b32849ad2fc7a5956a6811
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-06-20 15:16:33 +0100

    Lesson1/2 done

---
+++ src/Heading.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./LogoHotel.png";
+
+//console.log(LogoHotel)
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <img className="App-logo" src={logo} alt="Logo" />
+      CYF Hotel
+    </header>
+  );
+};
+
+export default Heading;
