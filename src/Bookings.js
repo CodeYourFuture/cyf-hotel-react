52a671292a73587536327ee673d656501da4b5ef
nasir ali <a.nasirali@outlook.com>
2023-06-21 11:50:51 +0100

    added api capabilities

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [booking, setBooking] = useState(FakeBookings);
+  const [booking, setBooking] = useState([]);
+
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then((response) => response.json())
+      .then((data) => setBooking(data));
+  });
   const search = (searchVal) => {
     console.info("TO DO!", searchVal);
   };
