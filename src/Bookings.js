48af99e2743359377bc9e50174acfd677c65cb4e
HereshT <hiresh.turky@gmail.com>
2023-06-21 21:41:58 +0100

    Task 23. Show an error message
    When loading bookings data from the /error endpoint, an error message is displayed on the screen.

--- src/Bookings.js
+++ src/Bookings.js
@@ -5,6 +5,7 @@ import SearchResults from "./SearchResults.js";
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const search = (searchVal) => {
     console.log("TO DO!", searchVal);
@@ -18,11 +19,20 @@ const Bookings = () => {
 
   useEffect(() => {
     setIsLoading(true);
-    fetch("https://cyf-react.glitch.me/delayed")
-      .then((res) => res.json())
+    fetch("https://cyf-react.glitch.me/error")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
       .then((data) => {
         console.log(data);
         setBookings(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
         setIsLoading(false);
       });
   }, []);
@@ -33,6 +43,8 @@ const Bookings = () => {
         <Search search={search} />
         {isLoading ? (
           <p>Loading... Please wait.</p>
+        ) : error ? (
+          <p>Error: {error}</p>
         ) : (
           <SearchResults results={bookings} />
         )}
