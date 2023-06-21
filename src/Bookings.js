be87d0d69ffa7ae13ad2f74e5aecd59c5190222d
nasir ali <a.nasirali@outlook.com>
2023-06-21 12:15:08 +0100

    its working now

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,30 +1,33 @@
 import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-// import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [booking, setBooking] = useState([]);
+  const [bookings, setBookings] = useState([]);
   const [filteredBookings, setFilteredBookings] = useState([]);
 
   useEffect(() => {
     fetch("https://cyf-react.glitch.me")
       .then((response) => response.json())
       .then((data) => {
-        setBooking(data);
+        setBookings(data);
         setFilteredBookings(data);
       });
-  });
+  }, []);
+
   const search = (searchVal) => {
     console.info("TO DO!", searchVal);
+    const searchedBookings = bookings.filter(
+      (searchedNames) => searchedNames.firstName.toLowerCase().includes(searchVal.toLowerCase()) || searchedNames.surname.toLowerCase().includes(searchVal.toLowerCase())
+    );
+    setFilteredBookings(searchedBookings);
   };
 
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        {/* <SearchResults results={FakeBookings} /> */}
-        <SearchResults booking={booking} />
+        <SearchResults booking={filteredBookings} />
       </div>
     </div>
   );
