5d78cb2beffcc018221188ab1cfb8c245d3887c7
nasir ali <a.nasirali@outlook.com>
2023-06-21 12:09:52 +0100

    testing the search parameter, not working now

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
+// import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
   const [booking, setBooking] = useState([]);
+  const [filteredBookings, setFilteredBookings] = useState([]);
 
   useEffect(() => {
     fetch("https://cyf-react.glitch.me")
       .then((response) => response.json())
-      .then((data) => setBooking(data));
+      .then((data) => {
+        setBooking(data);
+        setFilteredBookings(data);
+      });
   });
   const search = (searchVal) => {
     console.info("TO DO!", searchVal);
