a763ffe423ece203f90b98c45d2aaa6e654e140e
Adrian Ilovan <adrian88ilovan@yahoo.com>
2023-06-21 15:21:47 +0100

    Level2/14 Done

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
+  const [bookings, setBookings] = useState(FakeBookings);
+
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
@@ -12,7 +14,7 @@ const Bookings = () => {
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        {<SearchResults bookings={FakeBookings} />}
+        {<SearchResults bookings={bookings} />}
       </div>
     </div>
   );
