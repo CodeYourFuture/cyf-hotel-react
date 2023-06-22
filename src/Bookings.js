5b738fe673952d973dfcc7fafa76dab60bd1eb5c
Shadi38 <113238379+Shadi38@users.noreply.github.com>
2023-06-22 12:11:44 +0100

    finishing lesson 2

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
@@ -8,11 +8,13 @@ const Bookings = () => {
     console.info("TO DO!", searchVal);
   };
 
+  const [bookings, setBookings]=useState(FakeBookings);
+
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults results={FakeBookings} /> 
+        <SearchResults results={bookings} /> 
       </div>
     </div>
   );
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,48 +1,65 @@
 import moment from "moment";
 import React from "react";
+import { useState } from "react";
 function SearchResults(props) {
-    return (
-      <div>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">id</th>
-              <th scope="col">titles</th>
-              <th scope="col">first name</th>
-              <th scope="col">surname</th>
-              <th scope="col">email</th>
-              <th scope="col">room id</th>
-              <th scope="col">check in date</th>
-              <th scope="col">check out date</th>
-              <th scope="col">stay nights</th>
-            </tr>
-          </thead>
-          <tbody>
-            {props.results.map((result, index) => {
-              return (
-                <tr key={index}>
-                  <th scope="row">{result.id}</th>
-                  <td>{result.title}</td>
-                  <td>{result.firstName}</td>
-                  <td>{result.surname}</td>
-                  <td>{result.email}</td>
-                  <td>{result.roomId}</td>
-                  <td>{result.checkInDate}</td>
-                  <td>{result.checkOutDate}</td>
-                  <td>
-                    {moment(result.checkOutDate).diff(
-                      moment(result.checkInDate),
-                      "days"
-                    )}
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    );
-    
+  debugger;
+
+  const [selected, setSelected] = useState([]);
+
+  function handleClick(index) {
+    if (selected.includes(index)) {
+      setSelected((selected) => {
+        return selected.filter((item) => item !== index);
+      });
+    } else {
+      setSelected([...selected, index]);
+    }
+  }
+
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">titles</th>
+            <th scope="col">first name</th>
+            <th scope="col">surname</th>
+            <th scope="col">email</th>
+            <th scope="col">room id</th>
+            <th scope="col">check in date</th>
+            <th scope="col">check out date</th>
+            <th scope="col">stay nights</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.results.map((result, index) => {
+            return (
+               <tr
+                key={index}
+                className={selected.includes(index) ? "selected" : ""}
+                onClick={() => handleClick(index)}
+              >
+                <th scope="row">{result.id}</th>
+                <td>{result.title}</td>
+                <td>{result.firstName}</td>
+                <td>{result.surname}</td>
+                <td>{result.email}</td>
+                <td>{result.roomId}</td>
+                <td>{result.checkInDate}</td>
+                <td>{result.checkOutDate}</td>
+                <td>
+                  {moment(result.checkOutDate).diff(
+                    moment(result.checkInDate),
+                    "days"
+                  )}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 export default SearchResults;
-
