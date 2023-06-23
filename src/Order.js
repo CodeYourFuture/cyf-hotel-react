6283c228293b0e897b1b5e35ca6c7164386642cf
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2023-06-23 14:09:11 +0100

    Extract pizza order

---
+++ src/Order.js
@@ -0,0 +1,20 @@
+import React ,{useState} from "react";
+import RestaurantButton from "./RestaurantButton";
+
+
+const Order =()=>{
+
+const [orders,setOrders]=useState(0);
+
+const orderOne =()=>{
+  setOrders(orders+1)
+};
+    return(
+        <li>
+            Pizzas:{orders}          
+            <RestaurantButton onClick={orderOne}/>
+        </li>
+    )
+}
+
+export default Order;
--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,21 +1,13 @@
-import React, { useState } from "react";
-import RestaurantButton from "./RestaurantButton";
+import React from "react";
+import Order from "./Order";
 
 const Restaurant = () => {
-
-  const [orders,setOrders]=useState(0);
-
-  const order=()=>{
-    setOrders(orders+1)
-  };
-
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {orders}
-          <RestaurantButton onClick={order}/>
+          <Order/>
         </li>
       </ul>
     </div>
