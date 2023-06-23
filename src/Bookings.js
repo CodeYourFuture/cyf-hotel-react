b5d32663b99adb1931d31b467f0d67621265eb06
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2023-06-23 14:25:59 +0100

    passing bookings from a state variable

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,20 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
+  const[bookings,setBookings]=useState(FakeBookings);
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
 
 
-     
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults results={FakeBookings} />
+        <SearchResults results={bookings} />
       </div>
     </div>
   );
