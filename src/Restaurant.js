471fe855cfb548531fc9bbeb1fda77b929a2b62f
Afsha10 <unityofbeing1@gmail.com>
2023-06-23 21:17:40 +0100

    10. Adding more pizzas
    In the <Restaurant /> component, I created a new function named orderOne. The orderOne function doesn't take any parameters. I used the setOrders function to increment the orders state variable by 1. Then, added an onClick handler to the Add <button> that calls the orderOne function when it's being clicked.
    
    Tested: Click on the Add button a few times and verify that the number of pizzas increases accordingly and it did. :)

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -3,12 +3,15 @@ import "./Restaurant.css";
 
 const Restaurant = () => {
   const [orders, setOrders] = useState(0);
+  const orederOne = () => {
+    setOrders(orders => orders + 1);
+  }
   return (
     <div className="restaurant-orders">
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {orders} <button className="btn btn-primary">Add</button>
+          Pizzas: {orders} <button className="btn btn-primary" onClick={orederOne}>Add</button>
         </li>
       </ul>
     </div>
