d5dc5af0bf59b426a5a6b14f527f8b162763620b
Shadi38 <113238379+Shadi38@users.noreply.github.com>
2023-06-26 22:40:28 +0100

    completing level 18

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
+
 
 const Bookings = () => {
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
 
-  const [bookings, setBookings]=useState(FakeBookings);
+  //const [bookings, setBookings]=useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(()=>{
+    console.log("wait for loading data");
+    fetch(`https://cyf-react.glitch.me`)
+    .then((res) => res.json())
+    .then((data) => setBookings(data));} ,[bookings])
+  
 
   return (
     <div className="App-content">
