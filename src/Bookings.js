1b30ccf58328972b4bfe71371772270a576a301e
Mpanasetckiy <maxbuzz8694@gmail.com>
2023-06-26 22:11:57 +0100

    refactor(Bookings.js): remove unused import statements feat(Bookings.js): add search functionality to filter bookings by first name or surname feat(SearchResults.js): add highlight functionality to table rows when clicked

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,32 +1,33 @@
 import React, { useEffect, useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
+
 import FakeBookings from "./data/fakeBookings.json";
 
-
 const Bookings = () => {
-  let [bookings, setBookings] = useState([]);
-  let [searchResults, setSearchResults] = useState(bookings);
+  const [bookings, setBookings] = useState([]);
+  const [searchResults, setSearchResults] = useState(bookings);
 
   useEffect(() => {
     fetch("https://cyf-react.glitch.me")
-  .then((response) => response.json())
-  .then((data) => setBookings(data))}, []);
-  
-  const search = (searchVal) => {
-  console.info("TO DO!", searchVal);
-  if (searchVal === "") {
-      setBookings(bookings);
-  }
-  const filterBySearch = bookings.filter((booking) =>
-    booking.firstName.includes(searchVal)
-  );
-  console.log(filterBySearch)
-  setBookings(filterBySearch);
-  console.log(bookings);
+      .then((response) => response.json())
+      .then((data) => setBookings(data));
+  }, []);
+
+  const search = (value) => {
+    const searchVal = value.toLocaleLowerCase();
+    console.log("To search:", searchVal);
+
+    const filterBySearch = FakeBookings.filter(({ firstName, surname }) => {
+      return (
+        firstName.toLocaleLowerCase().includes(searchVal) ||
+        surname.toLocaleLowerCase().includes(searchVal)
+      );
+    });
+
+    setBookings(filterBySearch);
   };
-  
- 
+
   return (
     <div className="App-content">
       <div className="container">
