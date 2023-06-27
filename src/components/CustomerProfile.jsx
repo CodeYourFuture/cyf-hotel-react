efbf3309b4d02ee93e7331b5392061ea5a964359
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-06-27 15:10:39 +0100

    20. Display a customer profile - step 1

---
+++ src/components/CustomerProfile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CustomerProfile = ({ id }) => {
+  return (
+    <div>
+      <h3>Customer {id} Profile</h3>
+    </div>
+  );
+};
+
+export default CustomerProfile;
--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,49 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
+import CustomerProfile from "./CustomerProfile";
 
 const SearchResults = ({ results }) => {
-/*   const [isHighlighted, setIsHighlighted] = useState(false)
-  const changeColor = (e) => {
-    console.log(e.target.parentElement.className)
-    e.target.parentElement.className += 'seelcted'
-    setIsHighlighted(true)
-  } */
-  return (
-    <table className="table table-striped">
-      <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Title</th>
-          <th scope="col">First Name</th>
-          <th scope="col">Last Name</th>
-          <th scope="col">E-mail</th>
-          <th scope="col">Room Id</th>
-          <th scope="col">Check-in Date</th>
-          <th scope="col">Check-out Date</th>
-          <th scope="col">nights</th>
-        </tr>
-      </thead>
+  const [selectedCustomerId, setSelectedCustomerId] = useState(null);
 
-      <tbody>
-        {results.map((item, index) => {
-          const dateIn = moment(item.checkInDate);
-          const dateOut = moment(item.checkOutDate);
-          return (
-            <tr key={index} /* onClick={changeColor} */>
-              <th scope="row">{index + 1}</th>
-              <td>{item.title}</td>
-              <td>{item.firstName}</td>
-              <td>{item.surname}</td>
-              <td>{item.email}</td>
-              <td>{item.roomId}</td>
-              <td>{item.checkInDate}</td>
-              <td>{item.checkOutDate}</td>
-              <td>{dateOut.diff(dateIn, "days")}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+  const showProfile = (customerId) => {
+    setSelectedCustomerId(customerId);
+  };
+
+  return (
+    <div>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">E-mail</th>
+            <th scope="col">Room Id</th>
+            <th scope="col">Check-in Date</th>
+            <th scope="col">Check-out Date</th>
+            <th scope="col">nights</th>
+            <th scope="col">Actions</th> {/* New column for actions */}
+          </tr>
+        </thead>
+
+        <tbody>
+          {results.map((item, index) => {
+            const dateIn = moment(item.checkInDate);
+            const dateOut = moment(item.checkOutDate);
+            return (
+              <tr key={index}>
+                <th scope="row">{index + 1}</th>
+                <td>{item.title}</td>
+                <td>{item.firstName}</td>
+                <td>{item.surname}</td>
+                <td>{item.email}</td>
+                <td>{item.roomId}</td>
+                <td>{item.checkInDate}</td>
+                <td>{item.checkOutDate}</td>
+                <td>{dateOut.diff(dateIn, "days")}</td>
+                <td>
+                  <button
+                    onClick={() => showProfile(index + 1)}
+                    className="btn btn-primary"
+                  >
+                    Show profile
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      {selectedCustomerId && (<CustomerProfile id={selectedCustomerId} />)}
+    </div>
   );
 };
 
