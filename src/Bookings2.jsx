af6034a41b194a60e2a7175049fc626d5f358f0a
BEKIRKSU <bekirkscu@gmail.com>
2023-06-27 09:46:17 -0700

    Update Bookings2.jsx

--- src/Bookings2.jsx
+++ src/Bookings2.jsx
@@ -3,11 +3,14 @@ import BookingTable from './BookingTable';
 import bookingsData from './data/fakeBookings.json';
 
 const Bookings2 = () => {
+  useEffect(() => {
+    fetch('https://cyf-react.glitch.me');
+  }, []);
+
   return (
     <div>
       <h1>CYF Hotel Bookings</h1>
       <BookingTable bookings={bookingsData} />
-      
     </div>
   );
 };
