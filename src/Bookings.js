ac7e52dddfa33267e34d29f040cb5a6f5f651cb3
saqibjvd <70890519+saqibjvd@users.noreply.github.com>
2023-06-28 23:19:42 +0100

    Load bookings remotely
    Lesson 3
    
    task 16:  Load bookings remotely

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,14 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 // import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
 
 
-const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings); // task 14
-  
+// **Hints:**
 
+// - Replace `FakeBookings` in the bookings state and initialise it with `[]` (because we haven't fetched any results yet!)
+// - After calling the `fetch()` function, use `.then()` to handle the response. Try looking at your Pokemon app that you worked on in class for an example
+// - When the response comes back, use `setBookings` to update the results
+
+// **Test:** Verify the customers data are still displayed correctly in the table.
+
+
+const Bookings = () => {
+  const [bookings, setBookings] = useState([]); 
+  
+useEffect(() => {
+ console.log("hello there", useEffect);
+
+ fetch(`https://cyf-react.glitch.me`)
+   .then((res) => res.json())
+   .then((data) => {
+    console.log(data);
+     setBookings(data);
+   });
+
+}, []);
 
   const search = searchVal => {
     console.info("TO DO!", searchVal);
@@ -19,7 +38,8 @@ const Bookings = () => {
       <div className="container">
         <Search search={search} />
         {/* <SearchResults results={FakeBookings} /> */}
-      < SearchResults results={FakeBookings}/>
+        {/* <SearchResults results={FakeBookings} /> */}
+        <SearchResults results={[]} />
       </div>
     </div>
   );
--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,17 @@
 import React from "react";
 import SearchButton from "./SearchButton";
 
+
+// #### 17. Storing the search input in a state
+
+// **Instructions:** In the following, we will implement the functionality to search for a customer name given the text typed into the customer name field. In the `src/Search.js` file, declare a new state variable named `searchInput` with the corresponding setter function `setSearchInput` (hint: use the React function `useState`). The initial value of the `searchInput` variable can be an empty string. Add a `value` property to the `<input>` tag that is set to the new `searchInput` state variable. Create a new function `handleSearchInput` taking an `event` parameter. This function should use the `setSearchInput` function to update the state variable `searchInput` with what the user typed in the input field. Finally, add a `onChange` prop to the `<input>` tag that is set to the function `handleSearchInput`. Use `console.log()` to output the value received in the `handleSearchInput` function.
+
+// **Hint:** Use `event.target.value` to get the input value.
+
+// **Test:** In the developer console, check that everything you type in the search input is printed successively for each new character you enter.
+
+
+
 const Search = () => {
   return (
     <div className="search">
