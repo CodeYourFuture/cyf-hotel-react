3b5dc2668a8c8b68fafa76522e5ddacc7f847222
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-06-29 16:41:20 +0100

    update booking for exercise 16

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,13 +1,21 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import Axios from "axios";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
+
+  useEffect(()=> {
+      Axios.get("https://cyf-react.glitch.me").then((res) => {
+        setBookings(res.data);
+        console.log(res.data[0].firstName);
+      });
+  },[])
 
   return (
     <div className="App-content">
