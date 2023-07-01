3cd7cfa14b47fb0d633f4a833a4453273bc6b71e
saqibjvd <70890519+saqibjvd@users.noreply.github.com>
2023-07-01 15:41:12 +0100

    19. Implementing the search functionality
    19. Implementing the search functionality

--- src/Bookings.js
+++ src/Bookings.js
@@ -4,19 +4,45 @@ import SearchResults from "./SearchResults.js";
 // import SearchResults from "./SearchResults.js";
 // import FakeBookings from "./data/fakeBookings.json";
 
+// #### 19. Implementing the search functionality
+
+// **Instructions:** Still in the `<Bookings />` component, implement the `search` method. It must use the `searchVal` variable (that you just passed from the `<Search />` component) to **filter** the search results. The filter function should return bookings where `firstName` or `surname` match `searchVal`. Once filtered, use the `setBookings` function to update the results rendered in `<SearchResults />`.
+
+// **Test:** Verify that when you enter an existing first name or surname and submit the form, the results are filtered accordingly in the customers table.
+
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
+
+  const [reset, setReset] = useState([]);
 
   useEffect(() => {
     fetch(`https://cyf-react.glitch.me`)
       .then((res) => res.json())
       .then((data) => {
         setBookings(data);
+        setReset(data);
       });
   }, []);
 
   const search = (searchVal) => {
-    console.info("TO DO!", searchVal);
+    // console.log(bookings[0].firstName);
+    const filterSearch = bookings.filter(function (bookings) {
+      return bookings.firstName == searchVal || bookings.surname == searchVal;
+
+      //   console.log(bookings.firstName)
+      // console.log(filterSearch)
+      // return setBookings(filterSearch);
+    });
+
+    setBookings(filterSearch);
+    // console.log("this worked");
+    // console.info("TO DO!", searchVal);
+  };
+
+  const resetHandler = () => {
+    console.log(reset);
+
+    return setBookings(reset);
   };
 
   return (
@@ -26,6 +52,7 @@ const Bookings = () => {
         {/* <SearchResults results={FakeBookings} /> */}
         {/* <SearchResults results={FakeBookings} /> */}
         <SearchResults results={bookings} />
+        <button onClick={resetHandler}> Reset</button>
       </div>
     </div>
   );
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -4,59 +4,58 @@ import "./SearchResults.css";
 // import moment from "moment";
 // const numberOfNights()
 
-
-
 const SearchResults = (props) => {
-  
-  const[highlight, sethighlight] = useState(null);
-  
-  const {results} = props
+  const [highlight, sethighlight] = useState(null);
 
-  const toggleHighlight = (index) =>{
+  const { results } = props;
+
+  const toggleHighlight = (index) => {
     if (index == highlight) {
-      sethighlight(null)
-
+      sethighlight(null);
     } else {
-    sethighlight(index)
-  }
-}
+      sethighlight(index);
+    }
+  };
 
   return (
     <table className="table">
-      <thead >
-      </thead>
+      <thead></thead>
       <tbody>
-      <tr>
-        <td>ID</td>
-        <td>Tittle</td>
-        <td>First Name</td>
-        <td>Surname</td>
-        <td>Email</td>
-        <td>Room ID</td>
-        <td>check in Date</td>
-        <td>check out Date</td>
-        <td>No of Night stay</td>
-      </tr>
+        <tr>
+          <td>ID</td>
+          <td>Tittle</td>
+          <td>First Name</td>
+          <td>Surname</td>
+          <td>Email</td>
+          <td>Room ID</td>
+          <td>check in Date</td>
+          <td>check out Date</td>
+          <td>No of Night stay</td>
+        </tr>
 
-      {results.map((item, index) => {
-        return (
-          <tr onClick={() => toggleHighlight(index)} style={{backgroundColor: index == highlight ? "yellow" : "" }} key={index}>
-            <td>{item.id}</td>
-            <td>{item.title}</td>
-            <td>{item.firstName}</td>
-            <td>{item.surname}</td>
-            <td>{item.email}</td>
-            <td>{item.roomId}</td>
-            <td>{item.checkInDate}</td>
-            <td>{item.checkOutDate}</td>
-            <td>
-              {(new Date(`${item.checkOutDate}T00:00:01Z`) -
-                new Date(`${item.checkInDate}T00:00:01Z`)) /
-                86400000}
-            </td>
-          </tr>
-        );
-      })}
+        {results.map((item, index) => {
+          return (
+            <tr
+              onClick={() => toggleHighlight(index)}
+              style={{ backgroundColor: index == highlight ? "yellow" : "" }}
+              key={index}
+            >
+              <td>{item.id}</td>
+              <td>{item.title}</td>
+              <td>{item.firstName}</td>
+              <td>{item.surname}</td>
+              <td>{item.email}</td>
+              <td>{item.roomId}</td>
+              <td>{item.checkInDate}</td>
+              <td>{item.checkOutDate}</td>
+              <td>
+                {(new Date(`${item.checkOutDate}T00:00:01Z`) -
+                  new Date(`${item.checkInDate}T00:00:01Z`)) /
+                  86400000}
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
