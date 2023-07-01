e4b769e37610fb0ebfdc96ecd559cbbb4fb4b1db
softacoder <113185323+softacoder@users.noreply.github.com>
2023-07-01 12:18:34 +0100

    update

--- src/App.js
+++ src/App.js
@@ -6,24 +6,22 @@ import "./App.css";
 import Footer from "./Footer";
 import Restaurant from "./Restaurant";
 
-
 const App = () => {
   const contactDetails = [
     "123 Fake Street, London, E1 4UD",
     "hello@fakehotel.com",
     "0123 456789",
   ];
-  
+
   return (
     <div className="App">
       <Heading />
       <TouristInfoCards />
       <Bookings />
       <Restaurant />
-      <Footer contactDetails={contactDetails} /> 
+      <Footer contactDetails={contactDetails} />
     </div>
   );
 };
-  
 
 export default App;
