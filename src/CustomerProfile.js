7da097229372d0d300a859bd39d022e6174a73a2
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-07-01 15:46:22 +0100

    exercise 20 done
    Display a customer profile - step 1

---
+++ src/CustomerProfile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CustomerProfile = (props) => {
+  return (
+    <div>
+        {props.profile}
+    </div>
+  )
+}
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import moment from "moment";
+import { CustomerProfile } from "./CustomerProfile";
 
 const getNumbersOfNights = (checkInDate, checkOutDate) => {
   const date1 = moment(checkInDate);
@@ -9,20 +10,17 @@ const getNumbersOfNights = (checkInDate, checkOutDate) => {
 
 const Row = (props) => {
   const {
-    id,
-    title,
-    firstName,
-    surname,
-    email,
-    roomId,
-    checkInDate,
-    checkOutDate,
-  } = props;
+    id, title, firstName, surname, email, roomId, checkInDate, checkOutDate, setProfile,} = props;
+
   const night = getNumbersOfNights(checkInDate, checkOutDate);
 
   const [rowSelect, setRowSelect] = useState(false);
 
   const handleClick = () => setRowSelect(!rowSelect);
+
+  const handleProfile = () => {
+      setProfile(id)
+  }
 
   return (
     <tr
@@ -38,45 +36,53 @@ const Row = (props) => {
       <th>{checkInDate}</th>
       <th>{checkOutDate}</th>
       <th>{night}</th>
+      <th>
+        <button onClick={handleProfile} className="rwo-btn">Show profile</button>
+      </th>
     </tr>
   );
 };
-
 const Table = (props) => {
   const { data } = props;
+  const [profile ,setProfile ] = useState("")
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">id</th>
-          <th scope="col">Title</th>
-          <th scope="col">First name</th>
-          <th scope="col">Surname</th>
-          <th scope="col">Email</th>
-          <th scope="col">Room id</th>
-          <th scope="col">Check in date</th>
-          <th scope="col">Check out date</th>
-          <th scope="col">Night</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((column, index) => {
-          return (
-            <Row
-              key={index}
-              id={column.id}
-              title={column.title}
-              firstName={column.firstName}
-              surname={column.surname}
-              email={column.email}
-              roomId={column.roomId}
-              checkInDate={column.checkInDate}
-              checkOutDate={column.checkOutDate}
-            />
-          );
-        })}
-      </tbody>
-    </table>
+    <div>
+      <CustomerProfile profile={profile} />
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">Title</th>
+            <th scope="col">First name</th>
+            <th scope="col">Surname</th>
+            <th scope="col">Email</th>
+            <th scope="col">Room id</th>
+            <th scope="col">Check in date</th>
+            <th scope="col">Check out date</th>
+            <th scope="col">Night</th>
+            <th scope="col">Show profile</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((column, index) => {
+            return (
+              <Row
+                key={index}
+                id={column.id}
+                title={column.title}
+                firstName={column.firstName}
+                surname={column.surname}
+                email={column.email}
+                roomId={column.roomId}
+                checkInDate={column.checkInDate}
+                checkOutDate={column.checkOutDate}
+                setProfile={setProfile}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
