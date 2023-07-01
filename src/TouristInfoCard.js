2c60b3110c72aa7cdd445e10092e8368ea3e7a3d
softacoder <113185323+softacoder@users.noreply.github.com>
2023-07-01 12:31:44 +0100

    Update

--- src/TouristInfoCard.js
+++ src/TouristInfoCard.js
@@ -1,18 +1,22 @@
 import React from "react";
 
-const TouristInfoCard = props => {
-    return (
-      <div className="card">
-        <img src={`../images/${props.image}`} className="card-img-top" />
-        <div className="card-body">
-          <h2 className="card-title">{props.title}</h2>
-          <p className="card-description">{props.description}</p>
-          <a href={props.link} className="btn btn-primary" target="_blank">
-            More Information
-          </a>
-        </div>
+const TouristInfoCard = (props) => {
+  return (
+    <div className="card">
+      <img
+        src={`../images/${props.image}`}
+        alt={TouristInfoCard}
+        className="card-img-top"
+      />
+      <div className="card-body">
+        <h2 className="card-title">{props.title}</h2>
+        <p className="card-description">{props.description}</p>
+        <a href={props.link} className="btn btn-primary" target="_blank">
+          More Information
+        </a>
       </div>
-    );
-  };
-  
-  export default TouristInfoCard;
+    </div>
+  );
+};
+
+export default TouristInfoCard;
