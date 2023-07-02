e732daa4e75e39cd1aec942acf9cd25da25aea77
softacoder <113185323+softacoder@users.noreply.github.com>
2023-07-02 21:09:15 +0100

    Update pseudocode

--- src/App.js
+++ src/App.js
@@ -25,3 +25,16 @@ const App = () => {
 };
 
 export default App;
+
+// Pseudocode: React component called "App". It imports several other components, such as "Heading", "Bookings",
+// "TouristInfoCards", "Footer", and "Restaurant". It also imports a CSS file called "App.css" for styling. The App
+// component is a functional component written using arrow function syntax. It returns JSX (JavaScript XML), which
+// represents the structure and content of the component's rendered output.
+
+// Inside the App component, there is a constant variable contactDetails that holds an array of contact information.
+// The array contains a street address, email address, and phone number. The JSX code returned by the App component
+// represents the structure of the web page. It consists of a <div> element with the class name "App". Inside the <div>,
+// the imported components are rendered in order: <Heading />, <TouristInfoCards />, <Bookings />, <Restaurant />, and
+// <Footer />. The <Footer /> component is passed the contactDetails array as a prop. The export default App; statement
+// exports the App component as the default export of this module, allowing it to be imported and used in other parts of
+// the application.
--- src/Bookings.js
+++ src/Bookings.js
@@ -77,3 +77,39 @@ const Bookings = () => {
 };
 
 export default Bookings;
+
+// Pseudocode: This code represents a React component called "Bookings". It imports other components such as "Search",
+// "SearchResults", and "CustomerProfile". Inside the Bookings component, several state variables are declared using the
+//  useState hook: bookings, customerId, data, isLoading, and errorOccurred.
+
+// The code begins with importing necessary dependencies and components: React: The core React library. useEffect and
+// useState from React: Hooks used for managing side effects and state within functional components. The Search, SearchResults,
+// and CustomerProfile components from their respective files. The Bookings component is defined using the arrow function syntax.
+
+// Inside the Bookings component, several state variables are declared using the useState hook: bookings: Holds the list
+// of bookings data, initially an empty array. customerId: Holds the ID of the selected customer, initially null. data: Holds the
+// original unfiltered bookings data, initially an empty array. isLoading: Indicates whether the data is currently being
+// loaded, initially set to true. errorOccurred: Holds information about any error that occurred during data fetching,
+//initially an object with ok (indicating if the request was successful), status (HTTP status code), and statusText
+// (HTTP status text). The useEffect hook is used to fetch data from the server when the component is mounted (empty dependency array
+// [] passed as the second argument to useEffect). The effect function is defined inside the useEffect hook and is executed
+// when the component mounts.
+
+// The effect function is defined as an asynchronous function, fetchData. Inside fetchData, an HTTP GET request is made
+// to the URL "https://cyf-react.glitch.me" using the fetch function. If the response is not successful (status code
+// other than 200), an error is thrown, and the errorOccurred state variable is updated with the error details. If the
+// response is successful, the JSON data is extracted from the response using response.json(), and then both bookings
+// and data state variables are updated with the received data. Finally, the isLoading state variable is set to false
+// to indicate that data loading is complete. If an error occurs during the fetching process, it is caught and logged
+// to the console. The search function is defined to handle the filtering of bookings based on a search value.
+
+// If the searchVal parameter is empty or contains only whitespace characters after trimming, the original data is set as
+// the bookings. Otherwise, the bookings are filtered using the filter method, which checks if either the firstName or
+// the surname of each booking starts with the searchVal (case-insensitive comparison). The filtered bookings are then
+// set as the new value for the bookings state variable. The JSX code defines the structure and content of the rendered
+// output. The top-level element is a <div> with the class name "App-content". Inside this div, there is another <div>
+// with the class name "container". Inside the container, the Search component is rendered, which receives the search
+// function as a prop. Next, there is a conditional rendering based on the state variables errorOccurred and isLoading. If
+// an error occurred during data fetching (errorOccurred.ok === false), an error message is displayed showing the HTTP status
+// code and status text. If the data is still loading (isLoading === true), a loading message is displayed. If the data
+// has been loaded and no error occurred, the SearchResults component is rendered, passing the filtered `
