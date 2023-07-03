52eadcc4ca27b60b637de03cd1d3a25cda9f3d16
softacoder <113185323+softacoder@users.noreply.github.com>
2023-07-03 10:05:04 +0100

    Update

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -44,3 +44,17 @@ const CustomerProfile = (props) => {
 };
 
 export default CustomerProfile;
+
+// This code is a React component called CustomerProfile. It imports the necessary dependencies from the React library:
+//  React, useState, and useEffect. The component receives props as its parameter, which is an object containing data
+// passed from its parent component. The component uses the useState hook to create a state variable called customerProfileData
+// and a corresponding function called setCustomerProfileData. The initial value of customerProfileData is an empty string.
+// The useEffect hook is used to perform a side effect, which in this case is fetching data from an API. It is triggered
+// when the value of props.id changes. The effect makes an HTTP GET request to the specified URL (https://cyf-react.glitch.me/customers/${props.id}),
+// retrieves the response, and converts it to JSON using the res.json() method. Then, it updates the value of customerProfileData
+// by calling setCustomerProfileData with the fetched data. In the component's return statement, the fetched data is
+// displayed within a <div> container with the class name "customer-profile-container". It includes several <p> elements
+// that display different aspects of the customer's profile, such as ID, name, email, phone number, and VIP status.
+// The data from customerProfileData is accessed using dot notation (e.g., customerProfileData.id, customerProfileData.title).
+// Finally, the CustomerProfile component is exported as the default export, which allows it to be imported and used in
+// other parts of the application.
--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Footer = props => {
+const Footer = (props) => {
   return (
     <footer>
       <ul className="footer-contact">
@@ -13,3 +13,18 @@ const Footer = props => {
 };
 
 export default Footer;
+
+// This code is a React functional component called "Footer." It imports the React library and defines the Footer
+//component using an arrow function syntax. The component takes in a single parameter called "props," which is an
+// object that contains properties passed from the parent component. In this case, it expects a property called
+// "contactDetails" which should be an array. Inside the component's return statement, there is an HTML-like syntax.
+// This is called JSX (JavaScript XML) and allows you to write HTML-like code within your JavaScript. JSX is transpiled
+// into regular JavaScript by tools like Babel. The return statement contains a footer element. Inside the footer,
+// there is an unordered list element (ul) with a class name of "footer-contact." Within the ul element, there is a
+// JavaScript expression inside curly braces: {props.contactDetails.map((element, index) => (...)}. This expression maps
+// over the elements in the contactDetails array and returns a list item (li) for each element. The map function is used
+// to transform each element of the array into a new element, in this case, a list item. Each list item has a unique key
+// assigned using the key={index} attribute. The key is necessary to help React efficiently update the component when
+// the list changes. The content of each list item is the value of the current element in the contactDetails array.
+//Finally, the component is exported using the export default syntax, which allows other files to import and use the Footer
+// component.
