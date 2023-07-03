2014af6428493e6e9961b8119aa4a756b4cb4ec4
christina-mifsud <christina.mifsud@gmail.com>
2023-07-03 22:33:06 +0100

    Collaboration Note
    // This exercise was a collaborative effort between christina-mifsud and munozirianni1988

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import React from "react";
-
 import Header from "./Header";
 import Bookings from "./Bookings";
 import Restaurant from "./Restaurant";
 import TouristInfoCards from "./TouristInfoCards";
 import Footer from "./Footer";
+
+// This exercise was a collaborative effort between christina-mifsud and munozirianni1988
 
 const App = () => {
   return (
