0477fe779f507dc07f2932bcc5d780244f32a0dd
softacoder <113185323+softacoder@users.noreply.github.com>
2023-07-03 13:34:59 +0100

    Updates

--- src/Order.js
+++ src/Order.js
@@ -18,3 +18,22 @@ const Order = (props) => {
 };
 
 export default Order;
+
+// The code imports React and the useState hook from the "react" library. The useState hook is a built-in React hook that
+// allows functional components to manage state. The code also imports a component called "RestaurantButton" from a file
+// called "RestaurantButton.js". This component likely represents a button used to place an order in the restaurant. The
+// functional component Order is defined, which takes in props as its parameter. The props object is used to pass data
+// from the parent component to this component. Inside the Order component, the useState hook is used to initialize a
+// state variable called orders with an initial value of 0. The useState hook returns an array with two elements: the
+// current state value (orders) and a function (setOrders) to update that state value. The orderOne function is defined,
+// which increments the orders state value by 1 using the setOrders function provided by the useState hook. It uses the
+// functional form of setOrders, which takes the previous state value (prevOrders) as input and returns the updated
+//state value. The return statement renders JSX, which represents the structure and content of the component. It returns
+// a <li> element with the className "list-group-item". Inside the <li> element, it displays the props.orderType,
+// which represents the type of the order, likely passed as a prop from the parent component. Next, it renders a <span>
+// element with the className "badge badge-primary badge-pill", which is likely used for styling purposes. Inside the
+// <span> element, it displays the value of the orders state variable, representing the number of orders. Finally, it
+// renders the RestaurantButton component and passes the orderOne function as a prop called orderOne. This allows the
+// RestaurantButton component to trigger the orderOne function when the button is clicked. The Order component is
+// exported as the default export using the export default Order; statement, allowing it to be imported and used in
+// other files.
--- src/index.js
+++ src/index.js
@@ -4,3 +4,14 @@ import "./index.css";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// The import React from "react"; statement imports the React library, which is necessary to define and use React
+// components. The import ReactDOM from "react-dom"; statement imports the ReactDOM library, which provides methods for
+// rendering React components in the browser. The import "./index.css"; statement imports a CSS file called "index.css".
+// This file likely contains styles specific to the application's root component or global styles that should be applied
+// throughout the application. The import App from "./App"; statement imports the App component from a file called
+// "App.js" or "App.jsx". The App component is the root component of the application and represents the main entry point
+// for rendering the entire application. The ReactDOM.render(<App />, document.getElementById("root")); statement renders
+// the App component and mounts it into the HTML element with the id "root". The ReactDOM.render() method takes two
+// arguments: the first argument is the JSX expression <App /> which represents the root component to be rendered, and
+// the second argument is the DOM element where the component will be inserted.
