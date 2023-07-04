49dbb78d17638a1c6f78ac97be170742009f53f8
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-07-04 22:54:15 +0100

    10. Add more pizzas

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -2,12 +2,18 @@ import React, { useState } from "react";
 
 const Restaurant = () => {
   const [orders, setOrders] = useState(0);
+  const orderOne = () => {
+    setOrders(orders + 1);
+  };
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {orders} <button className="btn btn-primary">Add</button>
+          Pizzas: {orders}
+          <button className="btn btn-primary" onClick={orderOne}>
+            Add
+          </button>
         </li>
       </ul>
     </div>
