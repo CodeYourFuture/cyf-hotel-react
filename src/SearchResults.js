5b75db8957f59cd61ded9bf1b5f84b095f5189ce
AndriusIsin <isin.andrius@gmail.com>
2023-07-04 19:24:54 +0100

    updated 20, deleted TableBody component

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import TableHead from "./TableHead.js";
-import TableBody from "./TableBody.js";
+import TableInfo from "./TableInfo";
+import CustomerProfile from "./CustomerProfile";
 
 const SearchResults = (props) => {
+  const [showId, setShowId] = useState("");
+  const IdCheck = (id) => {
+    setShowId(id);
+    console.log({ showId });
+  };
   return (
-    <table className="table">
-      <TableHead />
-      <TableBody results={props.results} />
-    </table>
+    <>
+      <table className="table">
+        <TableHead />
+        <tbody>
+          {props.results.map((result, id) => {
+            return <TableInfo key={id} result={result} idCheck={IdCheck} />;
+          })}
+        </tbody>
+      </table>
+      {showId && <CustomerProfile id={showId} />}
+    </>
   );
 };
 export default SearchResults;
