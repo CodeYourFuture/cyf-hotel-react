57fe0bd42e14357a6e4a9ad84bc5373033ca723a
Adrian Ilovan <adrian88ilovan@yahoo.com>
2023-07-04 12:47:32 +0100

    L3/21 Done

--- src/CostumerProfile.js
+++ src/CostumerProfile.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const CostumerProfile = ({id}) => {
    
+   const [costumerProfile, setCostumerProfile] = useState(null)
+   
+   useEffect(() => {
+      const fetchCostumerProfile = async () => {
+         try {
+            const res = await fetch(`https://cyf-react.glitch.me/customers/${id}`);
+            const data = await res.json();
+            setCostumerProfile(data);
+         } catch (error) {
+            console.error("Error fetching Costumer Profile:", error);
+         }
+      };
+      if (id) {
+         fetchCostumerProfile();
+      }
+   }, [id]);
+
+   if (!id) {
+      return null;
+   }
+
    return(
       <div>
-         <h1>Costumer Profile {id}</h1>
+        {costumerProfile ? (
+         <div>
+            <h1>Costumer Profile {id}</h1>
+            <ul>
+               <li>Email: {costumerProfile.email}</li>
+               <li>VIP: {costumerProfile.vip ? "Yes" : "No"}</li>
+               <li>Phone Number: {costumerProfile.phoneNumber}</li>
+            </ul>
+         </div>
+        ) : (
+         <p>Loading Costumer Profile...</p>
+        )}
       </div>
-   )
-   
-}
+   );
+};
 
 export default CostumerProfile;
