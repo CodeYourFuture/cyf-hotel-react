7d3a58213b16687580e77fa7408b6baf970dd6fb
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-07-04 19:10:56 +0100

    2. Extract the header in its own component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
+
 import Bookings from "./Bookings";
 import "./App.css";
+import Heading from "./components/Heading";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Heading />
       <Bookings />
     </div>
   );
---
+++ src/components/Heading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <img src="https://play-lh.googleusercontent.com/-LLFboO3-LMZDXn9_2DyCtssJPXqxlbBciKoJ25o5S5wulGJo1QXme4HlFbevrYxUg" />
+      CYF Hotel
+    </header>
+  );
+};
+
+export default Heading;
