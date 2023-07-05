84178b48d90d75b8b37e0af3cd11c5e348a0c7d1
Olha Danylevska <odanilevskaya2@gmail.com>
2023-07-05 12:53:46 +0100

    sort the column

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -8,7 +8,7 @@ const SearchResults = ({ bookings, newCustomer }) => {
     const [clientId, setClientId] = useState("")
     const [customerProf, setCuctomerProf] = useState("")
     const [isShown, setIsShown] = useState(false);
-
+    const [rowClicked, setRowClicked] = useState("desc")
     let handleProfileClick = (client) => {
 
         setCuctomerProf(client)
@@ -19,22 +19,61 @@ const SearchResults = ({ bookings, newCustomer }) => {
         setActive(!active)
         setClientId(id)
     }
+
+    const handleSorting = (referenceCol) => {
+        console.log("hello")
+        if (rowClicked === "desc") {
+            bookings.sort((a, b) => {
+                let fa = a[referenceCol].toLowerCase(),
+                    fb = b[referenceCol].toLowerCase();
+
+                if (fa < fb) { return -1 }
+                if (fa > fb) { return 1 }
+                return 0;
+            })
+            setRowClicked("asc")
+        } else if (rowClicked === "asc") {
+            bookings.sort((a, b) => {
+                let fa = a[referenceCol].toLowerCase(),
+                    fb = b[referenceCol].toLowerCase();
+
+                if (fb < fa) { return -1 }
+                if (fb > fa) { return 1 }
+                return 0;
+            })
+            setRowClicked("desc")
+        }
+    }
+
+
+    const handleSortingNumbers = (refCol) => {
+        if (rowClicked === "desc") {
+            bookings.sort((a, b) => {
+                return a[refCol] - b[refCol]
+            })
+            setRowClicked("asc")
+        }
+        if (rowClicked === "asc") {
+            bookings.sort((a, b) => {
+                return b[refCol] - a[refCol]
+            })
+            setRowClicked("desc")
+        }
+    }
     return (
         <div className="table-holder">
             <table className="table">
-
                 <thead>
-
-                    <tr className="title-table">
-                        <th scope="col">ID</th>
-                        <th scope="col">Title</th>
-                        <th scope="col">First Name</th>
-                        <th scope="col">Surname</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Room ID</th>
-                        <th scope="col">Check In Date</th>
-                        <th scope="col">Check Out Date</th>
-                        <th scope="col">Nights</th>
+                    <tr className="title-table" >
+                        <th scope="col" onClick={() => (handleSortingNumbers("id"))}>ID</th>
+                        <th scope="col" onClick={() => (handleSorting("title"))}>Title</th>
+                        <th scope="col" onClick={() => (handleSorting("firstName"))}>First Name</th>
+                        <th scope="col" onClick={() => (handleSorting("surname"))}>Surname</th>
+                        <th scope="col" onClick={() => (handleSorting("email"))}>Email</th>
+                        <th scope="col" onClick={() => (handleSortingNumbers("roomId"))}>Room ID</th>
+                        <th scope="col" onClick={() => (handleSortingNumbers("chekInDate"))}>Check In Date</th>
+                        <th scope="col" onClick={() => (handleSortingNumbers("checkOutDate"))}>Check Out Date</th>
+                        <th scope="col" >Nights</th>
                         <th></th>
                     </tr>
                 </thead>
