3c7ae05ec22d1b95fa3590b4dfadcb19d2eeddf2
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-07-05 17:11:06 +0100

    14. Passing bookings from a state variable
    ? const Example = (props) / ({order}).
    first one need a property
    
    ?   "const [bookings, setBookings] = useState();
      setBookings(FakeBookings);" gives the eroor "o many re-renders. React limits the number of renders to prevent an infinite loop.
     ", so instead the current syntax is used

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./components/SearchResults.jsx";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const search = searchVal => {
+  const [bookings, setBookings] = useState(FakeBookings);
+  const search = (searchVal) => {
     console.info("TO DO!", searchVal);
   };
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults results={FakeBookings} />
+        <SearchResults results={bookings} />
       </div>
     </div>
   );
