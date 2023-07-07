41bc53adefbe6d4ca046654a0785615d2a00c714
Adrian Ilovan <adrian88ilovan@yahoo.com>
2023-07-07 18:10:22 +0100

    L3 Done last Update

--- src/Bookings.js
+++ src/Bookings.js
@@ -4,26 +4,37 @@ import SearchResults from "./SearchResults.js";
 
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
-  const [isLoading, setIsLoading] =useState(true);
-  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("https://cyf-react.glitch.me/error")
-    .then(response => response.json())
-    .then(json => {setBookings(json)
-                  setIsLoading(false);
-    })
-    .catch((error) => {console.log("Error fetching data:", error);
-                    setError("Error fetching data. Please try again later.");
-                    setIsLoading(false);
-          });
-  }, []); 
+    fetch("https://cyf-react.glitch.me")
+      .then((response) => response.json())
+      .then((json) => {
+        setBookings(json);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log("Error fetching data:", error);
+        setError("Error fetching data. Please try again later.");
+        setIsLoading(false);
+      });
+  }, []);
 
-  var search = searchVal => {
-    const filteredBookings = bookings.filter(booking => 
-      booking.firstName.includes(searchVal) || booking.surname.includes(searchVal)
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then((response) => response.json())
+      .then((json) => setBookings(json))
+      .catch((error) => console.log("Error fetching data:", error));
+  }, []);
+
+  var search = (searchVal) => {
+    const filteredBookings = bookings.filter(
+      (booking) =>
+        booking.firstName.includes(searchVal) ||
+        booking.surname.includes(searchVal)
     );
-    setBookings(filteredBookings)
+    setBookings(filteredBookings);
   };
 
   return (
@@ -31,11 +42,14 @@ const Bookings = () => {
       {isLoading ? (
         <h1>Loading...Please Wait</h1>
       ) : error ? (
-        <h1>{error}</h1> ) : (
+        <h1>{error}</h1>
+      ) : (
         <div className="container">
-        <Search search={search} />
-        {<SearchResults bookings={bookings} />}
-      </div>
+          <div className="container">
+            <Search search={search} />
+            <SearchResults bookings={bookings} />
+          </div>
+        </div>
       )}
     </div>
   );
