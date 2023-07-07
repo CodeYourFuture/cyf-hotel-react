c821b788e228a1fa99bf3e05d586e8ee43e90f4a
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-07-07 09:50:36 +0100

    search bar done

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,19 +1,32 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const[bookings, setBookings] = useState(FakeBookings)
-   const search = searchVal => {
-    console.info("TO DO!", searchVal);
-  };
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setBookings(data);
+      });
+  }, []);
+  
+  const search = (searchVal) => {
+   const filteredBookings = bookings.filter((booking) =>
+   booking.firstName.toLowerCase().includes(searchVal.toLowerCase()) ||
+   booking.surname.toLowerCase().includes(searchVal.toLowerCase())
+   );
+   setBookings(filteredBookings);
+  }
 
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults  results = {bookings}/> 
+        <SearchResults results={bookings} />
       </div>
     </div>
   );
