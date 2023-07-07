3e16fc35cf9162931e6a0f2fcfa8210e7e978266
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-07-07 18:06:53 +0100

    exercise 21

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
       <Heading />
       <TouristInfoCard />
       <Bookings />
-      <Restaurant />
+     <Restaurant />
       <Footer props = {address} />
     </div>
   );
