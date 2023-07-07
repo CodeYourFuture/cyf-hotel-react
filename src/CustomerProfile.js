b914cc1ea1fea734946fd11d818031f947ec0f10
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-07-07 18:05:57 +0100

    added props in customerprofile component

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -1,5 +1,20 @@
 import React from 'react';
-import SearchResults from './SearchResults';
 
-const customerProfile= (id) => {
-  return
+
+const CustomerProfile= (props) => {
+  
+
+return (
+  <div>
+    <ul className='customer'>
+      {props.id && <li>Customer_ID : {props.id}</li>}
+      {props.firstName && <li>Name : {props.firstName}</li>}
+      {props.surname && <li>surname : {props.surname}</li>}
+      {props.vip && <li>email : {props.email}</li>}
+      {props.phoneNumber && <li>phone number : {props.phoneNumber}</li>}
+    </ul>
+  </div>
+);
+}
+
+export default CustomerProfile;
