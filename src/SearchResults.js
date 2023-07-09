74cd4716b3ce1c4cc90444695eb9e166c1fc1587
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2023-07-09 21:57:09 +0100

    task 25

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -4,39 +4,90 @@ moment().format();
 import CustomerProfile from "./CustomerProfile";
 
 
-function Filter (what) {
-  
-}
 const SearchResults = (props) => {
   const [customerId, setCustomerId] = useState(null);
+  const [descendingOrder, setDescendingOrder] = useState(true); 
+  const [valueToSort, setValueToSort] = useState("id");
+
   function highlighted(event) {
       event.target.parentElement.className === "Highlighted"
       ? event.target.parentElement.className = ""
       : event.target.parentElement.className = "Highlighted"; 
     
   }
+
+  const handleSortingValue = (event) => {
+    let item = event.target.name;
+    console.log(item)
+    setValueToSort(item) 
+    setDescendingOrder(!descendingOrder);
+  }
+
+  function sortedResults () {
+    if(descendingOrder) {
+      return props.results.slice().sort((a, b) => a[valueToSort].toString().localeCompare(b[valueToSort].toString()));
+    } else {
+      return props.results
+        .slice()
+        .sort((a, b) => b[valueToSort].toString().localeCompare(a[valueToSort].toString()));
+    }
+  }
+  
   return (
     <>
       <table className="table">
         <thead>
           <tr>
-            <th scope="col">
-              id <span>Button</span>
+            <th scope="col" id="id">
+              id{" "}
+              <button name="id" onClick={handleSortingValue}>
+                {descendingOrder ? "A -> Z" : "Z-> A"}
+              </button>
             </th>
 
-            <th scope="col">title</th>
-            <th scope="col">first name</th>
-            <th scope="col">surname</th>
-            <th scope="col">email</th>
-            <th scope="col">room id</th>
-            <th scope="col">check in date</th>
+            <th scope="col" id="title">
+              title
+              <button name="title" onClick={handleSortingValue}>
+                {descendingOrder ? "A -> Z" : "Z-> A"}
+              </button>
+            </th>
+            <th scope="col" id="firstName">
+              first name
+              <button name="firstName" onClick={handleSortingValue}>
+                {descendingOrder ? "A -> Z" : "Z-> A"}
+              </button>
+            </th>
+            <th scope="col" id="surname">
+              surname
+              <button name="surname" onClick={handleSortingValue}>
+                {descendingOrder ? "A -> Z" : "Z-> A"}
+              </button>
+            </th>
+            <th scope="col" id="email">
+              email<br></br>
+              <button name="email" onClick={handleSortingValue}>
+                {descendingOrder ? "A -> Z" : "Z-> A"}
+              </button>
+            </th>
+            <th scope="col">
+              room id
+              <button name="roomId" onClick={handleSortingValue}>
+                {descendingOrder ? "A -> Z" : "Z-> A"}
+              </button>
+            </th>
+            <th scope="col">
+              check in date
+              <button name="checkInDate" onClick={handleSortingValue}>
+                {descendingOrder ? "A -> Z" : "Z-> A"}
+              </button>
+            </th>
             <th scope="col">check out date</th>
             <th scope="col">nights total</th>
             <th scope="col"></th>
           </tr>
         </thead>
         <tbody>
-          {props.results.map((result) => {
+          {sortedResults().map((result) => {
             function nightsTotal() {
               let dateOut = moment(result.checkOutDate);
               let dateIn = moment(result.checkInDate);
