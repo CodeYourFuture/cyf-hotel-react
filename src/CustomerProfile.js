c8c6cef5f0d7197fcf16a3d33402b416847bb6e0
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-07-10 23:50:06 +0100

    formatted

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -1,20 +1,17 @@
-import React from 'react';
+import React from "react";
 
-
-const CustomerProfile= (props) => {
-  
-
-return (
-  <div>
-    <ul className='customer'>
-      {props.id && <li>Customer_ID : {props.id}</li>}
-      {props.firstName && <li>Name : {props.firstName}</li>}
-      {props.surname && <li>surname : {props.surname}</li>}
-      {props.vip && <li>email : {props.email}</li>}
-      {props.phoneNumber && <li>phone number : {props.phoneNumber}</li>}
-    </ul>
-  </div>
-);
-}
+const CustomerProfile = (props) => {
+  return (
+    <div>
+      <ul className="customer">
+        {props.id && <li>Customer_ID : {props.id}</li>}
+        {props.firstName && <li>Name : {props.firstName}</li>}
+        {props.surname && <li>surname : {props.surname}</li>}
+        {props.vip && <li>email : {props.email}</li>}
+        {props.phoneNumber && <li>phone number : {props.phoneNumber}</li>}
+      </ul>
+    </div>
+  );
+};
 
 export default CustomerProfile;
