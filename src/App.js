5fe4c35ec9f73e3e269a4faf352befcc26077ad5
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-07-10 23:49:52 +0100

    loading and error done

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React from "react";
 import Heading from "./Heading";
 import TouristInfoCard from "./TouristInfoCards";
 import Bookings from "./Bookings";
-import Restaurant from "./Restaurant"
+import Restaurant from "./Restaurant";
 import "./App.css";
 import Footer from "./Footer";
 
 const App = () => {
   const address = [
-          "123 Fake Street, London, E1 4UD",
-          "hello@fakehotel.com",
-          "0123 456789",
-        ]
-  return (<div className="App">
+    "123 Fake Street, London, E1 4UD",
+    "hello@fakehotel.com",
+    "0123 456789",
+  ];
+  return (
+    <div className="App">
       <Heading />
       <TouristInfoCard />
       <Bookings />
-     <Restaurant />
-      <Footer props = {address} />
+      <Restaurant />
+      <Footer props={address} />
     </div>
   );
 };
--- src/Bookings.js
+++ src/Bookings.js
@@ -4,28 +4,37 @@ import SearchResults from "./SearchResults.js";
 
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch(`https://cyf-react.glitch.me`)
-      .then((res) => res.json())
+    fetch(`https://cyf-react.glitch.me/`)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("Something went wrong");
+        }
+        return response.json();
+      })
       .then((data) => {
         console.log(data);
         setBookings(data);
+        setLoading(false);
       });
   }, []);
-  
+
   const search = (searchVal) => {
-   const filteredBookings = bookings.filter((booking) =>
-   booking.firstName.toLowerCase().includes(searchVal.toLowerCase()) ||
-   booking.surname.toLowerCase().includes(searchVal.toLowerCase())
-   );
-   setBookings(filteredBookings);
-  }
+    const filteredBookings = bookings.filter(
+      (booking) =>
+        booking.firstName.toLowerCase().includes(searchVal.toLowerCase()) ||
+        booking.surname.toLowerCase().includes(searchVal.toLowerCase())
+    );
+    setBookings(filteredBookings);
+  };
 
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
+        <div>{loading ? <p>Loading...</p> : null}</div>
         <SearchResults results={bookings} />
       </div>
     </div>
