723f946f89c7197187f9ebf5eec55cc9a22db07c
Afsha10 <unityofbeing1@gmail.com>
2023-07-11 12:49:12 +0100

    21. Display a customer profile - step 2
    When a "Show profile" button is clicked in the table, we fetch the corresponding customer profile from https://cyf-react.glitch.me/customers/<ID> in the <CustomerProfile /> component. A customer profile should show the customer ID, their email, if they are VIP and their phone number in a list.
    
        Hint: We need to use useEffect and the correct dependency array. We'll need to fetch customers data from the API every time a "Show profile" button is clicked and render it accordingly.

--- src/components/CustomerProfile.jsx
+++ src/components/CustomerProfile.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+/*
+This is how we'd like to display the customer's profile:
 
-const CustomerProfile = ({id}) => {
-    return (
-        <p>Customer {id} Profile </p>
-    )
-}
+    "id": 1,
+    "email": "johndoe@doe.com",
+    "vip": true,
+    "phoneNumber": "+44 1632 960185"
+
+*/
+
+import React, { useState, useEffect } from "react";
+import "./CustomerProfile.css";
+
+const CustomerProfile = ({ id }) => {
+  const [customerProfile, setCustomerProfile] = useState({});
+
+  useEffect(() => {
+    console.log(id);
+    fetch(`https://cyf-react.glitch.me/customers/${id}`)
+      .then((res) => res.json())
+      .then((data) => setCustomerProfile(data));
+  }, [id]);
+
+  return (
+    <div className="profile-container">
+      <p>Customer Profile: </p>
+      <ul className="profile-list">
+        <li>ID: {customerProfile.id}</li>
+        <li>Email: {customerProfile.email}</li>
+        <li>VIP: {customerProfile.vip?.toString()}</li>
+        <li>Phone number: {customerProfile.phoneNumber}</li>
+      </ul>
+    </div>
+  );
+};
 
 export default CustomerProfile;
+
