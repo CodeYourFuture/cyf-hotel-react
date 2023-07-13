5cd447c88fe6ab19f07764b86de6dd4887704efc
batuncer <tuncerbaki@gmail.com>
2023-07-13 10:34:55 +0100

    excercise 3 is done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Bookings from "./Bookings";
 import "./App.css";
 import Heading from "./components/Heading";
+import TouristInfoCards from "./components/TouristInfoCards";
 
 const App = () => {
   return (
     <div className="App">
       <Heading />
+      <TouristInfoCards />
       <Bookings />
     </div>
   );
