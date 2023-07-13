4f3acfb420a2a9e10782e693c345768ecd4f4279
batuncer <tuncerbaki@gmail.com>
2023-07-13 10:26:06 +0100

    excercise 2 is done

---
+++ src/components/Heading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Heading = () => {
+    return (
+      <header className="App-header">
+        <img
+          src="https://image.flaticon.com/icons/svg/139/139899.svg"
+          alt="Hotel Logo"
+        />
+      </header>
+    );
+}
+
+export default Heading;
