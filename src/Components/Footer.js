4dd2a647c4319fe807aad223268736951105e354
Damilola Odumosu <dt.odumosu@gmail.com>
2023-07-19 22:09:27 +0100

    10. added more pizza with usestate

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,14 +1,12 @@
 import react from "react";
 
 const Footer = (props) =>{
-            console.log('props', props);
 
     return(
         <footer>
             <ul>
-                {props.address.map((element, index) => {
-                    console.log(element)
-                    console.log(index)
+                {props.address.map((element) => {
+                   
                     return <li key={element}>{element}</li>
 
                 })}
--- src/Restaurant.js
+++ src/Restaurant.js
@@ -2,17 +2,29 @@ import React, {useState} from "react";
 
 const Restaurant = () => {
   const [orders, setOrders] = useState(0)
+   
+  const orderOne = () => {
+         setOrders(orders + 1);
+
+  }
 
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {orders} <button className="btn btn-primary">Add</button>
+          Pizzas: {orders}{" "}
+          <button onClick={orderOne} className="btn btn-primary">
+            Add
+          </button>
         </li>
       </ul>
     </div>
   );
 };
+  
+  
+  
+
 
 export default Restaurant;
