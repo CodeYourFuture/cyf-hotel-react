842fc45ab0d4b30d3dd66ebd8b2ebbecc701f9ec
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-07-22 01:58:03 +0100

    Update Bookings.js

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
