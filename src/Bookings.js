2ac8b0522c91d350ad3034c6b3d97800af8cd7d5
malkitbenning <malkitbenning@gmail.com>
2023-07-26 00:08:08 +0100

    using more node server functions
    delete row
    search
    refresh data with remaining rows

--- src/Bookings.js
+++ src/Bookings.js
@@ -4,6 +4,7 @@ import SearchResults from "./SearchResults.js";
 import LoadingWait from "./LoadingWait.js";
 
 const Bookings = () => {
+  const [deleteRow, setDeleteRow] = useState(null);
   let [dataAvailable, setDataAvailable] = useState(false);
 
   useEffect(() => {
@@ -17,24 +18,47 @@ const Bookings = () => {
       });
   }, []);
 
+  useEffect(() => {
+    if (deleteRow)
+      fetch("https://malkit-hotel-server.glitch.me/bookings/" + deleteRow, { method: "DELETE" })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setBookings(data);
+          setMasterBookings(data);
+          setDataAvailable(true);
+        });
+  }, [deleteRow]);
+
   let [bookings, setBookings] = useState([]);
   let [masterBookings, setMasterBookings] = useState([]);
 
   const search = (searchVal) => {
-    fetch("https://malkit-hotel-server.glitch.me/bookings/search?term=" + searchVal)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        let filteredBookings = data;
-        setBookings(filteredBookings);
-      });
+    if (!searchVal) {
+      console.log("no search Val");
+      fetch("https://malkit-hotel-server.glitch.me/bookings/")
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          let filteredBookings = data;
+          setBookings(filteredBookings);
+        });
+    } else {
+      fetch("https://malkit-hotel-server.glitch.me/bookings/search?term=" + searchVal)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          let filteredBookings = data;
+          setBookings(filteredBookings);
+        });
+    }
   };
 
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        {dataAvailable ? <SearchResults results={bookings} /> : <LoadingWait />}
+        {dataAvailable ? <SearchResults results={bookings} setDeleteRow={setDeleteRow} /> : <LoadingWait />}
       </div>
     </div>
   );
--- src/TableBody.js
+++ src/TableBody.js
@@ -5,14 +5,7 @@ const TableBody = (props) => {
   return (
     <tbody>
       {props.bodyData.map((aBooking, index) => {
-        return (
-          <TableRow
-            key={index}
-            aBooking={aBooking}
-            customerProfileId={props.customerProfileId}
-            setCustomerProfileId={props.setCustomerProfileId}
-          ></TableRow>
-        );
+        return <TableRow key={index} aBooking={aBooking} customerProfileId={props.customerProfileId} setCustomerProfileId={props.setCustomerProfileId} setDeleteRow={props.setDeleteRow}></TableRow>;
       })}
     </tbody>
   );
