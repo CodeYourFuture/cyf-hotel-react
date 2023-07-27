9fab48b2b36aa70c5ac30070740c91597feab4ca
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-07-27 03:50:41 +0100

    Updated CustomerProfile.js based on recommendations from tech buddy.

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -2,22 +2,41 @@ import { useState, useEffect } from "react";
 
 const CustomerProfile = ({ id }) => {
   const [allCustomerData, setAllCustomerData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   function doFetchForCustomerProf() {
     fetch(`https://cyf-react.glitch.me/customers/${id}`)
-      .then((response) => response.json())
-      .then((data) => setAllCustomerData(data))
-      .catch((error) => console.error(error));
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch customer data.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setAllCustomerData(data);
+        setIsLoading(false);
+        setErrorMessage(null);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setErrorMessage(error.message);
+      });
   }
   useEffect(() => {
     if (id) {
       doFetchForCustomerProf();
     }
   }, [id]);
-  
-  if (!allCustomerData) {
-    return null;
+
+  if (isLoading) {
+    return <p>Please wait while the customer information is loading...</p>;
   }
+
+  if (errorMessage) {
+    return <p>{errorMessage}</p>;
+  }
+
   return (
     <div>
       <h2>{allCustomerData.id}</h2>
