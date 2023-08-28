ab97f56847a05216df99a90a3123ffdb94302c1d
Maksim Lukianenko <maxbuzz8694@gmail.com>
2023-08-28 18:58:46 +0100

    feat(db.js): add database configuration file to connect to PostgreSQL database feat(package.json): add body-parser dependency to parse request bodies feat(routes.js): add routes for handling customer-related requests fix(server.js): import and use body-parser middleware and routes module for better code organization and modularity

--- server/server.js
+++ server/server.js
@@ -2,27 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const { Pool } = require("pg");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
 
-const db = new Pool({
-  user: "maxbmaapc",
-  host: "localhost",
-  database: "cyf_hotels",
-  password: "",
-  port: 5432,
-});
+app.use(bodyParser.json());
 
-app.get("/customers", async (req, res) => {
-  try {
-    const result = await db.query(
-      "SELECT id, name, city, phone FROM customers"
-    );
-    res.json(result.rows);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "An error occurred" });
-  }
-});
+app.use(routes);
 
 app.listen(process.env.PORT || 8000, () => {
   console.log("Server is live on port: " + (process.env.PORT || 8000));
