204570ed448ac7900c872db8135cf36a6b96b67e
onurat <114289826+onurat@users.noreply.github.com>
2023-08-30 14:54:02 +0100

    Update BookingRow.jsx

--- src/BookingRow.jsx
+++ src/BookingRow.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 const BookingRow = ({ booking, onClick, isSelected }) => {
+  const calculateNumberOfNights = () => {
+    const oneDay = 24 * 60 * 60 * 1000; 
+    const checkInDate = new Date(booking.checkInDate);
+    const checkOutDate = new Date(booking.checkOutDate);
+    const nightCount = Math.round(Math.abs((checkOutDate - checkInDate) / oneDay));
+    return nightCount;
+  };
+
   return (
     <tr onClick={onClick} style={{ backgroundColor: isSelected ? "yellow" : "transparent" }}>
       <td>{booking.id}</td>
@@ -11,7 +19,7 @@ const BookingRow = ({ booking, onClick, isSelected }) => {
       <td>{booking.roomId}</td>
       <td>{booking.checkInDate}</td>
       <td>{booking.checkOutDate}</td>
-      <td>{booking.numberOfNights}</td>
+      <td>{calculateNumberOfNights()}</td>
     </tr>
   );
 };
