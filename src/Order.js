82c3c22e93ed67d9b831255d551843798dbeb658
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2023-12-17 15:58:56 +0000

    fixed typo

--- src/Order.js
+++ src/Order.js
@@ -1,20 +1,18 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import RestaurantButton from "./RestaurantButton";
 
+const Order = ({ orderType }) => {
+  const [order, setOrder] = useState(0);
 
-const Order = ({orderType})=>{
-
-const [order,setOrder]=useState(0);
-
-const orderOne =()=>{
-  setOrder(order+1)
+  const orderOne = () => {
+    setOrder(order + 1);
+  };
+  return (
+    <li>
+      {orderType} :{order}
+      <RestaurantButton onClick={orderOne} />
+    </li>
+  );
 };
-    return(
-        <li>
-             {orderType} :{orders}
-            <RestaurantButton onClick={orderOne} />
-        </li>
-    )
-}
 
 export default Order;
