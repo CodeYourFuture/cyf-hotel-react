d4aa030b1cf33a0e8712eb1fc4bbad1a5951aef9
Mor Khojasteh <mori.kh1987@gmail.com>
2024-01-23 20:58:00 +0000

    add images to the project

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,14 +1,11 @@
 import React from "react";
+import Logo from "../images/hotel.png";
 
 const Heading = () => {
   return (
     <div>
       <header className="App-header">CYF Hotel</header>
-      <img
-        className="logo1"
-        src="https://cdn-icons.flaticon.com/png/512/1926/premium/1926312.png?token=exp=1647280866~hmac=4697643cfd262b8ecfc87c21c40d22d3"
-        alt=""
-      />
+      <img className="logo1" src={Logo} alt="CYF Hotel" />
     </div>
   );
 };
--- src/components/TouristInfoCards.js
+++ src/components/TouristInfoCards.js
@@ -1,30 +1,30 @@
 import React from "react";
+import London from "../images/London.jpg";
+import Manchester from "../images/Manchester.jpg";
+import Glasgow from "../images/Glasgow.jpg";
 
 const cityNames = [
   {
     cityName: "London",
-    img:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/City_of_London%2C_seen_from_Tower_Bridge.jpg/1920px-City_of_London%2C_seen_from_Tower_Bridge.jpg",
+    img: London,
     description:
       "London, the United Kingdom's capital, stands on the River Thames in south-east England at the head of a 50-mile (80 km) estuary down to the North Sea. It has been a major settlement for two millennia.",
-    url: "https://en.wikipedia.org/wiki/London",
+    url: "https://en.wikipedia.org/wiki/London"
   },
   {
     cityName: "Manchester",
-    img:
-      "https://static.independent.co.uk/2021/09/09/10/iStock-1330512300%20%281%29.jpg?",
+    img: Manchester,
     description:
       "Manchester is a city and metropolitan borough in Greater Manchester, England. The city has the country's fifth-largest population at 547,627 and lies within the United Kingdom's second-most populous urban area, with a population of 2.7 million.",
-    url: "https://en.wikipedia.org/wiki/Manchester",
+    url: "https://en.wikipedia.org/wiki/Manchester"
   },
   {
     cityName: "Glasgow",
-    img:
-      "https://cdn.ricardo.com/careers/media/assets/shutterstock_10061808040-glasgow-town.jpg",
+    img: Glasgow,
     description:
       "Glasgow is the most populous city in Scotland and the fourth-most populous city in the United Kingdom, as well as being the 27th largest city by population in Europe.",
-    url: "https://en.wikipedia.org/wiki/Glasgow",
-  },
+    url: "https://en.wikipedia.org/wiki/Glasgow"
+  }
 ];
 
 const TouristInfoCards = () => {
